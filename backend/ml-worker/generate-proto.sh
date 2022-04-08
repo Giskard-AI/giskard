ce945cb8fe025709ae910dd56aa6f644a2e6a8a9
Andrey Avtomonov <andreybavt@gmail.com>
2022-04-08 22:43:52 +0200

    mode Makefile tasks

--- backend/ml-worker/generate-proto.sh
+++ backend/ml-worker/generate-proto.sh
@@ -1,5 +1,7 @@
 mkdir -p generated
-python3.7 -m grpc_tools.protoc \
+PYTHON="${1:-python3.7}"
+
+$PYTHON -m grpc_tools.protoc \
   -Iml_worker/proto \
   --python_out=generated \
   --grpc_python_out=generated \
