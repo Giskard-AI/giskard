a4d35e0207c7f6e28a82dc007fadf2c599e04a56
Andrey Avtomonov <andreybavt@gmail.com>
2022-04-14 00:24:48 +0200

    GSK-36 Remove PostHog and Fullstory

--- giskard-frontend/src/App.vue
+++ giskard-frontend/src/App.vue
@@ -8,39 +8,28 @@
               <div class="text-center">
                 <div class="headline my-5">Loading...</div>
                 <v-progress-circular
-                  size="100"
-                  indeterminate
-                  color="primary"
+                    size="100"
+                    indeterminate
+                    color="primary"
                 ></v-progress-circular>
               </div>
             </v-flex>
           </v-layout>
         </v-container>
       </v-main>
-      <router-view v-else />
+      <router-view v-else/>
       <NotificationsManager></NotificationsManager>
     </v-app>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import {Component, Vue} from "vue-property-decorator";
 import NotificationsManager from "@/components/NotificationsManager.vue";
-import { readIsLoggedIn } from "@/store/main/getters";
-import { dispatchCheckLoggedIn } from "@/store/main/actions";
-import posthog from "posthog-js";
+import {readIsLoggedIn} from "@/store/main/getters";
+import {dispatchCheckLoggedIn} from "@/store/main/actions";
 import {editor} from "monaco-editor";
 import IEditorOptions = editor.IEditorOptions;
-
-
-if (
-  !window.location.href.includes("127.0.0.1") &&
-  !window.location.href.includes("localhost")
-) {
-  posthog.init("phc_sIBMMHiIjqo3UYROGlNLb7Uo51FYpAfkTDmOMZmiKpD", {
-    api_host: "https://app.posthog.com",
-  });
-}
 
 @Component({
   components: {
