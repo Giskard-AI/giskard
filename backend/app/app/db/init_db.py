9d427a4b098d3da04eb91b98c49c3dbe0f4648b3
Andrey Avtomonov <andreybavt@gmail.com>
2022-04-16 00:01:20 +0200

    GSK-17 Create a demo project

--- backend/app/app/db/init_db.py
+++ backend/app/app/db/init_db.py
@@ -1,12 +1,19 @@
+import logging
+from io import BytesIO
+
+from fastapi import UploadFile
 from sqlalchemy.orm import Session
 
 from app import crud, schemas
+from app.api.api_v1.endpoints.third_party import save_model_file, save_requirements_file
+from app.core import files
 from app.core.config import settings
 from app.db import base  # noqa: F401
 
 # make sure all SQL Alchemy models are imported (app.db.base) before initializing DB
 # otherwise, SQL Alchemy might fail to initialize relationships properly
 # for more details: https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/28
+from app.schemas import ProjectModelCreateSchema, ProjectFileCreateSchema
 
 
 def init_db(db: Session) -> None:
@@ -15,6 +22,12 @@ def init_db(db: Session) -> None:
     # the tables un-commenting the next line
     # Base.metadata.create_all(bind=engine)
 
+    user = create_superuser(db)
+
+    create_demo_project(db, user)
+
+
+def create_superuser(db):
     user = crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
     if not user:
         user_in = schemas.UserCreate(
@@ -24,3 +37,43 @@ def init_db(db: Session) -> None:
             role_id=settings.UserRole.ADMIN.value,
         )
         user = crud.user.create(db, obj_in=user_in)  # noqa: F841
+    return user
+
+
+def create_demo_project(db, user):
+    demo_project = crud.project.get_by_key(db, query_key='giskard-demo-project')
+    if demo_project:
+        logging.info("Demo project already exists, skipping")
+    else:
+        project = crud.project.create(
+            db,
+            schemas.ProjectCreate(
+                name="Giskard demo project",
+                description="Try working with binary classification model and a credit scoring dataset"),
+            user.id
+        )
+        logging.info(f"Created demo project {project.key}")
+
+        model_path = "../demo_project/demo-model.pkl.zst"
+        with open(model_path, "rb") as model_file:
+            model_path, storage_dir = save_model_file(model_file, "Demo model", project.key)
+        logging.info(f"Created demo model file: {model_path}")
+
+        with open("../demo_project/demo-model-requirements.txt", "rb") as model_req_file:
+            requirements_path = save_requirements_file("Demo model", model_req_file, storage_dir)
+            logging.info(f"Created demo requirement file: {requirements_path}")
+
+            model = crud.project_model.create(
+                db,
+                ProjectModelCreateSchema(file_name="demo-model.pkl.zst",
+                                         location=str(model_path),
+                                         python_version="3.7.12",
+                                         requirements_file_location=str(requirements_path)),
+                project_id=project.id, owner_id=user.id)
+            logging.info(f"Created demo model entity {project.key}->{model.id}")
+
+        with open("../demo_project/demo-dataset.csv.zst", "rb") as ds_file:
+            ds = files.save_dataset_file(db, UploadFile("demo-dataset.csv.zst", BytesIO(ds_file.read())), project,
+                                         user.id)
+        logging.info(f"Created demo dataset {project.key}->{ds.id} ({ds.location})")
+        logging.info(f"Created demo dataset entity {project.key}->{ds.id}")
