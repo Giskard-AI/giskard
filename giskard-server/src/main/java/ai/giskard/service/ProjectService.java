d62eeb71c73ded1194719921a90eab429cc24dab
qgk75 <ggkk747578@gmail.com>
2022-04-20 09:00:02 +0200

    add invite uninvite methods to project's controller

--- giskard-server/src/main/java/ai/giskard/service/ProjectService.java
+++ giskard-server/src/main/java/ai/giskard/service/ProjectService.java
@@ -26,6 +26,53 @@ public class ProjectService {
         this.projectRepository = projectRepository;
     }
 
+    /**
+     * Update project
+     *
+     * @param id:             id of the project to update
+     * @param updatedProject: updated project
+     * @return: project updated
+     */
+    public Project update(@NotNull Long id, Project updatedProject) {
+        Project project = this.projectRepository.getById(id);
+        project.setName(updatedProject.getName());
+        project.setDatasets(updatedProject.getDatasets());
+        project.setDescription(updatedProject.getDescription());
+        project.setLocalDateTime(updatedProject.getLocalDateTime());
+        project.setOwner(updatedProject.getOwner());
+        project.setUsers(updatedProject.getUsers());
+        project.setModels(updatedProject.getModels());
+        this.projectRepository.save(project);
+        return project;
+    }
+
+    /**
+     * Create project
+     *
+     * @param updatedProject: updated project
+     * @return: project saved
+     */
+    public Project create(Project updatedProject) {
+        Project project = new Project();
+        project.setName(updatedProject.getName());
+        project.setDatasets(updatedProject.getDatasets());
+        project.setDescription(updatedProject.getDescription());
+        project.setLocalDateTime(updatedProject.getLocalDateTime());
+        project.setOwner(updatedProject.getOwner());
+        project.setUsers(updatedProject.getUsers());
+        project.setModels(updatedProject.getModels());
+        this.projectRepository.save(project);
+        return project;
+    }
+
+    /**
+     * Managing access control for specified project
+     * TODO: move it to permission
+     *
+     * @param projectId:   id of the project
+     * @param userDetails: user details
+     * @throws EntityAccessControlException
+     */
     public void accessControlById(@NotNull Long projectId, UserDetails userDetails) throws EntityAccessControlException {
         boolean isAdmin = userDetails.getAuthorities().stream().anyMatch(authority -> authority.getAuthority() == AuthoritiesConstants.ADMIN);
         User user = userRepository.getOneWithProjectsByLogin(userDetails.getUsername());
@@ -34,6 +81,48 @@ public class ProjectService {
         if (!isUserInGuestListProject && project.getOwner() != user && !isAdmin) {
             throw new EntityAccessControlException(EntityAccessControlException.Entity.PROJECT, projectId);
         }
+    }
+
+    /**
+     * Delete the project
+     *
+     * @param project: project to delete
+     * @return: boolean success
+     */
+    public boolean delete(Project project) {
+        this.projectRepository.delete(project);
+        this.projectRepository.flush();
+        return true;
+    }
+
+    /**
+     * Uninvite user from project guestlist
+     *
+     * @param id: id of the project
+     * @param userId: id of the user
+     * @return update project
+     */
+    public Project uninvite(Long id, Long userId) {
+        User user = this.userRepository.getById(userId);
+        Project project = this.projectRepository.getOneWithUsersById(id);
+        project.removeUser(user);
+        this.projectRepository.save(project);
+        return project;
+    }
+
+    /**
+     * Inviting user to the project guestlist
+     *
+     * @param id: id of the project
+     * @param userId: id of the user
+     * @return updated project
+     */
+    public Project invite(Long id, Long userId) {
+        User user = this.userRepository.getById(userId);
+        Project project = this.projectRepository.getOneWithUsersById(id);
+        project.addUser(user);
+        this.projectRepository.save(project);
+        return project;
     }
 
 }
