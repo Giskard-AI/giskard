c1c5950bc041d7aae5ba5a5ecf6c8755cda665c5
qgk75 <ggkk747578@gmail.com>
2022-05-01 13:40:42 +0200

    GSK-58 Project Migration: back: Solving test suites problems with project migration

--- giskard-server/src/main/java/ai/giskard/service/TestSuiteService.java
+++ giskard-server/src/main/java/ai/giskard/service/TestSuiteService.java
@@ -1,60 +1,35 @@
 package ai.giskard.service;
 
+import ai.giskard.domain.ml.TestSuite;
 import ai.giskard.repository.ProjectRepository;
 import ai.giskard.repository.ml.DatasetRepository;
 import ai.giskard.repository.ml.ModelRepository;
 import ai.giskard.repository.ml.TestRepository;
 import ai.giskard.repository.ml.TestSuiteRepository;
+import ai.giskard.web.dto.mapper.GiskardMapper;
 import ai.giskard.web.dto.ml.TestSuiteDTO;
+import ai.giskard.web.rest.errors.Entity;
+import ai.giskard.web.rest.errors.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class TestSuiteService {
     private final TestSuiteRepository testSuiteRepository;
     private final TestRepository testRepository;
     private final ModelRepository modelRepository;
     private final DatasetRepository datasetRepository;
     private final ProjectRepository projectRepository;
+    private final GiskardMapper giskardMapper;
 
-    public TestSuiteService(TestSuiteRepository testSuiteRepository,
-                            ModelRepository modelRepository,
-                            DatasetRepository datasetRepository,
-                            ProjectRepository projectRepository,
-                            TestRepository testRepository
-    ) {
-        this.testSuiteRepository = testSuiteRepository;
-        this.modelRepository = modelRepository;
-        this.datasetRepository = datasetRepository;
-        this.projectRepository = projectRepository;
-        this.testRepository = testRepository;
-    }
 
-    public Optional<TestSuiteDTO> updateTestSuite(TestSuiteDTO dto) {
-        return Optional
-            .of(testSuiteRepository.findById(dto.getId()))
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .map(testSuite -> {
-                testSuite.setName(dto.getName());
-                if (dto.getProjectId() != null) {
-                    projectRepository.findById(dto.getProjectId()).ifPresent(testSuite::setProject);
-                }
-                if (dto.getModel() != null) {
-                    modelRepository.findById(dto.getModel().getId()).ifPresent(testSuite::setModel);
-                }
-                if (dto.getTrainDataset() != null) {
-                    datasetRepository.findById(dto.getTrainDataset().getId()).ifPresent(testSuite::setTrainDataset);
-                }
-                if (dto.getTestDataset() != null) {
-                    datasetRepository.findById(dto.getTestDataset().getId()).ifPresent(testSuite::setTestDataset);
-                }
-                return testSuiteRepository.save(testSuite);
-            })
-            .map(TestSuiteDTO::new);
+    public TestSuite updateTestSuite(TestSuiteDTO dto) {
+        TestSuite testSuite = testSuiteRepository.findById(dto.getId()).orElseThrow(() -> new EntityNotFoundException(Entity.TEST_SUITE, dto.getId()));
+        giskardMapper.updateTestSuiteFromDTO(dto, testSuite);
+        return testSuiteRepository.save(testSuite);
     }
 
     public void deleteSuite(Long suiteId) {
