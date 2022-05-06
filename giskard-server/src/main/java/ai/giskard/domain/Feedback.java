d014f4641febadac181178cacb3d08e952630845
qgk75 <ggkk747578@gmail.com>
2022-05-06 23:26:29 +0200

    fix feedback models, import json library

--- giskard-server/src/main/java/ai/giskard/domain/Feedback.java
+++ giskard-server/src/main/java/ai/giskard/domain/Feedback.java
@@ -2,15 +2,28 @@ package ai.giskard.domain;
 
 import ai.giskard.domain.ml.Dataset;
 import ai.giskard.domain.ml.ProjectModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
-@Entity
+@Entity(name="feedbacks")
+@TypeDefs({
+    @TypeDef(name = "json", typeClass = JsonStringType.class),
+    @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+})
 public class Feedback {
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -35,6 +48,13 @@ public class Feedback {
     @ManyToOne
     private User user;
 
+    @Getter
+    @Setter
+    @OneToMany(mappedBy = "feedback", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Set<FeedbackReply> feedbackReplies = new HashSet<>();
+
+
     private String targetFeature;
 
     private LocalDateTime createdOn;
@@ -45,7 +65,11 @@ public class Feedback {
     private String feedbackChoice;
     private String feedbackMessage;
     @NotNull
+    @Type(type = "jsonb")
+    @Column(columnDefinition = "jsonb")
     private String userData;
     @NotNull
+    @Type(type = "jsonb")
+    @Column(columnDefinition = "jsonb")
     private String originalData;
 }
