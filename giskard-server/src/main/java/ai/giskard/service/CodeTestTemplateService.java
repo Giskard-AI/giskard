1c318d55e384c892a37d77b5792755b9e01c2a3b
Andrey Avtomonov <andreybavt@gmail.com>
2022-05-10 23:01:22 +0200

    fixed loading tests configs from jar

--- giskard-server/src/main/java/ai/giskard/service/CodeTestTemplateService.java
+++ giskard-server/src/main/java/ai/giskard/service/CodeTestTemplateService.java
@@ -4,27 +4,27 @@ import ai.giskard.domain.ml.CodeTestCollection;
 import ai.giskard.domain.ml.CodeTestTemplate;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.ResourcePatternUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.PathMatcher;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 @Service
+@RequiredArgsConstructor
 public class CodeTestTemplateService {
-    public static final String TEMPLATES_DIR = "classpath:aitest/code_test_templates";
-    public static final String TEMPLATE_SEARCH_PATTERN = "glob:**.yml";
+    public static final String TEMPLATES_LOCATION = "classpath*:aitest/code_test_templates/**yml";
+    private final ResourceLoader resourceLoader;
     private final Logger log = LoggerFactory.getLogger(CodeTestTemplateService.class);
     ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
 
@@ -39,20 +39,20 @@ public class CodeTestTemplateService {
     private void readTemplates() {
         CODE_TEST_TEMPLATES = new ArrayList<>();
         TESTS_BY_ID = new HashMap<>();
-        PathMatcher matcher = FileSystems.getDefault().getPathMatcher(TEMPLATE_SEARCH_PATTERN);
-
-        try (Stream<Path> walk = Files.walk(ResourceUtils.getFile(TEMPLATES_DIR).toPath())) {
-            walk.filter(matcher::matches).forEach(path -> {
+        try {
+            for (Resource resource : ResourcePatternUtils.getResourcePatternResolver(resourceLoader).getResources(TEMPLATES_LOCATION)) {
                 try {
-                    CodeTestCollection collection = mapper.readValue(path.toFile(), CodeTestCollection.class);
+                    InputStream inputStream = resource.getInputStream();
+                    CodeTestCollection collection = mapper.readValue(inputStream, CodeTestCollection.class);
                     CODE_TEST_TEMPLATES.add(collection);
-                    collection.items.forEach(test->{
+                    collection.items.forEach(test -> {
                         TESTS_BY_ID.put(test.getId(), test);
                     });
+                    log.debug("Laded {}", resource.getFilename());
                 } catch (IOException e) {
-                    log.error("Failed to read test template: {}", path, e);
+                    log.error("Failed to read test template: {}", resource.getFilename(), e);
                 }
-            });
+            }
         } catch (IOException e) {
             log.error("Failed to read code based test templates", e);
         }
