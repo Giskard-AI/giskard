fcca4668c5af58afc22f0088b5436f9269ba0241
Andrey Avtomonov <andreybavt@gmail.com>
2022-05-20 18:09:50 +0200

    post merge fixes

--- giskard-frontend/src/views/main/project/InspectorWrapper.vue
+++ giskard-frontend/src/views/main/project/InspectorWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid>
+  <v-container fluid v-if="inspection">
     <v-row
       no-gutters
       style='height: 60px;'
--- giskard-frontend/src/views/main/project/RowList.vue
+++ giskard-frontend/src/views/main/project/RowList.vue
@@ -3,26 +3,27 @@
     <v-row>
       <v-col cols='12' md='3'>
         <v-select
-          :items='filterTypes'
-          label='Filter'
-          v-model='selectedFilter'
-          item-value='out'
-          item-text='in'
+            :items='filterTypes'
+            label='Filter'
+            v-model='selectedFilter'
+            item-value='out'
+            item-text='in'
         ></v-select>
       </v-col>
     </v-row>
-    <v-container v-if='inspection!=null && isClassification(inspection.model.modelType) && selectedFilter===RowFilterType.CUSTOM'>
+    <v-container
+        v-if='inspection!=null && isClassification(inspection.model.modelType) && selectedFilter===RowFilterType.CUSTOM'>
       <v-row>
         <v-col cols='12' md='3'>
           <v-subheader class='pt-5 pl-0'>Actual value is between</v-subheader>
         </v-col>
         <v-col cols='12' md='1'>
           <v-text-field
-            :value='minActualThreshold'
-            step='0.001'
-            hide-details
-            type='number'
-            @change='(val)=>{this.minActualThreshold=val;}'
+              :value='minActualThreshold'
+              step='0.001'
+              hide-details
+              type='number'
+              @change='(val)=>{this.minActualThreshold=val;}'
           >
           </v-text-field>
         </v-col>
@@ -31,11 +32,11 @@
         </v-col>
         <v-col cols='12' md='1'>
           <v-text-field
-            :value='maxActualThreshold'
-            step='0.001'
-            hide-details
-            type='number'
-            @change='(val)=>{this.maxActualThreshold=val;}'
+              :value='maxActualThreshold'
+              step='0.001'
+              hide-details
+              type='number'
+              @change='(val)=>{this.maxActualThreshold=val;}'
           ></v-text-field>
         </v-col>
       </v-row>
@@ -45,11 +46,11 @@
         </v-col>
         <v-col cols='12' md='1'>
           <v-text-field
-            :value='minThreshold'
-            step='0.001'
-            hide-details
-            type='number'
-            @change='(val)=>{this.minThreshold=val;}'
+              :value='minThreshold'
+              step='0.001'
+              hide-details
+              type='number'
+              @change='(val)=>{this.minThreshold=val;}'
           ></v-text-field>
         </v-col>
         <v-col cols='12' md='1'>
@@ -57,11 +58,11 @@
         </v-col>
         <v-col cols='12' md='1'>
           <v-text-field
-            :value='maxThreshold'
-            step='0.001'
-            hide-details
-            type='number'
-            @change='(val)=>{this.maxThreshold=val;}'
+              :value='maxThreshold'
+              step='0.001'
+              hide-details
+              type='number'
+              @change='(val)=>{this.maxThreshold=val;}'
           ></v-text-field>
         </v-col>
       </v-row>
@@ -71,12 +72,12 @@
         </v-col>
         <v-col cols='12' md='1'>
           <v-text-field
-            :value='minDiffThreshold'
-            step='0.1'
-            hide-details
-            type='number'
-            @change='(val)=>{this.minDiffThreshold=val;}'
-            append-icon='mdi-percent-outline'
+              :value='minDiffThreshold'
+              step='0.1'
+              hide-details
+              type='number'
+              @change='(val)=>{this.minDiffThreshold=val;}'
+              append-icon='mdi-percent-outline'
           ></v-text-field>
         </v-col>
         <v-col cols='12' md='1'>
@@ -84,12 +85,12 @@
         </v-col>
         <v-col cols='12' md='1'>
           <v-text-field
-            :value='maxDiffThreshold'
-            step='0.1'
-            hide-details
-            type='number'
-            @change='(val)=>{this.maxDiffThreshold=val;}'
-            append-icon='mdi-percent-outline'
+              :value='maxDiffThreshold'
+              step='0.1'
+              hide-details
+              type='number'
+              @change='(val)=>{this.maxDiffThreshold=val;}'
+              append-icon='mdi-percent-outline'
           ></v-text-field>
         </v-col>
       </v-row>
@@ -111,9 +112,9 @@
         </v-col>
         <v-col cols='12' md='3'>
           <v-select
-            :items='labels'
-            v-model='thresholdLabel'
-            hide-details
+              :items='labels'
+              v-model='thresholdLabel'
+              hide-details
           ></v-select>
         </v-col>
         <v-col cols='12' md='2'>
@@ -121,12 +122,12 @@
         </v-col>
         <v-col cols='12' md='2'>
           <v-text-field
-            label='Min Threshold'
-            :value='minThreshold'
-            step='0.001'
-            hide-details
-            type='number'
-            @change='(val)=>{this.minThreshold=val;}'
+              label='Min Threshold'
+              :value='minThreshold'
+              step='0.001'
+              hide-details
+              type='number'
+              @change='(val)=>{this.minThreshold=val;}'
           ></v-text-field>
         </v-col>
         <v-col cols='12' md='1'>
@@ -134,12 +135,12 @@
         </v-col>
         <v-col cols='12' md='2'>
           <v-text-field
-            label='Max Threshold'
-            :value='maxThreshold'
-            step='0.001'
-            hide-details
-            type='number'
-            @change='(val)=>{this.maxThreshold=val;}'
+              label='Max Threshold'
+              :value='maxThreshold'
+              step='0.001'
+              hide-details
+              type='number'
+              @change='(val)=>{this.maxThreshold=val;}'
           >
             <template v-slot:append>
               %
@@ -153,27 +154,23 @@
 </template>
 
 <script lang='ts'>
-import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
-import { api } from '@/api';
-import { readToken } from '@/store/main/getters';
-import { commitAddNotification } from '@/store/main/mutations';
-import { Filter, InspectionDTO, RegressionUnit, RowFilterType } from '@/generated-sources';
+import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
+import {api} from '@/api';
+import {commitAddNotification} from '@/store/main/mutations';
+import {Filter, InspectionDTO, RegressionUnit, RowFilterType} from '@/generated-sources';
 import MultiSelector from '@/views/main/utils/MultiSelector.vue';
-import { isClassification } from '@/ml-utils';
+import {isClassification} from '@/ml-utils';
 
 /**
  * TODO: This class should be on the wrapper, no template for the moment
  */
 @Component({
-  components: { MultiSelector }
+  components: {MultiSelector}
 })
 export default class RowList extends Vue {
-  //@Prop({ required: true }) selectedId!: number;
-  @Prop({ required: true }) datasetId!: number;
-  @Prop({ required: true }) modelId!: number;
-  @Prop({ required: true }) currentRowIdx!: number;
-  @Prop({ required: true }) inspectionId!: number;
-  @Prop({ required: true }) shuffleMode!: boolean;
+  @Prop({required: true}) inspection!: InspectionDTO;
+  @Prop({required: true}) currentRowIdx!: number;
+  @Prop({required: true}) shuffleMode!: boolean;
 
   rows: Record<string, any>[] = [];
   numberOfRows: number = 0;
@@ -190,38 +187,34 @@ export default class RowList extends Vue {
   minThreshold = null;
   maxThreshold = null;
   minDiffThreshold = null;
-  maxDiffThreshold = null;
-  inspection = {} as InspectionDTO;
+  maxDiffThreshold?: number = undefined;
   filterTypes: any[] = [];
-  selectedFilter = null;
+  selectedFilter = RowFilterType.ALL;
   regressionThreshold: number = 0.1;
   percentRegressionUnit = true;
-  thresholdLabel: string = '';
+  thresholdLabel?: string = undefined;
   minActualThreshold = null;
   maxActualThreshold = null;
   isClassification = isClassification;
   RowFilterType = RowFilterType;
 
-
-  classifFiltersMap = [{ out: RowFilterType.ALL, in: 'All' }, {
-    out: RowFilterType.CORRECT,
-    in: 'Correct Predictions'
-  }, { out: RowFilterType.WRONG, in: 'Incorrect Predictions' }, {
-    out: RowFilterType.BORDERLINE,
-    in: 'Borderline'
-  }, { out: RowFilterType.CUSTOM, in: 'Custom' }];
-  regressionFiltersMap = [{ out: RowFilterType.ALL, in: 'All' }, {
-    out: RowFilterType.CORRECT,
-    in: 'Closest predictions (top 15%)'
-  }, { out: RowFilterType.WRONG, in: 'Most distant predictions (top 15%)' }, {
-    out: RowFilterType.CUSTOM,
-    in: 'Custom'
-  }];
+  classificationFiltersMap = [
+    {out: RowFilterType.ALL, in: 'All'},
+    {out: RowFilterType.CORRECT, in: 'Correct Predictions'},
+    {out: RowFilterType.WRONG, in: 'Incorrect Predictions'},
+    {out: RowFilterType.BORDERLINE, in: 'Borderline'},
+    {out: RowFilterType.CUSTOM, in: 'Custom'}
+  ];
+  regressionFiltersMap = [
+    {out: RowFilterType.ALL, in: 'All'},
+    {out: RowFilterType.CORRECT, in: 'Closest predictions (top 15%)'},
+    {out: RowFilterType.WRONG, in: 'Most distant predictions (top 15%)'},
+    {out: RowFilterType.CUSTOM, in: 'Custom'}
+  ];
 
 
   async mounted() {
-    await this.fetchDetails();
-    this.filterTypes = isClassification(this.inspection.model.modelType) ? this.classifFiltersMap : this.regressionFiltersMap;
+    this.filterTypes = isClassification(this.inspection.model.modelType) ? this.classificationFiltersMap : this.regressionFiltersMap;
     this.selectedFilter = this.filterTypes[0].out;
     this.thresholdLabel = this.labels[0];
     await this.fetchRowAndEmit(true);
@@ -234,24 +227,25 @@ export default class RowList extends Vue {
     await this.fetchRowAndEmit(false);
   }
 
-  @Watch('inspectionId')
+  @Watch('inspection.id')
   @Watch('regressionThreshold')
   @Watch('selectedFilter')
   @Watch('minThreshold')
   @Watch('maxThreshold')
   @Watch('minActualThreshold')
   @Watch('maxActualThreshold')
-  @Watch('targetLabel')
-  @Watch('predictedLabel')
+  @Watch('targetLabel', {deep: true})
+  @Watch('predictedLabel', {deep: true})
   @Watch('shuffleMode')
   @Watch('percentRegressionUnit')
   @Watch('thresholdLabel')
   @Watch('maxDiffThreshold')
   @Watch('minDiffThreshold')
-
-  async reloadAlways() {
+  async reloadAlways(nv, ov) {
+    if (JSON.stringify(nv) === JSON.stringify(ov)) {
+      return;
+    }
     await this.fetchRowAndEmit(true);
-
   }
 
   async fetchRowAndEmit(hasFilterChanged) {
@@ -263,6 +257,7 @@ export default class RowList extends Vue {
   /**
    * Calling fetch rows if necessary, i.e. when start or end of the page
    * @param rowIdxInResults index of the row in the results
+   * @param hasFilterChanged
    */
   public async fetchRows(rowIdxInResults: number, hasFilterChanged: boolean) {
     const remainder = rowIdxInResults % this.itemsPerPage;
@@ -277,8 +272,7 @@ export default class RowList extends Vue {
    * @param rowIdxInResults row's index in
    */
   public async getRow(rowIdxInResults) {
-    const remainder = rowIdxInResults % this.itemsPerPage;
-    this.rowIdxInPage = remainder;
+    this.rowIdxInPage = rowIdxInResults % this.itemsPerPage;
     return this.rows[this.rowIdxInPage];
   }
 
@@ -291,7 +285,7 @@ export default class RowList extends Vue {
   public async fetchRowsByRange(minRange: number, maxRange: number) {
     try {
       const props = {
-        'modelId': this.modelId,
+        'modelId': this.inspection.model.id,
         'minRange': minRange,
         'maxRange': maxRange,
         'isRandom': this.shuffleMode
@@ -307,26 +301,14 @@ export default class RowList extends Vue {
         'predictedLabel': this.predictedLabel,
         'rowFilter': this.selectedFilter!,
         'regressionUnit': this.percentRegressionUnit ? RegressionUnit.ABSDIFFPERCENT : RegressionUnit.ABSDIFF,
-        'thresholdLabel': this.thresholdLabel
+        'thresholdLabel': this.thresholdLabel!
 
       };
-      const response = await api.getDataFilteredByRange(readToken(this.$store), this.inspectionId, props, filter);
+      const response = await api.getDataFilteredByRange(this.inspection.id, props, filter);
       this.rows = response.data.data;
       this.numberOfRows = response.data.rowNb;
     } catch (error) {
-      commitAddNotification(this.$store, { content: error.response.data.detail, color: 'error' });
-    }
-  }
-
-  public async fetchDetails() {
-    try {
-      const response = await api.getLabelsForTarget(readToken(this.$store), this.inspectionId);
-      const responseInspection = await api.getInspection(readToken(this.$store), this.inspectionId);
-      this.labels = response.data;
-      this.inspection = responseInspection.data;
-
-    } catch (error) {
-      commitAddNotification(this.$store, { content: error.response.data.detail, color: 'error' });
+      commitAddNotification(this.$store, {content: error.response.data.detail, color: 'error'});
     }
   }
 }
