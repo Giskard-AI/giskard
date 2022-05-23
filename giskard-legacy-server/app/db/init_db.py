8b528c18925f1f9f694084c69df2063696074ed8
qgk75 <ggkk747578@gmail.com>
2022-05-23 14:15:46 +0200

    temporary: commenting the demo's model and dataset

--- giskard-legacy-server/app/db/init_db.py
+++ giskard-legacy-server/app/db/init_db.py
@@ -58,28 +58,28 @@ def create_demo_project(db, user):
         )
         logging.info(f"Created demo project {project.key}")
 
-        with open(demo_file_path("demo-model.pkl.zst"), "rb") as model_file:
-            model_path, storage_dir = save_model_file(model_file, "Demo model", project.key)
-        logging.info(f"Created demo model file: {model_path}")
-
-        with open(demo_file_path("demo-model-requirements.txt"), "rb") as model_req_file:
-            requirements_path = save_requirements_file("Demo model", model_req_file, storage_dir)
-            logging.info(f"Created demo requirement file: {requirements_path}")
-
-            model = crud.project_model.create(
-                db,
-                ProjectModelCreateSchema(file_name="demo-model.pkl.zst",
-                                         location=str(model_path),
-                                         python_version="3.7.12",
-                                         requirements_file_location=str(requirements_path)),
-                project_id=project.id, owner_id=user.id)
-            logging.info(f"Created demo model entity {project.key}->{model.id}")
-
-        with open(demo_file_path("demo-dataset.csv.zst"), "rb") as ds_file:
-            ds = files.save_dataset_file(db, UploadFile("demo-dataset.csv.zst", BytesIO(ds_file.read())), project,
-                                         user.id)
-        logging.info(f"Created demo dataset {project.key}->{ds.id} ({ds.location})")
-        logging.info(f"Created demo dataset entity {project.key}->{ds.id}")
+        # with open(demo_file_path("demo-model.pkl.zst"), "rb") as model_file:
+        #     model_path, storage_dir = save_model_file(model_file, "Demo model", project.key)
+        # logging.info(f"Created demo model file: {model_path}")
+        #
+        # with open(demo_file_path("demo-model-requirements.txt"), "rb") as model_req_file:
+        #     requirements_path = save_requirements_file("Demo model", model_req_file, storage_dir)
+        #     logging.info(f"Created demo requirement file: {requirements_path}")
+        #
+        #     model = crud.project_model.create(
+        #         db,
+        #         ProjectModelCreateSchema(file_name="demo-model.pkl.zst",
+        #                                  location=str(model_path),
+        #                                  python_version="3.7.12",
+        #                                  requirements_file_location=str(requirements_path)),
+        #         project_id=project.id, owner_id=user.id)
+        #     logging.info(f"Created demo model entity {project.key}->{model.id}")
+        #
+        # with open(demo_file_path("demo-dataset.csv.zst"), "rb") as ds_file:
+        #     ds = files.save_dataset_file(db, UploadFile("demo-dataset.csv.zst", BytesIO(ds_file.read())), project,
+        #                                  user.id)
+        # logging.info(f"Created demo dataset {project.key}->{ds.id} ({ds.location})")
+        # logging.info(f"Created demo dataset entity {project.key}->{ds.id}")
 
 
 def demo_file_path(fname):
