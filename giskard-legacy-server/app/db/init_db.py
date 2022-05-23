4b5b3dfc04f037250f9beb3c7dc47888885cc803
qgk75 <ggkk747578@gmail.com>
2022-05-23 19:09:28 +0200

    add demo projects

--- giskard-legacy-server/app/db/init_db.py
+++ giskard-legacy-server/app/db/init_db.py
@@ -27,7 +27,9 @@ def init_db(db: Session) -> None:
 
     user = create_superuser(db)
 
-    create_demo_project(db, user)
+    demo_names = ["credit", "enron", "zillow"]
+    for name in demo_names:
+        create_demo_project(name, db, user)
 
 
 def create_superuser(db):
@@ -43,8 +45,8 @@ def create_superuser(db):
     return user
 
 
-def create_demo_project(db, user):
-    demo_project = crud.project.get_by_key(db, query_key='giskard-demo-project')
+def create_demo_project(name, db, user):
+    demo_project = crud.project.get_by_key(db, query_key=name)
     if demo_project:
         logging.info("Demo project already exists, skipping")
     else:
@@ -52,35 +54,35 @@ def create_demo_project(db, user):
         project = crud.project.create(
             db,
             schemas.ProjectCreate(
-                name="Giskard demo project",
-                description="Try working with binary classification model and a credit scoring dataset"),
+                name=name,
+                description="Demo project for: " + name),
             user.id
         )
         logging.info(f"Created demo project {project.key}")
 
-        with open(demo_file_path("demo-model.pkl.zst"), "rb") as model_file:
+        with open(demo_file_path(name, "model.pkl.zst"), "rb") as model_file:
             model_path, storage_dir = save_model_file(model_file, "Demo model", project.key)
         logging.info(f"Created demo model file: {model_path}")
 
-        with open(demo_file_path("demo-model-requirements.txt"), "rb") as model_req_file:
+        with open(demo_file_path(name, "requirements.txt"), "rb") as model_req_file:
             requirements_path = save_requirements_file("Demo model", model_req_file, storage_dir)
             logging.info(f"Created demo requirement file: {requirements_path}")
 
             model = crud.project_model.create(
                 db,
-                ProjectModelCreateSchema(file_name="demo-model.pkl.zst",
+                ProjectModelCreateSchema(file_name="model.pkl.zst",
                                          location=str(model_path),
                                          python_version="3.7.12",
                                          requirements_file_location=str(requirements_path)),
                 project_id=project.id, owner_id=user.id)
             logging.info(f"Created demo model entity {project.key}->{model.id}")
 
-        with open(demo_file_path("demo-dataset.csv.zst"), "rb") as ds_file:
-            ds = files.save_dataset_file(db, UploadFile("demo-dataset.csv.zst", BytesIO(ds_file.read())), project,
+        with open(demo_file_path(name, "dataset.csv.zst"), "rb") as ds_file:
+            ds = files.save_dataset_file(db, UploadFile("dataset.csv.zst", BytesIO(ds_file.read())), project,
                                          user.id)
         logging.info(f"Created demo dataset {project.key}->{ds.id} ({ds.location})")
         logging.info(f"Created demo dataset entity {project.key}->{ds.id}")
 
 
-def demo_file_path(fname):
-    return settings.DEMO_PROJECT_DIR / fname
+def demo_file_path(folder, fname):
+    return settings.DEMO_PROJECT_DIR / folder / fname
