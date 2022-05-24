73799cf7716a1c8d378640702f3dc86c994cf961
Andrey Avtomonov <andreybavt@gmail.com>
2022-05-24 14:09:16 +0200

    removed legacy backend 👋

--- giskard-legacy-server/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import User
-from .role import Role
-from .project import Project
-from .project_file import ProjectModel, Dataset, ProjectFile
-from .feedback import Feedback
-from .feedback_reply import FeedbackReply
