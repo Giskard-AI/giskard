11282a0c4caa6de43e6936458b1563a624ca478c
Andrey Avtomonov <andreybavt@gmail.com>
2022-05-24 18:15:14 +0200

    fixed demo project creation

--- giskard-ml-worker/mltask_server_runner.py
+++ giskard-ml-worker/mltask_server_runner.py
@@ -2,25 +2,11 @@ import logging
 import os.path
 from concurrent import futures
 from logging.config import fileConfig
-from pathlib import Path
 
 import grpc
-from pydantic import BaseSettings
-from pydantic.class_validators import validator
 
+from settings import Settings
 from generated.ml_worker_pb2_grpc import add_MLWorkerServicer_to_server
-
-
-class Settings(BaseSettings):
-    port: int = 50051
-    host: str = '0.0.0.0'
-    max_workers: int = 10
-
-    environment: str = ""
-
-    class Config:
-        env_prefix = "GSK_"
-
 
 settings = Settings()
 
