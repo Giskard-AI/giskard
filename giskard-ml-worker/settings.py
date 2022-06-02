457105cb6d0fb097fce089b295cd4633aca6185f
Andrey Avtomonov <andreybavt@gmail.com>
2022-06-02 18:57:40 +0200

    improved logging and made ml worker messages size parameterizable

--- giskard-ml-worker/settings.py
+++ giskard-ml-worker/settings.py
@@ -5,7 +5,8 @@ class Settings(BaseSettings):
     port: int = 50051
     host: str = '0.0.0.0'
     max_workers: int = 10
-
+    max_send_message_length_mb: int = 1024
+    max_receive_message_length_mb: int = 1024
     environment: str = ""
 
     class Config:
--- giskard-server/src/main/java/ai/giskard/service/ml/MLWorkerService.java
+++ giskard-server/src/main/java/ai/giskard/service/ml/MLWorkerService.java
@@ -7,6 +7,7 @@ import io.grpc.ClientInterceptor;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.unit.DataSize;
 
 @Service
 public class MLWorkerService {
@@ -23,7 +24,11 @@ public class MLWorkerService {
             .intercept(clientInterceptor)
             // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
             // needing certificates.
-            .usePlaintext().build();
+            .usePlaintext()
+            .maxInboundMessageSize((int) DataSize.ofMegabytes(applicationProperties.getMaxInboundMLWorkerMessageMB()).toBytes())
+            .build();
+
+
         return new MLWorkerClient(channel);
     }
 
