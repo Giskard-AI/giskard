f42e5acd5589cd3f14a610f01e3f50a4fefbad55
princyiakov <princy@giskard.ai>
2022-06-02 16:56:49 +0200

    GSK-96 output in range unit test added

--- giskard-ml-worker/test/test_heuristic.py
+++ giskard-ml-worker/test/test_heuristic.py
@@ -5,7 +5,7 @@ from ml_worker.testing.functions import GiskardTestFunctions
 def _test_heuristic(german_credit_test_data: GiskardDataset, german_credit_model, threshold):
     tests = GiskardTestFunctions()
     results = tests.heuristic.test_right_label(
-        german_credit_test_data.slice(lambda df: df.head(len(df) // 2)),
+        actual_slice=german_credit_test_data.slice(lambda df: df.head(len(df) // 2)),
         model=german_credit_model,
         classification_label=german_credit_model.classification_labels[1],
         threshold=threshold
@@ -19,7 +19,7 @@ def _test_heuristic(german_credit_test_data: GiskardDataset, german_credit_model
 def test_heuristic_opposite(german_credit_test_data, german_credit_model):
     tests = GiskardTestFunctions()
     results = tests.heuristic.test_right_label(
-        german_credit_test_data.slice(lambda df: df.head(len(df)//2)),
+        actual_slice=german_credit_test_data.slice(lambda df: df.head(len(df) // 2)),
         model=german_credit_model,
         classification_label=german_credit_model.classification_labels[0],
         threshold=0.5
@@ -32,7 +32,7 @@ def test_heuristic_opposite(german_credit_test_data, german_credit_model):
 def test_right_label(german_credit_test_data, german_credit_model):
     tests = GiskardTestFunctions()
     results = tests.heuristic.test_right_label(
-        german_credit_test_data.slice(lambda df: df.head(len(df)//2)),
+        actual_slice=german_credit_test_data.slice(lambda df: df.head(len(df) // 2)),
         model=german_credit_model,
         classification_label=german_credit_model.classification_labels[1],
         threshold=0.5
@@ -51,7 +51,7 @@ def test_heuristic_pass_fail(german_credit_test_data, german_credit_model):
 def test_heuristic_filtered(german_credit_test_data, german_credit_model):
     tests = GiskardTestFunctions()
     results = tests.heuristic.test_right_label(
-        german_credit_test_data.slice(lambda df: df.head(10)),
+        actual_slice=german_credit_test_data.slice(lambda df: df.head(10)),
         model=german_credit_model,
         classification_label=german_credit_model.classification_labels[0],
         threshold=0.5
@@ -59,3 +59,44 @@ def test_heuristic_filtered(german_credit_test_data, german_credit_model):
 
     assert results.actual_slices_size[0] == 10
     assert round(results.metric, 2) == 0.40
+
+
+def _test_output_in_range_clf(german_credit_test_data: GiskardDataset, german_credit_model, threshold):
+    tests = GiskardTestFunctions()
+    results = tests.heuristic.test_output_in_range(
+        actual_slice=german_credit_test_data.slice(lambda df: df.head(len(df) // 2)),
+        model=german_credit_model,
+        classification_label=german_credit_model.classification_labels[1],
+        min_range=0.3,
+        max_range=0.7,
+        threshold=threshold,
+    )
+
+    assert results.actual_slices_size[0] == 500
+    assert round(results.metric, 2) == 0.34
+    return results.passed
+
+
+def test_output_in_range_clf(german_credit_test_data, german_credit_model):
+    assert _test_output_in_range_clf(german_credit_test_data, german_credit_model, 0.2)
+    assert not _test_output_in_range_clf(german_credit_test_data, german_credit_model, 0.9)
+
+
+def _test_output_in_range_reg(diabetes_dataset_with_target: GiskardDataset, linear_regression_diabetes, threshold):
+    tests = GiskardTestFunctions()
+    results = tests.heuristic.test_output_in_range(
+        actual_slice=diabetes_dataset_with_target.slice(lambda df: df.head(len(df) // 2)),
+        model=linear_regression_diabetes,
+        min_range=100,
+        max_range=150,
+        threshold=threshold,
+    )
+
+    assert results.actual_slices_size[0] == 221
+    assert round(results.metric, 2) == 0.28
+    return results.passed
+
+
+def test_output_in_range_reg(diabetes_dataset_with_target, linear_regression_diabetes):
+    assert _test_output_in_range_reg(diabetes_dataset_with_target, linear_regression_diabetes, 0.2)
+    assert not _test_output_in_range_reg(diabetes_dataset_with_target, linear_regression_diabetes, 0.9)
