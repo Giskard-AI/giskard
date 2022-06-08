0ebd26c731e6d5c39fe35972ba4acffa5f2bafe6
Andrey Avtomonov <andreybavt@gmail.com>
2022-06-08 09:35:45 +0200

    merge main

--- giskard-server/src/main/java/ai/giskard/service/InspectionService.java
+++ giskard-server/src/main/java/ai/giskard/service/InspectionService.java
@@ -131,7 +131,6 @@ public class InspectionService {
             case CORRECT -> selection = correctSelection;
             case WRONG -> selection = predictedClass.isNotEqualTo(targetClass);
             case CUSTOM -> {
-                DoubleColumn probPredicted = (DoubleColumn) predsTable.column(filter.getThresholdLabel());
                 selection = targetClass.isNotMissing();
                 if (filter.getPredictedLabel().length > 0) {
                     selection.and(predictedClass.isIn(filter.getPredictedLabel()));
@@ -139,7 +138,7 @@ public class InspectionService {
                 if (filter.getTargetLabel().length > 0) {
                     selection.and(targetClass.isIn(filter.getTargetLabel()));
                 }
-                if (filter.getThresholdLabel() !=  null) {
+                if (filter.getThresholdLabel() != null) {
                     DoubleColumn probPredicted = (DoubleColumn) predsTable.column(filter.getThresholdLabel());
                     if (filter.getMaxThreshold() != null) {
                         selection.and(probPredicted.isLessThanOrEqualTo(filter.getMaxThreshold()));
@@ -147,6 +146,7 @@ public class InspectionService {
                     if (filter.getMinThreshold() != null) {
                         selection.and(probPredicted.isGreaterThanOrEqualTo(filter.getMinThreshold()));
                     }
+                }
             }
             case BORDERLINE -> {
                 DoubleColumn absDiff = calculatedTable.doubleColumn("absDiff");
