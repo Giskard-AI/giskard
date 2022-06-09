783e664817018159dd120c2c6fd5b0ad805bcee0
qgk75 <ggkk747578@gmail.com>
2022-06-09 12:03:47 +0200

    GSK-139 add field for the key precomputed slugified with the project name

--- giskard-frontend/src/utils.ts
+++ giskard-frontend/src/utils.ts
@@ -9,3 +9,11 @@ export const removeLocalToken = () => localStorage.removeItem('token');
 export const getUserFullDisplayName = (user: UserDTO): string => {
     return user.displayName ? `${user.displayName} (${user.user_id})` : user.user_id;
 }
+
+export const toSlug = (str: string): string =>{
+   return str.toLowerCase()
+        .trim()
+        .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+        .replace(/\-\-+/g, '-');        // Replace multiple - with single -
+}
