6abcd4fb0007ebff43d894a8d182f18530301ca4
qgk75 <ggkk747578@gmail.com>
2022-06-21 00:26:29 +0200

    GSK-155 : add labeling feedback for the target

--- giskard-server/src/main/java/ai/giskard/service/InitService.java
+++ giskard-server/src/main/java/ai/giskard/service/InitService.java
@@ -86,6 +86,7 @@ public class InitService {
         germanCreditFeatureTypes.put("people_under_maintenance", FeatureType.NUMERIC);
         germanCreditFeatureTypes.put("telephone", FeatureType.CATEGORY);
         germanCreditFeatureTypes.put("foreign_worker", FeatureType.CATEGORY);
+        germanCreditFeatureTypes.put("default", FeatureType.CATEGORY);
 
         enronFeatureTypes.put("Subject", FeatureType.TEXT);
         enronFeatureTypes.put("Content", FeatureType.TEXT);
@@ -94,6 +95,7 @@ public class InitService {
         enronFeatureTypes.put("Hour", FeatureType.NUMERIC);
         enronFeatureTypes.put("Nb_of_forwarded_msg", FeatureType.NUMERIC);
         enronFeatureTypes.put("Year", FeatureType.NUMERIC);
+        enronFeatureTypes.put("Target", FeatureType.CATEGORY);
 
         zillowFeatureTypes.put("TypeOfDewelling", FeatureType.CATEGORY);
         zillowFeatureTypes.put("BldgType", FeatureType.CATEGORY);
@@ -118,6 +120,7 @@ public class InitService {
         zillowFeatureTypes.put("2ndFlrArea", FeatureType.NUMERIC);
         zillowFeatureTypes.put("Utilities", FeatureType.CATEGORY);
         zillowFeatureTypes.put("OverallQual", FeatureType.CATEGORY);
+        zillowFeatureTypes.put("SalePrice", FeatureType.NUMERIC);
     }
 
     private final Map<String, ProjectConfig> projects = createProjectConfigMap();
