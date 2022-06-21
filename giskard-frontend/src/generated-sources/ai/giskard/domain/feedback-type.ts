3cae173d6e176514e35468b7379022dba064081d
Andrey Avtomonov <andreybavt@gmail.com>
2022-06-21 10:22:46 +0200

    pushed forgotten j2ts generated enum

---
+++ giskard-frontend/src/generated-sources/ai/giskard/domain/feedback-type.ts
@@ -0,0 +1,9 @@
+/**
+ * Generated from ai.giskard.domain.FeedbackType
+ */
+export enum FeedbackType {
+    VALUE_PERTURBATION = 'value perturbation',
+    VALUE = 'value',
+    GENERAL = 'general',
+    TARGET = 'target'
+}
