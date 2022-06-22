a61c794ca727bca5142ad3bf988f018cea0cf86c
qgk75 <ggkk747578@gmail.com>
2022-06-22 10:02:34 +0200

    Revert "pushed forgotten j2ts generated enum"
    This reverts commit 45cca79062763aca10bc10460e473cbbc2590199.

--- giskard-frontend/src/generated-sources/ai/giskard/domain/feedback-type.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Generated from ai.giskard.domain.FeedbackType
- */
-export enum FeedbackType {
-    VALUE_PERTURBATION = 'value perturbation',
-    VALUE = 'value',
-    GENERAL = 'general',
-    TARGET = 'target'
-}
