cfa71024fbddbe7c66732f365a58f179e0108bce
Andrey Avtomonov <andreybavt@gmail.com>
2022-07-13 15:31:04 +0200

    GSK-184 Handle the case of missing feature_names

--- giskard-ml-worker/test/test_model_explanation.py
+++ giskard-ml-worker/test/test_model_explanation.py
@@ -7,17 +7,27 @@ from ml_worker.core.model_explanation import explain
 
 
 @pytest.mark.parametrize(
-    "ds_name, model_name",
+    "ds_name, model_name, include_feature_names",
     [
-        ("german_credit_test_data", "german_credit_model"),
-        ("german_credit_data", "german_credit_model"),
-        ("diabetes_dataset", "linear_regression_diabetes"),
-        ("diabetes_dataset_with_target", "linear_regression_diabetes")
+        ("german_credit_test_data", "german_credit_model", True),
+        ("german_credit_data", "german_credit_model", True),
+        ("diabetes_dataset", "linear_regression_diabetes", True),
+        ("diabetes_dataset_with_target", "linear_regression_diabetes", True),
+
+        ("german_credit_test_data", "german_credit_model", False),
+        ("german_credit_data", "german_credit_model", False),
+        ("diabetes_dataset", "linear_regression_diabetes", False),
+        ("diabetes_dataset_with_target", "linear_regression_diabetes", False)
     ]
 )
-def test_explain(ds_name: str, model_name: str, request):
+def test_explain(ds_name: str, model_name: str, include_feature_names: bool, request):
     ds: GiskardDataset = request.getfixturevalue(ds_name)
     model: GiskardModel = request.getfixturevalue(model_name)
+
+    # Try without feature names, it should also work
+    if not include_feature_names:
+        model.feature_names = None
+
     explanations = explain(model, ds, ds.df.iloc[0].to_dict())
 
     assert explanations and explanations.get('explanations')
