e480025521c7adda7bba84d6394f301c4f01a5f1
Alex Combessie <alex@giskard.ai>
2022-07-25 10:51:21 +0000

    New devcontainer config

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -2,6 +2,17 @@
 
 set -ex
 
+# Build the app with gradle
 ./gradlew build
-docker compose -f docker-compose.dev.yml -f docker-compose.yml up -d db
+
+# Boot the Java backend (which spins up a dev H2 database)
+export JHIPSTER_CORS_ALLOWED_ORIGINS=https://$CODESPACE_NAME-8080.githubpreview.dev
+gradlew -Pprod bootJar
+mv giskard-server/build/libs/giskard-server-*jar giskard-server/build/libs/giskard-server.jar || true && java -jar giskard-server/build/libs/giskard-server.jar &
+
+# Start the Python backend
+cd giskard-ml-worker && PYTHONPATH=generated .venv/bin/python main.py &
+
+# Run the frontend in dev mode
 cd giskard-frontend && npm install && npm run serve
+
