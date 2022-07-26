ac1b9de4a035c8f025dba973229f8ddb34906206
Andrey Avtomonov <andreybavt@gmail.com>
2022-07-26 19:38:27 +0200

    improve postStartCommand.sh

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -6,15 +6,16 @@ set -ex
 ./gradlew build
 
 # Boot the Java backend (which spins up a dev H2 database)
-export JHIPSTER_CORS_ALLOWED_ORIGINS=https://$CODESPACE_NAME-8080.githubpreview.dev
 ./gradlew -Pprod bootJar
 lsof -t -i:9000 | xargs -r kill
-mv giskard-server/build/libs/giskard-server-*jar giskard-server/build/libs/giskard-server.jar || true && java -jar giskard-server/build/libs/giskard-server.jar &
+
+nohup java -jar giskard-server/build/libs/giskard-server.jar > ~/backend.log &
 
 # Start the Python backend
-cd giskard-ml-worker && PYTHONPATH=generated .venv/bin/python main.py &
+lsof -t -i:50051 | xargs -r kill
+(cd giskard-ml-worker && PYTHONPATH=generated nohup .venv/bin/python main.py > ~/ml-worker.log &)
 
 # Run the frontend in dev mode
 lsof -t -i:8080 | xargs -r kill
-cd giskard-frontend && npm install && npm run serve
+(cd giskard-frontend && nohup npm run serve > ~/frontend.log &)
 
