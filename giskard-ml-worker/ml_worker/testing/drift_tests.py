8b6e9c80bf0556148a1c60f0630cb7c3f0b6111e
princyiakov <princy@giskard.ai>
2022-09-21 21:42:43 +0200

    GSK-277 earth movers threshold set to 0.2

--- giskard-ml-worker/ml_worker/testing/drift_tests.py
+++ giskard-ml-worker/ml_worker/testing/drift_tests.py
@@ -315,7 +315,7 @@ class DriftTests(AbstractTestCollection):
                                          reference_ds: GiskardDataset,
                                          actual_ds: GiskardDataset,
                                          column_name: str,
-                                         threshold: float = None) -> SingleTestResult:
+                                         threshold: float = 0.2) -> SingleTestResult:
         """
         Test if the earth movers distance between the actual and expected datasets is
         below the threshold for a given numerical feature
@@ -591,7 +591,7 @@ class DriftTests(AbstractTestCollection):
                                                     actual_slice: GiskardDataset,
                                                     model: GiskardModel,
                                                     classification_label=None,
-                                                    threshold=None) -> SingleTestResult:
+                                                    threshold=0.2) -> SingleTestResult:
         """
         Test if the Earth Mover’s Distance value between the reference and actual datasets is
         below the threshold for the classification labels predictions for classification
