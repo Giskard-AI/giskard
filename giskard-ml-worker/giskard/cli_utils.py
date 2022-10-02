9074d1a118eeece8fd5517c9b229c670ccbf68f8
andreybavt <andreybavt@gmail.com>
2022-10-03 01:37:14 +0200

    more CLI commands, added daemon process

---
+++ giskard-ml-worker/giskard/cli_utils.py
@@ -0,0 +1,38 @@
+import asyncio
+import hashlib
+import logging
+import os
+
+import sys
+from daemon import DaemonContext
+from daemon.runner import is_pidfile_stale
+from lockfile.pidlockfile import PIDLockFile
+
+from giskard.ml_worker.ml_worker import start_ml_worker
+from giskard.settings import run_dir
+
+logger = logging.getLogger(__name__)
+
+
+def remove_stale_pid_file(pid_file):
+    if is_pidfile_stale(pid_file):
+        logger.debug("Stale PID file found, removing it")
+        pid_file.break_lock()
+
+
+def create_pid_file_path(host, port):
+    hash = hashlib.sha1(f"{host}{port}{sys.executable}".encode("UTF-8")).hexdigest()
+    return run_dir / f"ml-worker-{hash}.pid"
+
+
+def run_daemon(host, port):
+    try:
+        log_path = run_dir / "ml-worker.log"
+        logger.info(f"Writing logs to {log_path}")
+        pid_file = PIDLockFile(create_pid_file_path(host, port))
+
+        with DaemonContext(pidfile=pid_file, stdout=open(log_path, 'w+t')):
+            logger.info(f"Daemon PID: {os.getpid()}")
+            asyncio.get_event_loop().run_until_complete(start_ml_worker(host, port))
+    except Exception as e:
+        print(e)
