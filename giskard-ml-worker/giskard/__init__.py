297cc5047c6a37c8ddbb62356ffe13713a3a0c6f
andreybavt <andreybavt@gmail.com>
2022-10-04 17:47:55 +0200

    removed ml worker from this repo

--- giskard-ml-worker/giskard/__init__.py
+++
@@ -1,3 +0,0 @@
-from giskard.ml_worker.utils.logging import configure_logging
-
-configure_logging()
--- giskard-ml-worker/test/__init__.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-
-def path(p):
-    return Path(__file__).parent / p
