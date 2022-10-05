82ef079f5a29dfe6ef959dff869a352856edc277
bonells96 <bonells96@gmail.com>
2022-10-05 16:43:25 +0200

    add statistical test methods

--- giskard-ml-worker/ml_worker/testing/metamorphic_tests.py
+++ giskard-ml-worker/ml_worker/testing/metamorphic_tests.py
@@ -1,7 +1,11 @@
 import pandas as pd
-import time
-import logging
+# import time
+# import logging
+import numpy as np
+import math
 from generated.ml_worker_pb2 import SingleTestResult, TestMessage, TestMessageType
+import scipy.stats
+
 
 from ml_worker.core.giskard_dataset import GiskardDataset
 from ml_worker.core.model import GiskardModel
@@ -42,6 +46,72 @@ class MetamorphicTests(AbstractTestCollection):
             results_df["perturbed_prediction"] = results_df["prediction"]
         return results_df, len(modified_rows)
 
+    @staticmethod
+    def paired_t_test_statistic(population_1, population_2):
+        """
+        Computes the test statistic of a paired t-test
+        
+        Inputs:
+            - population_1, np.array 1D: an array of 1 dimension with the observations of 1st group
+            - population_2, np.array 1D: an array of 1 dimension with the observations of 2nd group
+        
+        Output: the test statistic t
+        """
+
+        mean_diff = np.mean(population_1 - population_2)
+        s_diff = np.std(population_1 - population_2)
+        standard_errors = s_diff / math.sqrt(len(population_1))
+      
+        return mean_diff / standard_errors
+
+    def paired_t_test(self, population_1, population_2, quantile=.05, type="TWO"):
+        """
+        Computes the result of the paired t-test given 2 groups of observations and a level of significance
+        """
+        
+        statistic = self.paired_t_test_statistic(population_1, population_2)
+        if type == "LEFT":
+            "alternative shows pop_1<pop_2"
+            p_value = scipy.stats.t.sf(-statistic, df=len(population_1) - 1)
+        elif type == "RIGHT":
+            "alternative shows pop_1>pop_2"
+            p_value = scipy.stats.t.sf(statistic, df=len(population_1) - 1)
+        elif type == "TWO":
+            "alternative shows pop_1!=pop_2"
+            p_value = scipy.stats.t.sf(abs(statistic), df=len(population_1) - 1) * 2
+        else:
+            raise Exception("Incorrect type! The type has to be one of the following: ['UPPER', 'LOWER', 'EQUAL']")
+        # critical_value = paired_t_test_critical_value(threshold, len(population_1)-1, type)
+        if p_value > quantile:
+            return False, p_value
+        else:
+            return True, p_value
+
+    def equivalence_t_test(self, population_1, population_2, threshold=0.1, quantile=0.05):
+        """
+        Computes the equivalence test to show that mean 2 - threshold < mean 1 < mean 2 + threshold
+
+        Inputs:
+            - population_1, np.array 1D: an array of 1 dimension with the observations of 1st group
+            - population_2, np.array 1D: an array of 1 dimension with the observations of 2nd group
+            - threshold, float: small value that determines the maximal difference that can be between means
+            - quantile, float: the level of significance, is the 1-q quantile of the distribution
+        
+        Output, bool: True if the null is rejected and False if there is not enough evidence
+        """
+        population_2_up = population_2 + threshold
+        population_2_low = population_2 - threshold
+        if self.paired_t_test(population_1, population_2_low, quantile, type="RIGHT") and\
+           self.paired_t_test(population_1, population_2_up, quantile, type="LEFT"):
+            
+            print("null hypothesis rejected at a level of significance", quantile)
+            return True, max(self.paired_t_test(population_1, population_2_low, quantile, type="RIGHT")[1],
+                             self.paired_t_test(population_1, population_2_up, quantile, type="LEFT")[1])
+        else:
+            print("not enough evidence to reject null hypothesis")
+            return False, max(self.paired_t_test(population_1, population_2_low, quantile, type="RIGHT")[1],
+                              self.paired_t_test(population_1, population_2_up, quantile, type="LEFT")[1])
+
     @timer("Compare and predict the data")
     def _compare_prediction(self, results_df, prediction_task, output_sensitivity=None, flag=None):
         if flag == 'Invariance':
@@ -64,6 +134,19 @@ class MetamorphicTests(AbstractTestCollection):
 
         failed_idx = results_df.loc[~results_df.index.isin(passed_idx)].index.values
         return passed_idx, failed_idx
+
+    def _compare_probabilities(self, result_df, flag=None):
+
+        if flag == 'Invariance':
+            p_value = self.equivalence_t_test(result_df['prediction'], result_df['perturbed_prediction'])[1]
+      
+        elif flag == 'Increasing':
+            p_value = self.paired_t_test(result_df['prediction'], result_df['perturbed_prediction'], type='LEFT')[1]
+      
+        elif flag == 'Decreasing':
+            p_value = self.paired_t_test(result_df['prediction'], result_df['perturbed_prediction'], type='RIGHT')[1]
+      
+        return p_value
 
     def _test_metamorphic(self,
                           flag,
@@ -131,7 +214,8 @@ class MetamorphicTests(AbstractTestCollection):
             threshold(float):
               Threshold of the ratio of invariant rows
             output_sensitivity(float):
-                Optional. The threshold for ratio between the difference between perturbed prediction and actual prediction over
+                Optional. The threshold for ratio between the difference between perturbed prediction and
+                actual prediction over
                 the actual prediction for a regression model. We consider there is a prediction difference for
                 regression if the ratio is above the output_sensitivity of 0.1
 
@@ -262,3 +346,208 @@ class MetamorphicTests(AbstractTestCollection):
                                       perturbation_dict=perturbation_dict,
                                       classification_label=classification_label,
                                       threshold=threshold)
+    
+    def _test_metamorphic_stat(self,
+                               flag,
+                               actual_slice: GiskardDataset,
+                               model,
+                               perturbation_dict,
+                               threshold: float,
+                               classification_label=None,
+                               output_proba=True
+                               ) -> SingleTestResult:
+      
+        actual_slice.df.reset_index(drop=True, inplace=True)
+      
+        result_df, modified_rows_count = self._perturb_and_predict(actual_slice,
+                                                                   model,
+                                                                   perturbation_dict,
+                                                                   output_proba=output_proba,
+                                                                   classification_label=classification_label)
+        
+        p_value = self._compare_probabilities(result_df, flag)
+
+        messages = [TestMessage(
+            type=TestMessageType.INFO,
+            text=f"{modified_rows_count} rows were perturbed"
+        )]
+
+        return self.save_results(SingleTestResult(
+            actual_slices_size=[len(actual_slice)],
+            metric=p_value,
+            passed=p_value < threshold,
+            messages=messages))
+
+    def test_metamorphic_decreasing_stat(self,
+                                         df: GiskardDataset,
+                                         model,
+                                         perturbation_dict,
+                                         threshold=0.05,
+                                         classification_label=None
+                                         ):
+        """
+        Summary: Tests if the model probability decreases when the feature values are perturbed
+
+        Description: -
+        - For classification: Test if the model probability of a given classification_label is
+        decreasing after feature values perturbation.
+
+        - For regression: Test if the model prediction is decreasing after feature values perturbation.
+
+        The test is passed when the percentage of rows that are decreasing is higher than the threshold
+
+        Example : For a credit scoring model, the test is passed when an increase of wage by 10%,
+         default probability is decreasing for more than 50% of people in the dataset
+
+        Args:
+            df(GiskardDataset):
+              Dataset used to compute the test
+            model(GiskardModel):
+              Model used to compute the test
+            perturbation_dict(dict):
+              Dictionary of the perturbations. It provides the perturbed features as key
+              and a perturbation lambda function as value
+            threshold(float):
+              Threshold of the ratio of decreasing rows
+            classification_label(str):
+              Optional. One specific label value from the target column
+
+        Returns:
+            actual_slices_size:
+              Length of actual_slice tested
+            message:
+              Test result message
+            metric:
+              The ratio of decreasing rows over the perturbed rows
+            passed:
+              TRUE if metric > threshold
+        """
+
+        assert model.model_type != "classification" or classification_label in model.classification_labels, \
+            f'"{classification_label}" is not part of model labels: {",".join(model.classification_labels)}'
+
+        return self._test_metamorphic_stat(flag='Decreasing',
+                                           actual_slice=df,
+                                           model=model,
+                                           perturbation_dict=perturbation_dict,
+                                           classification_label=classification_label,
+                                           threshold=threshold)
+   
+    def test_metamorphic_increasing_stat(self,
+                                         df: GiskardDataset,
+                                         model,
+                                         perturbation_dict,
+                                         threshold=0.05,
+                                         classification_label=None
+                                         ):
+        """
+        Summary: Tests if the model probability decreases when the feature values are perturbed
+
+        Description: -
+        - For classification: Test if the model probability of a given classification_label is
+        decreasing after feature values perturbation.
+
+        - For regression: Test if the model prediction is decreasing after feature values perturbation.
+
+        The test is passed when the percentage of rows that are decreasing is higher than the threshold
+
+        Example : For a credit scoring model, the test is passed when an increase of wage by 10%,
+         default probability is decreasing for more than 50% of people in the dataset
+
+        Args:
+            df(GiskardDataset):
+              Dataset used to compute the test
+            model(GiskardModel):
+              Model used to compute the test
+            perturbation_dict(dict):
+              Dictionary of the perturbations. It provides the perturbed features as key
+              and a perturbation lambda function as value
+            threshold(float):
+              Threshold of the ratio of decreasing rows
+            classification_label(str):
+              Optional. One specific label value from the target column
+
+        Returns:
+            actual_slices_size:
+              Length of actual_slice tested
+            message:
+              Test result message
+            metric:
+              The ratio of decreasing rows over the perturbed rows
+            passed:
+              TRUE if metric > threshold
+        """
+
+        assert model.model_type != "classification" or classification_label in model.classification_labels, \
+            f'"{classification_label}" is not part of model labels: {",".join(model.classification_labels)}'
+
+        return self._test_metamorphic_stat(flag='Increasing',
+                                           actual_slice=df,
+                                           model=model,
+                                           perturbation_dict=perturbation_dict,
+                                           classification_label=classification_label,
+                                           threshold=threshold)
+
+    def test_metamorphic_invariance_stat(self,
+                                         df: GiskardDataset,
+                                         model,
+                                         perturbation_dict,
+                                         threshold=0.05,
+                                         classification_label=None
+                                         ):
+        """
+        Summary: Tests if the model probability decreases when the feature values are perturbed
+
+        Description: -
+        - For classification: Test if the model probability of a given classification_label is
+        decreasing after feature values perturbation.
+
+        - For regression: Test if the model prediction is decreasing after feature values perturbation.
+
+        The test is passed when the percentage of rows that are decreasing is higher than the threshold
+
+        Example : For a credit scoring model, the test is passed when an increase of wage by 10%,
+         default probability is decreasing for more than 50% of people in the dataset
+
+        Args:
+            df(GiskardDataset):
+              Dataset used to compute the test
+            model(GiskardModel):
+              Model used to compute the test
+            perturbation_dict(dict):
+              Dictionary of the perturbations. It provides the perturbed features as key
+              and a perturbation lambda function as value
+            threshold(float):
+              Threshold of the ratio of decreasing rows
+            classification_label(str):
+              Optional. One specific label value from the target column
+
+        Returns:
+            actual_slices_size:
+              Length of actual_slice tested
+            message:
+              Test result message
+            metric:
+              The ratio of decreasing rows over the perturbed rows
+            passed:
+              TRUE if metric > threshold
+        """
+
+        assert model.model_type != "classification" or classification_label in model.classification_labels, \
+            f'"{classification_label}" is not part of model labels: {",".join(model.classification_labels)}'
+
+        return self._test_metamorphic(flag='Invariance',
+                                      actual_slice=df,
+                                      model=model,
+                                      perturbation_dict=perturbation_dict,
+                                      classification_label=classification_label,
+                                      threshold=threshold)
+
+
+
+
+
+
+
+
+
