119ab842aef5c5d44513ddacda915275cf8f3c5b
thomasabou01 <thomas.abou@epita.fr>
2022-10-06 16:58:58 +0200

    GSK-108 Backend-Mlworker connection

--- giskard-server/src/main/java/ai/giskard/web/rest/controllers/ModelController.java
+++ giskard-server/src/main/java/ai/giskard/web/rest/controllers/ModelController.java
@@ -1,8 +1,10 @@
 package ai.giskard.web.rest.controllers;
 
+import ai.giskard.domain.InspectionSettings;
 import ai.giskard.domain.ml.Dataset;
 import ai.giskard.domain.ml.ModelType;
 import ai.giskard.domain.ml.ProjectModel;
+import ai.giskard.repository.ProjectRepository;
 import ai.giskard.repository.ml.DatasetRepository;
 import ai.giskard.repository.ml.ModelRepository;
 import ai.giskard.security.PermissionEvaluator;
@@ -31,6 +33,7 @@ public class ModelController {
     private final Logger log = LoggerFactory.getLogger(ModelController.class);
     private final ModelRepository modelRepository;
     private final DatasetRepository datasetRepository;
+    private final ProjectRepository projectRepository;
     private final GiskardMapper giskardMapper;
     private final PermissionEvaluator permissionEvaluator;
     private final ModelService modelService;
@@ -71,11 +74,12 @@ public class ModelController {
 
     @PostMapping("models/explain-text/{featureName}")
     @Transactional
-    public Map<String, String> explainText(@RequestParam @NotNull Long modelId, @RequestParam @NotNull Long datasetId, @PathVariable @NotNull String featureName, @RequestBody @NotNull PredictionInputDTO data) throws IOException {
+    public Map<String, String> explainText(@RequestParam @NotNull Long modelId, @RequestParam @NotNull Long datasetId, @RequestParam @NotNull Long projectId, @PathVariable @NotNull String featureName, @RequestBody @NotNull PredictionInputDTO data) throws IOException {
         ProjectModel model = modelRepository.getById(modelId);
         Dataset dataset = datasetRepository.getById(datasetId);
+        InspectionSettings inspectionSettings = projectRepository.getById(projectId).getInspectionSettings();
         permissionEvaluator.validateCanReadProject(model.getProject().getId());
-        return modelService.explainText(model, dataset, featureName, data.getFeatures()).getExplanationsMap();
+        return modelService.explainText(model, dataset, inspectionSettings, featureName, data.getFeatures()).getExplanationsMap();
     }
 
     @DeleteMapping("models/{modelId}")
