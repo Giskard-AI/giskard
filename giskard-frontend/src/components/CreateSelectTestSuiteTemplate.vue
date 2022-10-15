e201a980a157f24593b24cbd308a019c6f6e5e12
andreybavt <andreybavt@gmail.com>
2022-10-16 01:08:08 +0200

    added storybook

---
+++ giskard-frontend/src/components/CreateSelectTestSuiteTemplate.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {onMounted, ref} from 'vue'
+
+// reactive state
+const count = ref(0)
+
+// functions that mutate state and trigger updates
+function increment() {
+  count.value++
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+</script>
+
+<template>
+  <div>
+    <v-btn>Hello</v-btn>
+    <button @click="increment">Count is: {{ count }}</button>
+  </div>
+</template>
