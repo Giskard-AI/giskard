d38c2f13722a1284cbe1f1b20c98e9cb9618bed8
Rabah <rabah.khalek@gmail.com>
2022-11-29 15:43:37 +0100

    bypassed httpretty for h2o model

--- python-client/tests/model_upload/test_H2O.py
+++ python-client/tests/model_upload/test_H2O.py
@@ -1,11 +1,3 @@
-import httpretty
-gsk_url = "http://giskard-host:12345"
-token = "SECRET_TOKEN"
-auth = "Bearer SECRET_TOKEN"
-content_type = "multipart/form-data; boundary="
-model_name = "uploaded model"
-b_content_type = b"application/json"
-
 import os
 import shutil
 import tensorflow as tf
@@ -21,11 +13,11 @@ import h2o
 from h2o.automl import H2OAutoML
 import os
 
-@httpretty.activate(verbose=True, allow_net_connect=False)
+@pytest.mark.skip(
+    reason="GSK-388 H2O models unable to unpickle on Giskard"
+)
 def test_upload_H2O():
-    httpretty.register_uri(httpretty.POST, "http://giskard-host:12345/api/v2/project/models/upload")
-    client = GiskardClient(gsk_url, token)
-    project = GiskardProject(client.session, "test-project", 1)
+
     h2o.init()
 
     aml = H2OAutoML(max_models = 1, seed = 1)
@@ -47,11 +39,4 @@ def test_upload_H2O():
         preds = np.array(preds.as_data_frame()[['Yes','No']])
         return preds
 
-    project.upload_model(
-        prediction_function=prediction_function, #aml.leader.predict,
-        model_type='classification',
-        target = 'went_on_backorder',
-        feature_names=['national_inv','lead_time','in_transit_qty','forecast_3_month','forecast_6_month','forecast_9_month','sales_1_month','sales_3_month',
-                       'sales_6_month','sales_9_month','min_bank','potential_issue','pieces_past_due','perf_6_month_avg','perf_12_month_avg','local_bo_qty','deck_risk',
-                       'oe_constraint','ppap_risk','stop_auto_buy','rev_stop'],
-        classification_labels=['Yes','No'])
+    GiskardProject._validate_model_is_pickleable(prediction_function)
