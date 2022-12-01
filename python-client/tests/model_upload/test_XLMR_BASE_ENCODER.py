611accf55a18deae9f7a887f0d633f10378384a8
Rabah <rabah.khalek@gmail.com>
2022-12-01 13:38:57 +0100

    cleaned up test_XLMR_BASE_ENCODER.py

--- python-client/tests/model_upload/test_XLMR_BASE_ENCODER.py
+++ python-client/tests/model_upload/test_XLMR_BASE_ENCODER.py
@@ -8,10 +8,8 @@ def test_XLMR_BASE_ENCODER():
     xlmr_base = torchtext.models.XLMR_BASE_ENCODER
     model = xlmr_base.get_model()
     transform = xlmr_base.transform()
-    input_batch = ["Hello world", "How are you!"]
-
-    df = pd.DataFrame(input_batch,columns=['text'])
-
+    #input_batch = ["Hello world", "How are you!"]
+    #df = pd.DataFrame(input_batch,columns=['text'])
     def prediction_function(df):
         input_batch = df['text'].values.tolist()
         model_input = to_tensor(transform(input_batch), padding_value=1)
