53562798d6871a73703833185fcd9b58f929500d
Andrey Avtomonov <andreybavt@gmail.com>
2022-12-05 17:07:39 +0100

    sonar cleanup

--- backend/src/main/java/ai/giskard/repository/ml/TestSuiteRepository.java
+++ backend/src/main/java/ai/giskard/repository/ml/TestSuiteRepository.java
@@ -2,7 +2,6 @@ package ai.giskard.repository.ml;
 
 import ai.giskard.domain.ml.TestSuite;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
