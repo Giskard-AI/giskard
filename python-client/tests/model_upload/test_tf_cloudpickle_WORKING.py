5dc6856f4966c2456260a6a8233e2deb83848248
Rabah <rabah.khalek@gmail.com>
2022-12-22 12:36:44 +0100

    small update to test_tf_cloudpickle_WORKING.py

--- python-client/tests/model_upload/test_tf_cloudpickle_WORKING.py
+++ python-client/tests/model_upload/test_tf_cloudpickle_WORKING.py
@@ -18,7 +18,8 @@ def create_model():
 
     return model
 
-def test():
+
+def test_tf_cloudpickle_WORKING():
 
     # Create a basic model instance
     model = create_model()
@@ -26,17 +27,17 @@ def test():
     (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
 
     train_labels = train_labels[:100]
-    test_labels = test_labels[:100]
+    #test_labels = test_labels[:100]
 
     train_images = train_images[:100].reshape(-1, 28 * 28) / 255.0
-    test_images = test_images[:100].reshape(-1, 28 * 28) / 255.0
+    #test_images = test_images[:100].reshape(-1, 28 * 28) / 255.0
 
     # Create and train a new model instance.
     model = create_model()
     model.fit(train_images, train_labels, epochs=5)
 
     # Save the entire model as a SavedModel.
-    os.system('mkdir -p saved_model')
-    model.save('saved_model/my_model')
+    #os.system('mkdir -p saved_model')
+    #model.save('saved_model/my_model')
 
     GiskardProject._validate_model_is_pickleable(model)
