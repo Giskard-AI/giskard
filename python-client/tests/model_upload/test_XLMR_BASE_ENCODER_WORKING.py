1c8452c91bfaf62590544941d312334f5f597534
Rabah <rabah.khalek@gmail.com>
2022-12-22 12:44:06 +0100

    updated test_XLMR_BASE_ENCODER

---
+++ python-client/tests/model_upload/test_XLMR_BASE_ENCODER_WORKING.py
@@ -0,0 +1,19 @@
+from giskard.client.project import GiskardProject
+import torchtext
+from torchtext.functional import to_tensor
+
+def test_XLMR_BASE_ENCODER():
+
+    xlmr_base = torchtext.models.XLMR_BASE_ENCODER
+    model = xlmr_base.get_model()
+    transform = xlmr_base.transform()
+    #input_batch = ["Hello world", "How are you!"]
+    #df = pd.DataFrame(input_batch,columns=['text'])
+    def prediction_function(df):
+        input_batch = df['text'].values.tolist()
+        model_input = to_tensor(transform(input_batch), padding_value=1)
+        output = model(model_input)
+
+        return output.cpu().detach().numpy()
+
+    GiskardProject._validate_model_is_pickleable(prediction_function)
