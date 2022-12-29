2da4506c1a7dfeb1ba772505b506ed6043f54a95
Googleton <hugo.viallon.pro@gmail.com>
2022-12-29 17:25:07 +0100

    Move tabs to dynamic list for future use

--- frontend/src/views/main/admin/settings/Settings.vue
+++ frontend/src/views/main/admin/settings/Settings.vue
@@ -1,8 +1,7 @@
 <template>
   <v-container fluid id="container-project-tab" class="vertical-container">
-    <v-tabs>
-      <v-tab to="/main/admin/general"><v-icon left>mdi-cog</v-icon>General</v-tab>
-      <v-tab to="/main/admin/users"><v-icon left>people</v-icon>Users</v-tab>
+    <v-tabs v-if="tabs.length > 1">
+      <v-tab v-for="tab in tabs" :to="tab.to"><v-icon left>{{tab.icon}}</v-icon>{{tab.text}}</v-tab>
     </v-tabs>
     <keep-alive>
       <router-view></router-view>
@@ -11,6 +10,23 @@
 </template>
 
 <script setup lang="ts">
+import {onMounted, ref} from "vue";
+
+interface Tab {
+  to: string,
+  icon: string,
+  text: string
+}
+
+const tabs = ref<Tab[]>([]);
+
+// TODO: Check featureflags?
+onMounted(() => {
+  tabs.value = [
+    { to: "/main/admin/general", icon: "mdi-cog", text: "General"},
+    { to: "/main/admin/users", icon: "people", text: "Users"}
+  ]
+})
 
 </script>
 
