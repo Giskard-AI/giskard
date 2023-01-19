e7229ba8e4378522d2d5e0fa4de03d1dec24e891
Googleton <hugo.viallon.pro@gmail.com>
2023-01-19 23:54:18 +0100

    Start work on license validation

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -287,7 +287,7 @@ export const api = {
         downloadURL(`${API_V2_ROOT}/download/model/${id}`);
     },
     async editModelName(modelId: number, name: string) {
-      return apiV2.patch<unknown, ModelDTO>(`/models/${modelId}/name/${encodeURIComponent(name)}`, null)
+        return apiV2.patch<unknown, ModelDTO>(`/models/${modelId}/name/${encodeURIComponent(name)}`, null)
     },
     downloadDataFile(id: number) {
         downloadURL(`${API_V2_ROOT}/download/dataset/${id}`);
@@ -406,4 +406,11 @@ export const api = {
     async executeTestSuite(suiteId: number) {
         return apiV2.post<unknown, Array<TestExecutionResultDTO>>(`/testing/suites/execute`, {suiteId});
     },
+    async uploadLicense(form: FormData) {
+        return apiV2.post<unknown, unknown>(`/ee/license`, form, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+    }
 };
