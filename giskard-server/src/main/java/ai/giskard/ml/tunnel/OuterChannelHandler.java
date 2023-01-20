ba9bf02d92a174db726cbb7e06182e0f5f46091e
Andrey Avtomonov <andreybavt@gmail.com>
2023-01-20 18:21:37 +0100

    fix import

--- giskard-server/src/main/java/ai/giskard/ml/tunnel/OuterChannelHandler.java
+++ giskard-server/src/main/java/ai/giskard/ml/tunnel/OuterChannelHandler.java
@@ -9,6 +9,7 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.ByteBufFormat;
+import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.util.IllegalReferenceCountException;
 import lombok.Getter;
