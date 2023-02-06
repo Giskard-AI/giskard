b4391ed8b9815d55575bd9fdd479e17ddb4be8db
Andrey Avtomonov <andreybavt@gmail.com>
2023-02-06 15:35:19 +0100

    fix compilation

--- backend/src/main/java/ai/giskard/web/dto/PostImportProjectDTO.java
+++ backend/src/main/java/ai/giskard/web/dto/PostImportProjectDTO.java
@@ -1,6 +1,7 @@
 package ai.giskard.web.dto;
 
 import com.dataiku.j2ts.annotations.UIModel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.Map;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PostImportProjectDTO {
     private Map<String, String> mappedUsers;
     private String projectKey;
