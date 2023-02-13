9a502591c6793d2595e75182d0582a6be5fbc040
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-02-13 14:04:10 +0100

    Refacto upload of meta

--- backend/src/main/java/ai/giskard/web/rest/controllers/TestFunctionController.java
+++ backend/src/main/java/ai/giskard/web/rest/controllers/TestFunctionController.java
@@ -1,20 +1,16 @@
 package ai.giskard.web.rest.controllers;
 
-import ai.giskard.domain.TestFunction;
 import ai.giskard.repository.ml.TestFunctionRepository;
 import ai.giskard.service.TestFunctionService;
 import ai.giskard.web.dto.TestFunctionDTO;
 import ai.giskard.web.dto.mapper.GiskardMapper;
-import ai.giskard.web.dto.ml.ModelDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,24 +21,25 @@ public class TestFunctionController {
     private final TestFunctionRepository testFunctionRepository;
     private final TestFunctionService testFunctionService;
 
-    @GetMapping("/test-functions")
+    @GetMapping("/tests")
     @Transactional(readOnly = true)
     public List<TestFunctionDTO> findAll() {
         return testFunctionService.findAll();
     }
-    @GetMapping("/test-functions/{testId}")
+    @GetMapping("/tests/{testUuid}")
     @Transactional(readOnly = true)
-    public TestFunctionDTO getTestFunction(@PathVariable("testId") @NotNull UUID modelId) {
-        return giskardMapper.toDTO(testFunctionRepository.getById(modelId));
+    public TestFunctionDTO getTestFunction(@PathVariable("testUuid") @NotNull UUID testUuid) {
+        return giskardMapper.toDTO(testFunctionRepository.getById(testUuid));
     }
 
-    @PostMapping("/test-functions")
+    @PutMapping("/tests/{testUuid}")
     @Transactional
-    public TestFunctionDTO getTestFunction(@Valid @RequestBody TestFunctionDTO testFunction) {
+    public TestFunctionDTO updateTestFunction(@PathVariable("testUuid") @NotNull UUID testUuid,
+                                              @Valid @RequestBody TestFunctionDTO testFunction) {
         return testFunctionService.save(testFunction);
     }
 
-    @PostMapping("/test-functions/registry")
+    @PostMapping("/tests/registry")
     @Transactional
     public void getTestFunction(@Valid @RequestBody Collection<TestFunctionDTO> testFunctions) {
         testFunctionService.saveAll(testFunctions);
