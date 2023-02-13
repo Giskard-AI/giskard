893c83177b29852400707299260d9a6ca2e903c0
Andrey Avtomonov <andreybavt@gmail.com>
2023-02-14 00:43:11 +0100

    post merge fixes

--- python-client/giskard/models/pytorch/__init__.py
+++ python-client/giskard/models/pytorch/__init__.py
@@ -1,17 +1,14 @@
-from pathlib import Path
 from typing import Union
 
 import mlflow
 import numpy as np
 import pandas as pd
 import torch
-import yaml
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset as torch_dataset
 
 from giskard.core.core import SupportedModelTypes
-from giskard.core.model import Model
-from giskard.path_utils import get_size
+from giskard.core.model import MLFlowBasedModel
 
 
 class TorchMinimalDataset(torch_dataset):
@@ -26,63 +23,13 @@ class TorchMinimalDataset(torch_dataset):
         return torch.tensor(self.entries.iloc[idx].to_numpy(), dtype=self.torch_dtype)
 
 
-class PyTorchModel(Model):
-    loader_module = 'giskard.models.pytorch'
-    loader_class = 'PyTorchModel'
-
-    def __init__(self,
-                 clf,
-                 model_type: Union[SupportedModelTypes, str],
-                 torch_dtype=torch.float32,
-                 device='cpu',
-                 name: str = None,
-                 data_preprocessing_function=None,
-                 model_postprocessing_function=None,
-                 feature_names=None,
-                 classification_threshold=0.5,
-                 classification_labels=None) -> None:
-
-        super().__init__(clf,
-                         model_type,
-                         name,
-                         data_preprocessing_function,
-                         model_postprocessing_function,
-                         feature_names,
-                         classification_threshold,
-                         classification_labels)
-
-        self.device = device
-        self.torch_dtype = torch_dtype
-
-    @classmethod
-    def read_model_from_local_dir(cls, local_path):
-        return mlflow.pytorch.load_model(local_path)
-
-    def save_to_local_dir(self, local_path):
-
-        info = self._new_mlflow_model_meta()
+class PyTorchModel(MLFlowBasedModel):
+    def save_with_mflow(self, local_path, mlflow_meta: mlflow.models.Model):
         mlflow.pytorch.save_model(self.clf,
                                   path=local_path,
-                                  mlflow_model=info)
-        with open(Path(local_path) / 'giskard-model-meta.yaml', 'w') as f:
-            yaml.dump(
-                {
-                    "language_version": info.flavors['python_function']['python_version'],
-                    "loader_module": self.meta.loader_module,
-                    "loader_class": self.meta.loader_class,
-                    "language": "PYTHON",
-                    "model_type": self.meta.model_type.name.upper(),
-                    "threshold": self.meta.classification_threshold,
-                    "feature_names": self.meta.feature_names,
-                    "classification_labels": self.meta.classification_labels,
-                    "id": info.model_uuid,
-                    "name": self.meta.name,
-                    "size": get_size(local_path),
-                }, f, default_flow_style=False)
+                                  mlflow_model=mlflow_meta)
 
-        return info
-
-    def _raw_predict(self, data):
+    def clf_predict(self, data):
         self.clf.to(self.device)
         self.clf.eval()
 
@@ -111,3 +58,31 @@ class PyTorchModel(Model):
             predictions = self.model_postprocessing_function(predictions)
 
         return predictions.squeeze()
+
+    def __init__(self,
+                 clf,
+                 model_type: Union[SupportedModelTypes, str],
+                 torch_dtype=torch.float32,
+                 device='cpu',
+                 name: str = None,
+                 data_preprocessing_function=None,
+                 model_postprocessing_function=None,
+                 feature_names=None,
+                 classification_threshold=0.5,
+                 classification_labels=None) -> None:
+
+        super().__init__(clf=clf,
+                         model_type=model_type,
+                         name=name,
+                         data_preprocessing_function=data_preprocessing_function,
+                         model_postprocessing_function=model_postprocessing_function,
+                         feature_names=feature_names,
+                         classification_threshold=classification_threshold,
+                         classification_labels=classification_labels)
+
+        self.device = device
+        self.torch_dtype = torch_dtype
+
+    @classmethod
+    def read_model_from_local_dir(cls, local_path):
+        return mlflow.pytorch.load_model(local_path)
--- python-client/tests/pytorch/test_newspaper-classification_pytorch-dataset.py
+++ python-client/tests/pytorch/test_newspaper-classification_pytorch-dataset.py
@@ -1,20 +1,19 @@
-import pandas as pd
 import os
-from scipy import special
-
-import torch
-from torch import nn
-from torchtext.datasets import AG_NEWS
-from torchtext.data.utils import get_tokenizer
-from torchtext.vocab import build_vocab_from_iterator
-from torchtext.data.functional import to_map_style_dataset
-from torch.utils.data import Dataset as torch_dataset
-
-from giskard.client.giskard_client import GiskardClient
-from giskard import PyTorchModel, Dataset
-
 import re
+
 import httpretty
+import pandas as pd
+import torch
+from scipy import special
+from torch import nn
+from torch.utils.data import Dataset as torch_dataset
+from torchtext.data.functional import to_map_style_dataset
+from torchtext.data.utils import get_tokenizer
+from torchtext.datasets import AG_NEWS
+from torchtext.vocab import build_vocab_from_iterator
+
+from giskard import PyTorchModel, Dataset
+from giskard.client.giskard_client import GiskardClient
 
 url = "http://giskard-host:12345"
 token = "SECRET_TOKEN"
@@ -33,14 +32,17 @@ num_class = len(ag_news_label.keys())
 
 tokenizer = get_tokenizer('basic_english')
 
+
 def yield_tokens(data_iter):
     for _, text in data_iter:
         yield tokenizer(text)
+
 
 vocab = build_vocab_from_iterator(yield_tokens(train_iter), specials=["<unk>"])
 vocab.set_default_index(vocab["<unk>"])
 
 text_pipeline = lambda x: vocab(tokenizer(x))
+
 
 class PandasToTorch(torch_dataset):
     def __init__(self, df: pd.DataFrame):
@@ -55,8 +57,10 @@ class PandasToTorch(torch_dataset):
     def __getitem__(self, idx):
         return torch.tensor(self.entries['text'].iloc[idx]), torch.tensor([0])
 
+
 def my_softmax(x):
     return special.softmax(x, axis=1)
+
 
 class TextClassificationModel(nn.Module):
 
@@ -80,7 +84,6 @@ class TextClassificationModel(nn.Module):
 # @pytest.mark.skip(reason="WIP")
 @httpretty.activate(verbose=True, allow_net_connect=False)
 def test_newspaper_classification_pytorch_dataset():
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     vocab_size = len(vocab)
@@ -114,9 +117,9 @@ def test_newspaper_classification_pytorch_dataset():
 
     client = GiskardClient(url, token)
     # enron = client.create_project('test-project', "Email Classification", "Email Classification")
-    model_id = my_model.save(client, 'test-project', my_test_dataset)
+    my_model.upload(client, 'test-project', my_test_dataset)
 
-    assert re.match("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", model_id)
+    assert re.match("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", str(my_model.id))
 
     artifact_requests = [i for i in httpretty.latest_requests() if artifact_url_pattern.match(i.url)]
     assert len(artifact_requests) > 0
