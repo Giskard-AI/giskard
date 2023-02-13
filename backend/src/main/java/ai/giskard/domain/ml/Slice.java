257342a59d83a7137b206425c7b73a80c3267921
Andrey Avtomonov <andreybavt@gmail.com>
2023-02-13 13:01:03 +0100

    clean import

--- backend/src/main/java/ai/giskard/domain/ml/Slice.java
+++ backend/src/main/java/ai/giskard/domain/ml/Slice.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
