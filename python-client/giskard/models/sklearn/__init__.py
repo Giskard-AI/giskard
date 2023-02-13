988675487d8261acae0ca061ec7144d7c83dc031
Andrey Avtomonov <andreybavt@gmail.com>
2023-02-13 18:18:44 +0100

    cleaner model class structure

--- python-client/giskard/models/sklearn/__init__.py
+++ python-client/giskard/models/sklearn/__init__.py
@@ -1,10 +1,11 @@
 import mlflow
 
 from giskard.core.core import SupportedModelTypes
-from giskard.core.model import Model
+from giskard.core.model import MLFlowBasedModel
 
 
-class SKLearnModel(Model):
+class SKLearnModel(MLFlowBasedModel):
+
     def __init__(self, clf, model_type: SupportedModelTypes, name: str = None, data_preprocessing_function=None,
                  feature_names=None, classification_threshold=0.5, classification_labels=None) -> None:
 
@@ -13,15 +14,31 @@ class SKLearnModel(Model):
         if feature_names is None and hasattr(clf, 'feature_names_in_'):
             feature_names = list(getattr(clf, 'feature_names_in_'))
 
-        super().__init__(clf, model_type, name, data_preprocessing_function, feature_names,
-                         classification_threshold, classification_labels)
+        super().__init__(clf=clf, model_type=model_type, name=name,
+                         data_preprocessing_function=data_preprocessing_function,
+                         feature_names=feature_names,
+                         classification_threshold=classification_threshold,
+                         classification_labels=classification_labels)
+
+    def save_with_mflow(self, local_path, mlflow_meta):
+        if self.is_classification:
+            pyfunc_predict_fn = 'predict_proba'
+        elif self.is_regression:
+            pyfunc_predict_fn = 'predict'
+        else:
+            raise ValueError('Unsupported model type')
+
+        mlflow.sklearn.save_model(self.clf,
+                                  path=local_path,
+                                  pyfunc_predict_fn=pyfunc_predict_fn,
+                                  mlflow_model=mlflow_meta)
 
     @staticmethod
     def read_model_from_local_dir(local_path):
         return mlflow.sklearn.load_model(local_path)
 
-    def _raw_predict(self, data):
+    def clf_predict(self, df):
         if self.is_regression:
-            return self.clf.predict(data)
+            return self.clf.predict(df)
         else:
-            return self.clf.predict_proba(data)
+            return self.clf.predict_proba(df)
