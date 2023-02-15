80256a9ad2b749de1bbd1617207e86c9924eca29
Rabah <rabah.khalek@gmail.com>
2023-02-15 18:07:31 +0100

    updated the scan feature

--- python-client/giskard/core/scan.py
+++ python-client/giskard/core/scan.py
@@ -20,11 +20,12 @@ else:
     _in_ipython_session = False
 
 def test_performance( my_model: Model, my_dataset: Dataset):
-    output =  Suite() \
+    tot_res , list_res =  Suite() \
         .add_test(test_auc, threshold=0.8) \
         .add_test(test_f1, threshold=0.8) \
-        .run(actual_slice=my_dataset, model=my_model)[0]
-    print(output)
+        .run(actual_slice=my_dataset, model=my_model)
+    if not tot_res:
+        print("Your model failed some performance tests!")
 
 def _scan(model: Model,
           validate_ds: Dataset,
@@ -32,6 +33,7 @@ def _scan(model: Model,
 
     if scan_type == ScanType.VALIDATION:
         validate_model(model, validate_ds)
+        print("Your model and dataset passed the validation, and can be safely uploaded to Giskard!")
     if scan_type == ScanType.PERFORMANCE:
         test_performance(model, validate_ds)
 
