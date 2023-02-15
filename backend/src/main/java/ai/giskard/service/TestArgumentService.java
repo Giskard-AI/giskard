8c9fe19f10fa8d69de3cfe5f36e4b65282c2db6c
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-02-15 11:16:03 +0100

    Handle primitives (float, int, str, bool)

--- backend/src/main/java/ai/giskard/service/TestArgumentService.java
+++ backend/src/main/java/ai/giskard/service/TestArgumentService.java
@@ -59,11 +59,13 @@ public class TestArgumentService {
                 );
             }
             case "float" -> argumentBuilder.setFloat(Float.parseFloat(inputValue));
-            case "string" -> argumentBuilder.setString(inputValue);
+            case "int" -> argumentBuilder.setInt(Integer.parseInt(inputValue));
+            case "str" -> argumentBuilder.setStr(inputValue);
+            case "bool" -> argumentBuilder.setBool(Boolean.parseBoolean(inputValue));
             default ->
                 throw new IllegalArgumentException(String.format("Unknown test execution input type %s", testInputTypes.get(inputName)));
         }
-        
+
         return argumentBuilder.build();
     }
 
