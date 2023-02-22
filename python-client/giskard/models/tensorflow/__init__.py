3b2f114accf22d9426d5854041cde9b339896596
Rabah <rabah.khalek@gmail.com>
2023-02-22 13:37:19 +0100

    added warning to tensorflow init

--- python-client/giskard/models/tensorflow/__init__.py
+++ python-client/giskard/models/tensorflow/__init__.py
@@ -1,11 +1,12 @@
 import mlflow
 from typing import Union
-
+import logging
 import numpy as np
 
 from giskard.core.core import SupportedModelTypes
 from giskard.core.model import MLFlowBasedModel
 
+logger = logging.getLogger(__name__)
 
 class TensorFlowModel(MLFlowBasedModel):
     def __init__(self,
@@ -40,6 +41,11 @@ class TensorFlowModel(MLFlowBasedModel):
         predictions = self.clf.predict(data)
 
         if self.is_classification and predictions.shape[1] == 1:
+            logger.warning(f"\nYour binary classification model prediction is of the shape {predictions.shape}. \n"
+                    f"In Giskard we expect for binary {(predictions.shape[0], 2)} for binary classification models. \n"
+                    f"We automatically infered the second class prediction but please make sure that \n"
+                    f"the probability output of your model corresponds to the first label of the \n"
+                    f"classification_labels ({self.meta.classification_labels}) you provided us with.", exc_info=True)
             predictions = np.insert(predictions, 1, 1 - predictions[:, 0], axis=1)
 
         predictions = np.squeeze(np.array(predictions))
