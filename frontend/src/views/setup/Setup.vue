d6d282dd771f8fbc689aa05ecee86bb9f67f2cf1
Googleton <hugo.viallon.pro@gmail.com>
2023-02-28 11:22:23 +0100

    Improve the UI a bit

--- frontend/src/views/setup/Setup.vue
+++ frontend/src/views/setup/Setup.vue
@@ -5,20 +5,20 @@
         <img src="@/assets/logo_v2_full.png" alt="logo" width="480">
       </v-col>
     </v-row>
-    <v-row>
+    <v-row v-if="!done">
       <v-col>
         <v-card>
           <v-card-title>I need a license</v-card-title>
           <v-card-text>
-            Giskard requires a license to use its UI! You can get a FREE open source license mailed to you via the form
-            below. If you already have one, you can paste it on the right hand side of this page.
+            Giskard requires a license to use its UI. You can get a FREE open source license mailed to you via the form
+            below. If you already have one, you can upload it on the right hand side of this page.
 
             <v-form>
               <v-text-field label="First name" v-model="firstName"></v-text-field>
               <v-text-field label="Last name" v-model="lastName"></v-text-field>
               <v-text-field label="Email" v-model="email"></v-text-field>
               <v-text-field label="Company name" v-model="companyName"></v-text-field>
-              <v-btn text @click="submit">Submit</v-btn>
+              <v-btn :loading="loading" text @click="submit">Submit</v-btn>
             </v-form>
           </v-card-text>
         </v-card>
@@ -28,8 +28,21 @@
         <v-card>
           <v-card-title>I have a license</v-card-title>
           <v-card-text>
+            If you already have a license, you can upload your license file by pressing the button below.
+          </v-card-text>
+          <v-card-actions class="ml-2 mb-2">
             <v-btn small color="primary" @click="openFileInput">Upload license file</v-btn>
             <input type="file" ref="fileInput" style="display: none;" @change="onFileUpdate"/>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row v-if="done">
+      <v-col>
+        <v-card>
+          <v-card-title>Thank you for installing Giskard</v-card-title>
+          <v-card-text>You will be redirected to the app in a few seconds. If this does not happen, <a
+              @click="redirectToMain">click here</a>.
           </v-card-text>
         </v-card>
       </v-col>
@@ -43,38 +56,32 @@ import {ref} from "vue";
 import axios, {AxiosError} from "axios";
 import {useMainStore} from "@/stores/main";
 import {api} from "@/api";
+import {useRouter} from "vue-router/composables";
 
+const router = useRouter();
 
-const loading = ref<boolean>(true);
+const loading = ref<boolean>(false);
+const done = ref<boolean>(false);
 
 const mainStore = useMainStore();
 
 const fileInput = ref<any | null>(null);
-const currentStep = ref<number>(1);
-
 const firstName = ref<string>("");
 const lastName = ref<string>("");
 const email = ref<string>("");
 const companyName = ref<string>("");
 
 async function submit() {
-  // TODO: While waiting for the email template to be done, submitting automatically downloads the license ....
   try {
-    const license = await axios.post('https://hook.eu1.make.com/g81venzbf3ausl6b8xitgudtqo4ev39q', {
+    loading.value = true;
+    await axios.post('https://hook.eu1.make.com/g81venzbf3ausl6b8xitgudtqo4ev39q', {
       firstName: firstName.value,
       lastName: lastName.value,
       email: email.value,
       companyName: companyName.value
     });
-
-    const elem = document.createElement('a');
-    elem.setAttribute('href', 'data:text/plain;charset=utf-8,' + license.data)
-    elem.setAttribute('download', 'license.lic')
-
-    elem.style.display = 'none';
-    document.body.appendChild(elem);
-    elem.click();
-    document.body.removeChild(elem);
+    loading.value = false;
+    mainStore.addNotification({color: 'success', content: "License request submitted, please check your email!"});
   } catch (e: AxiosError) {
     mainStore.addNotification({color: 'error', content: e.response?.data.toString()});
   }
@@ -85,15 +92,17 @@ function openFileInput() {
 }
 
 async function onFileUpdate(event) {
-  loading.value = true;
-
   let formData = new FormData();
   formData.append('file', event.target.files[0]);
 
   await api.uploadLicense(formData);
   await mainStore.fetchAppSettings();
+  done.value = true;
+  setTimeout(() => redirectToMain(), 3000);
+}
 
-  loading.value = false;
+function redirectToMain() {
+  router.push("/");
 }
 
 </script>
