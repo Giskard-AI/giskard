186bfbf5313533364884aeb1f0de68f9137168a3
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-03-07 13:17:12 +0100

    Fixed Add to Test suite

--- backend/src/main/java/ai/giskard/web/dto/SuiteTestDTO.java
+++ backend/src/main/java/ai/giskard/web/dto/SuiteTestDTO.java
@@ -1,18 +1,21 @@
 package ai.giskard.web.dto;
 
+import com.dataiku.j2ts.annotations.UINullable;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Data;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Map;
 import java.util.UUID;
 
 @Data
 public class SuiteTestDTO {
+    @UINullable
     private long id;
     @JsonAlias("test_uuid")
-    @NotBlank
+    @NotNull
     private UUID testUuid;;
     private Map<@NotBlank String, @Valid TestInputDTO> testInputs;
 }
