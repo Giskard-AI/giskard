97df8d17bab21dce34c774a542563bbe648642e1
Googleton <hugo.viallon.pro@gmail.com>
2023-03-07 18:10:21 +0100

    UI rework, still need finishing touches discussed with JM

--- frontend/src/views/setup/Setup.vue
+++ frontend/src/views/setup/Setup.vue
@@ -5,56 +5,74 @@
         <img src="@/assets/logo_v2_full.png" alt="logo" width="480">
       </v-col>
     </v-row>
-    <v-row v-if="!done">
+    <v-row>
       <v-col>
-        <v-card class="fill-height">
-          <v-card-title>Request a license</v-card-title>
-          <v-card-text>
+        <v-stepper v-model="step" vertical>
+          <v-stepper-step step="1" :complete="step > 1">
+            Request a license
+            <!--            Set green-->
+            <small v-if="licenseRequestSubmitted">Your license request was submitted, please check your email.</small>
+          </v-stepper-step>
+          <v-stepper-content step="1">
             <p>
               Giskard server requires a license. A <span class="font-weight-bold">free</span> license can be obtained by
               registered using the form below. The license will be sent by email.
             </p>
-            <p>If you already have one, you can upload it on the right hand side of this page.</p>
+            <p>If you already have one, you can press the skip button.</p>
             <ValidationObserver ref="observer" v-slot="{ invalid }">
-              <v-form @keyup.enter="submit">
+              <v-form @keyup.enter="submit" style="max-width: 500px">
                 <ValidationProvider name="First name" mode="eager" rules="required" v-slot="{errors}">
-                  <v-text-field label="First name" v-model="firstName" :error-messages="errors"></v-text-field>
+                  <v-text-field label="First name*" v-model="firstName" :error-messages="errors"
+                                required></v-text-field>
                 </ValidationProvider>
                 <ValidationProvider name="Last name" mode="eager" rules="required" v-slot="{errors}">
-                  <v-text-field label="Last name" v-model="lastName" :error-messages="errors"></v-text-field>
+                  <v-text-field label="Last name*" v-model="lastName" :error-messages="errors"></v-text-field>
                 </ValidationProvider>
                 <ValidationProvider name="Email" mode="eager" rules="required|email" v-slot="{errors}">
-                  <v-text-field label="Email" v-model="email" :error-messages="errors"></v-text-field>
+                  <v-text-field label="Email*" v-model="email" :error-messages="errors"></v-text-field>
                 </ValidationProvider>
                 <v-text-field label="Company name" v-model="companyName"></v-text-field>
+                <ValidationProvider name="Agreement with the privacy policy" mode="eager"
+                                    :rules="{ required: { allowFalse: false } }"
+                                    v-slot="{errors}">
+                  <v-checkbox v-model="termsOfServiceAgree" dense :error-messages="errors">
+                    <template v-slot:label>
+                      You agree with our&nbsp;<a href="https://giskard-ai.github.io/giskard-privacy/policy.html">privacy
+                      policy</a>.
+                    </template>
+                  </v-checkbox>
+                </ValidationProvider>
+                <v-checkbox v-model="newsLetterAgree" dense
+                            label="Add me to your newsletter and keep me updated about Giskard."></v-checkbox>
+
                 <v-btn :loading="loading" color="primary" @click.prevent="submit">Submit</v-btn>
+                <!--                Delete this-->
+                <v-btn text @click="step = 2">Skip</v-btn>
               </v-form>
             </ValidationObserver>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-divider vertical class="ma-10"></v-divider>
-      <v-col>
-        <v-card class="fill-height">
-          <v-card-title>Use existing license</v-card-title>
-          <v-card-text class="flex-grow-1">
-            If you already have a license, you can upload your license file by pressing the button below.
-          </v-card-text>
-          <v-card-actions class="ml-2 mb-2 justify-center">
+          </v-stepper-content>
+
+          <v-stepper-step step="2" :complete="step > 2">
+            Upload license file
+          </v-stepper-step>
+          <v-stepper-content step="2">
+            <p>You can upload your license file by pressing the button below.</p>
+
             <v-btn color="primary" @click="openFileInput">Upload license file</v-btn>
             <input type="file" ref="fileInput" style="display: none;" @change="onFileUpdate"/>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row v-if="done">
-      <v-col>
-        <v-card>
-          <v-card-title>Thank you for installing Giskard</v-card-title>
-          <v-card-text>You will be redirected to the app in a few seconds. If this does not happen, <a
-              @click="redirectToMain">click here</a>.
-          </v-card-text>
-        </v-card>
+          </v-stepper-content>
+
+          <v-stepper-step step="3" :complete="step >= 3">
+            Launch Giskard
+          </v-stepper-step>
+          <v-stepper-content step="3">
+            <p>Your Giskard setup is now complete. You can now refresh this page or click the button below to open
+              Giskard.</p>
+
+            <v-checkbox label="Agree to send anonymous analytics to Giskard."></v-checkbox>
+            <v-btn color="primary" large @click="redirectToMain()">Launch Giskard</v-btn>
+          </v-stepper-content>
+        </v-stepper>
       </v-col>
     </v-row>
   </v-container>
@@ -71,7 +89,7 @@ import {useRouter} from "vue-router/composables";
 const router = useRouter();
 
 const loading = ref<boolean>(false);
-const done = ref<boolean>(false);
+const step = ref<number>(1);
 
 const mainStore = useMainStore();
 
@@ -80,6 +98,10 @@ const firstName = ref<string>("");
 const lastName = ref<string>("");
 const email = ref<string>("");
 const companyName = ref<string>("");
+const termsOfServiceAgree = ref<boolean>(false);
+const newsLetterAgree = ref<boolean>(false);
+
+const licenseRequestSubmitted = ref<boolean>(false);
 
 const observer = ref<any | null>(null);
 
@@ -91,12 +113,17 @@ async function submit() {
 
     try {
       loading.value = true;
+      licenseRequestSubmitted.value = false;
       await axios.post('https://hook.eu1.make.com/g81venzbf3ausl6b8xitgudtqo4ev39q', {
         firstName: firstName.value,
         lastName: lastName.value,
         email: email.value,
-        companyName: companyName.value
+        companyName: companyName.value,
+        newsletter: newsLetterAgree.value,
+        tos: termsOfServiceAgree.value
       });
+      step.value = 2;
+      licenseRequestSubmitted.value = true;
       mainStore.addNotification({color: 'success', content: "License request submitted, please check your email!"});
     } catch (e: AxiosError) {
       mainStore.addNotification({color: 'error', content: e.response?.data.toString()});
@@ -111,13 +138,16 @@ function openFileInput() {
 }
 
 async function onFileUpdate(event) {
+  if (!event.target.files[0]) {
+    return;
+  }
+
   let formData = new FormData();
   formData.append('file', event.target.files[0]);
 
   await api.uploadLicense(formData);
   await mainStore.fetchLicense();
-  done.value = true;
-  setTimeout(() => redirectToMain(), 3000);
+  step.value = 3;
 }
 
 function redirectToMain() {
