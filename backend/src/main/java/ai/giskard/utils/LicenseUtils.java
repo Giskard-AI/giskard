133a98db0ee71b818f87bd00ce0353c033ac593c
Andrey Avtomonov <andreybavt@gmail.com>
2023-03-09 23:31:02 +0100

    sonar

--- backend/src/main/java/ai/giskard/utils/LicenseUtils.java
+++ backend/src/main/java/ai/giskard/utils/LicenseUtils.java
@@ -1,6 +1,9 @@
 package ai.giskard.utils;
 
 public class LicenseUtils {
+    private LicenseUtils() {
+        throw new UnsupportedOperationException("Utility class shouldn't be initialized");
+    }
 
     public static boolean isLimitReached(Integer limit, Integer value) {
         if (limit == null)
