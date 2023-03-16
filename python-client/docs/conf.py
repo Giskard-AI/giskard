1361b81dd4accd627422780fe3b995df33e44fc9
Andrey Avtomonov <andreybavt@gmail.com>
2023-03-16 01:35:48 +0100

    test docs

--- python-client/docs/conf.py
+++ python-client/docs/conf.py
@@ -26,3 +26,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../giskard/'))
+
