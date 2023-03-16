a78d88046a1586144bd13dacdb477fa54fd03ebe
Andrey Avtomonov <andreybavt@gmail.com>
2023-03-16 02:09:23 +0100

    test docs

--- python-client/docs/conf.py
+++ python-client/docs/conf.py
@@ -14,7 +14,7 @@ release = '1.8.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = ["myst_parser", 'sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
