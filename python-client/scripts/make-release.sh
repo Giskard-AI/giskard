3b69502de188254d27844edb83f7a753caf78b2a
Andrey Avtomonov <andreybavt@gmail.com>
2023-03-17 17:17:38 +0100

    fix python client release script

--- python-client/scripts/make-release.sh
+++ python-client/scripts/make-release.sh
@@ -19,7 +19,7 @@ if [ -n "$1" ]; then
   sed -i '' "s/^version = \".*$/version = \"$1\"/" "$pyprojpath"
   git add "$pyprojpath"
   git commit -m "Version $1"
-  git tag -a "v$1" -m "v$1"
+  git tag -a "python-client-v$1" -m "v$1"
   git push
   git push origin "v$1"
 else
