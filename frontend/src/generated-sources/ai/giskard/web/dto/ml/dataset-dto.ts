b809e4ad7946dc137a505264f875e2189199987c
Rabah <rabah.khalek@gmail.com>
2023-03-28 12:27:25 +0200

    updated syntax of columndtype in the backend

--- frontend/src/generated-sources/ai/giskard/web/dto/ml/dataset-dto.ts
+++ frontend/src/generated-sources/ai/giskard/web/dto/ml/dataset-dto.ts
@@ -5,7 +5,7 @@ import type {ProjectDTO} from './project-dto';
  * Generated from ai.giskard.web.dto.ml.DatasetDTO
  */
 export interface DatasetDTO {
-    columnDTypes: {[key: string]: string};
+    columnDtypes: {[key: string]: string};
     compressedSizeBytes: number;
     createdDate: any /* TODO: Missing translation of java.time.Instant */;
     columnTypes: {[key: string]: ColumnType};
