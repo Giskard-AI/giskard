219c95c7937a96db789da48a104d22f697fe3594
Rabah <rabah.khalek@gmail.com>
2023-04-05 01:44:51 +0200

    updated docs

--- python-client/docs/conf.py
+++ python-client/docs/conf.py
@@ -14,14 +14,7 @@ release = '1.8.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser",
-              'sphinx.ext.todo',
-              'sphinx.ext.viewcode',
-              'sphinx.ext.autodoc',
-              'sphinx.ext.autosummary',
-              'sphinx_design'
-              ]
-myst_enable_extensions = ["colon_fence"]
+extensions = ["myst_parser", 'sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -30,7 +23,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 import os
 import sys
--- python-client/docs/guides/contribute-to-giskard/giskard-architecture.md
+++
@@ -1,7 +0,0 @@
----
-description: Giskard process architecture
----
-
-# Giskard architecture
-
-<figure><img src="../../.gitbook/assets/Architecture.jpg" alt=""><figcaption></figcaption></figure>
