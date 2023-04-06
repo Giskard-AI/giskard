51fe53d0d000d75d4ee13afeac000be239375ed3
Andrey Avtomonov <andreybavt@gmail.com>
2023-04-06 23:32:00 +0200

    fix build

--- python-client/setup.py
+++ python-client/setup.py
@@ -8,7 +8,7 @@ from setuptools.command.build_py import build_py
 
 class GrpcTool(Command):
     user_options = []
-    out_path = 'giskard/ml_worker/generated'
+    out_path = 'giskard/ml_worker/proto'
 
     def initialize_options(self):
         pass
@@ -25,7 +25,7 @@ class GrpcTool(Command):
                 with open(fname) as rfile:
                     content = rfile.read()
                     content = re.sub(
-                        '^import ([^\\. ]*?_pb2)', f'import giskard.ml_worker.generated.\g<1>',
+                        '^import ([^\\. ]*?_pb2)', f'import giskard.ml_worker.proto.\g<1>',
                         content, flags=re.MULTILINE)
                     with open(fname, 'w') as wfile:
                         wfile.write(content)
