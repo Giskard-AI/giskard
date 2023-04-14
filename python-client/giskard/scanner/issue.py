f948715b8bca37f29059d24d4de9d66b36304c3b
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-04-14 17:46:41 +0200

    Basic __repr__ method of issues

--- python-client/giskard/scanner/issue.py
+++ python-client/giskard/scanner/issue.py
@@ -7,14 +7,18 @@ from typing import Optional, Callable
 
 class Issue:
     def __init__(
-        self,
-        slice_fn: SliceFunction,
-        model: Optional[BaseModel] = None,
-        dataset: Optional[Dataset] = None,
-        test_results=None,
+            self,
+            slice_fn: SliceFunction,
+            model: Optional[BaseModel] = None,
+            dataset: Optional[Dataset] = None,
+            test_results=None,
+            test_name=None
     ):
         self.slice_fn = slice_fn
         self.model = model
         self.dataset = dataset
         self.test_results = test_results
-    
+        self.test_name = test_name
+
+    def __repr__(self):
+        return f"<Issue {(self.slice_fn.query.get_all_clauses()[0].to_pandas(), self.test_results.passed, self.test_results.metric,self.test_name)}>"
