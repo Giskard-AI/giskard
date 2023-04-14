bd288f0bad10b63c8749f5def806ff1b15ecd41e
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-04-14 10:03:36 +0200

    Refactoring

---
+++ frontend/src/generated-sources/ai/giskard/web/dto/catalog-dto.ts
@@ -0,0 +1,10 @@
+import type {SliceFunctionDTO} from './slice-function-dto';
+import type {TestFunctionDTO} from './test-function-dto';
+
+/**
+ * Generated from ai.giskard.web.dto.CatalogDTO
+ */
+export interface CatalogDTO {
+    slices: SliceFunctionDTO[];
+    tests: TestFunctionDTO[];
+}
---
+++ frontend/src/generated-sources/ai/giskard/web/dto/slice-function-dto.ts
@@ -0,0 +1,6 @@
+import type {CallableDTO} from './callable-dto';
+
+/**
+ * Generated from ai.giskard.web.dto.SliceFunctionDTO
+ */
+export interface SliceFunctionDTO extends CallableDTO {}
