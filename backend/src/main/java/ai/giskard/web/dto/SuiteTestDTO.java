687c425e51838e8e7862e5d730832ba955c1d7af
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-04-17 13:33:37 +0200

    Allow to see test when ML worker is not running

--- backend/src/main/java/ai/giskard/web/dto/SuiteTestDTO.java
+++ backend/src/main/java/ai/giskard/web/dto/SuiteTestDTO.java
@@ -14,6 +14,9 @@ import java.util.UUID;
 public class SuiteTestDTO {
     @UINullable
     private long id;
+
+    private TestFunctionDTO testFunction;
+
     @JsonAlias("test_uuid")
     @NotNull
     private UUID testUuid;
