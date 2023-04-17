1a66eabbc7e127689bfdac36086654eb484312ea
Rabah <rabah.khalek@gmail.com>
2023-04-17 10:47:31 +0200

    fixed catboost feature_names_ and improved console prints for factory methods

--- python-client/giskard/datasets/__init__.py
+++ python-client/giskard/datasets/__init__.py
@@ -1,5 +1,6 @@
 from typing import Dict, Optional, List
 import pandas as pd
+from rich.console import Console
 from giskard.datasets.base import Dataset
 from giskard.core.validation import configured_validate_arguments
 
@@ -11,6 +12,11 @@ def wrap_dataset(dataset: pd.DataFrame,
                  cat_columns: Optional[List[str]] = None,
                  infer_column_types: Optional[bool] = False,
                  column_types: Optional[Dict[str, str]] = None):
+    console = Console()
+    console.print("Your 'pandas.DataFrame' dataset is successfully wrapped by Giskard's 'Dataset' wrapper class.", style="bold green")
+    console.print("Check Giskard's [link=https://giskard.readthedocs.io/en/latest/reference/datasets/index.html]Datasets[/link]"
+                  " in the API reference documentation for more details.",
+                  style="bold blue")
     return Dataset(dataset,
                    name,
                    target,
