d56d16e9aba034201c55f3b63d90fb15e5ed13ef
Rabah <rabah.khalek@gmail.com>
2023-04-17 18:21:34 +0200

    fixed linter

--- python-client/giskard/ml_worker/testing/tests/__init__.py
+++ python-client/giskard/ml_worker/testing/tests/__init__.py
@@ -1,9 +1,11 @@
 from giskard.ml_worker.testing.tests import drift
 from giskard.ml_worker.testing.tests import performance
 from giskard.ml_worker.testing.tests import statistic
+from giskard.ml_worker.testing.tests import metamorphic
 
 __all__ = [
-    'statistic.py',
+    'statistic',
     'performance',
     'drift',
+    'metamorphic',
 ]
