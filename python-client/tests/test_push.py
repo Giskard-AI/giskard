01d37125a562aa2da83089bbfc13cde98b270c5c
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-04-20 18:02:48 +0200

    Added unit tests for contribution and perturbation test

--- python-client/tests/test_push.py
+++ python-client/tests/test_push.py
@@ -1,5 +1,15 @@
 from giskard.push.perturbation import perturbation
+from giskard.push.contribution import contribution
+
+def test_perturbation(german_credit_model, german_credit_data):
+    testl = []
+    for i in range(100):
+        testl.append(perturbation(german_credit_model, german_credit_data, i))
+    assert testl != []
 
 
-def test_perturbation(german_credit_model, german_credit_test_data):
-    perturbation(german_credit_model, german_credit_test_data, 0)
+def test_contribution(german_credit_model, german_credit_data):
+    testl = []
+    for i in range(100):
+        testl.append(contribution(german_credit_model, german_credit_data, i))
+    assert testl != []
