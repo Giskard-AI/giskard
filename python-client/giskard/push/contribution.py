5ca70eb0a3305a1584a793bb669e55353616f3a0
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-04-21 15:46:38 +0200

    Added push feature for regression

--- python-client/giskard/push/contribution.py
+++ python-client/giskard/push/contribution.py
@@ -1,54 +1,103 @@
 import numpy as np
 from scipy.stats import zscore
 from giskard.ml_worker.core.model_explanation import explain
+from giskard.ml_worker.testing.tests.performance import test_rmse
+from giskard.core.core import SupportedModelTypes
 
 
 def contribution(model, ds, idrow):  # data_aug_dict
-    shap_res = _contribution_push(model, ds, idrow)
-    values = ds.df.iloc[idrow]
-    training_label = values[ds.target]
-    prediction = model.clf.predict(ds.df.iloc[[idrow]])
+    if model.meta.model_type == SupportedModelTypes.CLASSIFICATION:
+        shap_res = _contribution_push(model, ds, idrow)
+        values = ds.df.iloc[idrow]
+        training_label = values[ds.target]
+        prediction = model.clf.predict(ds.df.iloc[[idrow]])
 
-    if shap_res is not None:
-        for el in shap_res:
-            # skip for now the following case
-            # if ds.column_types[el] == "category" and training_label != prediction and data_aug_dict(el, values):
-            #    print(f"Data augmentation recommended for the slice.............{el}",
-            #          el, values[el])
-            if training_label != prediction:  # use scan feature ?
+        if shap_res is not None:
+            for el in shap_res:
+                # skip for now the following case
+                # if ds.column_types[el] == "category" and training_label != prediction and data_aug_dict(el, values):
+                #    print(f"Data augmentation recommended for the slice.............{el}",
+                #          el, values[el])
+                if training_label != prediction:  # use scan feature ?
 
-                res = [{
-                    "sentence": "This value is responsible for the incorrect prediction",
-                    "action": "Slice",
-                    "value": str(values[el]),
-                    "key": str(el),
-                },
-                    {
-                    "sentence": "This correlation may apply to the whole dataset",
-                    "action": "Test",
-                    "value": str(values[el]),
-                    "key": str(el),
-                    }
-                ]
+                    res = [{
+                        "sentence": "This value is responsible for the incorrect prediction",
+                        "action": "Slice",
+                        "value": str(values[el]),
+                        "key": str(el),
+                    },
+                        {
+                            "sentence": "This correlation may apply to the whole dataset",
+                            "action": "Test",
+                            "value": str(values[el]),
+                            "key": str(el),
+                        }
+                    ]
 
-                return res
+                    return res
 
-            else:
-                res = [{
-                    "sentence": "This feature contributes a lot to the prediction",
-                    "action": "Slice",
-                    "value": str(values[el]),
-                    "key": str(el),
-                },
-                    {
-                    "sentence": "This correlation may apply to the whole dataset",
-                    "action": "Test",
-                    "value": str(values[el]),
-                    "key": str(el),
-                    }
-                ]
+                else:
+                    res = [{
+                        "sentence": "This feature contributes a lot to the prediction",
+                        "action": "Slice",
+                        "value": str(values[el]),
+                        "key": str(el),
+                    },
+                        {
+                            "sentence": "This correlation may apply to the whole dataset",
+                            "action": "Test",
+                            "value": str(values[el]),
+                            "key": str(el),
+                        }
+                    ]
 
-                return res
+                    return res
+
+    if model.meta.model_type == SupportedModelTypes.REGRESSION:
+        shap_res = _contribution_push(model, ds, idrow)
+        values = ds.df.iloc[idrow]
+        re = ds.__dict__
+        y = values[ds.target]
+        y_hat = model.clf.predict(ds.df.drop(columns=[ds.target]).iloc[[idrow]])
+        error = abs(y_hat - y)
+        rmse_res = test_rmse(ds, model).execute()
+        print(shap_res)
+        if shap_res is not None:
+            for el in shap_res:
+                print(error, rmse_res)
+                if abs(error - rmse_res.metric) / rmse_res.metric >= 0.2:  # use scan feature ?
+                    res = [{
+                        "sentence": "This value is responsible for the incorrect prediction",
+                        "action": "Slice",
+                        "value": str(values[el]),
+                        "key": str(el),
+                    },
+                        {
+                            "sentence": "This correlation may apply to the whole dataset",
+                            "action": "Test",
+                            "value": str(values[el]),
+                            "key": str(el),
+                        }
+                    ]
+
+                    return res
+
+                else:
+                    res = [{
+                        "sentence": "This feature contributes a lot to the prediction",
+                        "action": "Slice",
+                        "value": str(values[el]),
+                        "key": str(el),
+                    },
+                        {
+                            "sentence": "This correlation may apply to the whole dataset",
+                            "action": "Test",
+                            "value": str(values[el]),
+                            "key": str(el),
+                        }
+                    ]
+
+                    return res
 
 
 def _contribution_push(model, ds, idrow):  # done at each step
@@ -68,7 +117,10 @@ def _contribution_push(model, ds, idrow):  # done at each step
 
 
 def _get_shap_values(model, ds, idrow):  # from gRPC
-    return explain(model, ds, ds.df.iloc[idrow])["explanations"][model.meta.classification_labels[0]]
+    if model.meta.model_type == SupportedModelTypes.CLASSIFICATION:
+        return explain(model, ds, ds.df.iloc[idrow])["explanations"][model.meta.classification_labels[0]]
+    elif model.meta.model_type == SupportedModelTypes.REGRESSION:
+        return explain(model, ds, ds.df.iloc[idrow])["explanations"]["default"]
 
 
 def bins_count(model, dataframe):  # done at the beggining
