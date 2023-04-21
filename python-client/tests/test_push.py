21327301f008e5ce821598aa437d1f2c7e1ff2cf
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-04-21 15:46:38 +0200

    Added push feature for regression

--- python-client/tests/test_push.py
+++ python-client/tests/test_push.py
@@ -1,5 +1,6 @@
 from giskard.push.perturbation import perturbation
 from giskard.push.contribution import contribution
+from tests.fixtures.regression import linear_regression_diabetes, linear_regression_diabetes_raw
 
 
 def test_perturbation(german_credit_model, german_credit_data):
@@ -28,3 +29,22 @@ def test_contribution(german_credit_model, german_credit_data):
 #             testl.append(res)
 #     assert testl != []
 
+
+def test_contribution_reg(linear_regression_diabetes, diabetes_dataset_with_target):
+    testl = []
+    # print(diabetes_dataset.df.count())
+
+    for i in range(400):
+        # print(linear_regression_diabetes.__dict__)
+        res = contribution(linear_regression_diabetes, diabetes_dataset_with_target, i)
+        if res is not None:
+            testl.append(res)
+    assert testl != []
+
+def test_perturbation_reg(linear_regression_diabetes, diabetes_dataset_with_target):
+    testl = []
+    for i in range(400):
+        res = perturbation(linear_regression_diabetes, diabetes_dataset_with_target, i)
+        if res is not None:
+            testl.append(res)
+    assert testl != []
