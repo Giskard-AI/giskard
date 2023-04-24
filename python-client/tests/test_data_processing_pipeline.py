29836d0d428ebe2ff508aa573fbf6ec7d7c72c44
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-04-24 10:46:06 +0200

    Fixed test naming

--- python-client/tests/test_data_processing_pipeline.py
+++ python-client/tests/test_data_processing_pipeline.py
@@ -62,6 +62,15 @@ def test_chain(german_credit_data: Dataset):
 
 
 def test_transformation(german_credit_data: Dataset):
+    ds = german_credit_data.transform(transform_without_parenthesis)
+    assert np.all(ds.df.credit_amount == -2)
+    ds = german_credit_data.transform(transform_with_parenthesis)
+    assert np.all(ds.df.credit_amount == -1)
+    assert len(german_credit_data.df) == 1000
+    assert len(german_credit_data.df.credit_amount.unique()) > 1
+
+
+def test_transformation_without_annotation(german_credit_data: Dataset):
     def transform_without_annotation(x: pd.Series) -> pd.Series:
         x.credit_amount = -2
         return x
@@ -70,16 +79,7 @@ def test_transformation(german_credit_data: Dataset):
     assert np.all(ds.df.credit_amount == -2)
     assert len(german_credit_data.df) == 1000
     assert len(german_credit_data.df.credit_amount.unique()) > 1
-
-
-def test_transformation_without_annotation(german_credit_data: Dataset):
-    ds = german_credit_data.transform(transform_without_parenthesis)
-    assert np.all(ds.df.credit_amount == -2)
-    ds = german_credit_data.transform(transform_with_parenthesis)
-    assert np.all(ds.df.credit_amount == -1)
-    assert len(german_credit_data.df) == 1000
-    assert len(german_credit_data.df.credit_amount.unique()) > 1
-
+    
 
 def test_missing_arg_slicing_function():
     with pytest.raises(TypeError,
