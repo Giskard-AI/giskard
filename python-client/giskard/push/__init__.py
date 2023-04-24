d4d2ca3bac74d55df4f030a83b04d4a9724e66fd
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-04-24 17:34:20 +0200

    Working on wording for push features

--- python-client/giskard/push/__init__.py
+++ python-client/giskard/push/__init__.py
@@ -4,13 +4,13 @@ from giskard.core.core import SupportedModelTypes
 class Push:
     def __init__(self):
         self.__incorrect = [{
-            "action": "Open a new debugger session with similar examples",
-            "explanation": "Debugging similar examples may help you find common patterns",
+            "action": "Open a new debugger session with similar spurious examples",
+            "explanation": "Debugging similar examples may help you find common spurious patterns",
             "button": "Open debugger"
         },
             {
                 "action": "Generate a new performance difference test in the catalog",
-                "explanation": "This may help ensure this incorrect pattern is not common to the whole dataset",
+                "explanation": "This may help ensure this spurious pattern is not common to the whole dataset",
                 "button": "Create test"
             },
             {
@@ -26,8 +26,8 @@ class Push:
             "button": "Open debugger"
         },
             {
-                "action": "Generate a new drift difference test in the catalog",
-                "explanation": "This may help ensure this pattern is not common to the whole dataset",
+                "action": "Generate a test to check if this correlation holds with the whole dataset",
+                "explanation": "Correlations may be spurious, double check if it has a business sense",
                 "button": "Create test"
             },
             {
@@ -38,25 +38,19 @@ class Push:
         ]
 
         self.__perturbation = [{
-            "action": "Open a new debugger session with similar examples",
-            "explanation": "Debugging similar examples may help you find common patterns",
-            "button": "Open debugger"
+            "action": "Generate a robustness test in the catalog that slightly perturb this feature",
+            "explanation": "This will enable you to make sure the model is robust against small similar changes",
+            "button": "Create test"
         },
             {
-                "action": "Generate a new metamorphic test in the catalog",
-                "explanation": "This may help ensure this pattern is not common to the whole dataset",
-                "button": "Create test"
-            },
-            {
-                "action": "Save slice and continue debugging session",
-                "explanation": "Saving the slice will enable you to create tests more efficiently",
-                "button": "Save Slice"
+                "action": "Save the perturbation that made the model change and continue debugging session",
+                "explanation": "Saving this perturbation will enable you to create tests more efficiently",
+                "button": "Save Perturbation"
             }
-
         ]
 
     def incorrect(self, feature, value):
-        res = {"ke<y": str(feature),
+        res = {"key": str(feature),
                "value": str(value),
                "push": f"{str(feature)}=={str(value)} is responsible for the incorrect prediction",
                "push_list": self.__incorrect
@@ -72,12 +66,14 @@ class Push:
 
         return res
 
-    def perturbation(self,feature,value):
+    def perturbation(self, feature, value):
         res = {"key": str(feature),
                "value": str(value),
-               "push": f"A small variation (+20%) of {str(feature)}=={str(value)} makes the prediction change,"
-                       "check if this unrobust behavior generalizes to the whole dataset",
+               "push": f"A small variation of {str(feature)}=={str(value)} makes the prediction change",
                "push_list": self.__perturbation
                }
+
+        # Details about the perturbation for textual feature - Transforming into upper-case, Shuffeling ,
+        # Swapping tokens makes the prediction change
 
         return res
