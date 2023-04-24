3355f9aec520d3922691cbfb078d608033883163
Rabah <rabah.khalek@gmail.com>
2023-04-24 19:22:44 +0200

    fixed rest of unit-tests

--- python-client/tests/fixtures/enron_multilabel_classification.py
+++ python-client/tests/fixtures/enron_multilabel_classification.py
@@ -31,8 +31,10 @@ input_types = {
 @pytest.fixture()
 def enron_data() -> Dataset:
     logger.info("Fetching Enron Data")
+    df = pd.read_csv(path("test_data/enron_data.csv"), keep_default_na=False, na_values=["_GSK_NA_"])
+    df.drop(columns="Unnamed: 0", inplace=True)
     return Dataset(
-        df=pd.read_csv(path("test_data/enron_data.csv"), keep_default_na=False, na_values=["_GSK_NA_"], index_col=0),
+        df=df,
         target="Target", column_types=input_types)
 
 
