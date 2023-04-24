845f97d298c1b2f7f039aad858bfdc7c9f318c47
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-04-24 10:30:33 +0200

    Added validate_model to scan initialization

--- python-client/giskard/scanner/__init__.py
+++ python-client/giskard/scanner/__init__.py
@@ -1,6 +1,7 @@
 from ..models.base import BaseModel
 from ..datasets.base import Dataset
 from .performance import PerformanceScan
+from ..core.model_validation import validate_model
 
 
 def scan(model: BaseModel, dataset: Dataset, tests=None, method="tree", threshold=-0.1):
@@ -19,5 +20,7 @@ def scan(model: BaseModel, dataset: Dataset, tests=None, method="tree", threshol
         threshold (float):
             The threshold to detect low performance. Default is -0.1, meaning that variation of metrics below 10% will be considered relevant.
     """
+
+    validate_model(model=model, validate_ds=dataset)
     analyzer = PerformanceScan(model, dataset, tests)
     return analyzer.run(slicer=method, threshold=threshold)
