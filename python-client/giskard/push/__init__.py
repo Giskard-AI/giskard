b8056694b8a73053d54f97bb818daef276d01cba
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-04-25 15:31:19 +0200

    Added Perturbation class and modified Push class

--- python-client/giskard/push/__init__.py
+++ python-client/giskard/push/__init__.py
@@ -1,79 +1,97 @@
 from giskard.core.core import SupportedModelTypes
 
 
+class Perturbation:
+    def __init__(self, passed, perturbation_value):
+        self.passed = passed
+        self.perturbation_value = perturbation_value
+
+
 class Push:
-    def __init__(self):
-        self.__incorrect = [{
-            "action": "Open a new debugger session with similar spurious examples",
-            "explanation": "Debugging similar examples may help you find common spurious patterns",
-            "button": "Open debugger"
-        },
-            {
-                "action": "Generate a new performance difference test in the catalog",
-                "explanation": "This may help ensure this spurious pattern is not common to the whole dataset",
-                "button": "Create test"
-            },
-            {
-                "action": "Save slice and continue debugging session",
-                "explanation": "Saving the slice will enable you to create tests more efficiently",
-                "button": "Save Slice"
-            }
-        ]
+    key = None
+    value = None
+    push_title = None
+    push_details = None
+    perturbation_value = None
 
-        self.__highly_correlated = [{
-            "action": "Open a new debugger session with similar examples",
-            "explanation": "Debugging similar examples may help you find common patterns",
-            "button": "Open debugger"
-        },
-            {
-                "action": "Generate a test to check if this correlation holds with the whole dataset",
-                "explanation": "Correlations may be spurious, double check if it has a business sense",
-                "button": "Create test"
-            },
-            {
-                "action": "Save slice and continue debugging session",
-                "explanation": "Saving the slice will enable you to create tests more efficiently",
-                "button": "Save Slice"
-            }
-        ]
+    def __init__(self, push_type, feature, value, perturbation_value=None):
+        self.perturbation_value = perturbation_value
+        self.key = feature
+        self.value = value
+        if push_type == "contribution_wrong":
+            self._high_contribution_wrong_prediction(feature, value)
 
-        self.__perturbation = [{
-            "action": "Generate a robustness test in the catalog that slightly perturb this feature",
-            "explanation": "This will enable you to make sure the model is robust against small similar changes",
-            "button": "Create test"
-        },
-            {
-                "action": "Save the perturbation that made the model change and continue debugging session",
-                "explanation": "Saving this perturbation will enable you to create tests more efficiently",
-                "button": "Save Perturbation"
-            }
-        ]
+        if push_type == "contribution_only":
+            self._high_contribution_only(feature, value)
+        if push_type == "perturbation":
+            self._perturbation(feature, value)
 
-    def incorrect(self, feature, value):
-        res = {"key": str(feature),
-               "value": str(value),
-               "push": f"{str(feature)}=={str(value)} is responsible for the incorrect prediction",
-               "push_list": self.__incorrect
+    def _high_contribution_wrong_prediction(self, feature, value):
+        res = {"push_title": f"{str(feature)}=={str(value)} is responsible for the incorrect prediction",
+               "push_details":
+                   [{
+                       "action": "Open a new debugger session with similar spurious examples",
+                       "explanation": "Debugging similar examples may help you find common spurious patterns",
+                       "button": "Open debugger"
+                   },
+                       {
+                           "action": "Generate a new performance difference test in the catalog",
+                           "explanation": "This may help ensure this spurious pattern is not common to the whole dataset",
+                           "button": "Create test"
+                       },
+                       {
+                           "action": "Save slice and continue debugging session",
+                           "explanation": "Saving the slice will enable you to create tests more efficiently",
+                           "button": "Save Slice"
+                       }
+                   ]
                }
+        self.push_title = res["push_title"]
+        self.push_details = res["push_details"]
         return res
 
-    def high_correlation(self, feature, value):
-        res = {"key": str(feature),
-               "value": str(value),
-               "push": f"{str(feature)}=={str(value)} contributes a lot to the prediction",
-               "push_list": self.__highly_correlated
+    def _high_contribution_only(self, feature, value):
+        res = {"push_title": f"{str(feature)}=={str(value)} contributes a lot to the prediction",
+               "push_details":
+                   [{
+                       "action": "Open a new debugger session with similar examples",
+                       "explanation": "Debugging similar examples may help you find common patterns",
+                       "button": "Open debugger"
+                   },
+                       {
+                           "action": "Generate a test to check if this correlation holds with the whole dataset",
+                           "explanation": "Correlations may be spurious, double check if it has a business sense",
+                           "button": "Create test"
+                       },
+                       {
+                           "action": "Save slice and continue debugging session",
+                           "explanation": "Saving the slice will enable you to create tests more efficiently",
+                           "button": "Save Slice"
+                       }
+                   ]
                }
-
+        self.push_title = res["push_title"]
+        self.push_details = res["push_details"]
         return res
 
-    def perturbation(self, feature, value):
-        res = {"key": str(feature),
-               "value": str(value),
-               "push": f"A small variation of {str(feature)}=={str(value)} makes the prediction change",
-               "push_list": self.__perturbation
+    def _perturbation(self, feature, value):
+        res = {"push_title": f"A small variation of {str(feature)}=={str(value)} makes the prediction change",
+               "push_details":
+                   [{
+                       "action": "Generate a robustness test in the catalog that slightly perturb this feature",
+                       "explanation": "This will enable you to make sure the model is robust against small similar changes",
+                       "button": "Create test"
+                   },
+                       {
+                           "action": "Save the perturbation that made the model change and continue debugging session",
+                           "explanation": "Saving this perturbation will enable you to create tests more efficiently",
+                           "button": "Save Perturbation"
+                       }
+                   ]
                }
 
         # Details about the perturbation for textual feature - Transforming into upper-case, Shuffeling ,
         # Swapping tokens makes the prediction change
-
+        self.push_title = res["push_title"]
+        self.push_details = res["push_details"]
         return res
--- python-client/giskard/push/contribution.py
+++ python-client/giskard/push/contribution.py
@@ -7,7 +7,6 @@ from ..push import Push
 
 
 def contribution(model, ds, idrow):  # data_aug_dict
-    push = Push()
     if model.meta.model_type == SupportedModelTypes.CLASSIFICATION:
         shap_res = _contribution_push(model, ds, idrow)
         values = ds.df.iloc[idrow]
@@ -20,11 +19,11 @@ def contribution(model, ds, idrow):  # data_aug_dict
                 #    print(f"Data augmentation recommended for the slice.............{el}",
                 #          el, values[el])
                 if training_label != prediction:  # use scan feature ?
-                    res = push.incorrect(feature=el, value=values[el])
+                    res = Push(push_type="contribution_wrong", feature=el, value=values[el])
                     return res
 
                 else:
-                    res = push.high_correlation(feature=el, value=values[el])
+                    res = Push(push_type="contribution_only", feature=el, value=values[el])
                     return res
 
     if model.meta.model_type == SupportedModelTypes.REGRESSION:
@@ -40,11 +39,14 @@ def contribution(model, ds, idrow):  # data_aug_dict
             for el in shap_res:
                 # print(error, rmse_res)
                 if abs(error - rmse_res.metric) / rmse_res.metric >= 0.2:  # use scan feature ?
-                    res = push.incorrect(feature=el, value=values[el])
+                    res = Push(push_type="contribution_wrong",
+                               feature=el,
+                               value=values[el]
+                               )
                     return res
 
                 else:
-                    res = push.high_correlation(feature=el, value=values[el])
+                    res = Push(push_type="contribution_only", feature=el, value=values[el])
                     return res
 
 
