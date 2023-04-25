45d39bdd12e2dc178caaa35807a5a3708c0b1a9e
Matteo Dora <matteo@giskard.ai>
2023-04-25 19:14:35 +0200

    Fix decorator without arguments

--- python-client/giskard/scanner/decorators.py
+++ python-client/giskard/scanner/decorators.py
@@ -1,9 +1,16 @@
 import re
+import inspect
 
 from .registry import DetectorRegistry
 
 
 def detector(name=None, tags=None):
+    if inspect.isclass(name):
+        # If the decorator is used without arguments, the first argument is the class
+        cls = name
+        DetectorRegistry.register(_to_snake_case(cls.__name__), cls)
+        return cls
+
     def inner(cls):
         DetectorRegistry.register(name or _to_snake_case(cls.__name__), cls, tags)
         return cls
