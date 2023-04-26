50cf3cce9fe45ff84d156dbc8b89f82664ed986d
Rabah <rabah.khalek@gmail.com>
2023-04-26 13:20:36 +0200

    refactoring and new mask

---
+++ python-client/giskard/ml_worker/testing/tests/debug_masks.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+
+# Performance tests
+def incorrect_rows_mask(df: pd.DataFrame, targets: pd.Series, prediction: np.ndarray):
+    return df.index[targets != prediction].tolist()
+
+
+# TODO: Verify
+# Performance tests: RMSE, MAE, R2
+def top_nper_abs_err_rows_mask(df: pd.DataFrame, targets: pd.Series, raw_prediction: np.ndarray,
+                               debug_percent_rows: float = 0.3):
+    metric = abs(targets - raw_prediction)
+    top_n = round(debug_percent_rows * len(df))
+    return df.index[df.nlargest(top_n, 'metric')].tolist()
