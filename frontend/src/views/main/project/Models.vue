66d16e4c501178cb6b37905404da0329b7817478
Henrique Chaves <henrique@giskard.ai>
2023-05-02 12:01:55 +0200

    Use project-artifacts store to fetch data in Datasets and Models components

--- frontend/src/views/main/project/Models.vue
+++ frontend/src/views/main/project/Models.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="vertical-container">
-    <v-container v-if="models.length > 0" fluid class="vc">
+    <!-- <v-container v-if="models.length > 0" fluid class="vc"> -->
+    <v-container v-if="projectArtifactsStore.getModels.length > 0" fluid class="vc">
       <v-card flat>
         <v-row class="px-2 py-1 caption secondary--text text--lighten-3">
           <v-col cols="3">Name</v-col>
@@ -11,7 +12,7 @@
           <v-col cols="3">Actions</v-col>
         </v-row>
       </v-card>
-      <v-card class="grey lighten-5" v-for="m in                  models                  " :key="m.id" outlined tiled>
+      <v-card class="grey lighten-5" v-for="m in       projectArtifactsStore.getModels     " :key="m.id" outlined tiled>
         <v-row class="px-2 py-1 align-center">
           <v-col cols="3" class="font-weight-bold">
             <InlineEditText :text="m.name" :can-edit="isProjectOwnerOrAdmin" @save="(name) => renameModel(m.id, name)">
@@ -67,10 +68,13 @@ import { api } from '@/api';
 import InspectorLauncher from './InspectorLauncher.vue';
 import { ModelDTO } from '@/generated-sources';
 import mixpanel from "mixpanel-browser";
-import { onActivated, ref } from 'vue';
+import { onBeforeMount, ref } from 'vue';
 import DeleteModal from '@/views/main/project/modals/DeleteModal.vue';
 import InlineEditText from '@/components/InlineEditText.vue';
 import { useMainStore } from "@/stores/main";
+import { useProjectArtifactsStore } from "@/stores/project-artifacts";
+
+const projectArtifactsStore = useProjectArtifactsStore();
 
 const props = withDefaults(defineProps<{
   projectId: number,
@@ -79,21 +83,16 @@ const props = withDefaults(defineProps<{
   isProjectOwnerOrAdmin: false
 });
 
-const models = ref<ModelDTO[]>([]);
 const showInspectDialog = ref<boolean>(false);
 const modelToInspect = ref<ModelDTO | null>(null);
 
-const loadModelPickles = async () => {
-  models.value = await api.getProjectModels(props.projectId)
-  models.value.sort((a, b) => new Date(a.createdDate) < new Date(b.createdDate) ? 1 : -1);
-}
 
 async function deleteModelPickle(id: string) {
   mixpanel.track('Delete model', { id });
 
   let messageDTO = await api.deleteModelFiles(id);
   useMainStore().addNotification({ content: messageDTO.message });
-  await loadModelPickles();
+  await projectArtifactsStore.loadModels();
 }
 
 function downloadModelPickle(id: number) {
@@ -107,16 +106,12 @@ function cancelLaunchInspector() {
 
 async function renameModel(id: string, name: string) {
   mixpanel.track('Update model name', { id });
-  const savedDataset = await api.editModelName(id, name);
-  const idx = models.value.findIndex(f => f.id === id);
-  models.value[idx] = savedDataset;
-  models.value = [...models.value];
+  const savedModel = await api.editModelName(id, name);
+  projectArtifactsStore.updateModel(savedModel);
 }
 
-onActivated(() => loadModelPickles());
-
-defineExpose({
-  loadModelPickles
+onBeforeMount(async () => {
+  await projectArtifactsStore.setProjectId(props.projectId);
 })
 </script>
 
