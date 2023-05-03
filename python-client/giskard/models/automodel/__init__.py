eb15c88a271f4c31d35f79a273947a5514384d45
Rabah <rabah.khalek@gmail.com>
2023-05-03 23:54:05 +0200

    complete implementation of the __new__ logic

--- python-client/giskard/models/automodel/__init__.py
+++ python-client/giskard/models/automodel/__init__.py
@@ -1,71 +1,53 @@
-from pathlib import Path
-
+from re import match
 import pandas as pd
-import uuid
-from typing import Callable, Optional, Iterable, Any, Union
-import mlflow
+from typing import Callable, Optional, Iterable, Any
 
 from giskard.models import infer_giskard_cls
-from giskard.models.base import WrapperModel, MLFlowBasedModel
+from giskard.models.base import WrapperModel
 from giskard.core.core import ModelType
 
 
-class Model(WrapperModel):
+class Model:
     should_save_model_class = True
 
-    def __init__(
-            self,
-            model: Any,
-            model_type: ModelType,
-            data_preprocessing_function: Callable[[pd.DataFrame], Any] = None,
-            model_postprocessing_function: Callable[[Any], Any] = None,
-            name: Optional[str] = None,
-            feature_names: Optional[Iterable] = None,
-            classification_threshold: Optional[float] = 0.5,
-            classification_labels: Optional[Iterable] = None,
-            **kwargs
-    ) -> None:
-        super().__init__(model,
-                         model_type,
-                         data_preprocessing_function,
-                         model_postprocessing_function,
-                         name,
-                         feature_names,
-                         classification_threshold,
-                         classification_labels)
+    def __new__(cls, model: Any,
+                model_type: ModelType,
+                data_preprocessing_function: Callable[[pd.DataFrame], Any] = None,
+                model_postprocessing_function: Callable[[Any], Any] = None,
+                name: Optional[str] = None,
+                feature_names: Optional[Iterable] = None,
+                classification_threshold: Optional[float] = 0.5,
+                classification_labels: Optional[Iterable] = None,
+                **kwargs
+                ):
 
-        giskard_cls = infer_giskard_cls(model)
-        if not giskard_cls:
-            giskard_cls = WrapperModel
-
-        self.inferred_model = giskard_cls(
-            model=model,
-            model_type=model_type,
-            data_preprocessing_function=data_preprocessing_function,
-            model_postprocessing_function=model_postprocessing_function,
-            name=name,
-            feature_names=feature_names,
-            classification_threshold=classification_threshold,
-            classification_labels=classification_labels,
-            **kwargs)
-
-    def model_predict(self, df):
-        self.inferred_model.model_predict(df)
-
-    def save(self, local_path: Union[str, Path]) -> None:
-        """
-        The problem here is that we can't call simply self.inferred_model.save(local_path)
-        because it will save always the class of inferred_model as pkl, and not this one
-        so we have to pass by super().save(local_path) from here, which means we need
-        to repeat all the logic.
-        """
-
-        if isinstance(self.inferred_model, MLFlowBasedModel):
-            if not self.id:
-                self.id = uuid.uuid4()
-            self.inferred_model.save_model(local_path, mlflow.models.Model(model_uuid=str(self.id)))
-            super().save(local_path)
-            # TODO: not enough, PyTorch meta to be saved!
+        if not model:
+            raise ValueError("The 'Model' class requires a 'model' object. In case you want to create a custom "
+                             "class without a 'model' object, please use 'CustomModel' instead.")
         else:
-            super().save(local_path)
-            self.save_model(local_path)
+            cls_properties = {
+                name: getattr(cls, name) for name in dir(cls) if not match("__.*__", name)
+            }
+            giskard_cls = infer_giskard_cls(model)
+            if giskard_cls:
+                return type(giskard_cls.__name__, (giskard_cls,), cls_properties)(
+                    model=model,
+                    model_type=model_type,
+                    data_preprocessing_function=data_preprocessing_function,
+                    model_postprocessing_function=model_postprocessing_function,
+                    name=name,
+                    feature_names=feature_names,
+                    classification_threshold=classification_threshold,
+                    classification_labels=classification_labels,
+                    **kwargs)
+            else:
+                return type(cls.__name__, (WrapperModel,), cls_properties)(
+                    model=model,
+                    model_type=model_type,
+                    data_preprocessing_function=data_preprocessing_function,
+                    model_postprocessing_function=model_postprocessing_function,
+                    name=name,
+                    feature_names=feature_names,
+                    classification_threshold=classification_threshold,
+                    classification_labels=classification_labels,
+                    **kwargs)
