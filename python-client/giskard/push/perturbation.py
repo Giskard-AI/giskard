69f54d14981727f932b71e40cf41347720f0c251
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-05-04 16:21:05 +0200

    Fixed mad issue

--- python-client/giskard/push/perturbation.py
+++ python-client/giskard/push/perturbation.py
@@ -1,6 +1,7 @@
 from giskard.core.core import SupportedModelTypes
 from ..push import Push, Perturbation
 from .utils import slice_bounds
+import numpy as np
 def perturbation(model, ds, idrow):
     for feat, coltype in ds.column_types.items():
         perturbation_res = _perturb_and_predict(model, ds, idrow, feat, coltype)
@@ -25,30 +26,39 @@ def perturbation(model, ds, idrow):
 
 
 def _perturb_and_predict(model, ds, idrow, feature, coltype):  # done at each step
+    def mad(x):
+        med = np.median(x)
+        x = abs(x - med)
+        mad = np.median(x)
+        return mad
     ref_row = ds.df.iloc[[idrow]]
     row_perturbed = ref_row.copy()
     perturbation_val = None
     if coltype == "numeric":
-        mad = ds.df[feature].mad()
+        mad = mad(ds.df[feature])
         row_perturbed[feature] = _num_perturb(row_perturbed[feature], mad)
         perturbation_val = mad
-    if coltype == "text":
+        return _generate_perturbation(model, ds, ref_row, row_perturbed, perturbation_val)
+    elif coltype == "text":
         row_perturbed[feature] = _text_perturb(str(row_perturbed[feature]))
         perturbation_val = None  # @TODO: Add text support
-    # print(row_perturbed[feature])
-    # Predict probabilities for the perturbed input row using the given model
+        return _generate_perturbation(model, ds, ref_row, row_perturbed, perturbation_val)
+
+
+
+def _generate_perturbation(model,ds,ref_row,row_perturbed,perturbation_val):
     if model.meta.model_type == SupportedModelTypes.CLASSIFICATION:
         ref_prob = model.model.predict(ref_row)
         probabilities = model.model.predict(row_perturbed)  # .reshape(1, -1)
         return Perturbation(passed=ref_prob[0] != probabilities[0], perturbation_value=perturbation_val)
-    if model.meta.model_type == SupportedModelTypes.REGRESSION:
+    elif model.meta.model_type == SupportedModelTypes.REGRESSION:
         ref_val = model.model.predict(ref_row.drop(columns=[ds.target]))
         new_val = model.model.predict(row_perturbed.drop(columns=[ds.target]))  # .reshape(1, -1)
         return Perturbation(passed=(new_val - ref_val) / ref_val >= 0.2, perturbation_value=perturbation_val)
 
 
 def _num_perturb(val, mad):
-    return val + 4*mad  # 1.2  # 20% perturbation
+    return val + 3*mad  # 1.2  # 20% perturbation
 
 
 def _text_perturb(val):
