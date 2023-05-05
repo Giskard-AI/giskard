46fcef04915a19112c40289d182d86d77a9cbe68
Henrique Chaves <henriquechavesmm@gmail.com>
2023-05-05 15:50:19 +0200

    Add link to full documentation in Datasets and Models components

--- frontend/src/views/main/project/Models.vue
+++ frontend/src/views/main/project/Models.vue
@@ -59,6 +59,7 @@
     <v-container v-else>
       <p class="font-weight-medium secondary--text">There are no models in this project yet. Follow the code snippet below to upload a model 👇</p>
       <CodeSnippet :code-content="codeContent" :language="'python'"></CodeSnippet>
+      <p class="mt-4 font-weight-medium secondary--text">Check out the <a href="https://docs.giskard.ai/start/~/changes/QkDrbY9gX75RDMmAWKjX/guides/upload-your-model#2.-create-a-giskard-model" target="_blank">full documentation</a> for more information.</p>
     </v-container>
   </div>
 </template>
