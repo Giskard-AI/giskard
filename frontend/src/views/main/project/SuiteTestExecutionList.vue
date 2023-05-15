c7840554d324fe36c120ab356251ee66a6a39855
Googleton <hugo.viallon.pro@gmail.com>
2023-05-15 12:48:50 +0200

    Open sesame!

--- frontend/src/views/main/project/SuiteTestExecutionList.vue
+++ frontend/src/views/main/project/SuiteTestExecutionList.vue
@@ -17,6 +17,7 @@ import SuiteTestInfoModal from '@/views/main/project/modals/SuiteTestInfoModal.v
 import {api} from '@/api';
 import ConfirmModal from "@/views/main/project/modals/ConfirmModal.vue";
 import SuiteTestExecutionCard from "@/views/main/project/SuiteTestExecutionCard.vue";
+import {useRouter} from "vue-router/composables";
 
 const props = withDefaults(defineProps<{
     tests: {
@@ -38,7 +39,7 @@ async function debugTest(result: SuiteTestExecutionDTO, suiteTest: SuiteTestDTO)
 
   let model = props.execution.inputs.filter(input => input.name === 'model')[0].value;
 
-  let res = await api.runAdHocTest(10, suiteTest.testUuid, props.execution.inputs);
+  let res = await api.runAdHocTest(testSuiteStore.projectId!, suiteTest.testUuid, props.execution.inputs);
 
   let dataset = res.result[0].result.outputDfUuid;
 
@@ -46,6 +47,14 @@ async function debugTest(result: SuiteTestExecutionDTO, suiteTest: SuiteTestDTO)
     datasetId: dataset,
     modelId: model as string,
     name: "Debugging session ..."
+  });
+
+  await router.push({
+    name: 'inspection',
+    params: {
+      projectId: testSuiteStore.projectId!,
+      inspectionId: debuggingSession.id
+    }
   });
 }
 </script>
