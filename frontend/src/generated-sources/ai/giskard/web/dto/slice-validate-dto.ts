0767a75432c0937355be5482c45ee7bed728c0f5
Andrey Avtomonov <andreybavt@gmail.com>
2023-05-17 23:48:17 +0200

    frontend cleanup

--- frontend/src/generated-sources/ai/giskard/web/dto/slice-validate-dto.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Generated from ai.giskard.web.dto.SliceValidateDTO
- */
-export interface SliceValidateDTO {
-    code: string;
-    datasetId: any /* TODO: Missing translation of java.util.UUID */;
-}
