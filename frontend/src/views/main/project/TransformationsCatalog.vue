80cd03ab959e0228de8aad4318bace3e925ef3a1
Henrique Chaves <henrique@giskard.ai>
2023-05-24 18:16:05 +0200

    Replicate FiltersCatalog design to TransformationsCatalog

--- frontend/src/views/main/project/TransformationsCatalog.vue
+++ frontend/src/views/main/project/TransformationsCatalog.vue
@@ -17,9 +17,9 @@
                                         <v-list-item-content>
                                             <v-list-item-title class="test-title">
                                                 <div class="d-flex align-center">
-                                                    {{
-                                                        transformationFunction.displayName ?? transformationFunction.name
-                                                    }}
+                                                    <span class="list-func-name">
+                                                        {{ transformationFunction.displayName ?? transformationFunction.name }}
+                                                    </span>
                                                     <v-spacer class="flex-grow-1" />
                                                     <v-tooltip bottom v-if="transformationFunction.potentiallyUnavailable">
                                                         <template v-slot:activator="{ on, attrs }">
@@ -43,58 +43,76 @@
                         </v-list>
                     </v-col>
                     <v-col cols="8" v-if="selected" class="vc fill-height">
-                        <span class="text-h5">{{ selected.displayName ?? selected.name }}</span>
+                        <div class="py-2">
+                            <span class="selected-func-name">{{ selected.displayName ?? selected.name }}</span>
+                        </div>
+
                         <div class="vc overflow-x-hidden pr-5">
                             <v-alert v-if="selected.potentiallyUnavailable" color="warning" border="left" outlined colored-border icon="warning">
                                 <span>This filter is potentially unavailable. Start your external ML worker to display available filters.</span>
                                 <pre></pre>
                                 <StartWorkerInstructions />
                             </v-alert>
-                            <pre class="test-doc caption pt-5">{{ selected.doc }}</pre>
-                            <div class="pt-5">
-                                <div class="d-flex justify-space-between">
-                                    <span class="text-h6">Inputs</span>
+
+                            <div class="py-4" id="description-group">
+                                <v-expansion-panels multiple v-model="panel" flat>
+                                    <v-expansion-panel>
+                                        <v-expansion-panel-header class="pl-0">
+                                            <div class="d-flex align-center">
+                                                <v-icon left class="group-icon pb-1 mr-1">mdi-text-box</v-icon>
+                                                <span class="group-title">Description</span>
+                                                <v-icon v-if="panel.includes(0)" right>mdi-chevron-up</v-icon>
+                                                <v-icon v-else right>mdi-chevron-down</v-icon>
+                                            </div>
+                                            <template v-slot:actions>
+                                                <span></span>
+                                            </template>
+                                        </v-expansion-panel-header>
+                                        <v-expansion-panel-content>
+                                            <p class="selected-func-description pt-2 mb-4">{{ selectedFuncDescription }}</p>
+                                        </v-expansion-panel-content>
+                                    </v-expansion-panel>
+                                </v-expansion-panels>
+                            </div>
+
+                            <div id="inputs-group" class="py-4 mb-4">
+                                <div class="d-flex mb-4">
+                                    <v-icon left class="group-icon pb-1 mr-1">mdi-pencil-box</v-icon>
+                                    <span class="group-title">Inputs</span>
+                                    <v-spacer></v-spacer>
                                 </div>
-                                <v-list>
-                                    <v-list-item class="pl-0 pr-0">
-                                        <v-row>
-                                            <v-col>
-                                                <v-list-item-content>
-                                                    <v-list-item-title>Dataset</v-list-item-title>
-                                                    <v-list-item-subtitle class="text-caption">
-                                                        BaseDataset
-                                                    </v-list-item-subtitle>
-                                                </v-list-item-content>
-                                            </v-col>
-                                            <v-col>
-                                                <DatasetSelector :project-id="projectId" label="Dataset" :return-object="false" :value.sync="selectedDataset" />
-                                            </v-col>
-                                        </v-row>
-                                    </v-list-item>
-                                    <v-list-item class="pl-0 pr-0">
-                                        <v-row v-if="selected.cellLevel">
-                                            <v-col>
-                                                <v-list-item-content>
-                                                    <v-list-item-title>Column</v-list-item-title>
-                                                    <v-list-item-subtitle class="text-caption">
-                                                        str
-                                                    </v-list-item-subtitle>
-                                                </v-list-item-content>
-                                            </v-col>
-                                            <v-col>
-                                                <DatasetColumnSelector :project-id="projectId" :dataset="selectedDataset" :column-type="selected.columnType" :value.sync="selectedColumn" />
-                                            </v-col>
-                                        </v-row>
-                                    </v-list-item>
-                                </v-list>
-                                <SuiteInputListSelector :editing="true" :model-value="transformationArguments" :inputs="inputType" :project-id="props.projectId" />
-                                <v-row>
-                                    <v-col :align="'right'">
-                                        <v-btn width="100" small tile outlined class="primary" color="white" @click="runSlicingFunction">
-                                            Run
-                                        </v-btn>
-                                    </v-col>
-                                </v-row>
+
+                                <div>
+                                    <v-row>
+                                        <v-col>
+                                            <span class="input-name">Dataset: <span class="input-type">BaseDataset</span></span>
+                                        </v-col>
+                                        <v-col class="input-selector-column">
+                                            <DatasetSelector :project-id="projectId" label="Dataset" :return-object="false" :value.sync="selectedDataset" />
+                                        </v-col>
+                                    </v-row>
+                                </div>
+
+                                <div v-if="selected.cellLevel">
+                                    <v-row>
+                                        <v-col>
+                                            <span class="input-name">Column: <span class="input-type">str</span></span>
+                                        </v-col>
+                                        <v-col class="input-selector-column">
+                                            <DatasetColumnSelector :project-id="projectId" :dataset="selectedDataset" :column-type="selected.columnType" :value.sync="selectedColumn" />
+                                        </v-col>
+                                    </v-row>
+                                </div>
+
+                                <SuiteInputListSelector :editing="true" :model-value="transformationArguments" :inputs="inputType" :project-id="props.projectId" class="suite-input-list-selector" />
+
+                                <div class="d-flex">
+                                    <v-spacer></v-spacer>
+                                    <v-btn width="100" small class="primaryLightBtn" color="primaryLight" @click="runTransformationFunction">
+                                        Run
+                                    </v-btn>
+                                </div>
+
                                 <v-row v-if="transformationResult">
                                     <v-col>
                                         <span class="text-h6">Result</span>
@@ -108,22 +126,16 @@
                                         <DatasetTable :dataset-id="transformationResult.datasetId" :modifications="transformationResult.modifications" />
                                     </v-col>
                                 </v-row>
-                                <v-row>
-                                    <v-col>
-                                        <v-expansion-panels flat @change="resizeEditor">
-                                            <v-expansion-panel>
-                                                <v-expansion-panel-header class="grey lighten-5 pl-6 pr-4 py-2">Code</v-expansion-panel-header>
-                                                <v-divider></v-divider>
-                                                <v-expansion-panel-content class="pl-0 pr-1">
-                                                    <MonacoEditor ref="editor" v-model='selected.code' class='editor' language='python' style="height: 300px; min-height: 300px" :options="monacoOptions" />
-                                                </v-expansion-panel-content>
-                                            </v-expansion-panel>
-                                        </v-expansion-panels>
-                                    </v-col>
-                                </v-row>
+                            </div>
+
+                            <div id="code-group" class="py-4">
+                                <div class="d-flex">
+                                    <v-icon left class="group-icon pb-1 mr-1">mdi-code-braces-box</v-icon>
+                                    <span class="group-title">Source code</span>
+                                </div>
+                                <CodeSnippet class="mt-2" :codeContent="selected.code" :key="selected.name + '_source_code'"></CodeSnippet>
                             </div>
                         </div>
-
                     </v-col>
                 </v-row>
             </v-container>
@@ -151,6 +163,7 @@ import DatasetTable from "@/components/DatasetTable.vue";
 import SuiteInputListSelector from "@/components/SuiteInputListSelector.vue";
 import DatasetColumnSelector from "@/views/main/utils/DatasetColumnSelector.vue";
 import IEditorOptions = editor.IEditorOptions;
+import CodeSnippet from "@/components/CodeSnippet.vue";
 
 const l = MonacoEditor;
 let props = defineProps<{
@@ -169,6 +182,18 @@ const selectedColumn = ref<string | null>(null);
 let transformationArguments = ref<{ [name: string]: FunctionInputDTO }>({})
 
 const monacoOptions: IEditorOptions = inject('monacoOptions');
+const panel = ref<number[]>([0]);
+
+const selectedFuncDescription = computed(() => {
+    if (selected.value === null) {
+        return '';
+    }
+
+    if (selected.value.doc === null) {
+        return 'No description available.';
+    }
+    return selected.value.doc.split("Args:")[0];
+})
 monacoOptions.readOnly = true;
 
 function resizeEditor() {
@@ -210,7 +235,7 @@ onActivated(async () => {
     }
 });
 
-async function runSlicingFunction() {
+async function runTransformationFunction() {
     const params = Object.values(transformationArguments.value);
     if (selected.value!.cellLevel) {
         params.push({
@@ -288,5 +313,58 @@ const inputType = computed(() => chain(selected.value?.args ?? [])
     text-overflow: ellipsis;
     overflow: hidden;
     white-space: nowrap;
+}
+
+.input-name {
+    font-family: 'Roboto Mono', monospace;
+    opacity: 0.875;
+}
+
+.input-type {
+    font-size: 0.875rem;
+    opacity: 0.875;
+}
+
+.input-column {
+    width: 300px;
+}
+
+.selected-func-name {
+    font-weight: 600;
+    font-size: 1.75rem;
+}
+
+.selected-func-description {
+    white-space: break-spaces;
+}
+
+.group-title {
+    font-size: 1.25rem;
+    font-weight: 500;
+    letter-spacing: normal;
+}
+
+.group-icon {
+    color: #087038;
+    font-size: 1.25rem;
+    margin-top: 0.3rem;
+}
+
+.custom-input {
+    padding-top: 0.25rem;
+    padding-bottom: 0.25rem;
+}
+
+.input-selector-column {
+    padding-bottom: 1rem;
+    padding-top: 0.25rem;
+}
+
+.list-func-name {
+    font-weight: 500;
+}
+
+.suite-input-list-selector {
+    padding-top: 0;
 }
 </style>
