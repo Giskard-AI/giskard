08d5782596db2e05ca84d526fd20d2d5ee1a4d40
Matteo Dora <matteo@giskard.ai>
2023-05-24 18:08:36 +0200

    Fix import

--- python-client/giskard/scanner/performance/__init__.py
+++ python-client/giskard/scanner/performance/__init__.py
@@ -1,4 +1,4 @@
-from .model_bias_detector import PerformanceBiasDetector
+from .performance_bias_detector import PerformanceBiasDetector
 from .issues import PerformanceIssue
 
 __all__ = ["PerformanceBiasDetector", "PerformanceIssue"]
