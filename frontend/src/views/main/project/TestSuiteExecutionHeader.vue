fae13286d0a7018984504fa34b8b7dd911455e51
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-05-24 17:03:14 +0200

    Improved overview header

--- frontend/src/views/main/project/TestSuiteExecutionHeader.vue
+++ frontend/src/views/main/project/TestSuiteExecutionHeader.vue
@@ -1,35 +1,27 @@
 <template>
     <div class="d-flex w100 align-start">
-        <v-icon
-            :color="!props.execution ? 'grey' : props.execution.result === TestResult.PASSED ? Colors.PASS : Colors.FAIL"
-            size="64">{{
-                testResultIcon
-            }}
-        </v-icon>
-        <div>
-            <h2>
-                {{ suite.name }}
-            </h2>
-            <h4 v-if="!props.execution">
-                No execution has been performed yet!
-            </h4>
-            <h4 v-else-if="props.execution.result === TestResult.ERROR">
-                An error arose during the execution
-            </h4>
-            <h4 v-else-if="tests.length === 0">No test match the current filter</h4>
-            <h4 v-else-if="executedTests.length > 0" :style="{
-          color: successColor
-        } ">Success ratio: {{ successRatio.passed }} /
-                {{ successRatio.executed }}
-            </h4>
-            <p v-if="props.execution">
-                Executed: {{ props.execution.executionDate | date }}
-            </p>
-        </div>
-        <div class="flex-grow-1"/>
-        <v-btn icon @click="openLogs" color="secondary">
-            <v-icon>text_snippet</v-icon>
-        </v-btn>
+        <v-alert prominent :icon="testResultStyle.icon" outlined text :color="testResultStyle.color"
+                 class="flex-grow-1">
+            <v-row align="center">
+                <v-col class="grow">
+                    <h4 v-if="!props.execution">
+                        No execution has been performed yet!
+                    </h4>
+                    <h4 v-else-if="props.execution.result === TestResult.ERROR">
+                        An error arose during the execution. Check the <span @click="openLogs" class="clickable">execution logs.</span>
+                    </h4>
+                    <h4 v-else-if="tests.length === 0">No test match the current filter</h4>
+                    <h4 v-else-if="executedTests.length > 0">Test suite
+                        {{ props.execution.result === TestResult.PASSED ? 'passed' : 'failed' }}:
+                        <span v-if="successRatio.failed > 0">{{ plurialize('test', successRatio.failed) }} failed</span>
+                        <span v-if="successRatio.failed > 0 && successRatio.passed > 0">, </span>
+                        <span v-if="successRatio.passed > 0">{{ plurialize('test', successRatio.passed) }} passed</span>
+                        <span v-if="successRatio.failed > 0 || successRatio.passed > 0">. </span>
+                        Check the <span @click="openLogs" class="clickable">execution logs.</span>
+                    </h4>
+                </v-col>
+            </v-row>
+        </v-alert>
         <v-btn icon @click="openSettings" color="secondary" v-if="!compact">
             <v-icon>settings</v-icon>
         </v-btn>
@@ -40,52 +32,59 @@
 
 import {SuiteTestDTO, SuiteTestExecutionDTO, TestResult, TestSuiteExecutionDTO} from '@/generated-sources';
 import {computed} from 'vue';
-import {Colors, pickHexLinear, rgbToHex, SUCCESS_GRADIENT} from '@/utils/colors';
 import {api} from '@/api';
 import {$vfm} from 'vue-final-modal';
 import ExecutionLogsModal from '@/views/main/project/modals/ExecutionLogsModal.vue';
 import {storeToRefs} from 'pinia';
 import {useTestSuiteStore} from '@/stores/test-suite';
 import EditTestSuiteModal from "@/views/main/project/modals/EditTestSuiteModal.vue";
+import {plurialize} from "../../../utils/string.utils";
+import {Colors} from "@/utils/colors";
 
 const props = defineProps<{
-  tests: {
-    suiteTest: SuiteTestDTO
-      result?: SuiteTestExecutionDTO
-  }[],
-  execution?: TestSuiteExecutionDTO,
-  compact: boolean
+    tests: {
+        suiteTest: SuiteTestDTO
+        result?: SuiteTestExecutionDTO
+    }[],
+    execution?: TestSuiteExecutionDTO,
+    compact: boolean
 }>();
 
 const {suite, projectId} = storeToRefs(useTestSuiteStore());
 
-
-const testResultIcon = computed(() => {
-  if (!props.execution) {
-    return 'block'
-  }
-  switch (props.execution.result) {
-    case TestResult.PASSED:
-      return 'done';
-    case TestResult.FAILED:
-      return 'close';
-    default:
-      return 'error';
-  }
+const testResultStyle = computed(() => {
+    if (!props.execution) {
+        return {
+            icon: 'block',
+            color: 'dark-grey'
+        }
+    }
+    switch (props.execution.result) {
+        case TestResult.PASSED:
+            return {
+                icon: 'done',
+                color: Colors.PASS
+            }
+        case TestResult.FAILED:
+            return {
+                icon: 'close',
+                color: Colors.FAIL
+            }
+        default:
+            return {
+                icon: 'error',
+                color: Colors.FAIL
+            }
+    }
 })
-
-const executedTests = computed(() => !props.execution || props.execution.result === TestResult.ERROR ? []
-    : props.tests.filter(({result}) => result !== undefined));
-
 
 const successRatio = computed(() => ({
     passed: executedTests.value.filter(({result}) => result!.passed).length,
-    executed: executedTests.value.length
+    failed: executedTests.value.filter(({result}) => !result!.passed).length
 }))
 
-const successColor = computed(() => successRatio.value.executed === 0 ? Colors.PASS :
-    rgbToHex(pickHexLinear(SUCCESS_GRADIENT, successRatio.value.passed / successRatio.value.executed)));
-
+const executedTests = computed(() => !props.execution || props.execution.result === TestResult.ERROR ? []
+    : props.tests.filter(({result}) => result !== undefined));
 
 async function openSettings() {
     const project = await api.getProject(projectId.value!)
@@ -100,12 +99,19 @@ async function openSettings() {
 }
 
 function openLogs() {
-  $vfm.show({
-    component: ExecutionLogsModal,
-    bind: {
-      logs: props.execution?.logs
-    }
-  });
+    $vfm.show({
+        component: ExecutionLogsModal,
+        bind: {
+            logs: props.execution?.logs
+        }
+    });
 }
 </script>
+
+<style scoped lang="scss">
+.clickable {
+    cursor: pointer;
+    text-decoration: underline;
+}
+</style>
 
