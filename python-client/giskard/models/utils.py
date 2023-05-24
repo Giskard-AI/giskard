1e4068a9200e9a14cf63c9f57bad2bcc8a9abdcf
Rabah <rabah.khalek@gmail.com>
2023-05-24 10:33:48 +0200

    added numpy encoder for classification_labels

--- python-client/giskard/models/utils.py
+++ python-client/giskard/models/utils.py
@@ -1,5 +1,7 @@
 import itertools
 from collections.abc import Iterator
+from typing import List
+import numpy as np
 
 
 def map_to_tuples(data: Iterator):
@@ -10,3 +12,12 @@ def map_to_tuples(data: Iterator):
         return data
 
     return map(lambda x: (x,), data)
+
+
+def numpy_encoder(l: List):
+    e = l[0]
+    if isinstance(e, np.integer):
+        return [int(i) for i in l]
+    if isinstance(e, np.floating):
+        return [float(i) for i in l]
+    return l
