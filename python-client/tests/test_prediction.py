36e0b846a4f116ab1f837bf572498a5745857294
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-05-26 17:11:33 +0200

    import bug fix

--- python-client/tests/test_prediction.py
+++ python-client/tests/test_prediction.py
@@ -1,12 +1,14 @@
 from giskard.scanner.prediction.prediction_bias_detectors.overconfidence import OverconfidenceBiasDetector
 from giskard.scanner.prediction.prediction_bias_detectors.borderline import BorderlineBiasDetector
 
-from giskard.scanner.prediction.metrics import OverconfidenceMAE,BorderlineMAE
+from giskard.scanner.prediction.metrics import OverconfidenceMAE, BorderlineMAE
+
+
 def test_prediction_biais_detector(german_credit_model, german_credit_data):
     res = OverconfidenceBiasDetector(metrics=[OverconfidenceMAE()]).run(german_credit_model, german_credit_data)
-    res2= BorderlineBiasDetector(metrics=[BorderlineMAE()]).run(german_credit_model, german_credit_data)
-    
-    print(res,res2)
+    res2 = BorderlineBiasDetector(metrics=[BorderlineMAE()]).run(german_credit_model, german_credit_data)
+
+    print(res, res2)
 
 # from giskard.scanner.performance.model_bias_detector import ModelBiasDetector
 # def test_data_leakage(german_credit_model, german_credit_data):
