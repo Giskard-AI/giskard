2c03fa9da38aa1b6ae871adbc245bc935255d5b6
Matteo Dora <matteo@giskard.ai>
2023-05-30 16:19:53 +0200

    Fixing underconfidence detector with relative change

--- python-client/giskard/scanner/calibration/underconfidence_detector.py
+++ python-client/giskard/scanner/calibration/underconfidence_detector.py
@@ -13,7 +13,7 @@ from ..logger import logger
 
 @detector(name="underconfidence", tags=["underconfidence", "classification"])
 class UnderconfidenceDetector(LossBasedDetector):
-    def __init__(self, threshold=0.10, method="tree"):
+    def __init__(self, threshold=0.1, method="tree"):
         self.threshold = threshold
         self.method = method
 
@@ -31,7 +31,13 @@ class UnderconfidenceDetector(LossBasedDetector):
         # Empirical cost associated to underconfidence: difference between
         # the two most probable classes.
         ps = model.predict(dataset).raw
-        loss_values = -np.abs(np.diff(np.partition(-ps, 1, axis=-1)[:, :2], axis=-1).squeeze(-1))
+
+        # Absolute difference
+        # loss_values = -np.abs(np.diff(np.partition(-ps, 1, axis=-1)[:, :2], axis=-1).squeeze(-1))
+
+        # Relative difference
+        ps_2 = -np.partition(-ps, 1, axis=-1)[:, :2]
+        loss_values = ps_2.min(axis=-1) / ps_2.max(axis=-1)
 
         return pd.DataFrame({self.LOSS_COLUMN_NAME: loss_values}, index=dataset.df.index)
 
@@ -55,14 +61,13 @@ class UnderconfidenceDetector(LossBasedDetector):
             sliced_dataset = dataset_with_meta.slice(slice_fn)
 
             slice_loss = sliced_dataset.df[self.LOSS_COLUMN_NAME].mean()
-            relative_delta = (slice_loss - mean_loss) / mean_loss
 
             logger.debug(
-                f"{self.__class__.__name__}: Testing slice {slice_fn}\tLoss (slice) = {slice_loss:.3f} (global {mean_loss:.3f}) Δm = {relative_delta:.3f}"
+                f"{self.__class__.__name__}: Testing slice {slice_fn}\tLoss (slice) = {slice_loss:.3f} (global {mean_loss:.3f}) Δm = {np.nan:.3f}"
             )
 
-            if relative_delta > self.threshold:
-                level = "major" if relative_delta > 2 * self.threshold else "medium"
+            if slice_loss > (1 - self.threshold):
+                level = "major" if slice_loss > (1 - 2 * self.threshold) else "medium"
                 issues.append(
                     UnderconfidenceIssue(
                         model,
