6c9f07d8c5e319c432dfadb399bc9d7c6a91e38e
Henrique Chaves <henriquechavesmm@gmail.com>
2023-06-01 10:29:11 +0200

    Update CodeSnippet

--- frontend/src/components/CodeSnippet.vue
+++ frontend/src/components/CodeSnippet.vue
@@ -31,12 +31,13 @@ async function copyCode() {
 </script>
 
 <template>
-    <!-- //NOSONAR --><pre class="pre-block rounded pa-4"><code v-html="highlightedCode" class="code-block"/>
-        <v-btn class="copy-button" small icon @click="copyCode">
-            <v-icon small>mdi-content-copy</v-icon>
-          <span v-show="copied" class="copied-message">Copied</span>
-        </v-btn>
-    </pre>
+  <!-- //NOSONAR -->
+  <div class="pre-block rounded pa-4"><code v-html="highlightedCode" class="code-block" />
+    <v-btn class="copy-button" small icon @click="copyCode">
+      <v-icon small>mdi-content-copy</v-icon>
+      <span v-show="copied" class="copied-message">Copied</span>
+    </v-btn>
+  </div>
 </template>
 
 <style scoped>
