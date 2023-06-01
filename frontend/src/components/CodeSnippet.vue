ed1c8b671898e2180281307c883d0e74737efb14
Henrique Chaves <henrique@giskard.ai>
2023-06-01 02:11:23 +0200

    Add copy button to code snippet

--- frontend/src/components/CodeSnippet.vue
+++ frontend/src/components/CodeSnippet.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import hljs from "highlight.js";
 import "highlight.js/styles/github.css";
+import { copyToClipboard } from "@/global-keys";
+
 
 interface Props {
   codeContent: string;
@@ -13,20 +15,33 @@ const props = withDefaults(defineProps<Props>(), {
 });
 
 const highlightedCode = hljs.highlight(props.language, props.codeContent).value;
+
+async function copyCode() {
+  await copyToClipboard(props.codeContent);
+}
 </script>
 
 <template>
-  <pre class="pre-block rounded pa-4"><code v-html="highlightedCode" class="code-block"></code></pre>
+  <pre class="pre-block rounded pa-4">
+    <code v-html="highlightedCode" class="code-block"></code><v-btn class="copy-button" small icon @click="copyCode"><v-icon small>mdi-content-copy</v-icon></v-btn>
+  </pre>
 </template>
 
 <style scoped>
 .pre-block {
   background-color: #F4F4F4;
+  position: relative;
 }
 
 .code-block {
   background-color: #F4F4F4 !important;
   font-style: normal;
   font-weight: 500;
+}
+
+.copy-button {
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
 }
 </style>
