d65f1cce22c3f440c6b81218f02a8dc37edfa7a4
Rabah <rabah.khalek@gmail.com>
2023-06-01 00:11:22 +0000

    fixed is_overriden logic, fixed unit-tests

--- python-client/giskard/models/automodel/__init__.py
+++ python-client/giskard/models/automodel/__init__.py
@@ -98,18 +98,14 @@ class Model(CloudpickleBasedModel):
             is_overriden = cls.__name__ != 'Model'
             possibly_overriden_cls = cls if is_overriden else giskard_cls if giskard_cls else CloudpickleBasedModel
 
-            if is_overriden:
-                # Minimally model_predict should be overriden (the abstract method in CloudpickleBasedModel)
-                giskard_cls.model_predict = possibly_overriden_cls.model_predict
+            # No need to override save_model and load_model except if we don't support the model library, in which
+            # case giskard_cls == CloudpickleBasedModel.
+            # if save_model and load_model are overriden, replace them, if not, these equalities will be identities.
+            if is_overriden and giskard_cls != CloudpickleBasedModel:
+                possibly_overriden_cls.save_model = giskard_cls.save_model
+                possibly_overriden_cls.load_model = giskard_cls.load_model
 
-                # No need to override save_model and load_model except if we don't support the model library, in which
-                # case giskard_cls == CloudpickleBasedModel.
-                # if save_model and load_model are overriden, replace them, if not, these equalities will be identities.
-                if giskard_cls == CloudpickleBasedModel:
-                    giskard_cls.save_model = possibly_overriden_cls.save_model
-                    giskard_cls.load_model = possibly_overriden_cls.load_model
-
-            methods = dict(giskard_cls.__dict__)
+            methods = dict(possibly_overriden_cls.__dict__)
             output_cls = type(possibly_overriden_cls.__name__, (giskard_cls,), methods)
 
             obj = output_cls(
@@ -124,7 +120,7 @@ class Model(CloudpickleBasedModel):
                 **kwargs)
 
             # Important in order for the load method to be executed consistently
-            obj.meta.loader_class = giskard_cls.__name__
-            obj.meta.loader_module = giskard_cls.__module__
+            obj.meta.loader_class = possibly_overriden_cls.__name__
+            obj.meta.loader_module = possibly_overriden_cls.__module__
 
             return obj
