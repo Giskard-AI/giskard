2ae97bad02ec3c3021f9355f3d4266ab6343c7ad
Andrey Avtomonov <andreybavt@gmail.com>
2023-06-01 02:11:23 +0200

    move loader_module and loader_class to class props

--- python-client/giskard/models/sklearn/__init__.py
+++ python-client/giskard/models/sklearn/__init__.py
@@ -5,17 +5,24 @@ from giskard.core.model import Model
 
 
 class SKLearnModel(Model):
+    loader_module = 'giskard.models.sklearn'
+    loader_class = 'SKLearnModel'
+
     def __init__(self, clf, model_type: SupportedModelTypes, name: str = None, data_preprocessing_function=None,
-                 feature_names=None, classification_threshold=0.5, classification_labels=None, loader_module:
-                 str='giskard.models.sklearn',  loader_class: str = 'SKLearnModel') -> None:
+                 feature_names=None, classification_threshold=0.5, classification_labels=None) -> None:
 
         if classification_labels is None and hasattr(clf, 'classes_'):
             classification_labels = list(getattr(clf, 'classes_'))
         if feature_names is None and hasattr(clf, 'feature_names_in_'):
             feature_names = list(getattr(clf, 'feature_names_in_'))
 
-        super().__init__(clf, model_type, name, data_preprocessing_function, feature_names,
-                         classification_threshold, classification_labels, loader_module, loader_class)
+        super().__init__(clf=clf,
+                         model_type=model_type,
+                         name=name,
+                         data_preprocessing_function=data_preprocessing_function,
+                         feature_names=feature_names,
+                         classification_threshold=classification_threshold,
+                         classification_labels=classification_labels)
 
     @staticmethod
     def read_model_from_local_dir(local_path):
