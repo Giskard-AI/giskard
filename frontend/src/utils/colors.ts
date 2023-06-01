4c577658c12f5a040324df3a2cfac263f921314a
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 00:11:22 +0000

    Small improvements

--- frontend/src/utils/colors.ts
+++ frontend/src/utils/colors.ts
@@ -1,8 +1,8 @@
 export class Colors {
     static FAIL = '#F44336';
-    static FAIL_SURFACE = "#f8d7da";
-    static ON_FAIL_SURFACE = '#721c24';
+    static FAIL_SURFACE = "rgb(243, 225, 223)";
+    static ON_FAIL_SURFACE = 'rgb(225, 82, 65)';
     static PASS = '#4CAF50';
-    static PASS_SURFACE = "#d4edda";
-    static ON_PASS_SURFACE = '#155724';
+    static PASS_SURFACE = "rgb(227, 236, 226)";
+    static ON_PASS_SURFACE = "rgb(102, 173, 81)";
 }
