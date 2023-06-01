408843a86985e37b4cc5c45ddad299b3852b1715
Henrique Chaves <henrique@giskard.ai>
2023-06-01 02:11:23 +0200

    Add project-catalog-datasets and project-catalog-models routes

--- frontend/src/views/main/project/Catalog.vue
+++ frontend/src/views/main/project/Catalog.vue
@@ -1,25 +1,31 @@
 <template>
     <div class="vc mt-2 pb-0" v-if="catalog">
         <v-tabs>
-            <v-tab :to="{name: 'project-catalog-tests'}">
+            <v-tab :to="{ name: 'project-catalog-datasets' }">
+                Datasets
+            </v-tab>
+            <v-tab :to="{ name: 'project-catalog-models' }">
+                Models
+            </v-tab>
+            <v-tab :to="{ name: 'project-catalog-tests' }">
                 Tests
             </v-tab>
-            <v-tab :to="{name: 'project-catalog-slicing-functions'}">
+            <v-tab :to="{ name: 'project-catalog-slicing-functions' }">
                 Slicing functions
             </v-tab>
-            <v-tab :to="{name: 'project-catalog-transformation-functions'}">
+            <v-tab :to="{ name: 'project-catalog-transformation-functions' }">
                 Transformation functions
             </v-tab>
         </v-tabs>
-        <router-view/>
+        <router-view />
     </div>
-    <LoadingFullscreen v-else name="catalog"/>
+    <LoadingFullscreen v-else name="catalog" />
 </template>
 
 <script setup lang="ts">
-import {onActivated} from "vue";
-import {useCatalogStore} from "@/stores/catalog";
-import {storeToRefs} from "pinia";
+import { onActivated } from "vue";
+import { useCatalogStore } from "@/stores/catalog";
+import { storeToRefs } from "pinia";
 import LoadingFullscreen from "@/components/LoadingFullscreen.vue";
 
 let props = defineProps<{
@@ -28,7 +34,7 @@ let props = defineProps<{
 }>();
 
 const catalogStore = useCatalogStore();
-const {catalog} = storeToRefs(catalogStore);
+const { catalog } = storeToRefs(catalogStore);
 
 onActivated(async () => await catalogStore.loadCatalog(props.projectId));
 
