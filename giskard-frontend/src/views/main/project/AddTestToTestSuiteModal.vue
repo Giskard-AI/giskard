33ba7ca936912f9d83451f3e88679747777497a5
andreybavt <andreybavt@gmail.com>
2023-06-01 00:11:22 +0000

    adapt vue component to vue 2.7 style

---
+++ giskard-frontend/src/views/main/project/AddTestToTestSuiteModal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="text-center">Add test to an existing test suite</div>
+    <v-list>
+      <v-list-item v-for="s in testSuites">
+        <v-list-item-content>{{ s.name }}</v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <v-btn tile small>Create a new test suite</v-btn>
+    <div>
+      <v-btn tile small>Back</v-btn>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const testSuites = ref([])
+testSuites.value = [{
+  name: "Suite 1",
+  arguments: [{
+    name: "mymodel",
+    type: "GiskardModel"
+  }]
+}, {
+  name: "Suite 2"
+}, {
+  name: "Suite 3"
+}];
+</script>
+
+<style scoped>
+.v-list-item {
+    border-bottom:1px solid lightgrey;
+}
+</style>
