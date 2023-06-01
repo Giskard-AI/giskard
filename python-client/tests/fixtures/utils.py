df2fd3a355e0d2b6f55f8b7d46ee497268b5046c
mykytaalekseiev <mykyta@giskard.ai>
2023-06-01 02:11:23 +0200

    Added utils.py file with "autouse=True" fixture to reset random seeds every time we run new test.

---
+++ python-client/tests/fixtures/utils.py
@@ -0,0 +1,16 @@
+import random
+
+import torch
+import pytest
+import numpy as np
+
+
+SEED = 0
+
+
+@pytest.fixture(autouse=True)
+def set_rnd_seed_before_test():
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    print("SEED WAS SET")
