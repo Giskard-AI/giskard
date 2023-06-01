028a0faebdbd90bd906987544648ab6d16546d8e
Henrique Chaves <henrique@giskard.ai>
2023-06-01 00:11:22 +0000

    Fill stepper contents from 2 to 4

--- frontend/src/components/QuickStartSteepper.vue
+++ frontend/src/components/QuickStartSteepper.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import { MLWorkerInfoDTO } from "@/generated-sources";
 import { api } from "@/api";
+import { MLWorkerInfoDTO } from "@/generated-sources";
+import CodeSnippet from "./CodeSnippet.vue";
 
 const step = ref(1);
+const toggleArtifactType = ref<string>("dataset");
+const toggleTestType = ref<string>("scan");
 
 const allMLWorkerSettings = ref<MLWorkerInfoDTO[]>([]);
 const externalWorker = ref<MLWorkerInfoDTO | null>(null);
 
-const emit = defineEmits(['close']);
+const clientCodeContent = "# Code to create Giskard client here";
+const datasetCodeContent = "# Code to upload dataset here";
+const modelCodeContent = "# Code to upload model here";
+const scanCodeContent = "# Code to upload scan here";
+const manualTestCodeContent = "# Code to upload manual test here";
+
+const emit = defineEmits(["close"]);
 
 const close = () => {
   emit("close");
@@ -43,27 +52,62 @@ onMounted(async () => {
 
     <v-stepper-items>
       <v-stepper-content step="1">
-        <v-card class="mb-5" color="grey lighten-1" height="200px">
-          ML Worker not connected!
-        </v-card>
+        <div class="mb-6">
+          Is your External ML worker running? {{ externalWorker !== null }}
+        </div>
         <v-btn color="primary" @click="step = 2">Continue</v-btn>
         <v-btn class="ml-2" @click="close" flat>Cancel</v-btn>
       </v-stepper-content>
 
       <v-stepper-content step="2">
-        <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+        <div class="mb-6">
+          <p class="mb-2">Create a Giskard client with the following Python code:</p>
+          <CodeSnippet :codeContent="clientCodeContent"></CodeSnippet>
+        </div>
         <v-btn color="primary" @click="step = 3">Continue</v-btn>
         <v-btn class="ml-2" @click="close" flat>Cancel</v-btn>
       </v-stepper-content>
 
       <v-stepper-content step="3">
-        <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+        <div class="mb-6">
+          <p class="mb-2">Choose the type of artifact you want to create:</p>
+          <v-btn-toggle v-model="toggleArtifactType" borderless mandatory color="primary">
+            <v-btn value="dataset" class="py-5 px-4">
+              <span>Dataset</span>
+              <v-icon end class="pb-1 pl-1" :color="toggleArtifactType === 'dataset' ? 'primary' : ''">
+                stacked_bar_chart
+              </v-icon>
+            </v-btn>
+            <v-btn value="model" class="py-5 px-4">
+              <span>Model</span>
+              <v-icon end class="pb-1 pl-1" :color="toggleArtifactType === 'model' ? 'primary' : ''">
+                settings_suggest
+              </v-icon>
+            </v-btn>
+          </v-btn-toggle>
+          <p class="mt-4 mb-2">Then, create the artifact with the following Python code:</p>
+          <CodeSnippet v-show="toggleArtifactType === 'dataset'" :codeContent="datasetCodeContent"></CodeSnippet>
+          <CodeSnippet v-show="toggleArtifactType === 'model'" :codeContent="modelCodeContent"></CodeSnippet>
+        </div>
         <v-btn color="primary" @click="step = 4">Continue</v-btn>
         <v-btn class="ml-2" @click="close" flat>Cancel</v-btn>
       </v-stepper-content>
 
       <v-stepper-content step="4">
-        <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+        <div class="mb-6">
+          <p class="mb-2">Choose the type of test you want to upload:</p>
+          <v-btn-toggle v-model="toggleTestType" borderless mandatory color="primary">
+            <v-btn value="scan" class="py-5 px-4">
+              <span>Scan</span>
+            </v-btn>
+            <v-btn value="manual" class="py-5 px-4">
+              <span>Manual</span>
+            </v-btn>
+          </v-btn-toggle>
+          <p class="mt-4 mb-2">Then, upload the test with the following Python code:</p>
+          <CodeSnippet v-show="toggleTestType === 'scan'" :codeContent="scanCodeContent"></CodeSnippet>
+          <CodeSnippet v-show="toggleTestType === 'manual'" :codeContent="manualTestCodeContent"></CodeSnippet>
+        </div>
         <v-btn color="primary" @click="close">Close</v-btn>
         <v-btn class="ml-2" @click="step = 1" flat>Restart</v-btn>
       </v-stepper-content>
