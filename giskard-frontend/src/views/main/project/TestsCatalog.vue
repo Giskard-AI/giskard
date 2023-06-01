d047f94c7d4355ae23a6829adb93d505f2a6acaf
andreybavt <andreybavt@gmail.com>
2023-06-01 00:11:22 +0000

    fix styles

--- giskard-frontend/src/views/main/project/TestsCatalog.vue
+++ giskard-frontend/src/views/main/project/TestsCatalog.vue
@@ -1,107 +1,113 @@
 <template>
-  <v-container class="main-container">
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <div class="vc">
+    <v-tabs centered class="vc">
+      <v-tab>Tests</v-tab>
+      <v-tab>Test suites</v-tab>
+      <v-tab-item :transition="false" key="1" class="vc">
+        <div class="vc">
+          <v-container class="main-container vc">
+            <v-row class="fill-height">
+              <v-col cols="4" class="vc fill-height">
+                <v-list three-line v-if="testFunctions">
+                  <v-list-item-group v-model="selected" color="primary" mandatory>
+                    <template v-for="(test, index) in testFunctions">
+                      <v-divider/>
+                      <v-list-item :value="test">
+                        <v-list-item-content>
+                          <v-list-item-title v-text="test.name" class="test-title"></v-list-item-title>
 
+                          <v-list-item-subtitle v-if="test.tags">
+                            <v-chip class="mr-2" v-for="tag in sorted(test.tags)" x-small :color="pasterColor(tag)">
+                              {{ tag }}
+                            </v-chip>
+                          </v-list-item-subtitle>
+                        </v-list-item-content>
 
-    <!--    <v-row class="overflow-hidden">-->
-<!--      <v-col cols="4" style="overflow-y: scroll; " class="fill-height">-->
-<!--        <v-list three-line v-if="testFunctions">-->
-<!--          <v-list-item-group v-model="selected" color="primary" mandatory>-->
-<!--            <template v-for="(test, index) in testFunctions">-->
-<!--              <v-divider/>-->
-<!--              <v-list-item :value="test">-->
-<!--                <v-list-item-content>-->
-<!--                  <v-list-item-title v-text="test.name" class="test-title"></v-list-item-title>-->
+                      </v-list-item>
+                    </template>
+                  </v-list-item-group>
+                </v-list>
+              </v-col>
+              <v-col cols="8" v-if="selected" class="vc fill-height">
+                <div class="d-flex justify-space-between">
+                  <span class="text-h5">{{ selected.name }}</span>
+                  <v-btn small outlined tile class="primary" color="white">
+                    <v-icon dense class="pr-2">mdi-plus</v-icon>
+                    Add to test suite
+                  </v-btn>
+                </div>
+                <div class="vc overflow-x-hidden pr-5">
 
-<!--                  <v-list-item-subtitle v-if="test.tags">-->
-<!--                    <v-chip class="mr-2" v-for="tag in sorted(test.tags)" x-small :color="pasterColor(tag)">{{-->
-<!--                        tag-->
-<!--                      }}-->
-<!--                    </v-chip>-->
-<!--                  </v-list-item-subtitle>-->
-<!--                </v-list-item-content>-->
+                  <pre class="test-doc caption pt-5">{{ selected.doc }}</pre>
+                  <div class="pt-5">
+                    <div class="d-flex justify-space-between">
+                      <span class="text-h6">Inputs</span>
+                      <v-btn width="100" small tile outlined @click="tryMode = !tryMode">{{
+                          tryMode ? 'Cancel' : 'Try it'
+                        }}
+                      </v-btn>
+                    </div>
+                    <v-list>
+                      <v-list-item v-for="a in selected.arguments" class="pl-0 pr-0">
+                        <v-row>
+                          <v-col>
+                            <v-list-item-content>
+                              <v-list-item-title>{{ a.name }}</v-list-item-title>
+                              <v-list-item-subtitle class="text-caption">{{ a.type }}</v-list-item-subtitle>
+                              <v-list-item-action-text v-show="!!a.optional">Optional. Default: <code>{{
+                                  a.default
+                                }}</code>
+                              </v-list-item-action-text>
+                            </v-list-item-content>
+                          </v-col>
+                          <v-col>
+                            <template v-if="tryMode">
+                              <DatasetSelector :project-id="projectId" :label="a.name" :return-object="false"
+                                               v-if="a.type === 'GiskardDataset'" :value.sync="testArguments[a.name]"/>
+                              <ModelSelector :project-id="projectId" :label="a.name" :return-object="false"
+                                             v-if="a.type === 'GiskardModel'" :value.sync="testArguments[a.name]"/>
+                              <v-text-field
+                                  :step='a.type === "float" ? 0.1 : 1'
+                                  v-model="testArguments[a.name]"
+                                  v-if="['float', 'int'].includes(a.type)"
+                                  hide-details
+                                  single-line
+                                  type="number"
+                                  outlined
+                                  dense
+                              />
+                            </template>
+                          </v-col>
+                        </v-row>
+                      </v-list-item>
+                    </v-list>
+                    <v-row v-show="tryMode">
+                      <v-col :align="'right'">
+                        <v-btn width="100" small tile outlined class="primary" color="white" @click="runTest">Run
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                    <v-row style="height: 150px">
+                      <v-col v-if="testResult">
+                        <TestExecutionResultBadge :result="testResult"/>
+                      </v-col>
+                    </v-row>
+                  </div>
+                </div>
 
-<!--              </v-list-item>-->
-<!--            </template>-->
-<!--          </v-list-item-group>-->
-<!--        </v-list>-->
-<!--      </v-col>-->
-<!--      <v-col cols="8" v-if="selected" style="overflow-y: scroll;" class="fill-height">-->
-<!--        <div class="d-flex justify-space-between">-->
-<!--          <span class="text-h5">{{ selected.name }}</span>-->
-<!--          <v-btn small outlined tile class="primary" color="white">-->
-<!--            <v-icon dense class="pr-2">mdi-plus</v-icon>-->
-<!--            Add to test suite-->
-<!--          </v-btn>-->
-<!--        </div>-->
-<!--        <pre class="test-doc caption pt-5">{{ selected.doc }}</pre>-->
-<!--        <div class="pt-5">-->
-<!--          <div class="d-flex justify-space-between">-->
-<!--            <span class="text-h6">Inputs</span>-->
-<!--            <v-btn width="100" small tile outlined @click="tryMode = !tryMode">{{-->
-<!--                tryMode ? 'Cancel' : 'Try it'-->
-<!--              }}-->
-<!--            </v-btn>-->
-<!--          </div>-->
-<!--          <v-list>-->
-<!--            <v-list-item v-for="a in selected.arguments" class="pl-0 pr-0">-->
-<!--              <v-row>-->
-<!--                <v-col>-->
-<!--                  <v-list-item-content>-->
-<!--                    <v-list-item-title>{{ a.name }}</v-list-item-title>-->
-<!--                    <v-list-item-subtitle class="text-caption">{{ a.type }}</v-list-item-subtitle>-->
-<!--                    <v-list-item-action-text v-show="!!a.optional">Optional. Default: <code>{{ a.default }}</code>-->
-<!--                    </v-list-item-action-text>-->
-<!--                  </v-list-item-content>-->
-<!--                </v-col>-->
-<!--                <v-col>-->
-<!--                  <template v-if="tryMode">-->
-<!--                    <DatasetSelector :project-id="projectId" :label="a.name" :return-object="false"-->
-<!--                                     v-if="a.type === 'GiskardDataset'" :value.sync="testArguments[a.name]"/>-->
-<!--                    <ModelSelector :project-id="projectId" :label="a.name" :return-object="false"-->
-<!--                                   v-if="a.type === 'GiskardModel'" :value.sync="testArguments[a.name]"/>-->
-<!--                    <v-text-field-->
-<!--                        :step='a.type === "float" ? 0.1 : 1'-->
-<!--                        v-model="testArguments[a.name]"-->
-<!--                        v-if="['float', 'int'].includes(a.type)"-->
-<!--                        hide-details-->
-<!--                        single-line-->
-<!--                        type="number"-->
-<!--                        outlined-->
-<!--                        dense-->
-<!--                    />-->
-<!--                  </template>-->
-<!--                </v-col>-->
-<!--              </v-row>-->
-<!--            </v-list-item>-->
-<!--          </v-list>-->
-<!--          <v-row v-show="tryMode">-->
-<!--            <v-col :align="'right'">-->
-<!--              <v-btn width="100" small tile outlined class="primary" color="white" @click="runTest">Run</v-btn>-->
-<!--            </v-col>-->
-<!--          </v-row>-->
-<!--          <v-row v-if="testResult">-->
-<!--            <v-col>-->
-<!--              <TestExecutionResultBadge :result="testResult"/>-->
-<!--            </v-col>-->
-<!--          </v-row>-->
-<!--        </div>-->
-<!--      </v-col>-->
-<!--    </v-row>-->
+              </v-col>
+            </v-row>
+          </v-container>
+        </div>
+      </v-tab-item>
+      <v-tab-item :transition="false" key="2" class="vc">
+        <div class="vc">
+          <div class="debug">row</div>
+        </div>
+      </v-tab-item>
+    </v-tabs>
 
-    <!--    <v-tabs centered class="fill-height">-->
-    <!--      <v-tab>Tests</v-tab>-->
-    <!--      <v-tab>Test suites</v-tab>-->
-    <!--      <v-tab-item key="1" class="fill-height overflow-y-auto">-->
-    <!--        -->
-    <!--      </v-tab-item>-->
-    <!--      <v-tab-item key="2">2</v-tab-item>-->
-    <!--    </v-tabs>-->
-
-
-  </v-container>
+  </div>
 </template>
 
 <script lang="ts">
@@ -174,6 +180,7 @@ export default class TestsCatalog extends Vue {
   }
 
   get testFunctions() {
+    // @ts-ignore
     return _.sortBy(this.registry.functions, 'name');
   }
 
