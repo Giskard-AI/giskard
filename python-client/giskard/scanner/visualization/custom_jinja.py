cf42b33ab5049e05533af9a1eaa1fc6865b88f02
Matteo Dora <matteo@giskard.ai>
2023-06-01 00:11:22 +0000

    Better metrics formatting

--- python-client/giskard/scanner/visualization/custom_jinja.py
+++ python-client/giskard/scanner/visualization/custom_jinja.py
@@ -21,3 +21,16 @@ def pluralize(count, singular="", plural="s"):
     if isinstance(count, Sequence):
         count = len(count)
     return singular if count == 1 else plural
+
+
+def format_metric(value, n=3):
+    if isinstance(value, float):
+        if value > 1e6 or value < 1e-3:
+            return f"{value:.{n}e}"
+        else:
+            return f"{value:.{3}f}"
+
+    if isinstance(value, int) and abs(value) > 1e4:
+        return f"{value:.{n}e}"
+
+    return value
