cc2ccb11776c57e07292efc2af1f55f48ad1d7ec
Rabah <rabah.khalek@gmail.com>
2023-06-01 02:11:23 +0200

    fixed missing indent in pytorch model

--- python-client/giskard/models/pytorch/__init__.py
+++ python-client/giskard/models/pytorch/__init__.py
@@ -41,6 +41,25 @@ class TorchMinimalDataset(torch_dataset):
 
 
 class PyTorchModel(MLFlowBasedModel):
+    """A wrapper class for PyTorch models that extends the functionality of the
+    MLFlowBasedModel class.
+
+    Attributes:
+        model (nn.Module): The PyTorch model to be used for prediction.
+        model_type (ModelType): The type of the model, either 'classification' or 'regression'.
+        torch_dtype (TorchDType, optional): The data type to be used for input tensors. Defaults to 'float32'.
+        device (str, optional): The device to run the model on, either 'cpu' or 'cuda'. Defaults to 'cpu'.
+        name (str, optional): The name of the model. Defaults to None.
+        data_preprocessing_function (callable, optional): A function to preprocess input data. Defaults to None.
+        model_postprocessing_function (callable, optional): A function to postprocess model output. Defaults to None.
+        feature_names (list[str], optional): A list of feature names. Defaults to None.
+        classification_threshold (float, optional): The threshold value for classification. Defaults to 0.5.
+        classification_labels (list[str], optional): A list of classification labels. Defaults to None.
+        iterate_dataset (bool, optional): Whether to iterate over the dataset for prediction. Defaults to True.
+        device (str): The device to run the model on.
+        torch_dtype (TorchDType): The data type to be used for input tensors.
+        iterate_dataset (bool): Whether to iterate over the dataset for prediction.
+    """
 
     def __init__(
             self,
@@ -71,118 +90,107 @@ class PyTorchModel(MLFlowBasedModel):
         self.torch_dtype = torch_dtype
         self.iterate_dataset = iterate_dataset
 
+    @classmethod
+    def load_model(cls, local_dir):
+        return mlflow.pytorch.load_model(local_dir)
 
-@classmethod
-def load_model(cls, local_dir):
-    return mlflow.pytorch.load_model(local_dir)
+    def save_with_mlflow(self, local_path, mlflow_meta: mlflow.models.Model):
+        mlflow.pytorch.save_model(self.model, path=local_path, mlflow_model=mlflow_meta)
 
+    def _get_predictions_from_iterable(self, data):
+        # Fault tolerance: try to convert to the right format in special cases
+        if isinstance(data, pd.DataFrame):
+            data = TorchMinimalDataset(data, self.torch_dtype)
+        elif isinstance(data, DataLoader):
+            data = _get_dataset_from_dataloader(data)
 
-def save_with_mlflow(self, local_path, mlflow_meta: mlflow.models.Model):
-    mlflow.pytorch.save_model(self.model, path=local_path, mlflow_model=mlflow_meta)
+        # Create the data iterator
+        try:
+            data_iter = iter(data)
+        except TypeError as err:
+            raise ValueError(
+                f"The data exposed to your model must be iterable (instead, we got type={type(data)}). "
+                "Make sure that your data or your `data_preprocessing_function` outputs one of the following:\n"
+                "- pandas.DataFrame\n- torch.utils.data.Dataset\n- iterable with elements that are compatible with your model"
+            ) from err
 
+        try:
+            with torch.no_grad():
+                return torch.cat([self.model(*entry) for entry in map_to_tuples(data_iter)])
+        except ValueError as err:
+            raise ValueError(
+                "Running your model prediction on one element of your dataset returned an error.\n"
+                "Please check that your `data_preprocessing_function` returns an iterable of objects "
+                "that are valid inputs for your model."
+            ) from err
 
-def _get_predictions_from_iterable(self, data):
-    # Fault tolerance: try to convert to the right format in special cases
-    if isinstance(data, pd.DataFrame):
-        data = TorchMinimalDataset(data, self.torch_dtype)
-    elif isinstance(data, DataLoader):
-        data = _get_dataset_from_dataloader(data)
+    def _get_predictions_from_object(self, data):
+        try:
+            return self.model(data)
+        except ValueError as err:
+            raise ValueError(
+                "Running your model prediction returned an error.\n"
+                "Since you specified `iter_dataset=False`, please check that your `data_preprocessing_function` "
+                "returns an object that can be passed as input for your model. "
+            ) from err
 
-    # Create the data iterator
-    try:
-        data_iter = iter(data)
-    except TypeError as err:
+    def model_predict(self, data):
+        self.model.to(self.device)
+        self.model.eval()
+
+        if self.iterate_dataset:
+            predictions = self._get_predictions_from_iterable(data)
+        else:
+            predictions = self._get_predictions_from_object(data)
+
+        return predictions
+
+    def _convert_to_numpy(self, raw_predictions):
+        if isinstance(raw_predictions, torch.Tensor):
+            return raw_predictions.detach().numpy()
+
+        return np.asarray(raw_predictions)
+
+    def save_pytorch_meta(self, local_path):
+        with open(Path(local_path) / "giskard-model-pytorch-meta.yaml", "w") as f:
+            yaml.dump(
+                {
+                    "device": self.device,
+                    "torch_dtype": self.torch_dtype,
+                    "iterate_dataset": self.iterate_dataset,
+                },
+                f,
+                default_flow_style=False,
+            )
+
+    def save(self, local_path: Union[str, Path]) -> None:
+        super().save(local_path)
+        self.save_pytorch_meta(local_path)
+
+    @classmethod
+    def load(cls, local_dir, **kwargs):
+        pytorch_meta_file = Path(local_dir) / "giskard-model-pytorch-meta.yaml"
+        if pytorch_meta_file.exists():
+            with open(pytorch_meta_file) as f:
+                pytorch_meta = yaml.load(f, Loader=yaml.Loader)
+                kwargs["device"] = pytorch_meta["device"]
+                kwargs["torch_dtype"] = pytorch_meta["torch_dtype"]
+                kwargs["iterate_dataset"] = pytorch_meta.get("iterate_dataset")
+                return super().load(local_dir, **kwargs)
+        else:
+            raise ValueError(
+                f"Cannot load model ({cls.__module__}.{cls.__name__}), " f"{pytorch_meta_file} file not found"
+            )
+
+    def _get_dataset_from_dataloader(dl: DataLoader):
+        if not isinstance(dl.dataset, collections.defaultdict):
+            return dl.dataset
+
         raise ValueError(
-            f"The data exposed to your model must be iterable (instead, we got type={type(data)}). "
-            "Make sure that your data or your `data_preprocessing_function` outputs one of the following:\n"
-            "- pandas.DataFrame\n- torch.utils.data.Dataset\n- iterable with elements that are compatible with your model"
-        ) from err
-
-    try:
-        with torch.no_grad():
-            return torch.cat([self.model(*entry) for entry in map_to_tuples(data_iter)])
-    except ValueError as err:
-        raise ValueError(
-            "Running your model prediction on one element of your dataset returned an error.\n"
-            "Please check that your `data_preprocessing_function` returns an iterable of objects "
-            "that are valid inputs for your model."
-        ) from err
-
-
-def _get_predictions_from_object(self, data):
-    try:
-        return self.model(data)
-    except ValueError as err:
-        raise ValueError(
-            "Running your model prediction returned an error.\n"
-            "Since you specified `iter_dataset=False`, please check that your `data_preprocessing_function` "
-            "returns an object that can be passed as input for your model. "
-        ) from err
-
-
-def model_predict(self, data):
-    self.model.to(self.device)
-    self.model.eval()
-
-    if self.iterate_dataset:
-        predictions = self._get_predictions_from_iterable(data)
-    else:
-        predictions = self._get_predictions_from_object(data)
-
-    return predictions
-
-
-def _convert_to_numpy(self, raw_predictions):
-    if isinstance(raw_predictions, torch.Tensor):
-        return raw_predictions.detach().numpy()
-
-    return np.asarray(raw_predictions)
-
-
-def save_pytorch_meta(self, local_path):
-    with open(Path(local_path) / "giskard-model-pytorch-meta.yaml", "w") as f:
-        yaml.dump(
-            {
-                "device": self.device,
-                "torch_dtype": self.torch_dtype,
-                "iterate_dataset": self.iterate_dataset,
-            },
-            f,
-            default_flow_style=False,
+            f"We tried to infer the torch.utils.data.Dataset from your DataLoader. \n \
+                        The type we found was {dl.dataset} which we don't support. Please provide us \n \
+                        with a different iterable as output of your data_preprocessing_function."
         )
 
-
-def save(self, local_path: Union[str, Path]) -> None:
-    super().save(local_path)
-    self.save_pytorch_meta(local_path)
-
-
-@classmethod
-def load(cls, local_dir, **kwargs):
-    pytorch_meta_file = Path(local_dir) / "giskard-model-pytorch-meta.yaml"
-    if pytorch_meta_file.exists():
-        with open(pytorch_meta_file) as f:
-            pytorch_meta = yaml.load(f, Loader=yaml.Loader)
-            kwargs["device"] = pytorch_meta["device"]
-            kwargs["torch_dtype"] = pytorch_meta["torch_dtype"]
-            kwargs["iterate_dataset"] = pytorch_meta.get("iterate_dataset")
-            return super().load(local_dir, **kwargs)
-    else:
-        raise ValueError(
-            f"Cannot load model ({cls.__module__}.{cls.__name__}), " f"{pytorch_meta_file} file not found"
-        )
-
-
-def _get_dataset_from_dataloader(dl: DataLoader):
-    if not isinstance(dl.dataset, collections.defaultdict):
-        return dl.dataset
-
-    raise ValueError(
-        f"We tried to infer the torch.utils.data.Dataset from your DataLoader. \n \
-                    The type we found was {dl.dataset} which we don't support. Please provide us \n \
-                    with a different iterable as output of your data_preprocessing_function."
-    )
-
-
-def _convert_to_numpy(self, predictions):
-    return torch.cat(predictions).detach().squeeze(0).numpy()
+    def _convert_to_numpy(self, predictions):
+        return torch.cat(predictions).detach().squeeze(0).numpy()
