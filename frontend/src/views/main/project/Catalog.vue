6ee9ba08b6f0278328e70328cc9ae7b755eac124
Henrique Chaves <henrique@giskard.ai>
2023-06-01 00:11:22 +0000

    Remove tabs and add buttons to Catalog

--- frontend/src/views/main/project/Catalog.vue
+++ frontend/src/views/main/project/Catalog.vue
@@ -1,6 +1,28 @@
 <template>
     <div class="vc mt-2 pb-0" v-if="catalog">
-        <v-tabs>
+        <div>
+            <v-btn text outlined color="primary" class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-datasets' }">
+                <v-icon left small class="mr-1">mdi-database</v-icon>
+                Datasets
+            </v-btn>
+            <v-btn text outlined class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-models' }" color="primary">
+                <v-icon left small class="mr-1">mdi-cube-outline</v-icon>
+                Models
+            </v-btn>
+            <v-btn text outlined class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-tests' }" color="primary">
+                <v-icon left small class="mr-1">mdi-test-tube</v-icon>
+                Tests
+            </v-btn>
+            <v-btn text outlined class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-slicing-functions' }" color="primary">
+                <v-icon left small class="mr-1">mdi-knife</v-icon>
+                Slicing functions
+            </v-btn>
+            <v-btn text outlined class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-transformation-functions' }" color="primary">
+                <v-icon left small class="mr-1">mdi-chart-bell-curve</v-icon>
+                Transformation functions
+            </v-btn>
+        </div>
+        <!-- <v-tabs>
             <v-tab :to="{ name: 'project-catalog-datasets' }">
                 Datasets
             </v-tab>
@@ -16,7 +38,7 @@
             <v-tab :to="{ name: 'project-catalog-transformation-functions' }">
                 Transformation functions
             </v-tab>
-        </v-tabs>
+        </v-tabs> -->
         <router-view />
     </div>
     <LoadingFullscreen v-else name="catalog" />
@@ -46,3 +68,13 @@ onActivated(async () => {
     await router.push({ name: defaultRoute });
 });
 </script>
+
+<style scoped>
+.tab {
+    text-transform: none !important;
+}
+
+.v-btn--active.tab {
+    border-color: #087038 !important;
+}
+</style>
