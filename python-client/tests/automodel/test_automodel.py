c945bca857ef703a6f61b127f1cbc512103e01dc
Rabah <rabah.khalek@gmail.com>
2023-06-01 02:11:23 +0200

    simplified automodel __new__ and implemented useful error

--- python-client/tests/automodel/test_automodel.py
+++ python-client/tests/automodel/test_automodel.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
-
+import pytest
 import tests.utils
 from giskard import Dataset
 from giskard.models.automodel import Model
+from giskard.models.function import PredictionFunctionModel
+from giskard.models.sklearn import SKLearnModel
 
 
 class MyArbitraryModel(Model):
@@ -16,6 +18,41 @@ class MySklearnModel(Model):
         return self.model.predict_proba(some_df)
 
 
+def test_autoserializablemodel_abstract():
+    class UnsupportedModel:
+        ...
+
+    unsupported_model = UnsupportedModel()
+    with pytest.raises(NotImplementedError) as e:
+        Model(model=unsupported_model, model_type="regression")
+    assert e.match(
+        "We could not infer your model library. You need to define a subclass of Model where.*")
+
+
+def test_autoserializablemodel_arbitrary_default():
+    def prediction_function(df):
+        return df['x'] ** 2
+
+    my_model = Model(model=prediction_function, model_type="regression")
+
+    my_dataset = Dataset(df=pd.DataFrame({"x": [1., 2.], "y": [1, 2]}), target="y")
+
+    assert isinstance(my_model, PredictionFunctionModel)
+    tests.utils.verify_model_upload(my_model, my_dataset)
+
+
+def test_autoserializablemodel_sklearn_default(german_credit_raw_model, german_credit_data):
+    my_model = Model(
+        model=german_credit_raw_model,
+        model_type="classification",
+        classification_labels=german_credit_raw_model.classes_,
+        classification_threshold=0.5,
+    )
+
+    assert isinstance(my_model, SKLearnModel)
+    tests.utils.verify_model_upload(my_model, german_credit_data)
+
+
 def test_autoserializablemodel_arbitrary():
     my_model = MyArbitraryModel(
         model=lambda x: x ** 2,
@@ -24,6 +61,7 @@ def test_autoserializablemodel_arbitrary():
 
     my_dataset = Dataset(df=pd.DataFrame({"x": [1, 2], "y": [1, 2]}), target="y")
 
+    assert isinstance(my_model, PredictionFunctionModel)
     tests.utils.verify_model_upload(my_model, my_dataset)
 
 
@@ -35,4 +73,5 @@ def test_autoserializablemodel_sklearn(german_credit_raw_model, german_credit_da
         classification_threshold=0.5,
     )
 
+    assert isinstance(my_model, SKLearnModel)
     tests.utils.verify_model_upload(my_model, german_credit_data)
