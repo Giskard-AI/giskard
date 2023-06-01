81d3f132cceb2eaf1dfc98d5bc948c1e20c508ce
Henrique Chaves <henriquechavesmm@gmail.com>
2023-06-01 00:11:22 +0000

    Fix open/close debugging session

--- frontend/src/stores/debugging-sessions.ts
+++ frontend/src/stores/debugging-sessions.ts
@@ -5,12 +5,14 @@ import { api } from '@/api';
 interface State {
   projectId: number | null;
   debuggingSessions: Array<InspectionDTO>;
+  currentDebuggingSessionId: number | null;
 }
 
 export const useDebuggingSessionsStore = defineStore('debuggingSessions', {
   state: (): State => ({
     projectId: null,
     debuggingSessions: [],
+    currentDebuggingSessionId: null,
   }),
   getters: {},
   actions: {
@@ -30,11 +32,17 @@ export const useDebuggingSessionsStore = defineStore('debuggingSessions', {
     },
     async deleteDebuggingSession(inspectionId: number) {
       await api.deleteInspection(inspectionId);
+      if (this.currentDebuggingSessionId === inspectionId) {
+        this.currentDebuggingSessionId = null;
+      }
       await this.reload();
     },
     async updateDebuggingSessionName(inspectionId: number, inspection: InspectionCreateDTO) {
       await api.updateInspectionName(inspectionId, inspection);
       await this.reload();
+    },
+    setCurrentDebuggingSessionId(inspectionId: number | null) {
+      this.currentDebuggingSessionId = inspectionId;
     },
   },
 });
