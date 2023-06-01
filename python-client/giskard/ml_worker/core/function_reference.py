27e6c8107eedabe0a896c3b926db125f86809fe2
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 02:11:23 +0200

    Load and find references of object at runtime without pickling reference

--- python-client/giskard/ml_worker/core/function_reference.py
+++ python-client/giskard/ml_worker/core/function_reference.py
@@ -3,9 +3,10 @@ import os
 import pickle
 import posixpath
 import random
+import sys
 import uuid
 from pathlib import Path
-from typing import Any, Optional
+from typing import Any
 
 import cloudpickle
 
@@ -13,73 +14,71 @@ from client.giskard_client import GiskardClient
 from giskard.settings import settings
 
 
+def generate_func_id(name) -> str:
+    rd = random.Random()
+    rd.seed(hashlib.sha1(name.encode('utf-8')).hexdigest())
+    func_id = str(uuid.UUID(int=rd.getrandbits(128), version=4))
+    return str(func_id)
+
+
 class FunctionReference:
     func: Any
     func_name: str
-    func_id = str
-    reference: bytearray
 
-    def __init__(self, func: Any, func_name: str, func_id: Optional[str], reference: bytearray):
+    def __init__(self, func: Any, func_name: str):
         self.func = func
         self.func_name = func_name
-        self.func_id = func_id
-        self.reference = reference
 
     @classmethod
     def of(cls, func: Any):
         func_name = f"{func.__module__}.{func.__name__}"
-        reference = cloudpickle.dumps(func)
 
         if func_name.startswith('__main__'):
+            reference = cloudpickle.dumps(func)
+            cloudpickle.register_pickle_by_value()
             func_name += hashlib.sha1(reference).hexdigest()
 
-        # Ensure to always have the same UUID4 according to the function name
-        rd = random.Random()
-        rd.seed(hashlib.sha1(func_name.encode('utf-8')).hexdigest())
-        func_id = uuid.UUID(int=rd.getrandbits(128), version=4)
-
-        return cls(func, func_name, str(func_id), reference)
+        return cls(func, func_name)
 
     def save(self, client: GiskardClient, project_key) -> str:
-        local_dir = settings.home_dir / settings.cache_dir / project_key / "functions" / self.func_id
+        if not self.func_name.startswith('__main__'):
+            return self.func_name
+
+        func_id = generate_func_id(self.func_name)
+
+        local_dir = settings.home_dir / settings.cache_dir / project_key / "functions" / func_id
 
         if not local_dir.exists():
             os.makedirs(local_dir)
             with open(Path(local_dir) / 'giskard-function.pkl', 'wb') as f:
                 cloudpickle.dump(self.func, f)
                 if client is not None:
-                    client.log_artifacts(local_dir, posixpath.join(project_key, "functions", self.func_id))
+                    client.log_artifacts(local_dir, posixpath.join(project_key, "functions", func_id))
 
-        return self.func_id
+        return self.func_name
 
     @classmethod
-    def load(cls, client: GiskardClient, project_key: str, func_id: str):
-        local_dir = settings.home_dir / settings.cache_dir / project_key / "functions" / func_id
-
-        if client is None:
-            # internal worker case, no token based http client
-            assert local_dir.exists(), f"Cannot find existing function {project_key}.{func_id}"
+    def load(cls, client: GiskardClient, project_key: str, func_name: str):
+        if not func_name.startswith('__main__'):
+            [module_name, func_name] = func_name.split('.')
+            func = getattr(sys.modules[module_name], func_name)
         else:
-            client.load_artifact(local_dir, posixpath.join(project_key, "functions", func_id))
+            func_id = generate_func_id(func_name)
 
-        with open(Path(local_dir) / 'giskard-function.pkl', 'rb') as f:
-            func = pickle.load(f)
-            return cls.of(func)
+            local_dir = settings.home_dir / settings.cache_dir / project_key / "functions" / func_id
+
+            if client is None:
+                # internal worker case, no token based http client
+                assert local_dir.exists(), f"Cannot find existing function {project_key}.{func_id}"
+            else:
+                client.load_artifact(local_dir, posixpath.join(project_key, "functions", func_id))
+
+            with open(Path(local_dir) / 'giskard-function.pkl', 'rb') as f:
+                func = pickle.load(f)
+
+        return cls(func, func_name)
 
     @classmethod
     def _read_function_from_local_dir(cls, local_path: str):
         with open(local_path, 'rb') as ds_stream:
             return
-
-
-def test_method():
-    tid = FunctionReference.of(test_method).save(None, "test")
-    print(FunctionReference.load(None, 'test', tid).reference)
-
-    def test_anonymous():
-        print('Anon retrieved successfully')
-
-    anon_id = FunctionReference.of(test_anonymous).save(None, "test")
-    anon = FunctionReference.load(None, 'test', anon_id)
-    print(anon.reference)
-    anon.func()
