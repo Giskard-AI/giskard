4e568b97f7ac332ec46fb9c530e434938f1f5600
Rabah <rabah.khalek@gmail.com>
2023-06-01 02:11:23 +0200

    fixed lint

--- python-client/giskard/__init__.py
+++ python-client/giskard/__init__.py
@@ -7,14 +7,11 @@ from giskard.client.giskard_client import GiskardClient
 from giskard.client.project import Project
 from giskard.core.model import Dataset
 from giskard.core.model import Model
-from giskard.core.model import Model
 from giskard.ml_worker.generated.ml_worker_pb2 import SingleTestResult
 from giskard.ml_worker.testing.abstract_test_collection import AbstractTestCollection
 from giskard.ml_worker.testing.registry.decorators import test
 from giskard.ml_worker.utils.logging import configure_logging
 from giskard.models.pytorch import PyTorchModel
-from giskard.models.pytorch import PyTorchModel
-from giskard.models.sklearn import SKLearnModel
 from giskard.models.sklearn import SKLearnModel
 from giskard.models.tensorflow import TensorFlowModel
 
