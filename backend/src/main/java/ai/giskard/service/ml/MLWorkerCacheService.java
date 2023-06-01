67324744ddece79ab7d81b831c6ef94f697cab2e
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 02:11:23 +0200

    Fixed issue when adding test from catalog

--- backend/src/main/java/ai/giskard/service/ml/MLWorkerCacheService.java
+++ backend/src/main/java/ai/giskard/service/ml/MLWorkerCacheService.java
@@ -2,7 +2,9 @@ package ai.giskard.service.ml;
 
 import ai.giskard.ml.MLWorkerClient;
 import ai.giskard.ml.tunnel.MLWorkerTunnelService;
+import ai.giskard.repository.ProjectRepository;
 import ai.giskard.repository.ml.TestFunctionRepository;
+import ai.giskard.service.TestFunctionService;
 import ai.giskard.web.dto.TestFunctionDTO;
 import ai.giskard.web.dto.mapper.GiskardMapper;
 import ai.giskard.worker.MLWorkerGrpc;
@@ -11,10 +13,11 @@ import com.google.protobuf.Empty;
 import io.grpc.StatusRuntimeException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static ai.giskard.utils.GRPCUtils.convertGRPCObject;
 
@@ -24,15 +27,30 @@ public class MLWorkerCacheService {
 
     private final MLWorkerService mlWorkerService;
     private final MLWorkerTunnelService mlWorkerTunnelService;
+    private final TestFunctionService testFunctionService;
     private final TestFunctionRepository testFunctionRepository;
+    private final ProjectRepository projectRepository;
     private final GiskardMapper giskardMapper;
     private List<TestFunctionDTO> testFunctions = Collections.emptyList();
 
     @Transactional
+    public List<TestFunctionDTO> getCatalog(long projectId) {
+        return Stream.concat(testFunctionRepository.findAll().stream()
+                    .filter(t -> t.getTags().contains("pickle"))
+                    .map(giskardMapper::toDTO),
+                findGiskardTest(projectRepository.getById(projectId).isUsingInternalWorker()).stream())
+            .toList();
+    }
+
     public List<TestFunctionDTO> findGiskardTest(boolean isInternal) {
+        if (isInternal) {
+            // Only cache external ML worker
+            return getTestFunctions(true);
+        }
+
         if (mlWorkerTunnelService.isClearCacheRequested()) {
-            testFunctions = getTestFunctions(isInternal);
-            testFunctionRepository.saveAll(testFunctions.stream().map(giskardMapper::fromDTO).toList());
+            testFunctions = getTestFunctions(false);
+            testFunctionService.saveAll(testFunctions);
             mlWorkerTunnelService.setClearCacheRequested(false);
         }
 
