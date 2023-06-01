c2690ebce33adac1cd9a0e12ce2e1b9fde39c7d8
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 00:11:22 +0000

    Added some sentiment slicing functions by default

--- python-client/giskard/ml_worker/testing/functions/slicing.py
+++ python-client/giskard/ml_worker/testing/functions/slicing.py
@@ -11,3 +11,39 @@ def short_comment_slicing_fn(x: pd.Series, column_name: str, max_characters: int
 @slicing_function(name="Keyword lookup")
 def keyword_lookup_slicing_fn(x: pd.Series, column_name: str, keyword: str) -> bool:
     return any(word in x[column_name].lower() for word in [keyword])
+
+
+@slicing_function(name="Positive sentiment", row_level=False, tags=["sentiment"])
+def positive_sentiment_analysis(x: pd.DataFrame, column_name: str, threshold: float = 0.9) -> pd.DataFrame:
+    from transformers import pipeline
+    sentiment_pipeline = pipeline("sentiment-analysis")
+
+    # Limit text to 512 characters
+    sentences = list(map(lambda txt: txt[:512], list(x[column_name])))
+
+    return x.iloc[list(
+        map(lambda s: s['label'] == 'POSITIVE' and s['score'] >= threshold, sentiment_pipeline(sentences)))]
+
+
+@slicing_function(name="Offensive sentiment", row_level=False, tags=["sentiment"])
+def offensive_sentiment_analysis(x: pd.DataFrame, column_name: str, threshold: float = 0.9) -> pd.DataFrame:
+    from transformers import pipeline
+    sentiment_pipeline = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-offensive")
+
+    # Limit text to 512 characters
+    sentences = list(map(lambda txt: txt[:512], list(x[column_name])))
+
+    return x.iloc[list(
+        map(lambda s: s['label'] == 'offensive' and s['score'] >= threshold, sentiment_pipeline(sentences)))]
+
+
+@slicing_function(name="Irony sentiment", row_level=False, tags=["sentiment"])
+def irony_sentiment_analysis(x: pd.DataFrame, column_name: str, threshold: float = 0.9) -> pd.DataFrame:
+    from transformers import pipeline
+    sentiment_pipeline = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-irony")
+
+    # Limit text to 512 characters
+    sentences = list(map(lambda txt: txt[:512], list(x[column_name])))
+
+    return x.iloc[list(
+        map(lambda s: s['label'] == 'irony' and s['score'] >= threshold, sentiment_pipeline(sentences)))]
