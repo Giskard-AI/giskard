210d0020505884cecea77f2d1bdb995a3a1b3520
Andrey Avtomonov <andreybavt@gmail.com>
2023-06-01 02:11:23 +0200

    fix sonar

--- python-client/giskard/ml_worker/testing/tests/__init__.py
+++ python-client/giskard/ml_worker/testing/tests/__init__.py
@@ -2,6 +2,6 @@ from giskard.ml_worker.testing.tests import heuristic
 from giskard.ml_worker.testing.tests import performance
 
 __all__ = [
-    heuristic,
-    performance
+    'heuristic',
+    'performance'
 ]
