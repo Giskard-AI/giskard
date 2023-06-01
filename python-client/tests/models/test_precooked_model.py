a86a2307fa944e79ae52cba45ce9d5040a238e2f
Matteo Dora <matteo@giskard.ai>
2023-06-01 00:11:22 +0000

    WIP

--- python-client/tests/models/test_precooked_model.py
+++ python-client/tests/models/test_precooked_model.py
@@ -1,12 +1,12 @@
 import pytest
 import numpy as np
-
+import pandas as pd
 from giskard import slicing_function
 from giskard.models.precooked import PrecookedModel
 
 
-@slicing_function(row_level=False)
-def random_slice(df):
+@slicing_function(name="random", row_level=False)
+def random_slice(df: pd.DataFrame) -> pd.DataFrame:
     return df.sample(100)
 
 
@@ -18,8 +18,8 @@ def test_precooked_model(model, dataset, request):
     model = request.getfixturevalue(model)
     dataset = request.getfixturevalue(dataset)
     precooked_model = PrecookedModel.from_model(model, dataset)
-
-    assert np.all(precooked_model.predict(dataset).raw == model.predict(dataset).raw)
-
     data_slice = dataset.slice(random_slice)
+    # assert np.all(data_slice.df.columns == dataset.df.columns)
+    # assert np.all(precooked_model.predict(dataset).raw == model.predict(dataset).raw)
+
     assert np.all(precooked_model.predict(data_slice).raw == model.predict(data_slice).raw)
