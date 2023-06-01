86bce1a250d57f901260cde269a7b5bda1e48336
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 02:11:23 +0200

    Migrated drift tests to new catalog

--- python-client/giskard/ml_worker/testing/tests/__init__.py
+++ python-client/giskard/ml_worker/testing/tests/__init__.py
@@ -1,7 +1,9 @@
+from giskard.ml_worker.testing.tests import drift
 from giskard.ml_worker.testing.tests import heuristic
 from giskard.ml_worker.testing.tests import performance
 
 __all__ = [
     'heuristic',
-    'performance'
+    'performance',
+    'drift',
 ]
