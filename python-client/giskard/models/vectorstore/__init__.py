39bc54cd5accc808588ad1bc2cb87e3a3db6dbb6
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 00:11:22 +0000

    Updated LLM model

--- python-client/giskard/models/vectorstore/__init__.py
+++ python-client/giskard/models/vectorstore/__init__.py
@@ -7,20 +7,21 @@ import cloudpickle
 from langchain import OpenAI
 from langchain.chains import RetrievalQA
 
-from giskard.core.model import CustomModel
+from giskard.core.model import Model
 from langchain.vectorstores import Chroma
-import pandas as pd
 
 
-class RetrievalQAModel(CustomModel):
+class RetrievalQAModel(Model):
     model: RetrievalQA
+    should_save_model_class = True
+    model_postprocessing_function = None
 
     def __init__(self,
                  model: RetrievalQA,
                  name: str = None) -> None:
         self.model = model
         super().__init__(
-            model_type='LLM',
+            model_type='regression',
             name=name,
             feature_names=None,
             classification_threshold=None,
@@ -28,19 +29,9 @@ class RetrievalQAModel(CustomModel):
         )
 
     def save(self, local_path: Union[str, Path]) -> None:
-        """
-        MLFlow requires a target directory to be empty before the model is saved, thus we have to call
-        save_with_mflow first and then save the rest of the metadata
-        """
-        if not self.id:
-            self.id = uuid.uuid4()
+        super().save(local_path)
 
         vectorstore: Chroma = self.model.retriever.vectorstore
-
-        # Save previous state to not alter user settings
-        tmp_persist_directory = vectorstore._persist_directory
-        tmp_client_settings = vectorstore._client_settings
-        tmp_client = vectorstore._client
 
         try:
             import chromadb
@@ -51,35 +42,31 @@ class RetrievalQAModel(CustomModel):
                 "Please install it with `pip install chromadb`."
             )
 
-        vectorstore._persist_directory = (Path(local_path) / str(self.id) / "db").absolute().as_posix()
-        vectorstore._client_settings = chromadb.config.Settings(
-            chroma_db_impl="duckdb+parquet", persist_directory=vectorstore._persist_directory
-        )
-        vectorstore._client = chromadb.Client(vectorstore._client_settings)
-        vectorstore.persist()
+        clone = Chroma(persist_directory=(Path(local_path) / 'db').absolute().as_posix(),
+                       embedding_function=vectorstore._embedding_function)
+        clone._collection.add(**vectorstore._collection.get())
 
-        with open(Path(local_path) / str(self.id) / "embedding_function.pkl", "wb") as f:
+        clone.persist()
+
+        with open(Path(local_path) / "embedding_function.pkl", "wb") as f:
             cloudpickle.dump(vectorstore._embedding_function, f)
 
-        # Reset data
-        vectorstore._persist_directory = tmp_persist_directory
-        vectorstore._client_settings = tmp_client_settings
-        vectorstore._client = tmp_client
-
-        super().save(local_path)
-
-    def predict_df(self, df: pd.DataFrame):
+    def predict_df(self, df):
         return self.model.apply(list(df))
 
     @classmethod
     def load(cls, local_dir, **constructor_params):
         with open(Path(local_dir) / "embedding_function.pkl", "rb") as f:
+            print('file')
             embedding_function = cloudpickle.load(f)
+            print(embedding_function)
 
-            persist_directory = (Path(local_dir) / "db").absolute().as_posix()
+            persist_directory = (Path(local_dir) / 'db').absolute().as_posix()
             chroma = Chroma(persist_directory=persist_directory, embedding_function=embedding_function)
+            print(chroma)
+            chroma.as_retriever()
 
-            cls(RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=chroma.as_retriever()))
+            return cls(RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=chroma.as_retriever()))
 
 # For inspect= clf predict
 # Should save class to true
