ff4ecec4e70994f27a1ccd7d98b068bd65cfe75f
Andrey Avtomonov <andreybavt@gmail.com>
2023-06-01 02:11:23 +0200

    merge parent branch

--- python-client/giskard/__init__.py
+++ python-client/giskard/__init__.py
@@ -5,18 +5,18 @@ import sys
 
 from giskard.client.giskard_client import GiskardClient
 from giskard.client.project import Project
-from giskard.core.model import Model
-from giskard.models.sklearn import SKLearnModel
-from giskard.models.pytorch import PyTorchModel
-from giskard.models.tensorflow import TensorFlowModel
 from giskard.core.model import Dataset
+from giskard.core.model import Model
 from giskard.core.model import Model
 from giskard.ml_worker.generated.ml_worker_pb2 import SingleTestResult
 from giskard.ml_worker.testing.abstract_test_collection import AbstractTestCollection
 from giskard.ml_worker.testing.registry.decorators import test
 from giskard.ml_worker.utils.logging import configure_logging
-from giskard.models.sklearn import SKLearnModel
 from giskard.models.pytorch import PyTorchModel
+from giskard.models.pytorch import PyTorchModel
+from giskard.models.sklearn import SKLearnModel
+from giskard.models.sklearn import SKLearnModel
+from giskard.models.tensorflow import TensorFlowModel
 
 configure_logging()
 if sys.version_info >= (3, 8):
@@ -43,6 +43,6 @@ __all__ = [
     'GiskardClient',
     'AbstractTestCollection',
     'test',
-    'PyTorchModel'
+    'PyTorchModel',
     'TensorFlowModel'
 ]
