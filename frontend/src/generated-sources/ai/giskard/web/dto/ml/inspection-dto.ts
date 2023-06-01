e2a4fef8b49d2de0647aae14819b43c155ffc72b
Henrique Chaves <henrique@giskard.ai>
2023-06-01 00:11:22 +0000

    Update  InspectionDTO

--- frontend/src/generated-sources/ai/giskard/web/dto/ml/inspection-dto.ts
+++ frontend/src/generated-sources/ai/giskard/web/dto/ml/inspection-dto.ts
@@ -1,5 +1,5 @@
-import type {DatasetDTO} from './dataset-dto';
-import type {ModelDTO} from './model-dto';
+import type { DatasetDTO } from './dataset-dto';
+import type { ModelDTO } from './model-dto';
 
 /**
  * Generated from ai.giskard.web.dto.ml.InspectionDTO
@@ -8,4 +8,5 @@ export interface InspectionDTO {
     dataset: DatasetDTO;
     id: number;
     model: ModelDTO;
+    name?: string;
 }
