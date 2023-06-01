f8b05c563f0cc834e8177ddbca189b2290e601fb
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-06-01 00:11:22 +0000

    task/gsk-980 validation inside dataset constructor, infer_column_types by default (#965)
    * validation inside dataset constructor, infere_column_types by default
    
    * fixed validate_column_categorization
    
    * fixed wrap_dataset
    
    * fixed error in fixture
    
    * added missing index_col=0 to enron data fixture
    
    * moved target warning to validate_target
    
    * enhanced validate_column_types
    
    * fixed torch unit-test
    
    * fixed torch unit-test
    
    * fixed test_upload_df unit-test
    
    * fixed rest of unit-tests
    
    * fixed some linter issues, and restored tests/test_statistical.py
    
    * fixed rest of unit-tests
    
    * fixed linter issues
    
    * removed noisy warning due to conservative category_threshold

--- python-client/tests/test_upload.py
+++ python-client/tests/test_upload.py
@@ -22,13 +22,19 @@ def test_upload_df(diabetes_dataset: Dataset, diabetes_dataset_with_target: Data
         match_url_patterns(mr.request_history, datasets_url_pattern)
 
         with pytest.raises(Exception) as e:
-            diabetes_dataset.upload(client, "test-project")
-        assert e.match("target column is not present in the dataset")
+            Dataset(df=diabetes_dataset.df,
+                    column_types=diabetes_dataset.column_types, target=diabetes_dataset_with_target.target)
+        assert e.match(
+            "Invalid target parameter: 'target' column is not present in the dataset "
+            "with columns: \['age', 'sex', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6'\]")  # noqa
 
         with pytest.raises(Exception) as e:
             diabetes_dataset.column_types = {"test": "test"}
-            diabetes_dataset.upload(client, "test-project")
-        assert e.match("target column is not present in the dataset")
+            Dataset(df=diabetes_dataset.df,
+                    column_types=diabetes_dataset.column_types, target=diabetes_dataset_with_target.target)
+        assert e.match(
+            "Invalid target parameter: 'target' column is not present in the dataset "
+            "with columns: \['age', 'sex', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6'\]")  # noqa
 
 
 def _test_upload_model(model: SKLearnModel, ds: Dataset):
@@ -72,7 +78,8 @@ def _test_upload_model_exceptions(model: SKLearnModel, ds: Dataset):
                     name=model_name,
                     classification_labels=[0, 1],
                 ).upload(client, "test-project", ds)
-            assert e.match("Values .* in .* column are not declared in classification_labels parameter .* of the model: uploaded model") # noqa
+            assert e.match(
+                "Values .* in .* column are not declared in classification_labels parameter .* of the model: uploaded model")  # noqa
 
 
 @pytest.mark.parametrize(
