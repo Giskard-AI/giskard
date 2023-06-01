5479e6c98b6be487ccf7db90239bfc30cbdb384a
Andrey Avtomonov <andreybavt@gmail.com>
2023-06-02 01:40:00 +0200

    fix empty dataset prediction

--- python-client/giskard/models/base/__init__.py
+++ python-client/giskard/models/base/__init__.py
@@ -262,6 +262,8 @@ class BaseModel(ABC):
               The `probabilities` field will contain the predicted probabilities for the predicted class label.
               The `all_predictions` field will contain the predicted probabilities for all class labels for each example in the input dataset.
         """
+        if not len(dataset.df):
+            return ModelPredictionResults()
         timer = Timer()
 
         if get_cache_enabled():
