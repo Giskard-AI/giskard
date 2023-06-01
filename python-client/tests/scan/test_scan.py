58df4fab5318789c62e07b6149b7bbc18c445c53
Matteo Dora <matteo@giskard.ai>
2023-06-01 00:11:22 +0000

    Update test

--- python-client/tests/scan/test_scan.py
+++
@@ -1,5 +0,0 @@
-from giskard.scanner.performance.performance_scan import PerformanceScan
-
-
-def test_scan(german_credit_data, german_credit_model):
-    assert PerformanceScan(metrics=["f1"]).run(german_credit_model, german_credit_data)
