593496e586e60acbdd0e43da7e783d229260648c
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 00:11:22 +0000

    Improved cache and fixed shuffle mode

--- frontend/src/generated-sources/ai/giskard/web/dto/dataset-metadata-dto.ts
+++ frontend/src/generated-sources/ai/giskard/web/dto/dataset-metadata-dto.ts
@@ -4,9 +4,11 @@ import type {ColumnType} from './../../domain/column-type';
  * Generated from ai.giskard.web.dto.DatasetMetadataDTO
  */
 export interface DatasetMetadataDTO {
+    categoryFeatures: {[key: string]: string[]};
     columnDtypes?: {[key: string]: string} | null;
     columnTypes: {[key: string]: ColumnType};
     id: string;
     name: string;
+    numberOfRows: number;
     target?: string | null;
 }
--- python-client/giskard/core/dataset_caching.py
+++ python-client/giskard/core/dataset_caching.py
@@ -1,4 +1,4 @@
-import hashlib
+from xxhash import xxh3_128_hexdigest
 
 from giskard.datasets.base import Dataset, GISKARD_HASH_COLUMN
 
@@ -9,9 +9,9 @@ def generate_row_hashes(dataset: Dataset):
     if GISKARD_HASH_COLUMN in df:
         unknown_values = list(df[GISKARD_HASH_COLUMN].isna())
         df.loc[unknown_values, GISKARD_HASH_COLUMN] = list(
-            map(lambda row: hashlib.md5(f"({', '.join(map(lambda x: str(x), row))}".encode('utf-8')).hexdigest(),
+            map(lambda row: xxh3_128_hexdigest(f"({', '.join(map(lambda x: str(x), row))}".encode('utf-8')),
                 df.loc[unknown_values][dataset.columns].values))
     else:
         df[GISKARD_HASH_COLUMN] = list(
-            map(lambda row: hashlib.md5(f"({', '.join(map(lambda x: str(x), row))}".encode('utf-8')).hexdigest(),
+            map(lambda row: xxh3_128_hexdigest(f"({', '.join(map(lambda x: str(x), row))}".encode('utf-8')),
                 df[dataset.columns].values))
