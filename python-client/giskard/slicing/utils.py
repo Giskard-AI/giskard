3da67673a9f91a8abf64c31693cc17c963facbdd
Matteo Dora <matteo@giskard.ai>
2023-06-01 02:11:23 +0200

    Synchronize numerical slicer in text slicer

--- python-client/giskard/slicing/utils.py
+++ python-client/giskard/slicing/utils.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 from .slice import GreaterThan, LowerThan
+from .tree_slicer import DecisionTreeSlicer
+from .opt_slicer import OptSlicer
+from .category_slicer import CategorySlicer
+from .multiscale_slicer import MultiscaleSlicer
+from .bruteforce_slicer import BruteForceSlicer
 
 
 def get_slice_feature_interval(data_slice, feature, min_value=None, max_value=None):
@@ -20,3 +25,16 @@ def get_slice_feature_interval(data_slice, feature, min_value=None, max_value=No
 
 def get_slice_feature_intervals(slices, *args, **kwargs):
     return np.array([get_slice_feature_interval(s, *args, **kwargs) for s in slices])
+
+
+def get_slicer(slicer_name, dataset, target):
+    if slicer_name == "optimized":
+        return OptSlicer(dataset, target=target)
+    if slicer_name == "tree":
+        return DecisionTreeSlicer(dataset, target=target)
+    if slicer_name == "multiscale":
+        return MultiscaleSlicer(dataset, target=target)
+    if slicer_name == "bruteforce":
+        return BruteForceSlicer(dataset, target=target)
+
+    raise ValueError(f"Invalid slicer `{slicer_name}`.")
