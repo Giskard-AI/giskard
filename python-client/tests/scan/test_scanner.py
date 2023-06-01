afedfe3b99e694bc7e3f3518135611985d65f68a
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-06-01 20:59:24 +0200

    unit test

--- python-client/tests/scan/test_scanner.py
+++ python-client/tests/scan/test_scanner.py
@@ -30,10 +30,18 @@ def test_scanner_raises_exception_if_no_detectors_available(german_credit_data, 
     with pytest.raises(RuntimeError):
         scanner.analyze(german_credit_model, german_credit_data)
 
-def test_scanner_without_dataset():
+
+def test_classification_model_no_dataset(german_credit_model):
+    scanner = Scanner()
+    with pytest.raises(ValueError):
+        scanner.analyze(german_credit_model)
+
+
+def test_generative_model_no_dataset():
     from langchain import LLMChain, PromptTemplate
     from langchain.chat_models import ChatOpenAI
     from giskard import Model
+    from unittest.mock import patch
 
     llm = ChatOpenAI(model="gpt-3.5-turbo")
     prompt = PromptTemplate(template="{input}", input_variables=["input"])
@@ -41,4 +49,32 @@ def test_scanner_without_dataset():
 
     model = Model(chain, model_type='generative')
     scanner = Scanner()
-    assert scanner.analyze(model)
+
+
+    with patch('Scanner().analyze(Model()).load_default_dataset()'):
+        scanner.analyze(model)
+
+
+def test_generative_model_dataset():
+    from langchain import LLMChain, PromptTemplate
+    from langchain.chat_models import ChatOpenAI
+    from giskard import Model, Dataset
+    import pandas as pd
+
+    llm = ChatOpenAI(model="gpt-3.5-turbo")
+    prompt = PromptTemplate(template="{input}", input_variables=["input"])
+    chain = LLMChain(prompt=prompt, llm=llm)
+
+    model = Model(chain, model_type='generative')
+    dataset = Dataset(
+        pd.DataFrame(
+            {
+                "instruct": ["Paraphrase this", "Answer this question"],
+                "question": ["Who is the mayor of Rome?", "How many bridges are there in Paris?"],
+            }
+        ),
+        column_types={"instruct": "text", "question": "text"},
+    )
+
+    scanner = Scanner()
+    assert scanner.analyze(model, dataset)
