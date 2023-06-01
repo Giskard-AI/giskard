eb79adc7a9771b754fbba41530765e77c0fa9cd4
Rabah <rabah.khalek@gmail.com>
2023-06-01 02:11:23 +0200

    updated tensorflow init with new structure

--- python-client/giskard/models/tensorflow/__init__.py
+++ python-client/giskard/models/tensorflow/__init__.py
@@ -4,60 +4,41 @@ import yaml
 import numpy as np
 
 from giskard.core.core import SupportedModelTypes
-from giskard.core.model import Model
+from giskard.core.model import MLFlowBasedModel
 from giskard.path_utils import get_size
 
 
-class TensorFlowModel(Model):
-    loader_module = 'giskard.models.tensorflow'
-    loader_class = 'TensorFlowModel'
+class TensorFlowModel(MLFlowBasedModel):
     def __init__(self,
                  clf,
-                 model_type: SupportedModelTypes,
+                 model_type: Union[SupportedModelTypes, str],
                  name: str = None,
                  data_preprocessing_function=None,
+                 model_postprocessing_function=None,
                  feature_names=None,
                  classification_threshold=0.5,
                  classification_labels=None) -> None:
 
-        super().__init__(clf,
-                         model_type,
-                         name,
-                         data_preprocessing_function,
-                         feature_names,
-                         classification_threshold,
-                         classification_labels)
+        super().__init__(clf=clf,
+                         model_type=model_type,
+                         name=name,
+                         data_preprocessing_function=data_preprocessing_function,
+                         model_postprocessing_function=model_postprocessing_function,
+                         feature_names=feature_names,
+                         classification_threshold=classification_threshold,
+                         classification_labels=classification_labels)
+
 
     @classmethod
     def read_model_from_local_dir(cls, local_path):
         return mlflow.tensorflow.load_model(local_path)
 
-
-    def save_to_local_dir(self, local_path):
-        info = self._new_mlflow_model_meta()
-
+    def save_with_mflow(self, local_path, mlflow_meta: mlflow.models.Model):
         mlflow.tensorflow.save_model(self.clf,
                                   path=local_path,
-                                  mlflow_model=info)
-        with open(Path(local_path) / 'giskard-model-meta.yaml', 'w') as f:
-            yaml.dump(
-                {
-                    "language_version": info.flavors['python_function']['python_version'],
-                    "loader_module": self.meta.loader_module,
-                    "loader_class": self.meta.loader_class,
-                    "language": "PYTHON",
-                    "model_type": self.meta.model_type.name.upper(),
-                    "threshold": self.meta.classification_threshold,
-                    "feature_names": self.meta.feature_names,
-                    "classification_labels": self.meta.classification_labels,
-                    "id": info.model_uuid,
-                    "name": self.meta.name,
-                    "size": get_size(local_path),
-                }, f, default_flow_style=False)
+                                  mlflow_model=mlflow_meta)
 
-        return info
-
-    def _raw_predict(self, data):
+    def clf_predict(self, data):
         if self.is_regression:
             return self.clf.predict(data)
         else:
