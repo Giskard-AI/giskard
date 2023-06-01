d1ad97a69ccdc10c3e2f444d9b556acf90fe6e40
Henrique Chaves <henriquechavesmm@gmail.com>
2023-06-01 00:11:22 +0000

    Update code block component of SuiteTestInfoModal

--- frontend/src/views/main/project/modals/SuiteTestInfoModal.vue
+++ frontend/src/views/main/project/modals/SuiteTestInfoModal.vue
@@ -1,98 +1,73 @@
 <template>
-  <vue-final-modal
-      v-slot="{ close }"
-      v-bind="$attrs"
-      classes="modal-container"
-      content-class="modal-content"
-      v-on="$listeners"
-  >
-    <div class="text-center">
-        <v-card class="modal-card">
-            <v-card-title>
-                {{ suiteTest.test.displayName ?? suiteTest.test.name }}
-            </v-card-title>
-            <v-card-text class="card-content">
-                <pre class="test-doc caption pt-5">{{ suiteTest.test.doc }}</pre>
-                <div class="d-flex align-center">
-                    <p class="text-h6 pt-4">Inputs</p>
-                </div>
-                <TestInputListSelector v-if="suiteTest.test.args"
-                                       :test-inputs="suiteTest.functionInputs"
-                                       :test="testFunctionsByUuid[suiteTest.testUuid]"
-                                       :model-value="editedInputs"
-                                       :project-id="projectId"
-                                     :inputs="inputType"
-                                     @invalid="i => invalid = i"
-                                     @result="v => result = v"
-              />
-              <v-row>
-                  <v-col>
-                      <v-expansion-panels flat @change="resizeEditor">
-                          <v-expansion-panel>
-                              <v-expansion-panel-header class="pa-0">Code</v-expansion-panel-header>
-                              <v-expansion-panel-content class="pa-0">
-                                  <MonacoEditor
-                                          ref="editor"
-                                          v-model='suiteTest.test.code'
-                                          class='editor'
-                                          language='python'
-                                          style="height: 300px; min-height: 300px"
-                                          :options="monacoOptions"
-                    />
-                  </v-expansion-panel-content>
-                </v-expansion-panel>
-              </v-expansion-panels>
-            </v-col>
-          </v-row>
-        </v-card-text>
-          <v-card-actions>
-              <v-spacer/>
-              <v-btn color="error" text @click="() => removeTest(close)" class="pr-2">
-                  <v-icon>delete</v-icon>
-                  Remove test
-              </v-btn>
-              <v-btn color="primary" @click="() => saveEditedInputs(close)" :disabled="invalid">
-                  <v-icon>save</v-icon>
-                  Save
-              </v-btn>
-          </v-card-actions>
-      </v-card>
-    </div>
-  </vue-final-modal>
+    <vue-final-modal v-slot="{ close }" v-bind="$attrs" classes="modal-container" content-class="modal-content" v-on="$listeners">
+        <div class="text-center">
+            <v-card class="modal-card">
+                <v-card-title>
+                    {{ suiteTest.test.displayName ?? suiteTest.test.name }}
+                </v-card-title>
+                <v-card-text class="card-content">
+                    <pre class="test-doc caption pt-5">{{ suiteTest.test.doc }}</pre>
+                    <div class="d-flex align-center">
+                        <p class="text-h6 pt-4">Inputs</p>
+                    </div>
+                    <TestInputListSelector v-if="suiteTest.test.args" :test-inputs="suiteTest.functionInputs" :test="testFunctionsByUuid[suiteTest.testUuid]" :model-value="editedInputs" :project-id="projectId" :inputs="inputType" @invalid="i => invalid = i" @result="v => result = v" />
+                    <v-row>
+                        <v-col>
+                            <v-expansion-panels flat>
+                                <v-expansion-panel>
+                                    <v-expansion-panel-header class="pa-0">Code</v-expansion-panel-header>
+                                    <v-expansion-panel-content class="pa-0">
+                                        <CodeSnippet :codeContent="suiteTest.test.code"></CodeSnippet>
+                                    </v-expansion-panel-content>
+                                </v-expansion-panel>
+                            </v-expansion-panels>
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn text @click="close">
+                        Close
+                    </v-btn>
+                    <v-spacer />
+                    <v-btn color="error" text @click="() => removeTest(close)" class="pr-2">
+                        <v-icon>delete</v-icon>
+                        Remove test
+                    </v-btn>
+                    <v-btn color="primary" @click="() => saveEditedInputs(close)" :disabled="invalid">
+                        <v-icon>save</v-icon>
+                        Save
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </div>
+    </vue-final-modal>
 </template>
 
 <script setup lang="ts">
 
-import {FunctionInputDTO, SuiteTestDTO} from '@/generated-sources';
-import {computed, inject, onMounted, ref} from 'vue';
-import _, {chain} from 'lodash';
-import {storeToRefs} from 'pinia';
-import {useTestSuiteStore} from '@/stores/test-suite';
-import MonacoEditor from 'vue-monaco';
-import {api} from '@/api';
-import {editor} from 'monaco-editor';
+import { FunctionInputDTO, SuiteTestDTO } from '@/generated-sources';
+import { computed, inject, onMounted, ref } from 'vue';
+import _, { chain } from 'lodash';
+import { storeToRefs } from 'pinia';
+import { useTestSuiteStore } from '@/stores/test-suite';
+import { api } from '@/api';
 import TestInputListSelector from "@/components/TestInputListSelector.vue";
-import {useCatalogStore} from "@/stores/catalog";
-import {$vfm} from "vue-final-modal";
+import { useCatalogStore } from "@/stores/catalog";
+import { $vfm } from "vue-final-modal";
 import ConfirmModal from "@/views/main/project/modals/ConfirmModal.vue";
-import IEditorOptions = editor.IEditorOptions;
-
-const l = MonacoEditor;
-const monacoOptions: IEditorOptions = inject('monacoOptions');
-monacoOptions.readOnly = true;
+import CodeSnippet from '@/components/CodeSnippet.vue';
 
 const props = defineProps<{
     suiteTest: SuiteTestDTO
 }>();
 
-const {models, datasets, projectId, suite, inputs} = storeToRefs(useTestSuiteStore());
-const {reload} = useTestSuiteStore();
+const { models, datasets, projectId, suite, inputs } = storeToRefs(useTestSuiteStore());
+const { reload } = useTestSuiteStore();
 
 const editedInputs = ref<{ [input: string]: FunctionInputDTO }>({});
 const result = ref<{ [input: string]: FunctionInputDTO }>({});
-const editor = ref(null)
 
-const {testFunctionsByUuid} = storeToRefs(useCatalogStore())
+const { testFunctionsByUuid } = storeToRefs(useCatalogStore())
 
 const sortedArguments = computed(() => {
     return _.sortBy(_.values(props.suiteTest.test.args), value => {
@@ -102,12 +77,6 @@ const sortedArguments = computed(() => {
 
 
 const invalid = ref(false);
-
-function resizeEditor() {
-    setTimeout(() => {
-        editor.value.editor.layout();
-    })
-}
 
 onMounted(() => {
     editedInputs.value = Object.values(props.suiteTest.functionInputs)
@@ -180,5 +149,4 @@ async function removeTest(close) {
     flex-grow: 1;
     overflow: auto;
 }
-
 </style>
