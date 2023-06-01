4609e3d96f3dff4fd9aafd951b34bd0650778f0d
Henrique Chaves <henriquechavesmm@gmail.com>
2023-06-01 02:11:23 +0200

    Add QuickStartStepper to Project component

---
+++ frontend/src/components/QuickStartSteepper.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const step = ref(1)
+
+const emit = defineEmits(['close'])
+
+const close = () => {
+  emit('close')
+  step.value = 1
+}
+</script>
+
+<template>
+  <v-stepper v-model="step" flat>
+    <v-stepper-header id="stepper-header">
+      <v-stepper-step :complete="step > 1" step="1">Name of step 1</v-stepper-step>
+      <v-divider></v-divider>
+      <v-stepper-step :complete="step > 2" step="2">Name of step 2</v-stepper-step>
+      <v-divider></v-divider>
+      <v-stepper-step step="3">Name of step 3</v-stepper-step>
+    </v-stepper-header>
+
+    <v-stepper-items>
+      <v-stepper-content step="1">
+        <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+        <v-btn color="primary" @click="step = 2">Continue</v-btn>
+        <v-btn class="ml-2" @click="close" flat>Cancel</v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="2">
+        <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+        <v-btn color="primary" @click="step = 3">Continue</v-btn>
+        <v-btn class="ml-2" @click="close" flat>Cancel</v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="3">
+        <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+        <v-btn color="primary" @click="close">Close</v-btn>
+        <v-btn class="ml-2" @click="step = 1" flat>Restart</v-btn>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
+</template>
+
+<style scoped>
+#stepper-header {
+  background-color: transparent;
+  box-shadow: none !important;
+}
+
+.v-btn {
+  margin-bottom: 0.5rem;
+}
+</style>
--- frontend/src/stores/project-artifacts.ts
+++ frontend/src/stores/project-artifacts.ts
@@ -1,61 +1,63 @@
-import { DatasetDTO, ModelDTO } from "@/generated-sources";
-import { defineStore } from "pinia";
-import { useMainStore } from "@/stores/main";
-import { api } from "@/api";
+import { DatasetDTO, ModelDTO } from '@/generated-sources';
+import { defineStore } from 'pinia';
+import { useMainStore } from '@/stores/main';
+import { api } from '@/api';
 
 interface State {
-  projectId: number | null,
-    datasets: DatasetDTO[],
-    models: ModelDTO[]
+  projectId: number | null;
+  datasets: DatasetDTO[];
+  models: ModelDTO[];
 }
 
 export const useProjectArtifactsStore = defineStore('projectArtifacts', {
-    state: (): State => ({
-        projectId: null,
-        datasets: [],
-        models: []
-    }),
-    getters: {},
-    actions: {
-        async setProjectId(projectId: number) {
-            this.projectId = projectId;
-            await this.loadProjectArtifacts();
-        },
-        async loadDatasets() {
-          if (this.projectId === null) return;
-          this.datasets = await api.getProjectDatasets(this.projectId!);
-          this.datasets = this.datasets.sort((a, b) => new Date(a.createdDate) < new Date(b.createdDate) ? 1 : -1);
-        },
-        async loadModels() {
-          if (this.projectId === null) return;
-          this.models = await api.getProjectModels(this.projectId!);
-          this.models = this.models.sort((a, b) => new Date(a.createdDate) < new Date(b.createdDate) ? 1 : -1);
-        },
-        async loadProjectArtifacts() {
-            if (this.projectId === null) return;
-            const mainStore = useMainStore();
-            const loadingNotification = {content: 'Loading project artifacts', showProgress: true};
-            try {
-              mainStore.addNotification(loadingNotification);
-              await this.loadDatasets();
-              await this.loadModels();
-              mainStore.removeNotification(loadingNotification);
-          } catch (error) {
-              mainStore.removeNotification(loadingNotification);
-              mainStore.addNotification({content: `Error: ${error.message}`, color: 'error'});
-              await mainStore.checkApiError(error);
-          }
-        },
-        updateDataset(newDataset: DatasetDTO) {
-          const idx = this.datasets.findIndex(dataset => dataset.id === newDataset.id);
-          this.datasets[idx] = newDataset;
-          this.datasets = [...this.datasets];
-        },
-        updateModel(newModel: ModelDTO) {
-          const idx = this.models.findIndex(model => model.id === newModel.id);
-          this.models[idx] = newModel;
-          this.models = [...this.models];
+  state: (): State => ({
+    projectId: null,
+    datasets: [],
+    models: [],
+  }),
+  getters: {},
+  actions: {
+    async setProjectId(projectId: number, displayNotification: boolean = true) {
+      this.projectId = projectId;
+      await this.loadProjectArtifacts(displayNotification);
+    },
+    async loadDatasets() {
+      if (this.projectId === null) return;
+      this.datasets = await api.getProjectDatasets(this.projectId!);
+      this.datasets = this.datasets.sort((a, b) => (new Date(a.createdDate) < new Date(b.createdDate) ? 1 : -1));
+    },
+    async loadModels() {
+      if (this.projectId === null) return;
+      this.models = await api.getProjectModels(this.projectId!);
+      this.models = this.models.sort((a, b) => (new Date(a.createdDate) < new Date(b.createdDate) ? 1 : -1));
+    },
+    async loadProjectArtifacts(displayNotification: boolean = true) {
+      if (this.projectId === null) return;
+      const mainStore = useMainStore();
+
+      if (displayNotification) {
+        const loadingNotification = { content: 'Loading project artifacts', showProgress: true };
+        try {
+          mainStore.addNotification(loadingNotification);
+          await this.loadDatasets();
+          await this.loadModels();
+          mainStore.removeNotification(loadingNotification);
+        } catch (error) {
+          mainStore.removeNotification(loadingNotification);
+          mainStore.addNotification({ content: `Error: ${error.message}`, color: 'error' });
+          await mainStore.checkApiError(error);
         }
-    }
+      }
+    },
+    updateDataset(newDataset: DatasetDTO) {
+      const idx = this.datasets.findIndex(dataset => dataset.id === newDataset.id);
+      this.datasets[idx] = newDataset;
+      this.datasets = [...this.datasets];
+    },
+    updateModel(newModel: ModelDTO) {
+      const idx = this.models.findIndex(model => model.id === newModel.id);
+      this.models[idx] = newModel;
+      this.models = [...this.models];
+    },
+  },
 });
-            
