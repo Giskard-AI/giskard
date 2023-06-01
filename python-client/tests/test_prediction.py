1a81eba1cb242d4e549ce60bdc453200bcd7d1ce
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-06-01 02:11:23 +0200

    Fix ouput

--- python-client/tests/test_prediction.py
+++ python-client/tests/test_prediction.py
@@ -1,16 +1,22 @@
+from giskard.scanner.prediction.data_leakage import DataLeakageDetector
 from giskard.scanner.prediction.overconfidence import OverconfidenceBiasDetector
 from giskard.scanner.prediction.borderline import BorderlineBiasDetector
 from giskard.scanner.prediction.metrics import OverconfidenceMAE, BorderlineMAE
+from giskard.scanner.prediction.stochasticity import StochasticityDetector
 
 
-def test_prediction_bias_detector(german_credit_model, german_credit_data):
-    res = OverconfidenceBiasDetector(metrics=[OverconfidenceMAE()]).run(german_credit_model, german_credit_data)
-    res2 = BorderlineBiasDetector(metrics=[BorderlineMAE()]).run(german_credit_model, german_credit_data)
+def test_overconfidence_issue(german_credit_model, german_credit_data):
+    overconfidence_issues = OverconfidenceBiasDetector(metrics=[OverconfidenceMAE()]).run(german_credit_model, german_credit_data)
+    assert overconfidence_issues
 
-    assert res
-    assert res2
+def test_borderline_issue(german_credit_model, german_credit_data):
+    borderline_issues = BorderlineBiasDetector(metrics=[BorderlineMAE()]).run(german_credit_model, german_credit_data)
+    assert borderline_issues
 
-# from giskard.scanner.performance.model_bias_detector import ModelBiasDetector
-# def test_data_leakage(german_credit_model, german_credit_data):
-#     mbd = ModelBiasDetector(metrics=["dataleakage"]).run(german_credit_model, german_credit_data)
-#     print(mbd)
+def test_dataleakage_issue(german_credit_model, german_credit_data):
+    dataleakage_issues = DataLeakageDetector().run(german_credit_model, german_credit_data)
+    assert dataleakage_issues is None
+
+def test_stochasticity_issue(german_credit_model, german_credit_data):
+    stochasticity_issues = StochasticityDetector().run(german_credit_model, german_credit_data)
+    assert stochasticity_issues is None
