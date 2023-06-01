71b69faa1d1441fbad9fde78ef268a378e637343
Rabah <rabah.khalek@gmail.com>
2023-06-01 00:11:22 +0000

    added docs readme

--- python-client/giskard/datasets/__init__.py
+++ python-client/giskard/datasets/__init__.py
@@ -15,18 +15,26 @@ def wrap_dataset(dataset: pd.DataFrame,
     A function that wraps a Pandas DataFrame into a Giskard Dataset object, with optional validation of input arguments.
 
     Args:
-        dataset (pd.DataFrame): A Pandas DataFrame containing the data to be wrapped.
-        name (Optional[str]): A string representing the name of the dataset (default None).
-        target (Optional[str]): A string representing the name of the target variable (default None).
-        cat_columns (Optional[List[str]]): A list of strings representing the names of categorical columns (default None).
-        infer_column_types (Optional[bool]): A boolean indicating whether to infer column types automatically (default False).
-        column_types (Optional[Dict[str, str]]): A dictionary representing column names and their types (default None).
+        dataset (pd.DataFrame):
+            A Pandas DataFrame containing the data to be wrapped.
+        name (Optional[str]):
+            A string representing the name of the dataset (default None).
+        target (Optional[str]):
+            A string representing the name of the target variable (default None).
+        cat_columns (Optional[List[str]]):
+            A list of strings representing the names of categorical columns (default None).
+        infer_column_types (Optional[bool]):
+            A boolean indicating whether to infer column types automatically (default False).
+        column_types (Optional[Dict[str, str]]):
+            A dictionary representing column names and their types (default None).
 
     Returns:
-        Dataset: A Giskard Dataset object that wraps the input Pandas DataFrame.
+        Dataset:
+            A Giskard Dataset object that wraps the input Pandas DataFrame.
 
     Raises:
-        TypeError: If the input dataset is not a Pandas DataFrame or if cat_columns is not a list of strings.
+        TypeError:
+            If the input dataset is not a Pandas DataFrame or if cat_columns is not a list of strings.
     """
 
     print("Your 'pandas.DataFrame' dataset is successfully wrapped by Giskard's 'Dataset' wrapper class.")
