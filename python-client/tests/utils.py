67f1bf6fd74fda2061bdf8c62d83cf045d833e1c
Andrey Avtomonov <andreybavt@gmail.com>
2023-06-01 02:11:23 +0200

    remove httpretty

--- python-client/tests/utils.py
+++ python-client/tests/utils.py
@@ -12,8 +12,8 @@ def match_model_id(my_model_id):
     assert re.match("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", str(my_model_id))
 
 
-def match_url_patterns(httpretty_requests, url_pattern):
-    artifact_requests = [i for i in httpretty_requests if url_pattern.match(i.url)]
+def match_url_patterns(last_requests, url_pattern):
+    artifact_requests = [i for i in last_requests if url_pattern.match(i.url)]
     assert len(artifact_requests) > 0
     for req in artifact_requests:
         assert int(req.headers.get("Content-Length")) > 0
