4de1cad837d72eb7b4fb31b83baee325b60095fb
Andrey Avtomonov <andreybavt@gmail.com>
2023-06-01 02:11:23 +0200

    autoformat to make flake8 happy

--- python-client/tests/tensorflow/test_text_classification_1d_output.py
+++ python-client/tests/tensorflow/test_text_classification_1d_output.py
@@ -13,53 +13,47 @@ import tests.utils
 
 data_url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 
-dataset = tf.keras.utils.get_file("aclImdb_v1", data_url,
-                                  untar=True, cache_dir='.',
-                                  cache_subdir='')
+dataset = tf.keras.utils.get_file("aclImdb_v1", data_url, untar=True, cache_dir=".", cache_subdir="")
+
 
 @httpretty.activate(verbose=True, allow_net_connect=False)
 def test_text_classification():
 
+    dataset_dir = os.path.join(os.path.dirname(dataset), "aclImdb")
+    train_dir = os.path.join(dataset_dir, "train")
 
-    dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
-    train_dir = os.path.join(dataset_dir, 'train')
+    sample_file = os.path.join(train_dir, "pos/1181_9.txt")
 
-    sample_file = os.path.join(train_dir, 'pos/1181_9.txt')
-
-    remove_dir = os.path.join(train_dir, 'unsup')
+    remove_dir = os.path.join(train_dir, "unsup")
     shutil.rmtree(remove_dir)
 
     batch_size = 32
     seed = 42
 
-    raw_test_ds = tf.keras.utils.text_dataset_from_directory(
-        'aclImdb/test',
-        batch_size=batch_size)
+    raw_test_ds = tf.keras.utils.text_dataset_from_directory("aclImdb/test", batch_size=batch_size)
 
-    test_dataset = {'Review':[], 'Label':[]}
+    test_dataset = {"Review": [], "Label": []}
     for text_batch, label_batch in raw_test_ds.take(782):
-      for i in range(8):
-        test_dataset['Review'].append(text_batch.numpy()[i])
-        test_dataset['Label'].append(label_batch.numpy()[i])
-      test_df = pd.DataFrame.from_dict(test_dataset)
+        for i in range(8):
+            test_dataset["Review"].append(text_batch.numpy()[i])
+            test_dataset["Label"].append(label_batch.numpy()[i])
+        test_df = pd.DataFrame.from_dict(test_dataset)
 
     max_features = 10000
     sequence_length = 250
     vectorize_layer = tf.keras.layers.TextVectorization(
-        standardize='lower_and_strip_punctuation',
+        standardize="lower_and_strip_punctuation",
         max_tokens=max_features,
-        output_mode='int',
-        output_sequence_length=sequence_length)
+        output_mode="int",
+        output_sequence_length=sequence_length,
+    )
 
     batch_size = 32
     seed = 42
 
     raw_train_ds = tf.keras.utils.text_dataset_from_directory(
-        'aclImdb/train',
-        batch_size=batch_size,
-        validation_split=0.2,
-        subset='training',
-        seed=seed)
+        "aclImdb/train", batch_size=batch_size, validation_split=0.2, subset="training", seed=seed
+    )
 
     # Make a text-only dataset (without labels), then call adapt
     train_text = raw_train_ds.map(lambda x, y: x)
@@ -67,44 +61,46 @@ def test_text_classification():
 
     embedding_dim = 16
 
-    model = tf.keras.Sequential([
-      layers.Embedding(max_features + 1, embedding_dim),
-      layers.Dropout(0.2),
-      layers.GlobalAveragePooling1D(),
-      layers.Dropout(0.2),
-      layers.Dense(1)
-      ])
+    model = tf.keras.Sequential(
+        [
+            layers.Embedding(max_features + 1, embedding_dim),
+            layers.Dropout(0.2),
+            layers.GlobalAveragePooling1D(),
+            layers.Dropout(0.2),
+            layers.Dense(1),
+        ]
+    )
 
-    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-                  optimizer='adam',
-                  metrics=['accuracy'])
+    model.compile(
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False), optimizer="adam", metrics=["accuracy"]
+    )
 
-    export_model = tf.keras.Sequential([
-      vectorize_layer,
-      model
-    ])
+    export_model = tf.keras.Sequential([vectorize_layer, model])
 
     export_model.compile(
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False), optimizer="adam", metrics=['accuracy']
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False), optimizer="adam", metrics=["accuracy"]
     )
 
     def FromPandastoTFModelInput(test_df):
-        test_dataset= test_df.squeeze(axis=1)
+        test_dataset = test_df.squeeze(axis=1)
         test_dataset = list(test_dataset)
         return test_dataset
 
-    my_model = TensorFlowModel(name="Tensorflow_text_classification",
-                            clf=export_model,
-                            feature_names=['Review'],
-                            model_type="classification",
-                            classification_labels=['0', '1'],
-                            data_preprocessing_function=FromPandastoTFModelInput)
+    my_model = TensorFlowModel(
+        name="Tensorflow_text_classification",
+        clf=export_model,
+        feature_names=["Review"],
+        model_type="classification",
+        classification_labels=["0", "1"],
+        data_preprocessing_function=FromPandastoTFModelInput,
+    )
 
     # defining the giskard dataset
     my_test_dataset = Dataset(test_df.head(), name="test dataset", target="Label")
 
     artifact_url_pattern = re.compile(
-        "http://giskard-host:12345/api/v2/artifacts/test-project/models/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.*")
+        "http://giskard-host:12345/api/v2/artifacts/test-project/models/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.*"
+    )
     models_url_pattern = re.compile("http://giskard-host:12345/api/v2/project/test-project/models")
     settings_url_pattern = re.compile("http://giskard-host:12345/api/v2/settings")
 
@@ -115,7 +111,7 @@ def test_text_classification():
     url = "http://giskard-host:12345"
     token = "SECRET_TOKEN"
     client = GiskardClient(url, token)
-    my_model.upload(client, 'test-project', my_test_dataset)
+    my_model.upload(client, "test-project", my_test_dataset)
 
     tests.utils.match_model_id(my_model.id)
     tests.utils.match_url_patterns(httpretty.latest_requests(), artifact_url_pattern)
--- python-client/tests/tensorflow/test_text_classification_tfhub.py
+++ python-client/tests/tensorflow/test_text_classification_tfhub.py
@@ -11,19 +11,19 @@ import requests_mock
 import re
 import tests.utils
 
-#tfhub_handle_encoder = 'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1'
-#tfhub_handle_preprocess = 'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
+# tfhub_handle_encoder = 'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1'
+# tfhub_handle_preprocess = 'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
 tfhub_handle_preprocess = hub.load("https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
-tfhub_handle_preprocess = hub.KerasLayer(
-    "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
+tfhub_handle_preprocess = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
 tfhub_handle_encoder = hub.KerasLayer(
-    "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-128_A-2/2",
-    trainable=True)
+    "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-128_A-2/2", trainable=True
+)
+
 
 def test_text_classification_tfhub():
 
-    column_types={
-        'Target': "category",
+    column_types = {
+        "Target": "category",
         "Subject": "text",
         "Content": "text",
         "Week_day": "category",
@@ -33,52 +33,60 @@ def test_text_classification_tfhub():
         "Year": "numeric",
     }
 
-    data_filtered = pd.read_csv('tests/tensorflow/test_text_classification_tfhub.csv').dropna(axis=0)
+    data_filtered = pd.read_csv("tests/tensorflow/test_text_classification_tfhub.csv").dropna(axis=0)
 
     Y = data_filtered["Target"]
     X = data_filtered.drop(columns=["Target"])
-    X_train,X_test,Y_train,Y_test = model_selection.train_test_split(X, Y,test_size=0.20, random_state = 30, stratify = Y)
+    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(
+        X, Y, test_size=0.20, random_state=30, stratify=Y
+    )
 
-    #=======================================================================================
+    # =======================================================================================
 
-    classification_labels_mapping = {'REGULATION': 0,'INTERNAL': 1,'CALIFORNIA CRISIS': 2,'INFLUENCE': 3}
+    classification_labels_mapping = {"REGULATION": 0, "INTERNAL": 1, "CALIFORNIA CRISIS": 2, "INFLUENCE": 3}
 
-    Y = data_filtered['Target'].map(classification_labels_mapping)
-    X = data_filtered['Content']
-    X_train,X_test,Y_train,Y_test = model_selection.train_test_split(X, Y,test_size=0.20, random_state = 30, stratify = Y)
+    Y = data_filtered["Target"].map(classification_labels_mapping)
+    X = data_filtered["Content"]
+    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(
+        X, Y, test_size=0.20, random_state=30, stratify=Y
+    )
 
     test_df = pd.DataFrame(list(zip(list(X_test), list(Y_test))), columns=["Content", "Target"])
 
     from tensorflow.keras.utils import to_categorical
+
     Y_train = to_categorical(Y_train)
     Y_test = to_categorical(Y_test)
 
     def build_classifier_model():
 
-        text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-        preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
+        text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name="text")
+        preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name="preprocessing")
         encoder_inputs = preprocessing_layer(text_input)
-        encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
+        encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name="BERT_encoder")
         outputs = encoder(encoder_inputs)
-        net = outputs['pooled_output']
+        net = outputs["pooled_output"]
         net = tf.keras.layers.Dropout(0.1)(net)
-        net = tf.keras.layers.Dense(4, activation='softmax', name='classifier')(net)
+        net = tf.keras.layers.Dense(4, activation="softmax", name="classifier")(net)
         return tf.keras.Model(inputs=text_input, outputs=net)
 
     model = build_classifier_model()
 
-    my_model = TensorFlowModel(name="Tensorflow_text_classification_tfhub",
-                               clf=model,
-                               feature_names=['Content'],
-                               model_type="classification",
-                               classification_labels=['0','1','2','3'])
+    my_model = TensorFlowModel(
+        name="Tensorflow_text_classification_tfhub",
+        clf=model,
+        feature_names=["Content"],
+        model_type="classification",
+        classification_labels=["0", "1", "2", "3"],
+    )
 
     # defining the giskard dataset
 
-    my_test_dataset = Dataset(test_df.head(), name="test dataset", target="Target", cat_columns=['Week_day', 'Month'])
+    my_test_dataset = Dataset(test_df.head(), name="test dataset", target="Target", cat_columns=["Week_day", "Month"])
 
     artifact_url_pattern = re.compile(
-        "http://giskard-host:12345/api/v2/artifacts/test-project/models/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.*")
+        "http://giskard-host:12345/api/v2/artifacts/test-project/models/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.*"
+    )
     models_url_pattern = re.compile("http://giskard-host:12345/api/v2/project/test-project/models")
     settings_url_pattern = re.compile("http://giskard-host:12345/api/v2/settings")
 
@@ -91,11 +99,12 @@ def test_text_classification_tfhub():
         token = "SECRET_TOKEN"
         client = GiskardClient(url, token)
         # enron = client.create_project('test-project', "Email Classification", "Email Classification")
-        my_model.upload(client, 'test-project', my_test_dataset)
+        my_model.upload(client, "test-project", my_test_dataset)
 
         tests.utils.match_model_id(my_model.id)
         tests.utils.match_url_patterns(m.request_history, artifact_url_pattern)
         tests.utils.match_url_patterns(m.request_history, models_url_pattern)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     test_text_classification_tfhub()
