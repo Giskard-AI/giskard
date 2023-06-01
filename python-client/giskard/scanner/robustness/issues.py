4745bde84329f9e2918fdd38e257dd645292d488
Matteo Dora <matteo@giskard.ai>
2023-06-01 02:11:23 +0200

    Improve examples for robustness

--- python-client/giskard/scanner/robustness/issues.py
+++ python-client/giskard/scanner/robustness/issues.py
@@ -50,9 +50,29 @@ class RobustnessIssue(Issue):
         return ""
 
     def examples(self, n=3) -> pd.DataFrame:
-        idx = np.random.choice(self.info.fail_data_idx, min(len(self.info.fail_data_idx), n))
+        rng = np.random.default_rng(142)
+        idx = rng.choice(self.info.fail_data_idx, min(len(self.info.fail_data_idx), n), replace=False)
+        examples = self.dataset.df.loc[idx, (self.info.feature,)].copy()
 
-        return self.dataset.df.loc[idx]
+        original_preds = pd.Series(
+            self.model.predict(self.dataset).prediction,
+            index=self.dataset.df.index,
+        )
+        perturbed_preds = pd.Series(
+            self.info.perturbed_data_slice_predictions.prediction,
+            index=self.info.perturbed_data_slice.df.index,
+        )
+
+        # Add transformed feature
+        examples[f"{self.info.transformation_fn.name}({self.info.feature})"] = self.info.perturbed_data_slice.df.loc[
+            idx, self.info.feature
+        ]
+
+        # Add predictions
+        examples["Original prediction"] = original_preds.loc[examples.index]
+        examples["Prediction after perturbation"] = perturbed_preds.loc[examples.index]
+
+        return examples
 
     @property
     def importance(self) -> float:
