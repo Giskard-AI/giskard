a96e48ee52ccf8ff0aa436362122776b856bcf5f
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-06-01 00:11:22 +0000

    Added data_leakage and stochasticity

--- python-client/giskard/scanner/prediction/stochasticity.py
+++ python-client/giskard/scanner/prediction/stochasticity.py
@@ -1,9 +1,12 @@
 from giskard import Dataset
 from giskard.models.base import BaseModel
+from giskard.scanner.decorators import detector
+from giskard.scanner.issues import Issue
 from giskard.scanner.logger import logger
 import pandas as pd
 
 
+@detector(name="Stochasticity bias", tags=["prediction_bias", "classification", "regression"])
 class StochasticityDetector:
     def run(self, model: BaseModel, dataset: Dataset):
         logger.debug(
@@ -17,11 +20,18 @@ class StochasticityDetector:
         prediction_1 = pd.DataFrame(model.predict(dataset).prediction)
         prediction_2 = pd.DataFrame(model.predict(dataset).prediction)
         if not prediction_1.equals(prediction_2):
-                return StochasticityIssue()
+            return StochasticityIssue()
 
 
-class StochasticityIssue:
+class StochasticityIssue(Issue):
     """Stochasticity Issue"""
 
-    group = ""
-    info = "Stochasticity issue"  # TODO: Sentence TBD
+    group = "Prediction bias"
+    info = "Stochasticity issue"
+
+    def __repr__(self):
+        return f"<StochasticityIssue>"
+
+    @property
+    def description(self):
+        return f"Running the model twice on the same dataset returns different results"
