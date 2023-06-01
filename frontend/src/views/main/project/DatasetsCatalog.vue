ff981d76fd9e6d6f1d2a320b8e1779dee75b0506
Henrique Chaves <henrique@giskard.ai>
2023-06-01 00:11:22 +0000

    Add project-catalog-datasets and project-catalog-models routes

---
+++ frontend/src/views/main/project/DatasetsCatalog.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Datasets</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ frontend/src/views/main/project/ModelsCatalog.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Models</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
