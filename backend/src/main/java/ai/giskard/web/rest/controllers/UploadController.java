715f53a3b3c13ea25b9efc226e20a9dbe797114b
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 02:11:23 +0200

    Removed project keys from test functions

--- backend/src/main/java/ai/giskard/web/rest/controllers/UploadController.java
+++ backend/src/main/java/ai/giskard/web/rest/controllers/UploadController.java
@@ -35,6 +35,21 @@ public class UploadController {
         return uploadService.listArtifacts(projectKey, ArtifactType.fromDirectoryName(artifactType), artifactId);
     }
 
+    @GetMapping("artifacts/global/{artifactType}/{artifactId}/**")
+    public void downloadGlobalArtifact( @PathVariable String artifactType,
+                                 @PathVariable String artifactId,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response) throws IOException {
+        String path = (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+        String matchPattern = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        String resoursePath = new AntPathMatcher().extractPathWithinPattern(matchPattern, path);
+
+        try (InputStream artifactStream = uploadService.getArtifactStream("global", ArtifactType.fromDirectoryName(artifactType), artifactId, resoursePath)) {
+            IOUtils.copy(artifactStream, response.getOutputStream());
+            response.flushBuffer();
+        }
+    }
+
     @GetMapping("artifacts/{projectKey}/{artifactType}/{artifactId}/**")
     @PreAuthorize("@permissionEvaluator.canReadProjectKey(#projectKey)")
     public void downloadArtifact(@PathVariable String projectKey,
@@ -52,6 +67,21 @@ public class UploadController {
         }
     }
 
+    @PostMapping("artifacts/global/{artifactType}/{artifactId}/**")
+    public ResponseEntity<Void> uploadGlobalArtifact(@PathVariable String artifactType,
+                                               @PathVariable String artifactId,
+                                               HttpServletRequest request) throws IOException {
+        String path = (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+        String matchPattern = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        String resoursePath = new AntPathMatcher().extractPathWithinPattern(matchPattern, path);
+
+
+        try (InputStream uploadedStream = request.getInputStream()) {
+            uploadService.saveArtifact(uploadedStream, "global", ArtifactType.fromDirectoryName(artifactType), artifactId, resoursePath);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
     @PostMapping("artifacts/{projectKey}/{artifactType}/{artifactId}/**")
     @PreAuthorize("@permissionEvaluator.canWriteProjectKey(#projectKey)")
     public ResponseEntity<Void> uploadArtifact(@PathVariable String projectKey,
