0eda04972e98d7c9e08d0674e0654e2b991d9b83
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-06-01 00:11:22 +0000

    Numerical perturbation added with unit test, textual perturbation wip

--- python-client/giskard/scanner/perturbation.py
+++ python-client/giskard/scanner/perturbation.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from ..models.base import BaseModel
 from ..datasets.base import Dataset
-from giskard.scanner.unit_perturbation import df_std, generate_std_transfo
+from giskard.scanner.unit_perturbation import TransformationGenerator
 from giskard.ml_worker.testing.tests.metamorphic import test_metamorphic_invariance
 
 
@@ -11,9 +11,9 @@ class RobustnessScan:
         self.dataset = dataset
 
     def run(
-        self,
-        model: Optional[BaseModel] = None,
-        dataset: Optional[Dataset] = None,
+            self,
+            model: Optional[BaseModel] = None,
+            dataset: Optional[Dataset] = None,
     ):
         model = model or self.model
         dataset = dataset or self.dataset
@@ -24,16 +24,35 @@ class RobustnessScan:
             raise ValueError("You need to provide an evaluation dataset.")
 
         test_results = []
+        transformation = TransformationGenerator(model=model, dataset=dataset)
         for feature, feature_type in dataset.column_types.items():
-            if feature_type == "numerical":
-                std = df_std(dataset=dataset, feature=feature)
-                numerical_perturbation = generate_std_transfo(std)
-                test_results.append(
-                    test_metamorphic_invariance(
-                        model=model,
-                        dataset=dataset,
-                        transformation_function=numerical_perturbation,
-                    )
-                )
+            if feature_type == "numeric":
+                transformation_function = transformation.generate_std_transformation(feature)
+
+                test_result = test_metamorphic_invariance(
+                    model=model,
+                    dataset=dataset,
+                    transformation_function=transformation_function,
+                    threshold=0.8
+                ).execute()
+
+                if not test_result.passed:
+                    test_results.append(("passed", feature, test_result.metric))
+                else:
+                    test_results.append(("failed", feature, test_result.metric))
+
+            if feature_type == "text":
+                transformation_function = transformation.text_transformation(feature)
+                test_result = test_metamorphic_invariance(
+                    model=model,
+                    dataset=dataset,
+                    transformation_function=transformation_function,
+                    threshold=0.8
+                ).execute()
+                if not test_result.passed:
+                    test_results.append(("passed", feature, test_result.metric))
+                else:
+                    test_results.append(("failed", feature, test_result.metric))
+
 
         return test_results
