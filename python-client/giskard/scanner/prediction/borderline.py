35eb8ada7ca08a28b1efa96ecab203d6750e3bd5
Mathieu Roques <84331949+mathieuroques@users.noreply.github.com>
2023-06-01 02:11:23 +0200

    added decorators

--- python-client/giskard/scanner/prediction/borderline.py
+++ python-client/giskard/scanner/prediction/borderline.py
@@ -1,6 +1,9 @@
+from giskard.scanner.decorators import detector
 from giskard.scanner.prediction import PredictionBiasDetector
 from giskard.scanner.prediction.computing.borderline import ComputeBorderline
 
+
+@detector(name="Borderline bias", tags=["borderline_bias", "classification"])
 class BorderlineBiasDetector(PredictionBiasDetector):
     def _get_meta(self, model, dataset):
         return ComputeBorderline(model, dataset).get_dataset()
--- python-client/giskard/scanner/prediction/overconfidence.py
+++ python-client/giskard/scanner/prediction/overconfidence.py
@@ -1,7 +1,9 @@
+from giskard.scanner.decorators import detector
 from giskard.scanner.prediction.computing.overconfidence import ComputeOverconfidence
 from giskard.scanner.prediction import PredictionBiasDetector
 
 
+@detector(name="Overconfidence bias", tags=["prediction_bias", "classification"])
 class OverconfidenceBiasDetector(PredictionBiasDetector):
     def _get_meta(self, model, dataset):
         return ComputeOverconfidence(model,dataset).get_dataset()
