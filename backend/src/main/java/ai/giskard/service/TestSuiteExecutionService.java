79897c232123d193ac6d6f7471beb75ad0d11164
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 02:11:23 +0200

    Display logs

--- backend/src/main/java/ai/giskard/service/TestSuiteExecutionService.java
+++ backend/src/main/java/ai/giskard/service/TestSuiteExecutionService.java
@@ -78,7 +78,7 @@ public class TestSuiteExecutionService {
         } catch (Exception e) {
             log.error("Error while executing test suite {}", execution.getSuite().getName(), e);
             execution.setResult(TestResult.ERROR);
-            execution.setMessage(e.getMessage());
+            execution.setLogs(e.getMessage());
             throw e;
         } finally {
             execution.setCompletionDate(new Date());
---
+++ frontend/src/views/main/project/modals/ExecutionLogsModal.vue
@@ -0,0 +1,55 @@
+<template>
+  <vue-final-modal
+      v-slot="{ close }"
+      v-bind="$attrs"
+      classes="modal-container"
+      content-class="modal-content"
+      v-on="$listeners"
+  >
+    <div class="text-center">
+      <v-card>
+        <v-card-title>
+          Execution logs
+        </v-card-title>
+        <v-card-text>
+          <pre class="logs caption pt-5">{{ props.logs }}</pre>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" @click="close">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
+  </vue-final-modal>
+</template>
+
+<script setup lang="ts">
+
+const props = (defineProps<{
+  logs: string,
+}>());
+
+
+</script>
+
+<style scoped>
+::v-deep(.modal-container) {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+::v-deep(.modal-content) {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  margin: 0 1rem;
+  padding: 1rem;
+  min-width: 50vw;
+  max-height: 80vh;
+  overflow: auto;
+}
+
+.logs {
+  text-align: start;
+}
+</style>
