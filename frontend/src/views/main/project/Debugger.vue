46e6c921cd4d0ace185157c67440b5733f69d5f2
Henrique Chaves <henrique@giskard.ai>
2023-06-01 00:11:22 +0000

    Update panel header when is active

--- frontend/src/views/main/project/Debugger.vue
+++ frontend/src/views/main/project/Debugger.vue
@@ -67,27 +67,27 @@ onActivated(() => loadInspections());
       </v-row>
 
       <v-expansion-panels>
-        <v-row dense no-gutters class="mr-12 ml-6 caption secondary--text text--lighten-3 pb-2">
+        <v-row dense no-gutters class="mr-12 ml-6 caption secondary--text text--lighten-3 pb-2" v-if="displayComponents">
           <v-col cols="3">Name</v-col>
-          <v-col cols="2">Id</v-col>
+          <v-col cols="1">ID</v-col>
           <v-col cols="2">Created at</v-col>
           <v-col cols="1">Dataset name</v-col>
           <v-col cols="1">Dataset ID</v-col>
-          <v-col cols="1">Model name</v-col>
+          <v-col cols="2">Model name</v-col>
           <v-col cols="1">Model ID</v-col>
           <v-col cols="1">Actions</v-col>
         </v-row>
 
         <v-expansion-panel v-for="inspection in inspections" :key="inspection.id" v-show="displayComponents || activeInspection == inspection.id" @click="toggleActiveInspection(inspection.id)">
           <v-expansion-panel-header>
-            <v-row dense no-gutters class="align-center">
+            <v-row dense no-gutters class="align-center" v-if="displayComponents">
               <v-col cols="3">Generic name</v-col>
-              <v-col cols="2">{{ inspection.id }}</v-col>
+              <v-col cols="1">{{ inspection.id }}</v-col>
               <v-col cols="2">Generic date</v-col>
               <v-col cols="1">{{ inspection.dataset.name }}</v-col>
-              <v-col cols="1">{{ inspection.dataset.id }}</v-col>
-              <v-col cols="1">{{ inspection.model.name }}</v-col>
-              <v-col cols="1">{{ inspection.model.id }}</v-col>
+              <v-col cols="1" class="id-container" :title="inspection.dataset.id">{{ inspection.dataset.id }}</v-col>
+              <v-col cols="2">{{ inspection.model.name }}</v-col>
+              <v-col cols="1" class="id-container" :title="inspection.dataset.id">{{ inspection.model.id }}</v-col>
               <v-col cols="1">
                 <v-card-actions>
                   <v-btn icon color="accent" class="delete-button" @click.stop="deleteInspection(inspection.id)">
@@ -95,6 +95,49 @@ onActivated(() => loadInspections());
                   </v-btn>
                 </v-card-actions>
               </v-col>
+            </v-row>
+            <v-row dense no-gutters class="align-center" v-else>
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-icon v-on="on" class="pr-5" medium>info</v-icon>
+                </template>
+                <h3> Inspection </h3>
+                <div class="d-flex">
+                  <div> Id</div>
+                  <v-spacer />
+                  <div> {{ inspection.id }}</div>
+                </div>
+                <div class="d-flex">
+                  <div> Name</div>
+                  <v-spacer />
+                  <div class="pl-5"> Generic name </div>
+                </div>
+                <br />
+                <h3> Model </h3>
+                <div class="d-flex">
+                  <div> Id</div>
+                  <v-spacer />
+                  <div> {{ inspection.model.id }}</div>
+                </div>
+                <div class="d-flex">
+                  <div> Name</div>
+                  <v-spacer />
+                  <div class="pl-5"> {{ inspection.model.name }}</div>
+                </div>
+                <br />
+                <h3> Dataset </h3>
+                <div class="d-flex">
+                  <div> Id</div>
+                  <v-spacer />
+                  <div> {{ inspection.dataset.id }}</div>
+                </div>
+                <div class="d-flex">
+                  <div> Name</div>
+                  <v-spacer />
+                  <div class="pl-5"> {{ inspection.dataset.name }}</div>
+                </div>
+              </v-tooltip>
+              <h1 class="headline">Inspection: Generic name (ID: {{ inspection.id }})</h1>
             </v-row>
           </v-expansion-panel-header>
           <v-expansion-panel-content>
@@ -126,5 +169,12 @@ onActivated(() => loadInspections());
 .debugger-logo {
   max-width: 30%;
   margin-top: 2rem;
+}
+
+.id-container {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 0.5rem;
 }
 </style>
