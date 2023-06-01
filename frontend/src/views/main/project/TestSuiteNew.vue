c4173149957f029fa967a034b275161ada5693ac
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 02:11:23 +0200

    WIP use pinia

--- frontend/src/views/main/project/TestSuiteNew.vue
+++ frontend/src/views/main/project/TestSuiteNew.vue
@@ -14,61 +14,17 @@
     <v-row>
       <v-col cols="2">
         <v-tabs vertical v-model="tab">
-          <v-tab>Inputs & parameters</v-tab>
-          <v-tab>Tests</v-tab>
-          <v-tab>Configuration</v-tab>
-          <v-tab>Execution</v-tab>
-          <v-tab class="d-none">Compare executions</v-tab>
-          <v-tab class="d-none">Compare tests</v-tab>
+          <v-tab :to="{name:'test-suite-new-inputs'}">Inputs & parameters</v-tab>
+          <v-tab :to="{name:'test-suite-new-tests'}">Tests</v-tab>
+          <v-tab :to="{name:'test-suite-new-configuration'}">Configuration</v-tab>
+          <v-tab :to="{name:'test-suite-new-execution'}">Execution</v-tab>
+          <v-tab :to="{name:'test-suite-new-compare-executions'}" class="d-none">Compare executions</v-tab>
+          <v-tab :to="{name:'test-suite-new-compare-test'}" class="d-none">Compare tests</v-tab>
         </v-tabs>
       </v-col>
       <v-col cols="10">
         <v-tabs-items v-model="tab">
-          <v-tab-item :transition="false">
-            <div>Inputs</div>
-            <v-list>
-              <v-list-item v-for="(type, name) in inputs">
-                <v-list-item-title>{{ name }}: {{ type }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-tab-item>
-          <v-tab-item :transition="false">
-            <v-row v-if="registry">
-              <v-col cols="3">
-                <v-list three-line v-if="suite.tests">
-                  <v-list-item-group v-model="selectedTest" color="primary" mandatory>
-                    <template v-for="(test) in suite.tests">
-                      <v-divider/>
-                      <v-list-item :value="test">
-                        <v-list-item-content>
-                          <v-list-item-title v-text="registry.tests[test.testId].name"
-                                             class="test-title"></v-list-item-title>
-                        </v-list-item-content>
-                      </v-list-item>
-                    </template>
-                  </v-list-item-group>
-                </v-list>
-              </v-col>
-              <v-col v-if="selectedTest">
-                <v-row>
-                  <v-col>
-                    <TestSuiteTestDetails
-                        :project-id="projectId"
-                        :suite-id="suiteId"
-                        :test="registry.tests[selectedTest.testId]"
-                        :models="allModels"
-                        :datasets="allDatasets"
-                        :inputs="selectedTest.testInputs"
-                        :executions="testSuiteResults[selectedTest.testId]"
-                        @updateTestSuite="loadData"/>
-                  </v-col>
-                </v-row>
-              </v-col>
-            </v-row>
-          </v-tab-item>
-          <v-tab-item :transition="false">
-
-          </v-tab-item>
+          <router-view/>
           <v-tab-item :transition="false">
             <TestSuiteExecutions :project-id="props.projectId"
                                  :suite-id="props.suiteId"
@@ -100,25 +56,15 @@
 
 <script lang="ts" setup>
 
-import {api} from "@/api";
-import {computed, onMounted, ref, watch} from "vue";
-import {
-  DatasetDTO,
-  ModelDTO,
-  SuiteTestDTO,
-  TestCatalogDTO,
-  TestSuiteExecutionDTO,
-  TestSuiteNewDTO
-} from "@/generated-sources";
-import TestSuiteTestDetails from "@/views/main/project/TestSuiteTestDetails.vue";
+import {onMounted, ref, watch} from "vue";
+import {DatasetDTO, ModelDTO, TestCatalogDTO, TestSuiteExecutionDTO, TestSuiteNewDTO} from "@/generated-sources";
 import RunTestSuiteModal from '@/views/main/project/modals/RunTestSuiteModal.vue';
 import TestSuiteExecutions from '@/views/main/project/TestSuiteExecutions.vue';
-import useRouterTabsSynchronization from '@/utils/use-router-tabs-synchronization';
 import TestSuiteCompareExecutions from '@/views/main/project/TestSuiteCompareExecutions.vue';
 import TestSuiteCompareTest from '@/views/main/project/TestSuiteCompareTest.vue';
 import {useTrackJob} from '@/utils/use-track-job';
-import {chain} from 'lodash';
 import {useMainStore} from "@/stores/main";
+import {useTestSuiteStore} from '@/stores/test-suite';
 
 const props = defineProps<{
   projectId: number,
@@ -130,7 +76,6 @@ const mainStore = useMainStore();
 const suite = ref<TestSuiteNewDTO | null>(null);
 const registry = ref<TestCatalogDTO | null>(null);
 const tab = ref<any>(null);
-const selectedTest = ref<SuiteTestDTO | null>(null);
 const inputs = ref<{ [name: string]: string }>({});
 const allDatasets = ref<{ [key: string]: DatasetDTO }>({});
 const allModels = ref<{ [key: string]: ModelDTO }>({});
@@ -139,31 +84,10 @@ const executions = ref<TestSuiteExecutionDTO[]>();
 onMounted(() => loadData());
 watch(() => props.suiteId, () => loadData());
 
+const {loadTestSuite} = useTestSuiteStore();
+
 async function loadData() {
-  // Call api in parallel to shorten loading time
-  const [
-    inputResults,
-    suiteResults,
-    registryResult,
-    datasets,
-    models,
-    executionResults
-  ] = await Promise.all([
-    api.getTestSuiteNewInputs(props.projectId, props.suiteId),
-    api.getTestSuiteNew(props.projectId, props.suiteId),
-    api.getTestsCatalog(props.projectId),
-    api.getProjectDatasets(props.projectId),
-    api.getProjectModels(props.projectId),
-    api.listTestSuiteExecutions(props.projectId, props.suiteId)
-  ]);
-
-  inputs.value = inputResults;
-  suite.value = suiteResults;
-  registry.value = registryResult
-  executions.value = executionResults;
-
-  allDatasets.value = Object.fromEntries(datasets.map(x => [x.id, x]));
-  allModels.value = Object.fromEntries(models.map(x => [x.id, x]));
+  await loadTestSuite(props.projectId, props.suiteId);
 }
 
 watch(() => suite.value, () => {
@@ -172,31 +96,6 @@ watch(() => suite.value, () => {
   }
 })
 
-const testSuiteResults = computed(() => {
-  if (!executions.value) {
-    return {};
-  }
-
-  return chain(executions.value)
-      .map(execution => (execution.results ?? []).map(
-          result => ({
-            testResult: result,
-            testSuiteResult: execution
-          })
-      ))
-      .flatten()
-      .groupBy(result => result.testResult.test.testId)
-      .values();
-});
-
-useRouterTabsSynchronization([
-  'test-suite-new-inputs',
-  'test-suite-new-test',
-  'test-suite-new-configuration',
-  'test-suite-new-execution',
-  'test-suite-new-compare-executions',
-  'test-suite-new-compare-test'
-], tab);
 
 const {
   trackedJobs,
