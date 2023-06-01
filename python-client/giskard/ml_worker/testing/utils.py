d113ec11e2d95c69975cf1d816dfa5d7b6e7d6f3
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 02:11:23 +0200

    Removed unused method

--- python-client/giskard/ml_worker/testing/utils.py
+++ python-client/giskard/ml_worker/testing/utils.py
@@ -1,31 +1,13 @@
-import pandas as pd
-from functools import wraps
-from giskard.core.core import SupportedModelTypes
-from giskard.ml_worker.utils.logging import timer
 from enum import Enum
+from functools import wraps
+
+from giskard.core.core import SupportedModelTypes
 
 
 class Direction(Enum):
     Invariant = 0
     Increasing = 1
     Decreasing = -1
-
-
-@timer("Perturb data")
-def apply_perturbation_inplace(df: pd.DataFrame, perturbation_dict):
-    modified_rows = []
-    i = 0
-    for idx, r in df.iterrows():
-        added = False
-        for pert_col, pert_func in perturbation_dict.items():
-            original_value = r[pert_col]
-            new_value = pert_func(r)
-            if original_value != new_value and not added:
-                added = True
-                modified_rows.append(i)
-                df.loc[idx, pert_col] = new_value
-        i += 1
-    return modified_rows
 
 
 def validate_classification_label(func):
