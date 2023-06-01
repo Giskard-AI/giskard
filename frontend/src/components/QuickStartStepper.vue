ab63018416eec3b9ffe295e485e3ad0e3d5e6fd8
Andrey Avtomonov <andreybavt@gmail.com>
2023-06-01 02:11:23 +0200

    fix onboarding

--- frontend/src/components/QuickStartStepper.vue
+++ frontend/src/components/QuickStartStepper.vue
@@ -3,11 +3,9 @@
     <v-stepper-header id="stepper-header">
       <v-stepper-step :complete="step > 1" step="1">Start ML Worker</v-stepper-step>
       <v-divider></v-divider>
-      <v-stepper-step :complete="step > 2" step="2">Create Giskard client</v-stepper-step>
+      <v-stepper-step :complete="step > 2" step="2">Upload your dataset and model</v-stepper-step>
       <v-divider></v-divider>
-      <v-stepper-step :complete="step > 3" step="3">Upload Giskard artifacts</v-stepper-step>
-      <v-divider></v-divider>
-      <v-stepper-step step="4">Test your model</v-stepper-step>
+        <v-stepper-step step="3">Test your model</v-stepper-step>
     </v-stepper-header>
 
     <v-stepper-items>
@@ -16,86 +14,40 @@
           <p>ML Worker is already running!</p>
           <p>You can skip this step.</p>
         </div>
-        <div class="mb-6" v-else>
-          <StartWorkerInstructions></StartWorkerInstructions>
-        </div>
-        <div class="d-flex">
-          <v-btn color="primary" @click="step = 2">Continue</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn @click="close" text>Cancel</v-btn>
-        </div>
-      </v-stepper-content>
-
-      <v-stepper-content step="2">
-        <div class="mb-6">
-          <p class="mb-2">First, you must have access to an API Access Token.</p>
-          <div v-if="apiAccessToken && apiAccessToken.id_token">
-            <CodeSnippet :codeContent='`token = "${apiAccessToken.id_token}" # Your API Access Token`'></CodeSnippet>
+          <div class="mb-6" v-else>
+              <StartWorkerInstructions></StartWorkerInstructions>
           </div>
-          <div v-else>
-            <p>If you don't have one, you can click on the button below.</p>
-            <v-btn color="primaryLight" class="primaryLightBtn" @click="generateApiAccessToken">Generate</v-btn>
+          <div class="d-flex">
+              <v-btn color="primary" @click="step++">Continue</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn @click="close" text>Cancel</v-btn>
           </div>
-          <p class="my-2">Then, create a Giskard client with the following Python code:</p>
-          <CodeSnippet :codeContent="clientCodeContent"></CodeSnippet>
-        </div>
-        <div class="d-flex">
-          <v-btn color="primary" @click="step = 3">Continue</v-btn>
-          <v-btn color="ml-2" @click="step = 1">Back</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn @click="close" text>Cancel</v-btn>
-        </div>
       </v-stepper-content>
+        <v-stepper-content step="2">
+            <div class="mb-6">
+                <div v-if="apiAccessToken && apiAccessToken.id_token">
+                    <CodeSnippet :codeContent='uploadSnippet'></CodeSnippet>
+                </div>
+            </div>
+            <div class="d-flex">
+                <v-btn color="primary" @click="step++">Continue</v-btn>
+                <v-btn color="ml-2" @click="step--">Back</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn @click="close" text>Cancel</v-btn>
+            </div>
+        </v-stepper-content>
 
-      <v-stepper-content step="3" class="pl-0">
-        <p class="mb-2 ml-6">You can wrap your artifacts in two ways:</p>
-        <v-stepper vertical flat v-model="artifactsStep">
-          <v-stepper-step :complete="artifactsStep > 1" step="1" color="secondary">
-            Upload a dataset
-          </v-stepper-step>
-          <v-stepper-content step="1">
-            <CodeSnippet :codeContent="datasetCodeContent"></CodeSnippet>
-          </v-stepper-content>
-
-          <v-stepper-step :complete="artifactsStep > 2" step="2" color="secondary">
-            Upload a model
-          </v-stepper-step>
-          <v-stepper-content step="2">
-            <CodeSnippet :codeContent="modelCodeContent"></CodeSnippet>
-          </v-stepper-content>
-        </v-stepper>
-
-        <div class="d-flex">
-          <v-btn color="primary" class="ml-6" @click="() => {
-            if (artifactsStep === 1) {
-              artifactsStep = 2
-            } else {
-              step = 4
-              artifactsStep = 1
-            }
-          }">Continue</v-btn>
-          <v-btn color="ml-2" @click="() => {
-            if (artifactsStep === 1) {
-              step = 2
-            } else {
-              artifactsStep = 1
-            }
-          }">Back</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn @click="close" text>Cancel</v-btn>
-        </div>
-      </v-stepper-content>
-      <v-stepper-content step="4">
-        <div class="mb-6">
-          <p class="mb-2">Choose the type of test you want to perform:</p>
-          <v-btn-toggle v-model="toggleTestType" borderless mandatory color="primary">
-            <v-btn value="scan" class="py-5 px-4">
-              <span>Automatic Scan</span>
-            </v-btn>
-            <v-btn value="manual" class="py-5 px-4">
-              <span>Manual Testing</span>
-            </v-btn>
-          </v-btn-toggle>
+        <v-stepper-content step="3">
+            <div class="mb-6">
+                <p class="mb-2">Choose the type of test you want to perform:</p>
+                <v-btn-toggle v-model="toggleTestType" borderless mandatory color="primary">
+                    <v-btn value="scan" class="py-5 px-4">
+                        <span>Automatic Scan</span>
+                    </v-btn>
+                    <v-btn value="manual" class="py-5 px-4">
+                        <span>Manual Testing</span>
+                    </v-btn>
+                </v-btn-toggle>
           <div v-show="toggleTestType === 'scan'">
             <p class="mt-4 mb-2">To scan your model, run the following Python code:</p>
             <CodeSnippet :codeContent="scanCodeContent"></CodeSnippet>
@@ -107,7 +59,7 @@
         </div>
         <div class="d-flex">
           <v-btn color="primary" @click="close">Close</v-btn>
-          <v-btn color="ml-2" @click="step = 3">Back</v-btn>
+          <v-btn color="ml-2" @click="step--">Back</v-btn>
           <v-spacer></v-spacer>
           <v-btn class="ml-2" @click="step = 1; artifactsStep = 1" text>Restart</v-btn>
         </div>
@@ -117,15 +69,15 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref } from "vue";
-import { api } from "@/api";
-import { apiURL } from "@/env";
-import { MLWorkerInfoDTO, ProjectDTO, JWTToken } from "@/generated-sources";
+import {computed, onMounted, ref} from "vue";
+import {api} from "@/api";
+import {apiURL} from "@/env";
+import {JWTToken, MLWorkerInfoDTO, ProjectDTO} from "@/generated-sources";
 import CodeSnippet from "./CodeSnippet.vue";
 import StartWorkerInstructions from "./StartWorkerInstructions.vue";
 
 interface Props {
-  project: ProjectDTO;
+    project: ProjectDTO;
 }
 
 const props = defineProps<Props>();
@@ -138,14 +90,38 @@ const allMLWorkerSettings = ref<MLWorkerInfoDTO[]>([]);
 const externalWorker = ref<MLWorkerInfoDTO | null>(null);
 
 const clientCodeContent = computed(() => {
-  return `from giskard import GiskardClient
+    return `from giskard import GiskardClient
 
 url = "${apiURL}" # URL of your Giskard instance
 client = GiskardClient(url, token)`
 });
 
+
+const uploadSnippet = computed(() => {
+    // language=Python
+    return `from giskard import Dataset, Model, GiskardClient
+    from giskard.demo import titanic  # for demo purposes only 🛳️
+
+    original_model, original_df = titanic()  # Replace with your dataframe creation
+
+    # Create a Giskard client
+    token = "${apiAccessToken.value!.id_token}"
+    client = GiskardClient(
+        url="${apiURL}",  # URL of your Giskard instance
+        token=token
+    )
+
+    # Wrap your Pandas Dataframe and model with Giskard 🎁
+    giskard_dataset = Dataset(original_df, target="Survived", name="Titanic dataset")
+    giskard_model = Model(original_model, model_type="classification", name="Titanic model")
+
+    # Upload to the current project ✉️
+    giskard_dataset.upload(client, "${props.project.key}")
+    giskard_model.upload(client, "${props.project.key}")`
+});
+
 const datasetCodeContent = computed(() => {
-  return `import pandas as pd
+    return `import pandas as pd
 
 iris_df = pd.DataFrame({"sepal length": [5.1],
                         "sepal width": [3.5],
@@ -166,17 +142,19 @@ dataset_id = wrapped_dataset.upload(client, "${props.project.key}")`
 })
 
 const modelCodeContent = computed(() => {
-  return `import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-from giskard import wrap_model
+    // language=Python
+    return `import pandas as pd
+    import numpy as np
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.linear_model import LogisticRegression
+    from giskard import wrap_model
 
-scaler = StandardScaler()
-clf = LogisticRegression()
+    scaler = StandardScaler()
+    clf = LogisticRegression()
 
-def prediction_function(df: pd.DataFrame) -> np.ndarray:
-  #Scale all the numerical variables
+
+    def prediction_function(df: pd.DataFrame) -> np.ndarray:
+        #Scale all the numerical variables
   num_cols = ["sepal length", "sepal width"]
   df[num_cols] = scaler.transform(df[num_cols])
   
@@ -197,17 +175,24 @@ model_id = wrapped_model.upload(client, "${props.project.key}")`
 
 const scanCodeContent: string = `import giskard
 
-results = giskard.scan(wrapped_model, wrapped_dataset)
+results = giskard.scan(giskard_model, giskard_dataset)
 
-display(results)  # in your notebook`;
+# Show results in your notebook
+display(results)
+
+# Upload an automatically created test suite
+results.generate_test_suite("Test suite created by scan").upload(client, "${props.project.key}")`;
 
 const manualTestCodeContent = computed(() => {
-  return `from giskard import Suite, test_f1, DataQuality
+  // language=Python
+    return `from giskard import Suite
+from giskard.testing.tests.performance import test_f1
 
-suite = Suite()
-  .add_test(test_f1(actual_slice=wrapped_dataset))
-  .add_test(DataQuality(dataset=wrapped_dataset, column_name='species', category='Setosa'))
-  .save(client, "${props.project.key}")`
+suite = Suite() \\
+    .add_test(test_f1(dataset=giskard_dataset))
+
+suite.run(model=giskard_model)
+`
 });
 
 const emit = defineEmits(["close"]);
