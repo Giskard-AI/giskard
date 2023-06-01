69af574ec261709346f7ab83aab9694737000db2
Henrique Chaves <henriquechavesmm@gmail.com>
2023-06-01 02:11:23 +0200

    Persist current test suite + button to list all test suites

--- frontend/src/views/main/project/TestSuites.vue
+++ frontend/src/views/main/project/TestSuites.vue
@@ -1,18 +1,24 @@
 <template>
     <v-container fluid class="vc" v-if="testSuites.length > 0">
-        <div class="d-flex flex-row-reverse pb-4">
-            <v-btn color="primaryLight" class="primaryLightBtn" @click="createTestSuite">
-                <v-icon left>add</v-icon>
-                New test suite
-            </v-btn>
+        <div v-if="testSuitesStore.currentTestSuiteId === null">
+            <div class="d-flex flex-row-reverse pb-4">
+                <v-btn color="primaryLight" class="primaryLightBtn" @click="createTestSuite">
+                    <v-icon left>add</v-icon>
+                    New test suite
+                </v-btn>
+            </div>
+            <v-row>
+                <v-card elevation="2" @click.stop="openTestSuite(suite.id)" class="ma-2" style="width: 300px" v-for="suite in testSuites" :key="suite.id">
+                    <v-card-title>{{ suite.name }}</v-card-title>
+                    <v-card-subtitle>Tests: {{ suite.tests.length }}</v-card-subtitle>
+                    <v-card-text>{{ suite.projectKey }}</v-card-text>
+                </v-card>
+            </v-row>
         </div>
-        <v-row>
-            <v-card elevation="2" :to="{ name: 'test-suite-overview', params: { suiteId: suite.id } }" class="ma-2" style="width: 300px" v-for="suite in testSuites" :key="suite.id">
-                <v-card-title>{{ suite.name }}</v-card-title>
-                <v-card-subtitle>Tests: {{ suite.tests.length }}</v-card-subtitle>
-                <v-card-text>{{ suite.projectKey }}</v-card-text>
-            </v-card>
-        </v-row>
+        <div v-else>
+            <router-view />
+        </div>
+
     </v-container>
     <v-container v-else class="vc mt-6 fill-height">
         <v-alert class="text-center">
@@ -29,12 +35,13 @@
 </template>
 
 <script lang="ts" setup>
-
-import {api} from "@/api";
-import {onActivated} from "vue";
+import { api } from "@/api";
+import { onActivated } from "vue";
 import router from '@/router';
-import {useTestSuitesStore} from "@/stores/test-suites";
-import {storeToRefs} from "pinia";
+import { useTestSuitesStore } from "@/stores/test-suites";
+import { storeToRefs } from "pinia";
+import { useMainStore } from "@/stores/main";
+import { TYPE } from "vue-toastification";
 
 const props = defineProps<{
     projectId: number
@@ -43,8 +50,6 @@ const props = defineProps<{
 
 const testSuitesStore = useTestSuitesStore();
 const { testSuites } = storeToRefs(testSuitesStore);
-
-onActivated(async () => await testSuitesStore.loadTestSuites(props.projectId))
 
 async function createTestSuite() {
     const project = await api.getProject(props.projectId)
@@ -59,6 +64,36 @@ async function createTestSuite() {
     await testSuitesStore.reload()
     await router.push({ name: 'test-suite-overview', params: { suiteId: suite.toString() } });
 }
+
+async function openTestSuite(suiteId: number | null | undefined) {
+    if (!suiteId) {
+        useMainStore().addNotification({
+            content: 'Test suite not found',
+            color: TYPE.ERROR
+        });
+    }
+
+    testSuitesStore.setCurrentTestSuiteId(suiteId!);
+    await redirectToTestSuite(props.projectId.toString(), suiteId!.toString());
+}
+
+async function redirectToTestSuite(projectId: string, suiteId: string) {
+    await router.push({
+        name: 'test-suite-overview',
+        params: {
+            projectId,
+            suiteId
+        }
+    });
+}
+
+onActivated(async () => {
+    if (testSuitesStore.currentTestSuiteId !== null) {
+        await redirectToTestSuite(props.projectId.toString(), testSuitesStore.currentTestSuiteId.toString());
+    } else {
+        await testSuitesStore.loadTestSuites(props.projectId);
+    }
+})
 
 </script>
 
