d33f41464aae33d38e92090934e05e59bb0fa724
Rabah <rabah.khalek@gmail.com>
2023-06-01 02:11:23 +0200

    implemented NLP pytorch and tensorflow unit-tests

---
+++ python-client/tests/huggingface/test_sequence_classification_distilbert_base_uncased_tensorflow.py
@@ -0,0 +1,48 @@
+# For the complete tutorial, check: https://huggingface.co/docs/transformers/tasks/sequence_classification
+import pandas as pd
+
+from transformers import AutoTokenizer
+from transformers import TFAutoModelForSequenceClassification
+
+from giskard import HuggingFaceModel, Dataset
+import tests.utils
+
+
+def test_sequence_classification_distilbert_base_uncased_tensorflow():
+
+    tokenizer_distilbert_base_uncased = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+    id2label = {0: "NEGATIVE", 1: "POSITIVE"}
+    label2id = {"NEGATIVE": 0, "POSITIVE": 1}
+
+    model_distilbert_base_uncased = TFAutoModelForSequenceClassification.from_pretrained(
+        "distilbert-base-uncased", num_labels=2, id2label=id2label, label2id=label2id
+    )
+
+    text = "This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three."
+
+    raw_data = {
+        "text": text,
+        "label": "POSITIVE",
+    }
+    test_df = pd.DataFrame(raw_data, columns=["text", "label"], index=[0])
+    feature_names = ["text"]
+
+    def my_preproccessing_function(df):
+        return tokenizer_distilbert_base_uncased(list(df['text']), return_tensors="tf")
+
+    my_model = HuggingFaceModel(
+        name="distilbert-base-uncased",
+        clf=model_distilbert_base_uncased,
+        feature_names=feature_names,
+        model_type="classification",
+        classification_labels=list(label2id.keys()),
+        data_preprocessing_function=my_preproccessing_function
+    )
+
+    my_test_dataset = Dataset(test_df, name="test dataset", target="label")
+
+    tests.utils.verify_model_upload(my_model, my_test_dataset)
+
+if __name__ == "__main__":
+    test_sequence_classification_distilbert_base_uncased_tensorflow()
