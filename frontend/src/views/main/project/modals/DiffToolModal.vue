f74d53caf69b0c8bed17333752bf59c00a759f3d
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 02:11:23 +0200

    Added scroll to diffs

--- frontend/src/views/main/project/modals/DiffToolModal.vue
+++ frontend/src/views/main/project/modals/DiffToolModal.vue
@@ -6,38 +6,36 @@
         content-class="modal-content"
         v-on="$listeners"
     >
-        <v-form @submit.prevent="">
-            <v-card class="modal-card">
-                <v-card-title>
-                    Differences
-                </v-card-title>
-                <v-card-text>
-                    <v-row>
-                        <v-col cols=6>
-                            <span v-for="diff in diffs" v-if="!diff.added"
-                                  :class="{'removed': diff.removed}">{{ diff.value }}</span>
-                        </v-col>
-                        <v-col cols=6>
-                            <span v-for="diff in diffs" v-if="!diff.removed"
-                                  :class="{'added': diff.added}">{{ diff.value }}</span>
-                        </v-col>
-                    </v-row>
-                </v-card-text>
+        <v-card class="modal-card">
+            <v-card-title>
+                Differences
+            </v-card-title>
+            <v-card-text class="card-content">
+                <v-row>
+                    <v-col cols=6>
+                        <span v-for="diff in diffs" v-if="!diff.added"
+                              :class="{'removed': diff.removed}">{{ diff.value }}</span>
+                    </v-col>
+                    <v-col cols=6>
+                        <span v-for="diff in diffs" v-if="!diff.removed"
+                              :class="{'added': diff.added}">{{ diff.value }}</span>
+                    </v-col>
+                </v-row>
+            </v-card-text>
 
-                <v-divider></v-divider>
+            <v-divider></v-divider>
 
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                        color="primary"
-                        text
-                        @click="close"
-                    >
-                        Close
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-form>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                    color="primary"
+                    text
+                    @click="close"
+                >
+                    Close
+                </v-btn>
+            </v-card-actions>
+        </v-card>
     </vue-final-modal>
 </template>
 
@@ -77,5 +75,16 @@ const diffs = computed(() => Diff.diffChars(props.oldValue, props.newValue));
 
 .removed {
     background: #ff5631;
+}
+
+.modal-card {
+    max-height: 80vh;
+    display: flex;
+    flex-direction: column;
+}
+
+.card-content {
+    flex-grow: 1;
+    overflow: auto;
 }
 </style>
