05d0c4effa8b36198012f72cda365d79303edbc2
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 02:11:23 +0200

    Added small descriptions for the slicing and transformation functions

--- python-client/giskard/ml_worker/testing/functions/slicing.py
+++ python-client/giskard/ml_worker/testing/functions/slicing.py
@@ -4,37 +4,59 @@ from giskard.ml_worker.testing.registry.slicing_function import slicing_function
 
 
 @slicing_function(name='Short comment')
-def short_comment_slicing_fn(x: pd.Series, column_name: str, max_characters: int = 5) -> bool:
-    return len(x[column_name].split()) <= max_characters
+def short_comment_slicing_fn(x: pd.Series, column_name: str, max_words: int = 5) -> bool:
+    """
+    Filter the rows where the specified 'column_name' contains a short comment, defined as one with at most 'max_words'.
+    """
+    return len(x[column_name].split()) <= max_words
 
 
 @slicing_function(name="Keyword lookup")
 def keyword_lookup_slicing_fn(x: pd.Series, column_name: str, keywords: list[str]) -> bool:
+    """
+    Filter the rows where the specified 'column_name' contains at least one of the specified 'keywords'.
+    """
     return any(word in x[column_name].lower() for word in keywords)
 
 
 @slicing_function(name="Positive sentiment", row_level=False, tags=["sentiment"])
 def positive_sentiment_analysis(x: pd.DataFrame, column_name: str, threshold: float = 0.9) -> pd.DataFrame:
+    """
+    Filter the rows where the specified 'column_name' has a positive sentiment, as determined by a pre-trained sentiment analysis model.
+    """
     return _sentiment_analysis(x, column_name, threshold, None, "POSITIVE")
 
 
 @slicing_function(name="Offensive sentiment", row_level=False, tags=["sentiment"])
 def offensive_sentiment_analysis(x: pd.DataFrame, column_name: str, threshold: float = 0.9) -> pd.DataFrame:
+    """
+    Filter the rows where the specified 'column_name' has a offensive sentiment, as determined by a pre-trained sentiment analysis model.
+    """
     return _sentiment_analysis(x, column_name, threshold, "cardiffnlp/twitter-roberta-base-offensive", "offensive")
 
 
 @slicing_function(name="Irony sentiment", row_level=False, tags=["sentiment"])
 def irony_sentiment_analysis(x: pd.DataFrame, column_name: str, threshold: float = 0.9) -> pd.DataFrame:
+    """
+    Filter the rows where the specified 'column_name' has a ironic sentiment, as determined by a pre-trained sentiment analysis model.
+    """
     return _sentiment_analysis(x, column_name, threshold, "cardiffnlp/twitter-roberta-base-irony", "irony")
 
 
 @slicing_function(name="Hate sentiment", row_level=False, tags=["sentiment"])
 def hate_sentiment_analysis(x: pd.DataFrame, column_name: str, threshold: float = 0.9) -> pd.DataFrame:
+    """
+    Filter the rows where the specified 'column_name' has a hateful sentiment, as determined by a pre-trained sentiment analysis model.
+    """
     return _sentiment_analysis(x, column_name, threshold, "cardiffnlp/twitter-roberta-base-hate", "hate")
 
 
 @slicing_function(name="Emotion sentiment", row_level=False, tags=["sentiment"])
 def emotion_sentiment_analysis(x: pd.DataFrame, column_name: str, emotion: str, threshold: float = 0.9) -> pd.DataFrame:
+    """
+    Filter the rows where the specified 'column_name' has an emotion matching 'emotion', as determined by a pre-trained sentiment analysis model.
+    Possible emotion are: 'optimism', 'anger', 'sadness', 'joy'
+    """
     return _sentiment_analysis(x, column_name, threshold, "cardiffnlp/twitter-roberta-base-emotion", emotion)
 
 
