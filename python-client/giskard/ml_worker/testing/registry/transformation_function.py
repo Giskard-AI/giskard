f69cc56da1d32e703fd6bcee856b949af157bf89
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-01 02:11:23 +0200

    Apply transformation to cell level

--- python-client/giskard/ml_worker/testing/registry/transformation_function.py
+++ python-client/giskard/ml_worker/testing/registry/transformation_function.py
@@ -14,7 +14,7 @@ from giskard.ml_worker.testing.registry.decorators_utils import validate_arg_typ
     make_all_optional_or_suite_input, set_return_type
 from giskard.ml_worker.testing.registry.registry import get_object_uuid, tests_registry
 
-TransformationFunctionType = Callable[..., pd.Series]
+TransformationFunctionType = Callable[..., Union[pd.Series, pd.DataFrame]]
 
 default_tags = ['transformation']
 
@@ -22,6 +22,7 @@ default_tags = ['transformation']
 class TransformationFunction(Savable[TransformationFunctionType, DatasetProcessFunctionMeta]):
     func: TransformationFunctionType = None
     row_level: bool = True
+    cell_level: bool = False
     params = {}
     is_initialized = False
 
@@ -29,9 +30,11 @@ class TransformationFunction(Savable[TransformationFunctionType, DatasetProcessF
     def _get_name(cls) -> str:
         return 'transformations'
 
-    def __init__(self, func: TransformationFunctionType, row_level=True):
+    def __init__(self, func: TransformationFunctionType, row_level=True, cell_level=False):
         self.func = func
         self.row_level = row_level
+        self.cell_level = cell_level
+
         test_uuid = get_object_uuid(func)
         meta = tests_registry.get_test(test_uuid)
         if meta is None:
@@ -47,8 +50,17 @@ class TransformationFunction(Savable[TransformationFunctionType, DatasetProcessF
 
         return self
 
-    def execute(self, data: Union[pd.Series, pd.DataFrame]):
-        if self.row_level:
+    def execute(self, data: pd.DataFrame) -> pd.DataFrame:
+
+        if self.cell_level:
+            actual_params = {k: v for k, v in self.params.items() if k is not 'column_name'}
+
+            def apply(row: pd.Series) -> pd.Series:
+                row[self.params['column_name']] = self.func(row[self.params['column_name']], **actual_params)
+                return row
+
+            return data.apply(apply, axis=1)
+        elif self.row_level:
             return data.apply(lambda row: self.func(row, **self.params), axis=1)
         else:
             return self.func(data, **self.params)
@@ -90,7 +102,9 @@ class TransformationFunction(Savable[TransformationFunctionType, DatasetProcessF
 
 
 def transformation_function(_fn: Union[TransformationFunctionType, Type[TransformationFunction]] = None,
-                            row_level=True, name=None,
+                            row_level=True,
+                            cell_level=False,
+                            name=None,
                             tags: Optional[List[str]] = None):
     def inner(func: Union[TransformationFunctionType, Type[TransformationFunction]]) -> TransformationFunction:
 
@@ -101,7 +115,7 @@ def transformation_function(_fn: Union[TransformationFunctionType, Type[Transfor
                                        type='TRANSFORMATION'))
         if inspect.isclass(func) and issubclass(func, TransformationFunction):
             return func
-        return _wrap_transformation_function(func, row_level)()
+        return _wrap_transformation_function(func, row_level, cell_level)()
 
     if callable(_fn):
         return functools.wraps(_fn)(inner(_fn))
@@ -109,10 +123,12 @@ def transformation_function(_fn: Union[TransformationFunctionType, Type[Transfor
         return inner
 
 
-def _wrap_transformation_function(original: Callable, row_level: bool):
-    transformation_fn = functools.wraps(original)(TransformationFunction(original, row_level))
+def _wrap_transformation_function(original: Callable, row_level: bool, cell_level: bool):
+    transformation_fn = functools.wraps(original)(TransformationFunction(original, row_level, cell_level))
 
-    validate_arg_type(transformation_fn, 0, pd.Series if row_level else pd.DataFrame)
+    if not cell_level:
+        validate_arg_type(transformation_fn, 0, pd.Series if row_level else pd.DataFrame)
+
     drop_arg(transformation_fn, 0)
 
     make_all_optional_or_suite_input(transformation_fn)
