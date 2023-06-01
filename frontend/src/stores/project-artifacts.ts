94f4b9495368e6d8112a18d5c1a6f5b88a4a65f6
Henrique Chaves <henrique@giskard.ai>
2023-06-01 02:11:23 +0200

    Add update methods to project-artifacts store

--- frontend/src/stores/project-artifacts.ts
+++ frontend/src/stores/project-artifacts.ts
@@ -33,11 +33,22 @@ export const useProjectArtifactsStore = defineStore('projectArtifacts', {
         async loadModels() {
           if (this.projectId === null) return;
           this.models = await api.getProjectModels(this.projectId!);
+          this.models = this.models.sort((a, b) => new Date(a.createdDate) < new Date(b.createdDate) ? 1 : -1);
         },
         async loadProjectArtifacts() {
             if (this.projectId === null) return;
             await this.loadDatasets();
             await this.loadModels();
+        },
+        updateDataset(newDataset: DatasetDTO) {
+          const idx = this.datasets.findIndex(dataset => dataset.id === newDataset.id);
+          this.datasets[idx] = newDataset;
+          this.datasets = [...this.datasets];
+        },
+        updateModel(newModel: ModelDTO) {
+          const idx = this.models.findIndex(model => model.id === newModel.id);
+          this.models[idx] = newModel;
+          this.models = [...this.models];
         }
     }
 });
