cdfdef932bfffb7fa2bc336905bb708011ae4ad4
andreybavt <andreybavt@gmail.com>
2023-06-01 02:11:23 +0200

    remove tailwind

--- giskard-frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
