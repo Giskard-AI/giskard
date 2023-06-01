85affd2f07bd2415b61db9507dd6c430a833809f
Matteo Dora <matteo@giskard.ai>
2023-06-01 02:11:23 +0200

    Refactoring of performance tests and scanner
    - performs performance tests internally
    - support for different types of issues
    - refactoring of scan to be arbitrarily extensible and configurable

---
+++ python-client/giskard/scanner/issues.py
@@ -0,0 +1,37 @@
+from ..datasets import Dataset
+from ..models.base import BaseModel
+from ..ml_worker.testing.registry.slicing_function import SlicingFunction
+
+from typing import Optional
+
+
+class IssueInfo:
+    pass
+
+
+class Issue:
+    group: str = "Other"
+
+    domain: str
+    metric: str
+    deviation: str
+    description: str
+
+    def __init__(
+        self,
+        model: BaseModel,
+        dataset: Dataset,
+        level: str,
+        info: Optional[IssueInfo] = None,
+    ):
+        self.model = model
+        self.dataset = dataset
+        self.level = level.lower()
+        self.info = info
+
+    def __repr__(self):
+        return f"<Issue level='{self.level}'>"
+
+    @property
+    def is_major(self):
+        return self.level == "major"
