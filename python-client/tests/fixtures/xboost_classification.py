5d67471dd75c5897ec30bc4a5c66c1fd892e687f
Matteo Dora <matteo@giskard.ai>
2023-06-01 00:11:22 +0000

    Make linter happy!

--- python-client/tests/fixtures/xboost_classification.py
+++ python-client/tests/fixtures/xboost_classification.py
@@ -7,9 +7,7 @@ from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 
 from giskard.datasets.base import Dataset
-from giskard.ml_worker.utils.logging import Timer
 from giskard import Model
-from tests import path
 
 logger = logging.getLogger(__name__)
 
@@ -24,19 +22,15 @@ def breast_cancer_data() -> Dataset:
     raw_data = load_breast_cancer(as_frame=True)
     df = pd.concat([raw_data.data, raw_data.target], axis=1)
     column_types = {col: "numeric" for col in raw_data.data.columns}
-    return Dataset(
-        df,
-        name="breast_cancer",
-        target="target",
-        column_types=column_types
-    )
+    return Dataset(df, name="breast_cancer", target="target", column_types=column_types)
+
 
 @pytest.fixture()
 def breast_cancer_model(breast_cancer_data: Dataset) -> Model:
     X_train, X_test, y_train, y_test = train_test_split(
-        breast_cancer_data.df.loc[:, breast_cancer_data.df.columns!=TARGET_COLUMN_NAME],
+        breast_cancer_data.df.loc[:, breast_cancer_data.df.columns != TARGET_COLUMN_NAME],
         breast_cancer_data.df[TARGET_COLUMN_NAME],
-        random_state=RANDOM_SEED
+        random_state=RANDOM_SEED,
     )
     xgb = XGBClassifier(objective='binary:logistic')
     xgb.fit(X_train, y_train)
@@ -46,5 +40,5 @@ def breast_cancer_model(breast_cancer_data: Dataset) -> Model:
         feature_names=X_test.columns,
         name="breast_cancer_xgboost",
         classification_threshold=0.5,
-        classification_labels=[0, 1]
+        classification_labels=[0, 1],
     )
