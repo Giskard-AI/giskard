d836b3dce7ef037383a376ab9e4dd8516db4036c
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-05 09:17:46 +0200

    Added test filter in comparison screen

--- frontend/src/views/main/project/TestSuiteCompareExecutions.vue
+++ frontend/src/views/main/project/TestSuiteCompareExecutions.vue
@@ -3,11 +3,12 @@
     <v-col v-for="comparison in executionComparisons" :key="comparison.execution.id" class="pa-2" cols="6">
       <v-card>
         <v-card-title>
-          <div class="d-flex flex-column align-left pb-4">
-            <span class="input-execution">Model: {{ getModel(comparison.execution) }}</span>
-            <span class="input-execution">Dataset: {{ getDataset(comparison.execution) }}</span>
-          </div>
-          <TestSuiteExecutionHeader :execution="comparison.execution" :tests="comparison.tests" compact />
+            <div class="d-flex flex-column align-left pb-4">
+                <span class="input-execution">Model: {{ getModel(comparison.execution) }}</span>
+                <span class="input-execution">Dataset: {{ getDataset(comparison.execution) }}</span>
+            </div>
+            <TestSuiteExecutionHeader :execution="comparison.execution" :tests="comparison.tests" compact
+                                      :try-mode="false"/>
         </v-card-title>
         <v-card-text>
           <SuiteTestExecutionList :tests="comparison.tests" compact is-past-execution />
@@ -19,26 +20,26 @@
 
 <script lang="ts" setup>
 
-import { SuiteTestDTO, SuiteTestExecutionDTO, TestFunctionDTO, TestSuiteExecutionDTO } from '@/generated-sources';
-import { computed, ComputedRef } from 'vue';
-import { storeToRefs } from 'pinia';
-import { useTestSuiteStore } from '@/stores/test-suite';
-import { useRoute } from 'vue-router/composables';
+import {SuiteTestDTO, SuiteTestExecutionDTO, TestFunctionDTO, TestSuiteExecutionDTO} from '@/generated-sources';
+import {computed, ComputedRef} from 'vue';
+import {storeToRefs} from 'pinia';
+import {statusFilterOptions, useTestSuiteStore} from '@/stores/test-suite';
+import {useRoute} from 'vue-router/composables';
 import SuiteTestExecutionList from '@/views/main/project/SuiteTestExecutionList.vue';
 import TestSuiteExecutionHeader from '@/views/main/project/TestSuiteExecutionHeader.vue';
-import { useCatalogStore } from "@/stores/catalog";
-import { chain } from 'lodash';
+import {useCatalogStore} from "@/stores/catalog";
+import {chain} from 'lodash';
 
-const { executions, models, datasets, inputs, suite } = storeToRefs(useTestSuiteStore());
-const { testFunctionsByUuid } = storeToRefs(useCatalogStore());
+const {executions, models, datasets, inputs, suite, statusFilter, searchFilter} = storeToRefs(useTestSuiteStore());
+const {testFunctionsByUuid} = storeToRefs(useCatalogStore());
 
 type ExecutionComparison = {
-  execution: TestSuiteExecutionDTO,
-  tests: {
-    suiteTest: SuiteTestDTO,
-    test: TestFunctionDTO,
-    result?: SuiteTestExecutionDTO
-  }[]
+    execution: TestSuiteExecutionDTO,
+    tests: {
+        suiteTest: SuiteTestDTO,
+        test: TestFunctionDTO,
+        result?: SuiteTestExecutionDTO
+    }[]
 }
 
 const route = useRoute();
@@ -63,16 +64,31 @@ const executionComparisons: ComputedRef<ExecutionComparison[]> = computed(() => 
     : [];
 
   results.forEach(result => {
-    result.tests = suite.value === null ? [] : suite.value!.tests
-      .map(suiteTest => ({
-        suiteTest,
-        test: testFunctionsByUuid.value[suiteTest.testUuid],
-        result: result.execution?.results?.find(result => result.test.id === suiteTest.id)
-      }));
+      result.tests = suite.value === null ? [] : suite.value!.tests
+          .map(suiteTest => ({
+              suiteTest,
+              test: testFunctionsByUuid.value[suiteTest.testUuid],
+              result: result.execution?.results?.find(result => result.test.id === suiteTest.id)
+          }))
+          .filter(({result}) => statusFilterOptions.find(opt => statusFilter.value === opt.label)!.filter(result))
+          .filter(({suiteTest}) => {
+              const test = suiteTest.test;
+
+              const keywords = searchFilter.value.split(' ')
+                  .map(keyword => keyword.trim().toLowerCase())
+                  .filter(keyword => keyword !== '');
+              return keywords.filter(keyword =>
+                  test.name.toLowerCase().includes(keyword)
+                  || test.doc?.toLowerCase()?.includes(keyword)
+                  || test.displayName?.toLowerCase()?.includes(keyword)
+                  || test.tags?.filter(tag => tag.includes(keyword))?.length > 0
+              ).length === keywords.length;
+          });
   })
 
   return results;
 });
+
 const modelByUuid = computed(() => chain(models.value)
   .keyBy('id')
   .value()
