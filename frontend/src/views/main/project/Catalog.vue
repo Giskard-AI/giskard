45e64ed66860ea7e6fe2804fedaa84ac4a5d0571
Henrique Chaves <henrique@giskard.ai>
2023-06-05 15:23:55 +0200

    Use route name instead of path

--- frontend/src/views/main/project/Catalog.vue
+++ frontend/src/views/main/project/Catalog.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="vc mt-2 pb-0" v-if="catalog">
         <div>
-            <v-btn text outlined color="primary" class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-datasets' }" @click="trackTabChange('datasets')">
+            <v-btn text outlined color="primary" class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-datasets' }" @click="trackTabChange('project-catalog-datasets')">
                 <v-icon left small class="mr-1">mdi-database</v-icon>
                 Datasets
             </v-btn>
-            <v-btn text outlined color="primary" class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-models' }" @click="trackTabChange('models')">
+            <v-btn text outlined color="primary" class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-datasets' }" @click="trackTabChange('project-catalog-datasets')">
                 <v-icon left small class="mr-1">mdi-cube-outline</v-icon>
                 Models
             </v-btn>
-            <v-btn text outlined color="primary" class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-tests' }" @click="trackTabChange('tests')">
+            <v-btn text outlined color="primary" class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-tests' }" @click="trackTabChange('project-catalog-tests')">
                 <v-icon left small class="mr-1">mdi-test-tube</v-icon>
                 Tests
             </v-btn>
-            <v-btn text outlined color="primary" class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-slicing-functions' }" @click="trackTabChange('slicing-functions')">
+            <v-btn text outlined color="primary" class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-slicing-functions' }" @click="trackTabChange('project-catalog-slicing-functions')">
                 <v-icon left small class="mr-1">mdi-knife</v-icon>
                 Slicing functions
             </v-btn>
-            <v-btn text outlined color="primary" class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-transformation-functions' }" @click="trackTabChange('transformation-functions')">
+            <v-btn text outlined color="primary" class="ma-1 rounded-pill tab" :to="{ name: 'project-catalog-transformation-functions' }" @click="trackTabChange('project-catalog-transformation-functions')">
                 <v-icon left small class="mr-1">mdi-chart-bell-curve</v-icon>
                 Transformation functions
             </v-btn>
@@ -54,8 +54,8 @@ const refreshingRef = ref<() => void>();
 
 function trackTabChange(name: string) {
     mixpanel.track("Catalog tab change", {
-        fromTab: route.path.split('/').pop(),
-        toTab: name
+        from: route.name,
+        to: name
     });
 }
 
