2ff5bf857b3cb248e355bddf07a487081d2db552
Henrique Chaves <henrique@giskard.ai>
2023-06-05 11:52:44 +0200

    Track transformation function execution in Catalog

--- frontend/src/views/main/project/TransformationsCatalog.vue
+++ frontend/src/views/main/project/TransformationsCatalog.vue
@@ -162,9 +162,10 @@ import DatasetTable from "@/components/DatasetTable.vue";
 import SuiteInputListSelector from "@/components/SuiteInputListSelector.vue";
 import DatasetColumnSelector from "@/views/main/utils/DatasetColumnSelector.vue";
 import { alphabeticallySorted } from "@/utils/comparators";
-import {extractArgumentDocumentation} from "@/utils/python-doc.utils";
+import { extractArgumentDocumentation } from "@/utils/python-doc.utils";
 import IEditorOptions = editor.IEditorOptions;
 import CodeSnippet from "@/components/CodeSnippet.vue";
+import mixpanel from "mixpanel-browser";
 
 let props = defineProps<{
     projectId: number,
@@ -230,6 +231,10 @@ async function runTransformationFunction() {
             value: selectedColumn.value
         })
     }
+
+    mixpanel.track("Run transformation function from Catalog", {
+        transformationFunctionName: selected.value!.name,
+    });
 
     transformationResult.value = await api.datasetProcessing(props.projectId, selectedDataset.value!, [{
         uuid: selected.value!.uuid,
