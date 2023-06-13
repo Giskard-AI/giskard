97005a854a1bca1011988d1b8c282dec0987d6d5
Henrique Chaves <henriquechavesmm@gmail.com>
2023-06-13 10:31:05 +0200

    Send worker status every 5 seconds

--- backend/src/main/java/ai/giskard/web/socketio/SocketIOController.java
+++ backend/src/main/java/ai/giskard/web/socketio/SocketIOController.java
@@ -1,21 +1,25 @@
 package ai.giskard.web.socketio;
 
+import ai.giskard.service.ml.MLWorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import com.corundumstudio.socketio.SocketIOServer;
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
-
 
 @RestController
 public class SocketIOController {
 
     @Autowired
     private SocketIOServer socketServer;
+    private final MLWorkerService mlWorkerService;
 
-    public SocketIOController(SocketIOServer socketServer) {
+
+    public SocketIOController(SocketIOServer socketServer, MLWorkerService mlWorkerService) {
         this.socketServer = socketServer;
+        this.mlWorkerService = mlWorkerService;
     }
 
 
@@ -25,7 +29,11 @@ public class SocketIOController {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                socketServer.getBroadcastOperations().sendEvent("worker-status", "test");
+                socketServer.getBroadcastOperations().sendEvent("worker-status",
+                    new HashMap<String, Boolean>() {{
+                        put("connected", mlWorkerService.isExternalWorkerConnected());
+                    }}
+                );
             }
         }, 0, 5000);
     }
