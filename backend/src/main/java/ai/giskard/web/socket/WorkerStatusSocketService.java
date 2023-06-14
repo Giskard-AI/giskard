b53b893466adee99c071e0b865acb1066f2555b2
Kevin Messiaen <contact@kevin-messiaen.fr>
2023-06-14 10:05:06 +0200

    Better naming

--- backend/src/main/java/ai/giskard/web/socket/WorkerStatusSocketService.java
+++ backend/src/main/java/ai/giskard/web/socket/WorkerStatusSocketService.java
@@ -1,18 +1,18 @@
-package ai.giskard.web.socketio;
+package ai.giskard.web.socket;
 
 import ai.giskard.service.ml.MLWorkerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
-@Controller
+@Service
 @RequiredArgsConstructor
-public class SocketIOController {
+public class WorkerStatusSocketService {
 
     private final MLWorkerService mlWorkerService;
     private final SimpMessagingTemplate simpMessagingTemplate;
