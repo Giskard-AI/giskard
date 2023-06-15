9370a7820ff82f7ec7d2559c53a2ade05de1f1fb
Henrique Chaves <henriquechavesmm@gmail.com>
2023-06-15 14:29:16 +0200

    Display notification if state changes

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -2,6 +2,8 @@ import { reactive } from 'vue';
 import { Client } from '@stomp/stompjs';
 import { apiURL } from './env';
 import { httpUrlToWsUrl } from './utils';
+import { useMainStore } from './stores/main';
+import { TYPE } from 'vue-toastification';
 
 export const state = reactive({
   workerStatus: {
@@ -14,6 +16,21 @@ const client = new Client({
   onConnect: () => {
     client.subscribe('/topic/worker-status', message => {
       const data = JSON.parse(message.body);
+
+      if (!state.workerStatus.connected && data.connected) {
+        useMainStore().addNotification({
+          content: 'ML Worker is connected!',
+          color: TYPE.SUCCESS,
+        });
+      }
+
+      if (state.workerStatus.connected && !data.connected) {
+        useMainStore().addNotification({
+          content: 'ML Worker is disconnected!',
+          color: TYPE.ERROR,
+        });
+      }
+
       state.workerStatus.connected = data.connected;
     });
   },
