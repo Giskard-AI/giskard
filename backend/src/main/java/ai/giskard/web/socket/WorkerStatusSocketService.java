6b0a6691316ab76023c1a2ec558cec01db4cfdaa
Henrique Chaves <henriquechavesmm@gmail.com>
2023-07-06 12:33:56 +0200

    Add EventListener to UpdateWorkerStatusEvent

--- backend/src/main/java/ai/giskard/web/socket/WorkerStatusSocketService.java
+++ backend/src/main/java/ai/giskard/web/socket/WorkerStatusSocketService.java
@@ -1,17 +1,14 @@
 package ai.giskard.web.socket;
 
+import ai.giskard.event.UpdateWorkerStatusEvent;
 import ai.giskard.service.ml.MLWorkerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.messaging.SessionSubscribeEvent;
-
-import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
 
 @Service
 @RequiredArgsConstructor
@@ -20,24 +17,17 @@ public class WorkerStatusSocketService {
     private final MLWorkerService mlWorkerService;
     private final SimpMessagingTemplate simpMessagingTemplate;
 
-    @PostConstruct
-    public void init() {
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Map<String, Boolean> data = new HashMap<>();
-                data.put("connected", mlWorkerService.isExternalWorkerConnected());
-
-                simpMessagingTemplate.convertAndSend(
-                        "/topic/worker-status",
-                        data);
-            }
-        }, 0, 60000);
+    @EventListener
+    public void handleSessionSubscribeEvent(SessionSubscribeEvent event) {
+        sendCurrentStatus();
     }
 
     @EventListener
-    public void handleSessionSubscribeEvent(SessionSubscribeEvent event) {
+    public void handleUpdateWorkerStatusEvent(UpdateWorkerStatusEvent event) {
+        sendCurrentStatus();
+    }
+
+    public void sendCurrentStatus() {
         Map<String, Boolean> data = new HashMap<>();
         data.put("connected", mlWorkerService.isExternalWorkerConnected());
 
