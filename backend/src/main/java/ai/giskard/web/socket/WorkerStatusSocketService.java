705ac3eb4600aebea94bdd921cbf86f90ef6bf93
Henrique Chaves <henriquechavesmm@gmail.com>
2023-07-06 10:01:05 +0200

    Send message to websocket channel whenever new client subscribes

--- backend/src/main/java/ai/giskard/web/socket/WorkerStatusSocketService.java
+++ backend/src/main/java/ai/giskard/web/socket/WorkerStatusSocketService.java
@@ -2,8 +2,10 @@ package ai.giskard.web.socket;
 
 import ai.giskard.service.ml.MLWorkerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
@@ -31,6 +33,16 @@ public class WorkerStatusSocketService {
                         "/topic/worker-status",
                         data);
             }
-        }, 0, 5000);
+        }, 0, 60000);
+    }
+
+    @EventListener
+    public void handleSessionSubscribeEvent(SessionSubscribeEvent event) {
+        Map<String, Boolean> data = new HashMap<>();
+        data.put("connected", mlWorkerService.isExternalWorkerConnected());
+
+        simpMessagingTemplate.convertAndSend(
+                "/topic/worker-status",
+                data);
     }
 }
