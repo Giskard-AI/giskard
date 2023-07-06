8fc47b1650643520ee6e8ec03b904cb908ab7b7f
Andrey Avtomonov <andreybavt@gmail.com>
2023-07-06 12:37:17 +0200

    j2ts

--- frontend/src/generated-sources/ai/giskard/web/dto/ml/project-dto.ts
+++ frontend/src/generated-sources/ai/giskard/web/dto/ml/project-dto.ts
@@ -1,12 +1,11 @@
-import type { MLWorkerType } from './../../../domain/mlworker-type';
-import type { UserDTO } from './../user/user-dto';
+import type {MLWorkerType} from './../../../domain/mlworker-type';
+import type {UserDTO} from './../user/user-dto';
 
 /**
  * Generated from ai.giskard.web.dto.ml.ProjectDTO
  */
 export interface ProjectDTO {
-    createdDate: any /* TODO: Missing translation of java.time.Instant */
-    ;
+    createdDate: any /* TODO: Missing translation of java.time.Instant */;
     description: string;
     guests: UserDTO[];
     id: number;
--- frontend/src/generated-sources/ai/giskard/web/dto/ml/project-post-dto.ts
+++ frontend/src/generated-sources/ai/giskard/web/dto/ml/project-post-dto.ts
@@ -2,9 +2,8 @@
  * Generated from ai.giskard.web.dto.ml.ProjectPostDTO
  */
 export interface ProjectPostDTO {
-  description: string;
-  id?: number | null;
-  inspectionSettings?: any /* TODO: Missing translation of ai.giskard.web.dto.ml.InspectionSettings */ | null;
-  key?: string | null;
-  name: string;
+    description: string;
+    id?: number | null;
+    key?: string | null;
+    name: string;
 }
