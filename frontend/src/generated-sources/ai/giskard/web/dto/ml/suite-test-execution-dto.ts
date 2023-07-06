ce241be3fd61dc4fa804dd4c6b0625c2d9988cc8
Andrey Avtomonov <andreybavt@gmail.com>
2023-07-06 12:36:51 +0200

    j2ts

--- frontend/src/generated-sources/ai/giskard/web/dto/ml/suite-test-execution-dto.ts
+++ frontend/src/generated-sources/ai/giskard/web/dto/ml/suite-test-execution-dto.ts
@@ -1,24 +1,24 @@
-import type { SuiteTestDTO } from './../suite-test-dto';
-import type { TestResultMessageDTO } from './test-result-message-dto';
-import type { TestResult } from './../../../domain/ml/test-result';
+import type {SuiteTestDTO} from './../suite-test-dto';
+import type {TestResultMessageDTO} from './test-result-message-dto';
+import type {TestResult} from './../../../domain/ml/test-result';
 
 /**
  * Generated from ai.giskard.web.dto.ml.SuiteTestExecutionDTO
  */
 export interface SuiteTestExecutionDTO {
-  actualSlicesSize: number[];
-  inputs: { [key: string]: string };
-  messages: TestResultMessageDTO[];
-  metric: number;
-  missingCount: number;
-  missingPercent: number;
-  partialUnexpectedIndexList: number[];
-  referenceSlicesSize: number[];
-  status: TestResult;
-  test: SuiteTestDTO;
-  unexpectedCount: number;
-  unexpectedIndexList: number[];
-  unexpectedPercent: number;
-  unexpectedPercentNonmissing: number;
-  unexpectedPercentTotal: number;
+    actualSlicesSize: number[];
+    inputs: {[key: string]: string};
+    messages: TestResultMessageDTO[];
+    metric: number;
+    missingCount: number;
+    missingPercent: number;
+    partialUnexpectedIndexList: number[];
+    referenceSlicesSize: number[];
+    status: TestResult;
+    test: SuiteTestDTO;
+    unexpectedCount: number;
+    unexpectedIndexList: number[];
+    unexpectedPercent: number;
+    unexpectedPercentNonmissing: number;
+    unexpectedPercentTotal: number;
 }
