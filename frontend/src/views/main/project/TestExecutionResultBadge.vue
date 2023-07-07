58821272d513b075fbaebc5f6352844467f01ad9
Henrique Chaves <henriquechavesmm@gmail.com>
2023-07-07 14:05:24 +0200

    Migrate TestExecutionResultBadge to Composition API

--- frontend/src/views/main/project/TestExecutionResultBadge.vue
+++ frontend/src/views/main/project/TestExecutionResultBadge.vue
@@ -1,28 +1,17 @@
 <template>
   <div>
-    <v-alert
-        border='left'
-        type='error'
-        v-if="result.status === 'ERROR'">
+    <v-alert border='left' type='error' v-if="result.status === 'ERROR'">
       {{ result.message }}
     </v-alert>
     <template v-for='testResult in result.result'>
-      <v-alert
-        tile
-        class='test-results'
-        :color='getBadgeColor(result.status)'
-        :type='TEST_RESULT_DATA[result.status].type'
-        dismissible
-        close-icon='mdi-close'
-      >
+      <v-alert tile class='test-results' :color='getBadgeColor(result.status)' :type='TEST_RESULT_DATA[result.status].type' dismissible close-icon='mdi-close'>
         <div class='d-flex justify-space-between align-center'>
           <div class='text-h6'>Test {{ result.status.toLowerCase() }}</div>
           <div class='text-body-2'>{{ result.executionDate | date }}</div>
         </div>
         <div class='d-flex justify-space-between align-center'>
           <div class='text-body-2'>Metric: {{ testResult.result.metric }}</div>
-          <div class='text-body-2 text-right'
-               v-if='testResult.result.messages && testResult.result.messages.length'>
+          <div class='text-body-2 text-right' v-if='testResult.result.messages && testResult.result.messages.length'>
             {{ testResult.result.messages[0].text }}
           </div>
           <!--              <a class="text-body-2 results-link text-decoration-underline">Full results</a>-->
@@ -32,31 +21,20 @@
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
+<script setup lang="ts">
 import { TestResult, TestTemplateExecutionResultDTO } from '@/generated-sources';
 import { testStatusToColor } from '@/views/main/tests/test-utils';
-import { Prop } from 'vue-property-decorator';
-import Component from 'vue-class-component';
 import { TEST_RESULT_DATA } from '@/utils/tests.utils';
 
-@Component({
-  computed: {
-    TEST_RESULT_DATA() {
-      return TEST_RESULT_DATA;
-    }
-  }
-})
-export default class TestExecutionResultBadge extends Vue {
-  @Prop({ required: true }) result!: TestTemplateExecutionResultDTO;
+interface Props {
+  result: TestTemplateExecutionResultDTO;
+}
 
-  getBadgeColor(testStatus: TestResult) {
-    return testStatusToColor(testStatus);
-  }
+const props = defineProps<Props>();
 
+function getBadgeColor(testStatus: TestResult) {
+  return testStatusToColor(testStatus);
 }
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
