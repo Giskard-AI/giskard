4c56d91ecc641a5c132caa95bd1a6c1aa37cef5a
Henrique Chaves <henriquechavesmm@gmail.com>
2023-07-17 14:03:47 +0200

    Code smell

--- backend/src/main/java/ai/giskard/event/UpdateWorkerStatusEvent.java
+++ backend/src/main/java/ai/giskard/event/UpdateWorkerStatusEvent.java
@@ -3,8 +3,7 @@ package ai.giskard.event;
 import org.springframework.context.ApplicationEvent;
 
 public class UpdateWorkerStatusEvent extends ApplicationEvent {
-    public boolean isConnected;
-
+    private final boolean isConnected;
     public UpdateWorkerStatusEvent(Object source, boolean isConnected) {
         super(source);
         this.isConnected = isConnected;
