d8147ea31658daf9473d4e2c1294964019fc4978
Henrique Chaves <henriquechavesmm@gmail.com>
2023-07-18 11:41:22 +0200

    Fix AddDebuggingSessionModal input errors

--- frontend/src/components/AddDebuggingSessionModal.vue
+++ frontend/src/components/AddDebuggingSessionModal.vue
@@ -1,99 +1,3 @@
-<script setup lang="ts">
-import { api } from '@/api';
-import { TYPE } from "vue-toastification";
-import { DatasetDTO, ModelDTO } from "@/generated-sources";
-import DatasetSelector from '@/views/main/utils/DatasetSelector.vue';
-import ModelSelector from '@/views/main/utils/ModelSelector.vue';
-import { computed, onActivated, ref } from "vue";
-import { useMainStore } from "@/stores/main";
-import { useDebuggingSessionsStore } from "@/stores/debugging-sessions";
-import { state } from "@/socket";
-import StartWorkerInstructions from "@/components/StartWorkerInstructions.vue";
-
-
-const debuggingSessionsStore = useDebuggingSessionsStore();
-
-interface Props {
-  projectId: number;
-}
-
-const props = defineProps<Props>();
-
-const datasets = ref<DatasetDTO[]>([]);
-const models = ref<ModelDTO[]>([]);
-const currentStep = ref(1);
-const loading = ref(false);
-
-const dialog = ref(false);
-const sessionName = ref("");
-const selectedDataset = ref<DatasetDTO | null>(null);
-const selectedModel = ref<ModelDTO | null>(null);
-
-const isMLWorkerConnected = computed(() => {
-  return state.workerStatus.connected;
-});
-
-const missingValues = computed(() => {
-  if (selectedDataset.value === null || selectedModel.value === null) {
-    return true;
-  }
-  return false;
-});
-
-const emit = defineEmits(['createDebuggingSession'])
-
-async function createNewDebuggingSession() {
-
-  if (!isMLWorkerConnected.value) {
-    useMainStore().addNotification({
-      content: 'ML Worker is not connected. Please start the ML Worker first and try again.',
-      color: TYPE.ERROR,
-    });
-    throw new Error('ML Worker is not connected. Please start the ML Worker first and try again.');
-  }
-
-  loading.value = true;
-  try {
-    const newDebuggingSession = await debuggingSessionsStore.createDebuggingSession({
-      datasetId: selectedDataset.value!.id,
-      modelId: selectedModel.value!.id,
-      name: sessionName.value,
-      sample: true
-    });
-    debuggingSessionsStore.setCurrentDebuggingSessionId(newDebuggingSession.id);
-    emit('createDebuggingSession', newDebuggingSession);
-  } finally {
-    loading.value = false;
-    closeDialog();
-  }
-}
-
-function closeDialog() {
-  resetInputs();
-  dialog.value = false;
-}
-
-function resetInputs() {
-  selectedDataset.value = null;
-  selectedModel.value = null;
-  sessionName.value = "";
-  currentStep.value = 1;
-}
-
-async function loadDatasets() {
-  datasets.value = await api.getProjectDatasets(props.projectId);
-}
-
-async function loadModels() {
-  models.value = await api.getProjectModels(props.projectId);
-}
-
-onActivated(async () => {
-  await loadDatasets();
-  await loadModels();
-});
-</script>
-
 <template>
   <div class="text-center">
     <v-dialog v-model="dialog" width="60vw">
@@ -107,9 +11,9 @@ onActivated(async () => {
         <v-card-title class="headline">Create a new debugging session</v-card-title>
         <v-card-text>
           <v-text-field label="Session name (optional)" v-model="sessionName" class="selector" outlined dense hide-details></v-text-field>
-          <ModelSelector :projectId="projectId" :value.sync="selectedModel" class="selector"></ModelSelector>
+          <ModelSelector :projectId="projectId" :value.sync="selectedModelId" :return-object="false" label="Model" class="selector"></ModelSelector>
           <v-spacer></v-spacer>
-          <DatasetSelector :projectId="projectId" :value.sync="selectedDataset" :return-object="true" label="Dataset" class="selector"></DatasetSelector>
+          <DatasetSelector :projectId="projectId" :value.sync="selectedDatasetId" :return-object="false" label="Dataset" class="selector"></DatasetSelector>
         </v-card-text>
         <v-card-actions>
           <v-btn text @click="closeDialog">Cancel</v-btn>
@@ -128,6 +32,82 @@ onActivated(async () => {
     </v-dialog>
   </div>
 </template>
+
+<script setup lang="ts">
+import { TYPE } from "vue-toastification";
+import DatasetSelector from '@/views/main/utils/DatasetSelector.vue';
+import ModelSelector from '@/views/main/utils/ModelSelector.vue';
+import { computed, ref } from "vue";
+import { useMainStore } from "@/stores/main";
+import { useDebuggingSessionsStore } from "@/stores/debugging-sessions";
+import { state } from "@/socket";
+import StartWorkerInstructions from "@/components/StartWorkerInstructions.vue";
+
+const debuggingSessionsStore = useDebuggingSessionsStore();
+
+interface Props {
+  projectId: number;
+}
+
+const props = defineProps<Props>();
+
+const currentStep = ref(1);
+const loading = ref(false);
+const dialog = ref(false);
+const sessionName = ref("");
+const selectedDatasetId = ref<string | null>();
+const selectedModelId = ref<string | null>();
+
+const isMLWorkerConnected = computed(() => {
+  return state.workerStatus.connected;
+});
+
+const missingValues = computed(() => {
+  if (selectedDatasetId.value === null || selectedModelId.value === null) {
+    return true;
+  }
+  return false;
+});
+
+const emit = defineEmits(['createDebuggingSession'])
+
+async function createNewDebuggingSession() {
+  if (!isMLWorkerConnected.value) {
+    useMainStore().addNotification({
+      content: 'ML Worker is not connected. Please start the ML Worker first and try again.',
+      color: TYPE.ERROR,
+    });
+    throw new Error('ML Worker is not connected. Please start the ML Worker first and try again.');
+  }
+
+  loading.value = true;
+  try {
+    const newDebuggingSession = await debuggingSessionsStore.createDebuggingSession({
+      datasetId: selectedDatasetId.value!,
+      modelId: selectedModelId.value!,
+      name: sessionName.value,
+      sample: true
+    });
+    debuggingSessionsStore.setCurrentDebuggingSessionId(newDebuggingSession.id);
+    emit('createDebuggingSession', newDebuggingSession);
+  } finally {
+    loading.value = false;
+    closeDialog();
+  }
+}
+
+function closeDialog() {
+  resetInputs();
+  dialog.value = false;
+}
+
+function resetInputs() {
+  selectedDatasetId.value = null;
+  selectedModelId.value = null;
+  sessionName.value = "";
+  currentStep.value = 1;
+}
+</script>
 
 <style scoped>
 .selector {
