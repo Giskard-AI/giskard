678a018f05a18eb9e078d50f7e34d939b563c41a
Weixuan Xiao <weixuan@giskard.ai>
2023-07-27 09:16:53 +0200

    Fix after merging main to WebSocket MLWorker

--- backend/src/main/java/ai/giskard/service/TestSuiteExecutionService.java
+++ backend/src/main/java/ai/giskard/service/TestSuiteExecutionService.java
@@ -20,6 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
