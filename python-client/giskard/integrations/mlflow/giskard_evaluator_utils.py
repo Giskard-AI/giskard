89d894803b12569c37cdf7045119dcc66d882347
Rabah <rabah.khalek@gmail.com>
2023-07-28 14:35:01 +0200

    fixed format

--- python-client/giskard/integrations/mlflow/giskard_evaluator_utils.py
+++ python-client/giskard/integrations/mlflow/giskard_evaluator_utils.py
@@ -7,14 +7,13 @@ import langchain.chains.base
 from giskard import Model, Dataset, scan
 from giskard.core.core import SupportedModelTypes
 from giskard.core.model_validation import ValidationFlags
-from giskard.models.pyfunc import PyFuncModel
 
 logger = logging.getLogger(__name__)
 
 gsk_model_types = {
     "classifier": SupportedModelTypes.CLASSIFICATION,
     "regressor": SupportedModelTypes.REGRESSION,
-    "text": SupportedModelTypes.TEXT_GENERATION
+    "text": SupportedModelTypes.TEXT_GENERATION,
 }
 alphanumeric_map = {
     ">=": "greater than or equal to",
@@ -34,6 +33,7 @@ def unwrap_python_model_from_pyfunc(pyfunc_model):
     :return: unwrapped model
     """
     import importlib
+
     module = pyfunc_model.metadata.flavors["python_function"]["loader_module"]
     flavor = importlib.import_module(module)
     return flavor.load_model(pyfunc_model.metadata.get_model_info().model_uri)
@@ -43,32 +43,36 @@ def process_text(some_string):
     for k, v in alphanumeric_map.items():
         some_string = some_string.replace(k, v)
     some_string = some_string.replace("data slice", "data slice -")
-    some_string = re.sub(r'[^A-Za-z0-9_\-. /]+', '', some_string)
+    some_string = re.sub(r"[^A-Za-z0-9_\-. /]+", "", some_string)
 
     return some_string
 
 
 def setup_dataset(dataset, evaluator_config):
     data = dataset.features_data.copy()
-    data[dataset.targets_name] = dataset.labels_data
+    target = dataset.targets_name if hasattr(dataset, "targets_name") else None
+    if target:
+        data[target] = dataset.labels_data
     dataset_config = evaluator_config.get("dataset_config", None)
     if dataset_config is None:
-        return Dataset(df=data, target=dataset.targets_name, name=dataset.name)
+        return Dataset(df=data, target=target, name=dataset.name)
     else:
         config_set = set(dataset_config.keys())
         sign = inspect.signature(Dataset)
         sign_set = set(sign.parameters.keys())
         if config_set.issubset(sign_set):
             if "target" not in config_set:
-                dataset_config["target"] = dataset.targets_name
+                dataset_config["target"] = target
             if "name" not in config_set:
                 dataset_config["name"] = dataset.name
 
             return Dataset(df=data, **dataset_config)
         else:
-            raise ValueError(f"The provided parameters {config_set - sign_set} in dataset_config are not valid. "
-                             f"Make sure to pass only the attributes of giskard.Dataset "
-                             f"(see https://docs.giskard.ai/en/latest/reference/datasets).")
+            raise ValueError(
+                f"The provided parameters {config_set - sign_set} in dataset_config are not valid. "
+                f"Make sure to pass only the attributes of giskard.Dataset "
+                f"(see https://docs.giskard.ai/en/latest/reference/datasets)."
+            )
 
 
 def setup_model(model, model_type, feature_names, evaluator_config):
@@ -82,9 +86,7 @@ def setup_model(model, model_type, feature_names, evaluator_config):
             model = unwrapped_model
 
     if model_config is None:
-        return Model(model=model,
-                     model_type=gsk_model_types[model_type],
-                     feature_names=feature_names)
+        return Model(model=model, model_type=gsk_model_types[model_type], feature_names=feature_names)
     else:
         config_set = set(model_config.keys())
         sign = inspect.signature(Model)
@@ -95,12 +97,13 @@ def setup_model(model, model_type, feature_names, evaluator_config):
             if "feature_names" not in config_set:
                 model_config["feature_names"] = feature_names
 
-            return Model(model=model,
-                         **model_config)
+            return Model(model=model, **model_config)
         else:
-            raise ValueError(f"The provided parameters {config_set - sign_set} in model_config are not valid. "
-                             f"Make sure to pass only the attributes of giskard.Model "
-                             f"(see https://docs.giskard.ai/en/latest/reference/models).")
+            raise ValueError(
+                f"The provided parameters {config_set - sign_set} in model_config are not valid. "
+                f"Make sure to pass only the attributes of giskard.Model "
+                f"(see https://docs.giskard.ai/en/latest/reference/models)."
+            )
 
 
 def setup_scan(giskard_model, giskard_dataset, evaluator_config):
@@ -117,5 +120,7 @@ def setup_scan(giskard_model, giskard_dataset, evaluator_config):
         if config_set.issubset(sign_set):
             return scan(model=giskard_model, dataset=giskard_dataset, validation_flags=validation_flags, **scan_config)
         else:
-            raise ValueError(f"The provided parameters {config_set - sign_set} in scan_config are not valid. "
-                             f"Make sure to pass only the attributes of giskard.scan.")
+            raise ValueError(
+                f"The provided parameters {config_set - sign_set} in scan_config are not valid. "
+                f"Make sure to pass only the attributes of giskard.scan."
+            )
