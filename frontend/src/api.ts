e98d2b9e5aaf095d15dfe002af7084960d0e30ce
Weixuan Xiao <weixuan@giskard.ai>
2023-07-28 10:37:47 +0200

    Add a popover for input of HF access token

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios';
 import { apiURL } from '@/env';
-import { getLocalToken, removeLocalToken } from '@/utils';
+import { getLocalToken, removeLocalToken, getLocalHFToken } from '@/utils';
 import Vue from 'vue';
 
 import {
@@ -66,6 +66,15 @@ function jwtRequestInterceptor(config) {
     return config;
 }
 
+function hfRequestInterceptor(config) {
+    // Do something before request is sent
+    let hfToken = getLocalHFToken();
+    if (hfToken && config && config.headers) {
+        config.headers.Authorization = `Bearer ${hfToken}`;
+    }
+    return config;
+}
+
 function authHeaders(token: string | null) {
     return {
         headers: {
@@ -81,6 +90,9 @@ const axiosProject = axios.create({
 });
 const apiV2 = axios.create({
     baseURL: API_V2_ROOT,
+});
+const huggingface = axios.create({
+    baseURL: "https://huggingface.co/"
 });
 
 function unpackInterceptor(response) {
@@ -158,6 +170,8 @@ async function errorInterceptor(error) {
 apiV2.interceptors.response.use(unpackInterceptor, errorInterceptor);
 apiV2.interceptors.request.use(jwtRequestInterceptor);
 
+huggingface.interceptors.request.use(hfRequestInterceptor);
+
 axios.interceptors.request.use(jwtRequestInterceptor);
 
 // this is to automatically parse responses from the projects API, be it array or single objects
@@ -190,8 +204,8 @@ function downloadURL(urlString) {
 }
 
 export const api = {
-    async getHuggingFaceToken(spaceId: string) {
-      return await axios.get<unknown, any>(`https://huggingface.co/api/spaces/${spaceId}/jwt`);
+    async getHuggingFaceSpacesToken(spaceId: string) {
+      return await huggingface.get<unknown, any>(`https://huggingface.co/api/spaces/${spaceId}/jwt`);
     },
     async logInGetToken(username: string, password: string) {
         return apiV2.post<unknown, JWTToken>(`/authenticate`, {username, password});
---
+++ frontend/src/components/HuggingFaceTokenPopover.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-dialog v-model="opened" persistent width="500">
+    <v-card>
+      <v-card-title>
+        Running on Hugging Face Spaces
+      </v-card-title>
+      <v-card-text>
+        <p>Giskard Client and ML Worker need a Hugging Face access token to access your private Hugging Face Spaces.</p>
+        <p>Access your <a href="https://huggingface.co/settings/tokens">Hugging Face settings</a> to get one.</p>
+
+        <ValidationProvider name="Hugging Face access token" mode="eager" rules="required|min:30" v-slot="{errors}">
+          <v-text-field v-model="hfAccessToken" label="Hugging Face access token" type="text" :error-messages="errors"></v-text-field>
+        </ValidationProvider>
+
+        See <a href="https://huggingface.co/docs/hub/security-tokens#user-access-tokens">Hugging Face docs</a> for more information.
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="accent" text @click="$emit('submit', hfAccessToken);">
+          Fetch
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+
+import { ref } from "vue";
+
+const hfAccessToken = ref<string>("");
+const opened = ref<boolean>(true);
+
+</script>
