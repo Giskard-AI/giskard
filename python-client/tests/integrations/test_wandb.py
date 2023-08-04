dba9570e80a301817587a85dee341ddb12f6bac3
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-08-04 16:19:52 +0200

    functional tests implemented (GSK-1535)

--- python-client/tests/integrations/test_wandb.py
+++ python-client/tests/integrations/test_wandb.py
@@ -1,7 +1,8 @@
 import wandb
 import pytest
+import re
 
-# from giskard import scan
+from giskard import scan
 
 wandb.setup(wandb.Settings(mode="disabled", program=__name__, program_relpath=__name__, disable_code=True))
 
@@ -10,14 +11,44 @@ wandb.setup(wandb.Settings(mode="disabled", program=__name__, program_relpath=__
     "dataset_name,model_name",
     [
         ("german_credit_data", "german_credit_model"),
+        ("breast_cancer_data", "breast_cancer_model"),
+        ("drug_classification_data", "drug_classification_model"),
+        ("diabetes_dataset_with_target", "linear_regression_diabetes"),
+        ("hotel_text_data", "hotel_text_model"),
     ],
 )
-def test_logging_to_wandb(dataset_name, model_name, request):
-    giskard_dataset = request.getfixturevalue(dataset_name)
-    request.getfixturevalue(model_name)
+def test_fast(dataset_name, model_name, request):
+    dataset = request.getfixturevalue(dataset_name)
+    model = request.getfixturevalue(model_name)
+    _to_wandb(model, dataset)
 
-    giskard_dataset.to_wandb()
 
-    # scan_results = scan(giskard_model, giskard_dataset)
-    # scan_results.generate_test_suite()
-    # scan_results.
+@pytest.mark.parametrize(
+    "dataset_name,model_name",
+    [
+        ("enron_data_full", "enron_model"),
+        ("medical_transcript_data", "medical_transcript_model"),
+        ("fraud_detection_data", "fraud_detection_model"),
+        ("amazon_review_data", "amazon_review_model"),
+    ],
+)
+@pytest.mark.slow
+def test_slow(dataset_name, model_name, request):
+    dataset = request.getfixturevalue(dataset_name)
+    model = request.getfixturevalue(model_name)
+    _to_wandb(model, dataset)
+
+
+def _to_wandb(model, dataset):
+    # verify that the logging of a dataset works
+    dataset.to_wandb()
+
+    # verify that the logging of scan results works
+    scan_results = scan(model, dataset)
+    scan_results.to_wandb()
+
+    # verify that the logging of test suite results works
+    test_suite_results = scan_results.generate_test_suite().run()
+    test_suite_results.to_wandb()
+
+    assert re.match("^[0-9a-z]{8}$", str(wandb.run.id))
