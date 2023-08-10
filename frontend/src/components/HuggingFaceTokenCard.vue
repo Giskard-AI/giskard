af137822745f98497944815d510ddb6b9bafd5d0
Weixuan Xiao <weixuan@giskard.ai>
2023-08-10 12:05:37 +0200

    Polish HF access token input by user test feedback
    Add outline to text field.
    Open HF Settings page in a new tab/window.

--- frontend/src/components/HuggingFaceTokenCard.vue
+++ frontend/src/components/HuggingFaceTokenCard.vue
@@ -5,13 +5,13 @@
     </v-card-title>
     <v-card-text>
       <p>Giskard Client and ML Worker need a Hugging Face access token to access your private Hugging Face Spaces.</p>
-      <p>Access your <a href="https://huggingface.co/settings/tokens">Hugging Face settings</a> to get one.</p>
+      <p>Access your <a href="https://huggingface.co/settings/tokens" target=”_blank”>Hugging Face settings</a> to get one.</p>
 
       <ValidationProvider name="Hugging Face access token" mode="eager" rules="required|min:30" v-slot="{errors}">
-        <v-text-field v-model="hfAccessToken" label="Hugging Face access token" type="text" :error-messages="errors"></v-text-field>
+        <v-text-field outlined autofocus v-model="hfAccessToken" label="Hugging Face access token" type="text" :error-messages="errors"/>
       </ValidationProvider>
 
-      See <a href="https://huggingface.co/docs/hub/security-tokens#user-access-tokens">Hugging Face docs</a> for more information.
+      See <a href="https://huggingface.co/docs/hub/security-tokens#user-access-tokens" target=”_blank”>Hugging Face docs</a> for more information.
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
