3db3af9bd2d346f6dd4dc23e881b7cb7518717c7
Weixuan Xiao <weixuan@giskard.ai>
2023-08-16 13:49:51 +0200

    Remove duplicated code detected by SonarLint
    Make `DatasetProcessFunctionMetaDTO` child of `FunctionMetaDTO` in Java.

--- backend/src/main/java/ai/giskard/ml/dto/MLWorkerWSDatasetProcessFunctionMetaDTO.java
+++ backend/src/main/java/ai/giskard/ml/dto/MLWorkerWSDatasetProcessFunctionMetaDTO.java
@@ -2,32 +2,8 @@ package ai.giskard.ml.dto;
 
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
-public class MLWorkerWSDatasetProcessFunctionMetaDTO implements MLWorkerWSBaseDTO {
-    private String uuid;
-
-    private String name;
-
-    private String displayName;
-
-    private Integer version;
-
-    private String module;
-
-    private String doc;
-
-    private String moduleDoc;
-
-    private List<MLWorkerWSTestFunctionArgumentDTO> args;
-
-    private List<String> tags;
-
-    private String code;
-
-    private String type;
-
+public class MLWorkerWSDatasetProcessFunctionMetaDTO extends MLWorkerWSFunctionMetaDTO {
     private Boolean cellLevel;
 
     private String columnType;
