bb876c277804a4ef83ac20fb2ca559d08c984d29
Weixuan Xiao <weixuan@giskard.ai>
2023-08-26 16:40:45 +0200

    Remove HF Spaces token from `ApiTokenCard`
    To avoid too many synchronizations of the state between components.

--- frontend/src/components/ApiTokenCard.vue
+++ frontend/src/components/ApiTokenCard.vue
@@ -1,66 +1,28 @@
 <template>
-  <div height="100%">
-    <v-card height="100%">
-      <v-card-title class="font-weight-light secondary--text">API Access Token</v-card-title>
-      <v-card-text>
-        <div class="mb-2">
-          <v-btn small tile color="primaryLight" class="primaryLightBtn" @click="generateToken">Generate</v-btn>
-          <v-btn v-if="apiAccessToken && apiAccessToken.id_token" small tile color="secondary" class="ml-2" @click="copyToken">
-            Copy
-            <v-icon right dark>mdi-content-copy</v-icon>
-          </v-btn>
-        </div>
-        <v-row>
-          <v-col>
-            <div class="token-area-wrapper" v-if="apiAccessToken && apiAccessToken.id_token">
-              <CodeSnippet :code-content="apiAccessToken.id_token"/>
-            </div>
-          </v-col>
-        </v-row>
-        <v-row v-if="apiAccessToken && apiAccessToken.id_token">
-          <v-col class="text-right">
-            Expires on <span>{{ apiAccessToken.expiryDate | date }}</span>
-          </v-col>
-        </v-row>
-      </v-card-text>
-    </v-card>
-    <br/>
-    <v-card v-if="mainStore.appSettings?.isRunningOnHfSpaces" height="100%">
-      <v-card-title class="font-weight-light secondary--text">Giskard Space Token</v-card-title>
-      <v-card-text v-if="!needFetchWithHFAccessToken">
-        <div class="mb-2">
-          <v-btn small tile color="primaryLight" class="primaryLightBtn" @click="generateHFToken">Generate</v-btn>
-          <v-btn v-if="hfSpacesToken" small tile color="secondary" class="ml-2" @click="copyHFToken">
-            Copy
-            <v-icon right dark>mdi-content-copy</v-icon>
-          </v-btn>
-        </div>
-        <v-row v-if="hfSpacesToken">
-          <v-col>
-            <div class="token-area-wrapper">
-              <CodeSnippet :code-content="hfSpacesToken"/>
-            </div>
-          </v-col>
-        </v-row>
-        <v-row v-if="hfSpacesToken">
-          <v-col class="text-right">
-            Expires on <span>{{ hfSpacesTokenExpiredDate.toLocaleString() }}</span>
-          </v-col>
-        </v-row>
-      </v-card-text>
-      <HuggingFaceTokenCard v-else-if="state.workerStatus.connected" @submit="fetchAndSaveHFSpacesTokenWithAccessToken"/>
-      <v-card-text v-else>
-        <div class="mb-2">
-          <v-btn small tile color="primaryLight" class="primaryLightBtn" @click="generateHFToken">Generate</v-btn>
-        </div>
-        <v-row>
-          <v-col>
-            Please enter your Hugging Face access token in Giskard Settings page to generate this token.
-          </v-col>
-        </v-row>
-      </v-card-text>
-    </v-card>
-  </div>
+  <v-card height="100%">
+    <v-card-title class="font-weight-light secondary--text">API Access Token</v-card-title>
+    <v-card-text>
+      <div class="mb-2">
+        <v-btn small tile color="primaryLight" class="primaryLightBtn" @click="generateToken">Generate</v-btn>
+        <v-btn v-if="apiAccessToken && apiAccessToken.id_token" small tile color="secondary" class="ml-2" @click="copyToken">
+          Copy
+          <v-icon right dark>mdi-content-copy</v-icon>
+        </v-btn>
+      </div>
+      <v-row>
+        <v-col>
+          <div class="token-area-wrapper" v-if="apiAccessToken && apiAccessToken.id_token">
+            <CodeSnippet :code-content="apiAccessToken.id_token"/>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row v-if="apiAccessToken && apiAccessToken.id_token">
+        <v-col class="text-right">
+          Expires on <span>{{ apiAccessToken.expiryDate | date }}</span>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script setup lang="ts">
@@ -71,11 +33,6 @@ import {copyToClipboard} from "@/global-keys";
 import {useMainStore} from "@/stores/main";
 import {TYPE} from "vue-toastification";
 import CodeSnippet from "@/components/CodeSnippet.vue";
-import { state } from "@/socket";
-import { saveLocalHFToken } from "@/utils";
-import HuggingFaceTokenCard from "./HuggingFaceTokenCard.vue";
-
-import { attemptFetchHFSpacesToken } from "@/hf-utils";
 
 const mainStore = useMainStore();
 
@@ -98,47 +55,6 @@ async function generateToken() {
 
 async function copyToken() {
   await copyToClipboard(apiAccessToken.value?.id_token);
-  mainStore.addNotification({content: "Copied to clipboard", color: TYPE.SUCCESS});
-}
-
-const hfSpacesToken = ref<string | null>(null);
-const hfSpacesTokenExpiredDate = ref<Date>(new Date());
-const needFetchWithHFAccessToken = ref<boolean>(false);
-
-function updateHFTokenExpiration() {
-  hfSpacesTokenExpiredDate.value = new Date();
-  hfSpacesTokenExpiredDate.value.setDate(hfSpacesTokenExpiredDate.value.getDate() + 1); // Expire in 24 hours
-}
-
-async function generateHFToken() {
-  if (mainStore.appSettings?.isRunningOnHfSpaces) {
-    attemptFetchHFSpacesToken((token) => {
-      hfSpacesToken.value = token;
-      updateHFTokenExpiration();
-      needFetchWithHFAccessToken.value = false;
-    }, () => {
-      // Access Token seems invalidated or private Hugging Face Spaces
-      needFetchWithHFAccessToken.value = true;
-    });
-  }
-}
-
-async function fetchAndSaveHFSpacesTokenWithAccessToken(accessToken: string) {
-  if (mainStore.appSettings!.isRunningOnHfSpaces) {
-    saveLocalHFToken(accessToken);
-    await attemptFetchHFSpacesToken((token) => {
-      needFetchWithHFAccessToken.value = false;
-      hfSpacesToken.value = token;
-      updateHFTokenExpiration();
-    }, () => {
-      needFetchWithHFAccessToken.value = true;
-      mainStore.addNotification({content: 'Invalid Hugging Face access token', color: TYPE.ERROR});
-    });
-  }
-}
-
-async function copyHFToken() {
-  await copyToClipboard(hfSpacesToken.value);
   mainStore.addNotification({content: "Copied to clipboard", color: TYPE.SUCCESS});
 }
 
