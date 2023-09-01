15aa7c35909b99c9c28e046a483d1de8019ec22c
Weixuan Xiao <weixuan@giskard.ai>
2023-09-01 17:24:42 +0200

    Allow to set a default API key from properties or env
    Letting MLWorker in HF connect

--- backend/src/main/java/ai/giskard/domain/ApiKey.java
+++ backend/src/main/java/ai/giskard/domain/ApiKey.java
@@ -4,12 +4,14 @@ import ai.giskard.security.GalleryDatabaseOperationListener;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.apache.commons.lang3.RandomStringUtils;
 
 import java.util.UUID;
 
 @Entity(name = "api_keys")
 @Getter
+@Setter
 @NoArgsConstructor
 @EntityListeners(GalleryDatabaseOperationListener.class)
 public class ApiKey extends AbstractAuditingEntity {
--- backend/src/main/java/ai/giskard/service/ApiKeyService.java
+++ backend/src/main/java/ai/giskard/service/ApiKeyService.java
@@ -1,11 +1,15 @@
 package ai.giskard.service;
 
+import ai.giskard.config.ApplicationProperties;
 import ai.giskard.domain.ApiKey;
 import ai.giskard.domain.User;
 import ai.giskard.repository.ApiKeyRepository;
+import ai.giskard.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,11 +22,26 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 @RequiredArgsConstructor
 public class ApiKeyService {
+    private final Logger log = LoggerFactory.getLogger(ApiKeyService.class);
+
     private final ApiKeyRepository apiKeyRepository;
+    private final ApplicationProperties applicationProperties;
+    private final UserRepository userRepository;
     private final Map<String, ApiKey> apiKeysCache = new ConcurrentHashMap<>();
 
     @PostConstruct
     private void initCache() {
+        if (applicationProperties.getDefaultApiKey() != null) {
+            // Add a key provided from env to connect external MLWorker in HF
+            Optional<User> user = userRepository.findOneByLogin("admin");
+            if (user.isPresent() && ApiKey.doesStringLookLikeApiKey(applicationProperties.getDefaultApiKey())) {
+                ApiKey key = new ApiKey(user.get());
+                key.setKey(applicationProperties.getDefaultApiKey());
+                apiKeysCache.put(applicationProperties.getDefaultApiKey(), key);
+            } else {
+                log.warn("API Key provided but not conforming format.");
+            }
+        }
         apiKeyRepository.findAll().forEach(apiKey -> apiKeysCache.put(apiKey.getKey(), apiKey));
     }
 
