e7288fcc9134d64e73068edd0fe63305b896969d
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-09-05 14:27:59 +0200

    removed deprecated imports

--- python-client/giskard/scanner/__init__.py
+++ python-client/giskard/scanner/__init__.py
@@ -1,7 +1,5 @@
 from typing import Optional
 
-from giskard.core.model_validation import ValidationFlags
-
 from ..datasets.base import Dataset
 from ..models.base import BaseModel
 from .logger import logger
@@ -40,7 +38,6 @@ def scan(
     only=None,
     verbose=True,
     raise_exceptions=False,
-    validation_flags: Optional[ValidationFlags] = ValidationFlags(),
 ):
     """
     Scan a model with a dataset.
@@ -57,9 +54,6 @@ def scan(
         raise_exceptions (bool):
             Whether to raise an exception if detection errors are encountered. By default, errors are logged and
             handled gracefully, without interrupting the scan.
-        validation_flags (ValidationFlags):
-            Collection of flags to activate/deactivate model_validation flags
-
     """
     scanner = Scanner(params, only=only)
     return scanner.analyze(model, dataset, verbose=verbose, raise_exceptions=raise_exceptions)
