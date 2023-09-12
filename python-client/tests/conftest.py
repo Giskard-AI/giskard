9a0173d941ef7a6746b1afecc4d665ceb475a58a
Hartorn <hartorn.github@gmail.com>
2023-09-12 12:22:10 +0200

    Adding Github summary action for Java test

--- python-client/tests/conftest.py
+++ python-client/tests/conftest.py
@@ -29,7 +29,8 @@ def _write_details(writer: TextIOWrapper, title: str, content: str):
         writer.write(f"```\n{content}\n```\n")
         writer.write("\n</details>\n")
 
-def _write_report(writer: TextIOWrapper, report : BaseReport):
+
+def _write_report(writer: TextIOWrapper, report: BaseReport):
     writer.write(f"\n\n<h4 id=user-content-{report.html_id}>{report.nodeid}</h4>\n\n")
     writer.write(f"```python\n{report.longreprtext}\n```\n")
     _write_details(writer, "Log", report.caplog)
@@ -45,7 +46,9 @@ def pytest_terminal_summary(
     with file.open("w", encoding="utf-8") as writer:
         # Add overall test results
         writer.write("### Pytest report\n")
-        writer.write(f"Test results: {ExitCode(exitstatus).name} {'✅' if exitstatus == ExitCode.OK else '❌'}\n\n")
+        writer.write(
+            f"Test results: {ExitCode(exitstatus).name} {'✅' if exitstatus == ExitCode.OK else '❌'}\n\n"
+        )
 
         writer.write("\n| Status |Count |\n")
         writer.write("| :---: | :---: |\n")
@@ -72,7 +75,12 @@ def pytest_terminal_summary(
         writer.write("<details>\n")
         writer.write("<summary>Expand here</summary>\n\n")
 
-        all_tests : List[Tuple[str, str]] = [(v.nodeid, k) for k, l in terminalreporter.stats.items() if k and k != "warnings" for v in l]
+        all_tests: List[Tuple[str, str]] = [
+            (v.nodeid, k)
+            for k, report_list in terminalreporter.stats.items()
+            if k and k != "warnings"
+            for v in report_list
+        ]
         for name, status in sorted(all_tests, key=lambda elt: elt[0]):
             writer.write(f"- {name} {status.upper()}\n")
         writer.write("\n</details>\n\n")
@@ -81,11 +89,15 @@ def pytest_terminal_summary(
         # Note: html id/anchor must be prefixed by user-content- for Github
         writer.write("### Summary of failures and errors\n\n")
         for error in errors:
-            error.html_id = str(uuid4()) 
-            writer.write(f"- [Error : {error.nodeid.split('::')[-1]}](#user-content-{error.html_id})\n") 
+            error.html_id = str(uuid4())
+            writer.write(
+                f"- [Error : {error.nodeid.split('::')[-1]}](#user-content-{error.html_id})\n"
+            )
         for failure in failures:
-            failure.html_id = str(uuid4()) 
-            writer.write(f"- [Failure : {failure.nodeid.split('::')[-1]}](#user-content-{failure.html_id})\n") 
+            failure.html_id = str(uuid4())
+            writer.write(
+                f"- [Failure : {failure.nodeid.split('::')[-1]}](#user-content-{failure.html_id})\n"
+            )
 
         writer.write("\n### Errors\n\n")
         for error in errors:
