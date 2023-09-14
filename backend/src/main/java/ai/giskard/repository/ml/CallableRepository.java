ab5f0140d39e7d2c4459ed1423dd3ae897cd2eb0
Kevin Messiaen <114553769+kevinmessiaen@users.noreply.github.com>
2023-09-14 18:46:33 +0700

    Feature/gsk 1243 project export doesnt work (#1204)
    * Fixed recursive loop when ewporting project
    
    * Fixed export and import serialization
    
    * WIP
    
    * Includes slices, tests and transformations in export
    
    * Reading back data
    
    * Reading back data
    
    * Fixed import of suite
    
    * Fixed equals of callable objects
    
    * Polish code for maintainability from SonarLint
    
    * Remove unused export project function
    
    * Use simple notification to notify failed exporting
    
    * Fix error handling in async export project
    
    * Added equals
    
    * Hash code call super
    
    ---------
    
    Co-authored-by: Andrey Avtomonov <andreybavt@gmail.com>
    Co-authored-by: Inoki <weixuan@giskard.ai>

--- backend/src/main/java/ai/giskard/repository/ml/CallableRepository.java
+++ backend/src/main/java/ai/giskard/repository/ml/CallableRepository.java
@@ -4,11 +4,27 @@ import ai.giskard.domain.Callable;
 import ai.giskard.repository.MappableJpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @NoRepositoryBean
 public interface CallableRepository<E extends Callable> extends MappableJpaRepository<E, UUID> {
 
     int countByDisplayName(String displayName);
+
+    default List<E> saveAllIfNotExists(Collection<E> entities) {
+        List<E> existing = findAllById(entities.stream().map(E::getUuid).collect(Collectors.toSet()));
+
+        entities.stream()
+            .filter(Predicate.not(existing::contains))
+            .forEach(callable -> callable.getArgs().forEach(functionArgument -> functionArgument.setFunction(callable)));
+        return saveAll(entities.stream()
+            .filter(Predicate.not(existing::contains))
+            .toList()
+        );
+    }
 
 }
