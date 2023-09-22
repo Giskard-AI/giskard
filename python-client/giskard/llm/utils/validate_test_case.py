995cdbbd2dfbcd311feebf69bf0a6233d62db3c6
Kevin Messiaen <kevin.messiaen@icloud.com>
2023-09-22 17:43:43 +0700

    Added format instruction and examples

--- python-client/giskard/llm/utils/validate_test_case.py
+++ python-client/giskard/llm/utils/validate_test_case.py
@@ -39,6 +39,18 @@ You are a prompt QA responsible for evaluating the LLM answer based on test asse
 ``` 
 {model_description}
 ```
+
+**Format Instructions**: 
+Please provide a JSON object as a response, including the following keys:
+- Score: A number ranging from 1 to 5:
+  - 1 indicates that the answer does not meet the criteria at all
+  - 3 indicates that the answer can be improved
+  - 5 indicates that the answer completely meets the criteria
+- Reason: A text that clearly explains the given score
+- Tip: A text that offers a clear and descriptive suggestion on how to enhance the model
+
+Example: 
+{{"score": 1, "reason": "The answer is missing the required key 'tips' in the JSON response", "tip": "To ensure that the schema is respected, provide an example of the expected JSON response"}}
 """,
         ),
         (
