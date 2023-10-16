23791fde98a9ff9b60fdb0ddbf57ffa128e8c42a
Matteo Dora <matteo@giskard.ai>
2023-10-16 19:25:43 +0200

    Add little test for LLM utils

--- tests/scan/test_llm_utils.py
+++ tests/scan/test_llm_utils.py
@@ -1,11 +1,45 @@
-from giskard.scanner.llm.utils import load_default_dataset
+from unittest.mock import patch
+from giskard.scanner.llm.utils import llm, llm_fn_call
+
+import openai
+
+RAW_DEMO_OPENAI_RESPONSE = {
+    "id": "chatcmpl-abc123",
+    "object": "chat.completion",
+    "created": 1677858242,
+    "model": "gpt-3.5-turbo-0613",
+    "usage": {"prompt_tokens": 13, "completion_tokens": 7, "total_tokens": 20},
+    "choices": [{"message": {"role": "assistant", "content": "This is a test!"}, "finish_reason": "stop", "index": 0}],
+}
+
+DEMO_OPENAI_RESPONSE = openai.openai_object.OpenAIObject.construct_from(RAW_DEMO_OPENAI_RESPONSE)
 
 
-def test_default_dataset_loader():
-    dataset = load_default_dataset()
+@patch("giskard.scanner.llm.utils.openai")
+def test_llm_call(openai):
+    openai.ChatCompletion.create.return_value = DEMO_OPENAI_RESPONSE
+    res = llm([{"role": "system", "content": "Hello"}], temperature=0.11, max_tokens=1)
 
-    assert dataset.columns.tolist() == ["type", "question", "best_answer"]
-    assert dataset.column_types["type"] == "category"
-    assert dataset.column_types["question"] == "text"
-    assert dataset.column_types["best_answer"] == "text"
-    assert len(dataset.df) > 100
+    assert openai.ChatCompletion.create.call_count == 1
+    assert openai.ChatCompletion.create.call_args[1]["messages"] == [{"role": "system", "content": "Hello"}]
+    assert openai.ChatCompletion.create.call_args[1]["temperature"] == 0.11
+    assert openai.ChatCompletion.create.call_args[1]["max_tokens"] == 1
+
+    assert res == "This is a test!"
+
+
+@patch("giskard.scanner.llm.utils.openai")
+def test_llm_function_call(openai):
+    openai.ChatCompletion.create.return_value = DEMO_OPENAI_RESPONSE
+
+    res = llm_fn_call(
+        [{"role": "system", "content": "Hello"}], functions=[{"name": "demo"}], temperature=0.11, max_tokens=1
+    )
+
+    assert openai.ChatCompletion.create.call_count == 1
+    assert openai.ChatCompletion.create.call_args[1]["functions"] == [{"name": "demo"}]
+    assert openai.ChatCompletion.create.call_args[1]["messages"] == [{"role": "system", "content": "Hello"}]
+    assert openai.ChatCompletion.create.call_args[1]["temperature"] == 0.11
+    assert openai.ChatCompletion.create.call_args[1]["max_tokens"] == 1
+
+    assert res == {"role": "assistant", "content": "This is a test!"}
