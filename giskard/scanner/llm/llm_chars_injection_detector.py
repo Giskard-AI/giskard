772e3859bbfee58dbb45bb6c94f9aeef742ce54b
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-10-17 12:20:42 +0200

    typo

--- giskard/scanner/llm/llm_chars_injection_detector.py
+++ giskard/scanner/llm/llm_chars_injection_detector.py
@@ -56,12 +56,12 @@ class LLMCharsInjectionDetector:
             for char in self.control_chars:
                 injected_sequence = char * self.num_repetitions
 
-                def _add_prefix(df):
+                def _add_suffix(df):
                     dx = df.copy()
                     dx[feature] = dx[feature].astype(str) + injected_sequence
                     return dx
 
-                perturbed_dataset = dataset_sample.transform(_add_prefix, row_level=False)
+                perturbed_dataset = dataset_sample.transform(_add_suffix, row_level=False)
 
                 predictions = model.predict(perturbed_dataset)
 
