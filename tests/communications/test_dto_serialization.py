d10b897c80b5dad128d4bf99dd6b2f30c3c452e4
Inoki <weixuan@giskard.ai>
2023-10-20 20:33:28 +0800

    [GSK-1508] Add unit tests for WebSocket-based ML Worker communication (python) (#1277)
    * Add unit tests for DTO serialization in push (python)
    
    (Squashed commits of @Inokinoki)
    
    Contributes to #GSK-1508
    
    * Move WebSocket test code to `tests/`
    
    * Update pdm lock file
    
    * Fix lockfile
    
    ---------
    
    Co-authored-by: Bazire <bazire@giskard.ai>
    Co-authored-by: Hartorn <hartorn.github@gmail.com>

---
+++ tests/communications/test_dto_serialization.py
@@ -0,0 +1,374 @@
+from typing import Dict, List, Optional, Type
+
+import json
+from pathlib import Path
+
+import pydantic
+import pytest
+from packaging import version
+from polyfactory.factories.pydantic_factory import ModelFactory
+from pydantic import BaseModel
+
+import giskard
+import giskard.ml_worker.websocket
+
+IS_PYDANTIC_V2 = version.parse(pydantic.version.VERSION) >= version.parse("2.0")
+if IS_PYDANTIC_V2:
+    from pydantic.fields import FieldInfo as ModelField
+else:
+    from pydantic.fields import ModelField
+
+FILTERED_CLASSES = [BaseModel]
+
+MANDATORY_FIELDS = {
+    "ArtifactRef": ["id"],
+    "Catalog": ["tests", "slices", "transformations"],
+    "DataFrame": ["rows"],
+    "DataRow": ["columns"],
+    "DatasetMeta": [],
+    "DatasetProcessFunctionMeta": ["uuid"],
+    "DatasetProcessing": ["datasetId", "totalRows"],
+    "DatasetProcessingFunction": [],
+    "DatasetProcessingParam": ["dataset"],
+    "DatasetRowModificationResult": ["rowId", "modifications"],
+    "EchoMsg": ["msg"],
+    "Empty": [],
+    "ErrorReply": ["error_str", "error_type"],
+    "Explain": ["explanations"],
+    "ExplainParam": ["model", "dataset", "columns"],
+    "ExplainText": ["weights"],
+    "ExplainTextParam": ["model", "feature_name", "columns", "column_types"],
+    "Explanation": ["per_feature"],
+    "FuncArgument": ["name", "none"],
+    "FunctionMeta": ["uuid"],
+    "GenerateTestSuite": [],
+    "GenerateTestSuiteParam": ["project_key"],
+    "GeneratedTestInput": ["name", "value", "is_alias"],
+    "GeneratedTestSuite": ["test_uuid"],
+    "GetInfo": [
+        "platform",
+        "interpreter",
+        "interpreterVersion",
+        "installedPackages",
+        "mlWorkerId",
+        "isRemote",
+        "pid",
+        "processStartTime",
+        "giskardClientVersion",
+    ],
+    "GetInfoParam": ["list_packages"],
+    "GetPushParam": ["model", "dataset", "target", "column_types", "column_dtypes"],
+    "GetPushResponse": [],
+    "IdentifierSingleTestResult": ["id", "result"],
+    "ModelMeta": [],
+    "NamedSingleTestResult": ["testUuid", "result"],
+    "PartialUnexpectedCounts": ["count"],
+    "Platform": ["machine", "node", "processor", "release", "system", "version"],
+    "Push": ["kind", "push_title", "push_details"],
+    "PushAction": ["object_uuid"],
+    "PushDetails": ["action", "explanation", "button", "cta"],
+    "RunAdHocTest": [],
+    "RunAdHocTestParam": ["testUuid"],
+    "RunModelForDataFrame": [],
+    "RunModelForDataFrameParam": ["model", "dataframe", "target", "column_types", "column_dtypes"],
+    "RunModelParam": ["model", "dataset", "inspectionId", "project_key"],
+    "SingleTestResult": ["passed"],
+    "SuiteInput": ["name", "type"],
+    "SuiteTestArgument": ["id", "testUuid"],
+    "TestFunctionArgument": ["name", "type", "optional", "default", "argOrder"],
+    "TestMessage": ["type", "text"],
+    "TestSuite": ["is_error", "is_pass", "logs"],
+    "TestSuiteParam": [],
+    "WeightsPerFeature": [],
+    "WorkerReply": [],
+}
+
+OPTIONAL_FIELDS = {
+    "ArtifactRef": ["project_key", "sample"],
+    "Catalog": [],
+    "DataFrame": [],
+    "DataRow": [],
+    "DatasetMeta": ["target"],
+    "DatasetProcessFunctionMeta": [
+        "name",
+        "displayName",
+        "version",
+        "module",
+        "doc",
+        "moduleDoc",
+        "args",
+        "tags",
+        "code",
+        "type",
+        "cellLevel",
+        "columnType",
+        "processType",
+    ],
+    "DatasetProcessing": ["filteredRows", "modifications"],
+    "DatasetProcessingFunction": ["slicingFunction", "transformationFunction", "arguments"],
+    "DatasetProcessingParam": ["functions"],
+    "DatasetRowModificationResult": [],
+    "EchoMsg": [],
+    "Empty": [],
+    "ErrorReply": ["detail"],
+    "Explain": [],
+    "ExplainParam": [],
+    "ExplainText": ["words"],
+    "ExplainTextParam": [],
+    "Explanation": [],
+    "FuncArgument": [
+        "model",
+        "dataset",
+        "float",
+        "int",
+        "str",
+        "bool",
+        "slicingFunction",
+        "transformationFunction",
+        "kwargs",
+        "args",
+    ],
+    "FunctionMeta": [
+        "name",
+        "displayName",
+        "version",
+        "module",
+        "doc",
+        "moduleDoc",
+        "args",
+        "tags",
+        "code",
+        "type",
+        "debugDescription",
+    ],
+    "GenerateTestSuite": ["tests"],
+    "GenerateTestSuiteParam": ["inputs"],
+    "GeneratedTestInput": [],
+    "GeneratedTestSuite": ["inputs"],
+    "GetInfo": [],
+    "GetInfoParam": [],
+    "GetPushParam": ["dataframe", "push_kind", "cta_kind"],
+    "GetPushResponse": ["contribution", "perturbation", "overconfidence", "borderline", "action"],
+    "IdentifierSingleTestResult": ["arguments"],
+    "ModelMeta": ["model_type"],
+    "NamedSingleTestResult": [],
+    "PartialUnexpectedCounts": ["value"],
+    "Platform": [],
+    "Push": ["key", "value"],
+    "PushAction": ["arguments"],
+    "PushDetails": [],
+    "RunAdHocTest": ["results"],
+    "RunAdHocTestParam": ["arguments", "debug"],
+    "RunModelForDataFrame": ["all_predictions", "prediction", "probabilities", "raw_prediction"],
+    "RunModelForDataFrameParam": [],
+    "RunModelParam": [],
+    "SingleTestResult": [
+        "is_error",
+        "messages",
+        "props",
+        "metric",
+        "missing_count",
+        "missing_percent",
+        "unexpected_count",
+        "unexpected_percent",
+        "unexpected_percent_total",
+        "unexpected_percent_nonmissing",
+        "partial_unexpected_index_list",
+        "partial_unexpected_counts",
+        "unexpected_index_list",
+        "output_df",
+        "number_of_perturbed_rows",
+        "actual_slices_size",
+        "reference_slices_size",
+        "output_df_id",
+    ],
+    "SuiteInput": ["modelMeta", "datasetMeta"],
+    "SuiteTestArgument": ["arguments"],
+    "TestFunctionArgument": [],
+    "TestMessage": [],
+    "TestSuite": ["results"],
+    "TestSuiteParam": ["tests", "globalArguments"],
+    "WeightsPerFeature": ["weights"],
+    "WorkerReply": [],
+}
+ALIASED_FIELDS = {
+    "FuncArgument": {"float_arg": "float", "int_arg": "int", "str_arg": "str", "bool_arg": "bool", "is_none": "none"}
+}
+
+
+def get_all_dto_classes() -> List[Type[BaseModel]]:
+    klasses: List[Type[BaseModel]] = []
+    for name in dir(giskard.ml_worker.websocket):
+        maybe_klass = getattr(giskard.ml_worker.websocket, name)
+        if isinstance(maybe_klass, type) and issubclass(maybe_klass, BaseModel) and maybe_klass not in FILTERED_CLASSES:
+            klasses.append(maybe_klass)
+
+    return klasses
+
+
+def get_fields(klass: Type[BaseModel]) -> Dict[str, ModelField]:
+    if IS_PYDANTIC_V2:
+        return klass.model_fields
+    return klass.__fields__
+
+
+def get_name(name: str, field: ModelField) -> str:
+    return field.alias if field.alias is not None else name
+
+
+def get_alias(field: ModelField) -> Optional[str]:
+    if IS_PYDANTIC_V2:
+        return field.alias
+    return field.alias if field.has_alias else None
+
+
+def is_required(field: ModelField) -> bool:
+    if IS_PYDANTIC_V2:
+        return field.is_required()
+    return field.required
+
+
+ALL_DTOS = [pytest.param((klass), id=klass.__name__) for klass in get_all_dto_classes()]
+
+
+# Goal is to ensure every DTO is tested properly
+def test_list_mandatory_all_classes():
+    missing_classes: List[Type[BaseModel]] = []
+    for param_set in ALL_DTOS:
+        klass = param_set.values[0]
+        if klass.__name__ not in MANDATORY_FIELDS.keys():
+            missing_classes.append(klass.__name__)
+    output = "\n  -".join([elt.__qualname__ for elt in missing_classes])
+    if len(missing_classes) > 0:
+        raise ValueError(f"All dtos should be tested here, missing ones are :\n  -{output}")
+
+
+def test_list_optional_all_classes():
+    missing_classes: List[Type[BaseModel]] = []
+    for param_set in ALL_DTOS:
+        klass = param_set.values[0]
+        if klass.__name__ not in OPTIONAL_FIELDS.keys():
+            missing_classes.append(klass.__name__)
+    output = "\n  -".join([elt.__qualname__ for elt in missing_classes])
+    if len(missing_classes) > 0:
+        raise ValueError(f"All dtos should be tested here, missing ones are :\n  -{output}")
+
+
+# Goal is to ensure that no fields is becoming mandatory by error
+@pytest.mark.parametrize("klass", ALL_DTOS)
+def test_dto_ensure_all_mandatory_values(klass: Type[BaseModel]):
+    # Here we ensure all mandatory keys are present in dto_mandatory
+    mandatory_field_names = [get_name(name, field) for name, field in get_fields(klass).items() if is_required(field)]
+    assert set(mandatory_field_names) == set(MANDATORY_FIELDS.get(klass.__name__, []))
+
+
+# Goal is to ensure that no fields is becoming optional by error
+@pytest.mark.parametrize("klass", ALL_DTOS)
+def test_dto_ensure_all_optional_values(klass: Type[BaseModel]):
+    # Here we ensure all optional keys are present in dto_mandatory
+    mandatory_field_names = [
+        get_name(name, field) for name, field in get_fields(klass).items() if not is_required(field)
+    ]
+    assert set(mandatory_field_names) == set(OPTIONAL_FIELDS.get(klass.__name__, []))
+
+
+# Goal is to ensure that no alias has been changed by error
+@pytest.mark.parametrize("klass", ALL_DTOS)
+def test_dto_verify_mapping(klass: Type[BaseModel]):
+    # Here we ensure all optional keys are present in dto_mandatory
+    mapping_dto = ALIASED_FIELDS.get(klass.__name__, {})
+    aliased_fields = []
+    attr_name_aliased = []
+
+    for name, field in get_fields(klass).items():
+        if get_alias(field) is not None:
+            aliased_fields.append(field.alias)
+            attr_name_aliased.append(name)
+            assert mapping_dto.get(name) == field.alias
+    # Ensure no alias have been removed or created by error
+    assert set(aliased_fields) == set(mapping_dto.values())
+    assert set(attr_name_aliased) == set(mapping_dto.keys())
+
+
+@pytest.mark.parametrize("klass", ALL_DTOS)
+def test_serialisation_dtos(klass: BaseModel):
+    fixtures_dir = Path(__file__).parent / "fixtures"
+    with (fixtures_dir / "with_alias.json").open("r", encoding="utf-8") as fp:
+        all_datas = json.load(fp)
+        for instance_dict in all_datas[klass.__name__]:
+            # Should not fail
+            instance = klass(**instance_dict)
+            assert json.loads(instance.json(by_alias=True)) == instance_dict
+
+
+def print_all_mapping():
+    res = {}
+    for param_set in ALL_DTOS:
+        dto = param_set.values[0]
+        mandatory_field_names = {
+            name: get_alias(field) for name, field in get_fields(dto).items() if get_alias(field) is not None
+        }
+        if len(mandatory_field_names) > 0:
+            res[dto.__name__] = mandatory_field_names
+    print(res)
+
+
+def print_all_mandatory_values():
+    res = {}
+    for param_set in ALL_DTOS:
+        dto = param_set.values[0]
+        mandatory_field_names = [get_name(name, field) for name, field in get_fields(dto).items() if is_required(field)]
+        res[dto.__name__] = mandatory_field_names
+    print(res)
+
+
+def print_all_optional_values():
+    res = {}
+    for param_set in ALL_DTOS:
+        dto = param_set.values[0]
+        mandatory_field_names = [
+            get_name(name, field) for name, field in get_fields(dto).items() if not is_required(field)
+        ]
+        res[dto.__name__] = mandatory_field_names
+    print(res)
+
+
+class CustomFactory(ModelFactory):
+    __allow_none_optionals__ = False
+    __is_base_factory__ = True
+    __randomize_collection_length__ = True
+    __min_collection_length__ = 0
+    __max_collection_length__ = 1
+
+
+def pydantic_factory(klass: Type[BaseModel]):
+    factory = CustomFactory.create_factory(model=klass)
+    factory.__base_factory_overrides__ = {BaseModel: CustomFactory}
+
+    return factory
+
+
+def generate_serialisation_data():
+    all_snapshots = {}
+    for param_set in ALL_DTOS:
+        dto = param_set.values[0]
+        factory = pydantic_factory(dto)
+        factory.seed_random(1)
+        all_snapshots[dto.__name__] = factory.batch(3)
+
+    fixtures_dir = Path(__file__).parent / "fixtures"
+    fixtures_dir.mkdir(parents=True, exist_ok=True)
+    with (fixtures_dir / "with_alias.json").open("w", encoding="utf-8") as fp:
+        json.dump(
+            {k: [json.loads(elt.json(by_alias=True)) for elt in v] for k, v in all_snapshots.items()},
+            fp,
+            allow_nan=False,
+            indent=2,
+        )
+
+
+if __name__ == "__main__":
+    print_all_mandatory_values()
+    print_all_optional_values()
+    print_all_mapping()
+    generate_serialisation_data()
---
+++ tests/communications/test_listener_utils.py
@@ -0,0 +1,85 @@
+import pytest
+
+from giskard.ml_worker import websocket
+from giskard.ml_worker.exceptions.IllegalArgumentError import IllegalArgumentError
+from giskard.ml_worker.websocket.listener import extract_debug_info, function_argument_to_ws, parse_function_arguments
+
+
+TEST_PROJECT_KEY = "123"
+TEST_MODEL_ID = "231"
+TEST_DATASET_ID = "321"
+
+
+def test_extract_debug_info():
+    request_arguments = [
+        websocket.FuncArgument(
+            name="model", model=websocket.ArtifactRef(project_key=TEST_PROJECT_KEY, id=TEST_MODEL_ID), none=False
+        ),
+        websocket.FuncArgument(
+            name="dataset", dataset=websocket.ArtifactRef(project_key=TEST_PROJECT_KEY, id=TEST_DATASET_ID), none=False
+        ),
+    ]
+    debug_info = extract_debug_info(request_arguments)
+    assert debug_info["project_key"] == TEST_PROJECT_KEY
+    assert debug_info["suffix"] == " | <model:" + TEST_MODEL_ID + "> | <dataset:" + TEST_DATASET_ID + ">"
+
+
+TEST_FUNC_ARGUMENT_FLOAT = 114.514
+TEST_FUNC_ARGUMENT_INT = 114
+TEST_FUNC_ARGUMENT_STR = "giskard"
+TEST_FUNC_ARGUMENT_BOOL = False
+
+
+def test_function_argument_to_ws():
+    with pytest.raises(IllegalArgumentError):
+        function_argument_to_ws(
+            {
+                "list": [],  # List should not pass
+            }
+        )
+
+    # Domain classes creation should be tested somewhere else, do not test them.
+    #   "dataset": Dataset,
+    #   "model": BaseModel,
+    #   "slicingFunction": SlicingFunction,
+    #   "transformFunction": TransformationFunction,
+    kwargs = {
+        "float": TEST_FUNC_ARGUMENT_FLOAT,
+        "int": TEST_FUNC_ARGUMENT_INT,
+        "str": TEST_FUNC_ARGUMENT_STR,
+        "bool": TEST_FUNC_ARGUMENT_BOOL,
+        "dict": {},
+    }
+    args = function_argument_to_ws(kwargs)
+    assert len(args) == len(kwargs.values())
+    assert args[0].float_arg == TEST_FUNC_ARGUMENT_FLOAT
+    assert args[1].int_arg == TEST_FUNC_ARGUMENT_INT
+    assert args[2].str_arg == TEST_FUNC_ARGUMENT_STR
+    assert args[3].bool_arg == TEST_FUNC_ARGUMENT_BOOL  # Processed as an integer, filed in GSK-1557
+    assert args[4].kwargs == str({})
+
+
+def test_parse_function_arguments():
+    with pytest.raises(IllegalArgumentError):
+        parse_function_arguments(None, [websocket.FuncArgument(name="invalid", none=False)])
+
+    # Domain classes need client to download, do not test them here.
+    #   "dataset": Dataset,
+    #   "model": BaseModel,
+    #   "slicingFunction": SlicingFunction,
+    #   "transformFunction": TransformationFunction,
+    args = [
+        websocket.FuncArgument(name="none", none=True),
+        websocket.FuncArgument(name="float", none=False, float=TEST_FUNC_ARGUMENT_FLOAT),
+        websocket.FuncArgument(name="int", none=False, int=TEST_FUNC_ARGUMENT_INT),
+        websocket.FuncArgument(name="str", none=False, str=TEST_FUNC_ARGUMENT_STR),
+        websocket.FuncArgument(name="bool", none=False, bool=TEST_FUNC_ARGUMENT_BOOL),
+        websocket.FuncArgument(name="kwargs", none=False, kwargs=f"kwargs['bool1'] ={TEST_FUNC_ARGUMENT_BOOL}"),
+    ]
+    kwargs = parse_function_arguments(None, args)
+    assert "none" not in kwargs.keys()  # None should not be here
+    assert "float" in kwargs.keys() and kwargs["float"] == TEST_FUNC_ARGUMENT_FLOAT
+    assert "int" in kwargs.keys() and kwargs["int"] == TEST_FUNC_ARGUMENT_INT
+    assert "str" in kwargs.keys() and kwargs["str"] == TEST_FUNC_ARGUMENT_STR
+    assert "bool" in kwargs.keys() and kwargs["bool"] == TEST_FUNC_ARGUMENT_BOOL
+    assert "bool1" in kwargs.keys() and kwargs["bool1"] == TEST_FUNC_ARGUMENT_BOOL
