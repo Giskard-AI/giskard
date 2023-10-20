e9c62557b100d7b04ea0fbaf8215a5892e9921cb
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-10-20 17:25:53 +0200

    cleanup of PromptInjection submodule (2)

--- giskard/scanner/llm/prompt_injection/promptinject/__init__.py
+++ giskard/scanner/llm/prompt_injection/promptinject/__init__.py
@@ -1,5 +1,3 @@
 from .prompting import build_prompts
-from .run import run_prompts_api
-from .visualization import build_dataframe, dataframe_to_csv
 
-__all__ = ["build_prompts", "run_prompts_api", "build_dataframe", "dataframe_to_csv"]
+__all__ = ["build_prompts"]
