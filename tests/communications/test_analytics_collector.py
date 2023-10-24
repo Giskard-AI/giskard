53bdc43b110e2d3ed75f1bb874f7ea71e74b50a4
Andrey Avtomonov <andreybavt@gmail.com>
2023-10-24 19:46:05 +0200

    GSK-1963: ignore mixpanel errors

---
+++ tests/communications/test_analytics_collector.py
@@ -0,0 +1,9 @@
+from mixpanel import Consumer
+
+from utils.analytics_collector import GiskardAnalyticsCollector
+
+
+def test_tracking_doesnt_throw_errors():
+    ac = GiskardAnalyticsCollector()
+    ac.mp._consumer = Consumer(events_url="https://invalid.url")
+    ac.track("test")
