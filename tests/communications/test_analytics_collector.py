53d341a440509290c53f0c353c11cc0a5f828529
Andrey Avtomonov <andreybavt@gmail.com>
2023-10-25 14:23:04 +0200

    fix test

--- tests/communications/test_analytics_collector.py
+++ tests/communications/test_analytics_collector.py
@@ -1,9 +1,13 @@
 from mixpanel import Consumer
 
 from giskard.utils.analytics_collector import GiskardAnalyticsCollector
+from giskard.settings import settings
 
 
 def test_tracking_doesnt_throw_errors():
+    analytics_flag = settings.disable_analytics
+    settings.disable_analytics = False
     ac = GiskardAnalyticsCollector()
     ac.mp._consumer = Consumer(events_url="https://invalid.url")
     ac.track("test")
+    settings.disable_analytics = analytics_flag
