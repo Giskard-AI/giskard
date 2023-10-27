838911d11ca726dbec7742a7b5e9cb6134254d1d
Googleton <hugo.viallon.pro@gmail.com>
2023-10-27 15:54:34 +0200

    Change cache to be LRU instead of FIFO

--- giskard/ml_worker/utils/cache.py
+++ giskard/ml_worker/utils/cache.py
@@ -1,28 +1,26 @@
 import hashlib
+from collections import OrderedDict
 
 
 class SimpleCache:
-    def __init__(self, max_results=20):
-        self.results = {}
-        self.order = []  # To maintain the order in which results were added
+    def __init__(self, max_results=128):
+        self.results = OrderedDict()
         self.max_results = max_results
 
     def add_result(self, obj, result):
-        # Calculate the hash of the object
         obj_hash = hashlib.md5(repr(obj).encode()).hexdigest()
 
-        # Store the result with the object's hash as the key
+        # If the cache is full, remove the least recently used item
+        if len(self.results) >= self.max_results:
+            self.results.popitem(last=False)
+
         self.results[obj_hash] = result
 
-        # Add the object's hash to the order list and remove the oldest result if necessary
-        self.order.append(obj_hash)
-        if len(self.order) > self.max_results:
-            oldest_obj_hash = self.order.pop(0)
-            del self.results[oldest_obj_hash]
-
     def get_result(self, obj):
-        # Calculate the hash of the object
         obj_hash = hashlib.md5(repr(obj).encode()).hexdigest()
 
-        # Return the result for the given object if it exists
-        return self.results.get(obj_hash)
+        if obj_hash in self.results:
+            self.results.move_to_end(obj_hash)
+            return self.results[obj_hash]
+        else:
+            return None
