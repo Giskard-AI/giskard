292f0973cf70eb268eccb71e2d36d9bf9fc0505e
Weixuan Xiao <weixuan@giskard.ai>
2023-11-01 10:37:43 +0100

    Add tests for borderline push with CTA

--- tests/communications/test_websocket_actor_push.py
+++ tests/communications/test_websocket_actor_push.py
@@ -418,3 +418,108 @@ def test_websocket_actor_get_push_perturbation_wrong_cta(kind, row, cta_kind, ge
         with pytest.raises(AttributeError, match=".*transformation_functions.*"):
             # Should raise no transformation_function attribute exception
             listener.get_push(client=client, params=params)
+
+
+@pytest.mark.parametrize("kind,row", [EXPECTED_COUNTS_GERMAN_CREDIT_PUSH_KIND_SAMPLE_INDEX[websocket.PushKind.BORDERLINE]])
+@pytest.mark.parametrize("cta_kind", [
+    websocket.CallToActionKind.CREATE_TEST,
+    websocket.CallToActionKind.ADD_TEST_TO_CATALOG,
+])
+def test_websocket_actor_get_push_borderline(kind, row, cta_kind, german_credit_model, german_credit_data):
+    assert EXPECTED_COUNTS["german_credit_model"][kind][row] != 0
+    push_kind = websocket.PushKind[kind.upper()]
+
+    model = german_credit_model
+    dataset = german_credit_data
+
+    project_key = str(uuid.uuid4())
+    with utils.MockedClient(mock_all=False) as (client, mr), utils.MockedProjectCacheDir(project_key):
+        utils.local_save_model_under_giskard_home_cache(model, project_key)
+        utils.local_save_dataset_under_giskard_home_cache(dataset, project_key)
+
+        utils.register_uri_for_model_meta_info(mr, model, project_key)
+        utils.register_uri_for_dataset_meta_info(mr, dataset, project_key)
+
+        # Upload tests and datasets
+        utils.register_uri_for_any_tests_artifact_info_upload(mr, register_files=True)
+        utils.register_uri_for_any_dataset_artifact_info_upload(mr, register_files=True)
+
+        # Pick the given row
+        given_row = dataset.df.iloc[row]
+        dataframe = websocket.DataFrame(
+            rows=[
+                websocket.DataRow(columns={
+                    str(k): str(v) for k, v in given_row.items()
+                }),
+            ],
+        )
+
+        params = websocket.GetPushParam(
+            model=websocket.ArtifactRef(project_key=project_key, id=str(model.id)),
+            dataset=websocket.ArtifactRef(project_key=project_key, id=str(dataset.id)),
+            dataframe=dataframe,
+            target=dataset.target,
+            column_types=dataset.column_types,
+            column_dtypes=dataset.column_dtypes,
+            push_kind=push_kind,
+            cta_kind=cta_kind,
+            rowIdx=row,
+        )
+        reply = listener.get_push(client=client, params=params)
+        assert isinstance(reply, websocket.GetPushResponse)
+        assert reply.__dict__[kind] # The given push should not be `None`
+        assert reply.action # Action should not be `None`
+
+
+@pytest.mark.parametrize("kind,row", [
+    EXPECTED_COUNTS_GERMAN_CREDIT_PUSH_KIND_SAMPLE_INDEX[k] for k in EXPECTED_COUNTS_GERMAN_CREDIT_PUSH_KIND_SAMPLE_INDEX if k != websocket.PushKind.BORDERLINE
+])
+@pytest.mark.parametrize("cta_kind", [
+    websocket.CallToActionKind.CREATE_TEST,
+    websocket.CallToActionKind.ADD_TEST_TO_CATALOG,
+])
+def test_websocket_actor_get_push_non_borderline_test_cta(kind, row, cta_kind, german_credit_model, german_credit_data):
+    assert EXPECTED_COUNTS["german_credit_model"][kind][row] != 0
+    push_kind = websocket.PushKind[kind.upper()]
+
+    model = german_credit_model
+    dataset = german_credit_data
+
+    project_key = str(uuid.uuid4())
+    with utils.MockedClient(mock_all=False) as (client, mr), utils.MockedProjectCacheDir(project_key):
+        utils.local_save_model_under_giskard_home_cache(model, project_key)
+        utils.local_save_dataset_under_giskard_home_cache(dataset, project_key)
+
+        utils.register_uri_for_model_meta_info(mr, model, project_key)
+        utils.register_uri_for_dataset_meta_info(mr, dataset, project_key)
+
+        # Upload tests and datasets
+        utils.register_uri_for_any_slices_artifact_info_upload(mr, register_files=True)
+        utils.register_uri_for_any_tests_artifact_info_upload(mr, register_files=True)
+        utils.register_uri_for_any_dataset_artifact_info_upload(mr, register_files=True)
+
+        # Pick the given row
+        given_row = dataset.df.iloc[row]
+        dataframe = websocket.DataFrame(
+            rows=[
+                websocket.DataRow(columns={
+                    str(k): str(v) for k, v in given_row.items()
+                }),
+            ],
+        )
+
+        params = websocket.GetPushParam(
+            model=websocket.ArtifactRef(project_key=project_key, id=str(model.id)),
+            dataset=websocket.ArtifactRef(project_key=project_key, id=str(dataset.id)),
+            dataframe=dataframe,
+            target=dataset.target,
+            column_types=dataset.column_types,
+            column_dtypes=dataset.column_dtypes,
+            push_kind=push_kind,
+            cta_kind=cta_kind,
+            rowIdx=row,
+        )
+        reply = listener.get_push(client=client, params=params)
+        assert isinstance(reply, websocket.GetPushResponse)
+        assert reply.__dict__[kind] # The given push should not be `None`
+        assert reply.action # Action should not be `None`
