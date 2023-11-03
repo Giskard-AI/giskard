e4261a9f270eaf567a82337bfc511e45442b832b
Matteo Dora <matteo@giskard.ai>
2023-11-03 19:01:37 +0100

    Try to also import torch

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,7 @@ from pathlib import Path
 # We import lightgbm as the first thing here to avoid a bug on macOS that happens when lgbm is imported after torch.
 # See https://github.com/dmlc/xgboost/issues/7039#issuecomment-860910066
 import lightgbm  # noqa: F401
+import torch  # noqa: F401
 
 
 def path(p):
