204e1379495e1dd697a0f4e9e9520a5be748e486
Matteo <matteo@giskard.ai>
2023-11-05 19:21:43 +0100

    Add cost estimate to LLM scan (#1532)
    * Add cost estimation to LLM scan
    
    * Remove abstract method from LLMClient
    
    * Count model calls in injection detectors
    
    * Fix typo
    
    * Add measured costs for LLM-assisted scan
    
    * Fix sentence
    
    * Fix usage of internal logger
    
    ---------
    
    Co-authored-by: Rabah Abdul Khalek <rabah.khalek@gmail.com>

--- giskard/llm/client/base.py
+++ giskard/llm/client/base.py
@@ -2,8 +2,15 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Any, Dict, Optional
 
+from .logger import LLMLogger
+
 
 class LLMClient(ABC):
+    @property
+    @abstractmethod
+    def logger(self) -> LLMLogger:
+        ...
+
     @abstractmethod
     def complete(
         self,
@@ -13,6 +20,7 @@ class LLMClient(ABC):
         temperature=0.5,
         max_tokens=None,
         function_call: Optional[Dict] = None,
+        caller_id: Optional[str] = None,
     ):
         ...
 
