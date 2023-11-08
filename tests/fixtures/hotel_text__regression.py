0784a072b4b09ce3c1b6c4af32386a33793f97ee
Matteo <matteo@giskard.ai>
2023-11-08 12:35:56 +0100

    Add scope "session" to fixtures to avoid expensive computation (#1543)
    * Add scope "session" to all expensive fixtures
    
    * Fix fixtures for soem tests
    
    * Finish working on fixtures
    
    * Fix typo
    
    * Fix Diabetes dataset
    
    * Remove user warnings on test_upload_models
    
    ---------
    
    Co-authored-by: Hartorn <hartorn.github@gmail.com>
    Co-authored-by: Hartorn <bazire@giskard.ai>
    Co-authored-by: Rabah Abdul Khalek <rabah.khalek@gmail.com>

--- tests/fixtures/hotel_text__regression.py
+++ tests/fixtures/hotel_text__regression.py
@@ -1,17 +1,17 @@
-from pathlib import Path
 from typing import Iterable
 
-import pytest
+from pathlib import Path
+
 import pandas as pd
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer
+import pytest
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
 
 from giskard import Dataset
-from tests.url_utils import fetch_from_ftp
 from giskard.models.sklearn import SKLearnModel
-
+from tests.url_utils import fetch_from_ftp
 
 # Data.
 DATA_URL = "ftp://sys.giskard.ai/pub/unit_test_resources/hotel_text_regression_dataset/Hotel_Reviews.csv"
@@ -31,13 +31,18 @@ def load_data(**kwargs) -> pd.DataFrame:
     return df
 
 
-@pytest.fixture
-def hotel_text_data() -> Dataset:
+@pytest.fixture(scope="session")
+def hotel_text_raw_data():
     fetch_from_ftp(DATA_URL, DATA_PATH)
 
     raw_data = load_data(nrows=105)[[FEATURE_COLUMN_NAME, TARGET_COLUMN_NAME]]
+    return raw_data
+
+
+@pytest.fixture()
+def hotel_text_data(hotel_text_raw_data) -> Dataset:
     wrapped_data = Dataset(
-        raw_data,
+        hotel_text_raw_data,
         name="hotel_text_regression_dataset",
         target=TARGET_COLUMN_NAME,
         column_types={FEATURE_COLUMN_NAME: "text"},
@@ -56,10 +61,10 @@ def adapt_vectorizer_input(df: pd.DataFrame) -> Iterable:
     return df
 
 
-@pytest.fixture
-def hotel_text_model(hotel_text_data) -> SKLearnModel:
-    x = hotel_text_data.df[[FEATURE_COLUMN_NAME]]
-    y = hotel_text_data.df[TARGET_COLUMN_NAME]
+@pytest.fixture(scope="session")
+def hotel_text_raw_model(hotel_text_raw_data) -> SKLearnModel:
+    x = hotel_text_raw_data[[FEATURE_COLUMN_NAME]]
+    y = hotel_text_raw_data[TARGET_COLUMN_NAME]
 
     pipeline = Pipeline(
         steps=[
@@ -70,9 +75,13 @@ def hotel_text_model(hotel_text_data) -> SKLearnModel:
     )
 
     pipeline.fit(x, y)
+    return pipeline
 
+
+@pytest.fixture()
+def hotel_text_model(hotel_text_raw_model) -> SKLearnModel:
     wrapped_model = SKLearnModel(
-        pipeline, model_type="regression", name="hotel_text_regression", feature_names=[FEATURE_COLUMN_NAME]
+        hotel_text_raw_model, model_type="regression", name="hotel_text_regression", feature_names=[FEATURE_COLUMN_NAME]
     )
 
     return wrapped_model
