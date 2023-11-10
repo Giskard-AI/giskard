abb9b5f4c3364bb7de318c4947e9cb73dff8b952
Hartorn <hartorn.github@gmail.com>
2023-11-10 14:24:09 +0100

    Adding memray

--- tests/models/pytorch/test_sst2_pytorch.py
+++ tests/models/pytorch/test_sst2_pytorch.py
@@ -4,5 +4,6 @@ import tests.utils
 
 
 @pytest.mark.memory_expensive
+@pytest.mark.skip(reason="Too memory expensive, to be replaced with smaller one")
 def test_sst2_pytorch_upload(sst2_model, sst2_data):
     tests.utils.verify_model_upload(sst2_model, sst2_data)
