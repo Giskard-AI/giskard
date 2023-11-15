80be22fce6b349762ecf825399b5a1f635eb759b
Kevin Messiaen <kevin.messiaen@icloud.com>
2023-11-15 10:28:32 +0100

    Fixed issue with dataset id being wrong due to slice being applied

--- giskard/testing/tests/calibration.py
+++ giskard/testing/tests/calibration.py
@@ -74,6 +74,7 @@ def test_overconfidence_rate(
     if not model.is_classification:
         raise ValueError("This test is only applicable to classification models.")
 
+    dataset_id = str(dataset.id)
     if slicing_function is not None:
         dataset = dataset.slice(slicing_function)
 
@@ -89,7 +90,7 @@ def test_overconfidence_rate(
     # --- debug ---
     failed_indexes = dict()
     if not passed:
-        failed_indexes[str(dataset.id)] = list(
+        failed_indexes[dataset_id] = list(
             dataset.df.index.get_indexer_for(overconfidence_mask[overconfidence_mask].index)
         )
     # ---
@@ -153,6 +154,7 @@ def test_underconfidence_rate(
     if not model.is_classification:
         raise ValueError("This test is only applicable to classification models.")
 
+    dataset_id = str(dataset.id)
     if slicing_function is not None:
         dataset = dataset.slice(slicing_function)
 
@@ -165,7 +167,7 @@ def test_underconfidence_rate(
     # --- debug ---
     failed_indexes = dict()
     if not passed:
-        failed_indexes[str(dataset.id)] = list(
+        failed_indexes[dataset_id] = list(
             dataset.df.index.get_indexer_for(underconfidence_mask[underconfidence_mask].index)
         )
     # ---
