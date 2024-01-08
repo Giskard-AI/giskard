e1989951284f8e0122d4f981daa4dd21f4c04fae
Kevin Messiaen <kevin.messiaen@icloud.com>
2024-01-08 10:53:41 +0700

    Improved tests

--- tests/registry/test_giskard_test.py
+++ tests/registry/test_giskard_test.py
@@ -102,9 +102,13 @@ class _TmpModule(object):
         self.temp_dir.__exit__(type, value, traceback)
 
 
-def test_load_external_module():
+@pytest.mark.parametrize(
+    "module_def",
+    [SINGLE_FILE_MODULE, MULTIPLE_FILES_MODULE],
+)
+def test_load_external_module(module_def):
     with tempfile.TemporaryDirectory() as tmp_test_folder:
-        with _TmpModule(SINGLE_FILE_MODULE) as module:
+        with _TmpModule(module_def) as module:
             assert module.my_test().execute()
 
             module.my_test.save(tmp_test_folder)
@@ -114,73 +118,3 @@ def test_load_external_module():
 
         my_test = giskard.GiskardTest.load(Path(tmp_test_folder), meta.uuid, meta)
         assert my_test().execute()
-
-
-def test_load_external_modules():
-    with tempfile.TemporaryDirectory() as tmp_test_folder:
-        with _TmpModule(MULTIPLE_FILES_MODULE) as module:
-            assert module.my_test().execute()
-
-            module.my_test.save(tmp_test_folder)
-            meta = module.my_test.meta
-
-        del module
-
-        my_test = giskard.GiskardTest.load(Path(tmp_test_folder), meta.uuid, meta)
-        assert my_test().execute()
-
-
-@pytest.mark.skip(reason="Should we pickle whole module by value?")
-def test_load_external_module_multiple_levels():
-    try:
-        with open("tests.py", "w") as f, open("utils.py", "w") as f_utils:
-            f_utils.write(
-                """
-def do_test():
-  return True
-
-"""
-            )
-            f.write(
-                """
-import giskard
-from my_mocked_module.utils import do_test
-
-@giskard.test()
-def my_test():
-  return do_test()
-
-"""
-            )
-
-        utils_spec = importlib.util.spec_from_file_location("my_mocked_module.utils", "utils.py")
-        my_mocked_module_utils = importlib.util.module_from_spec(utils_spec)
-        sys.modules["my_mocked_module.utils"] = my_mocked_module_utils
-        utils_spec.loader.exec_module(my_mocked_module_utils)
-
-        tests_spec = importlib.util.spec_from_file_location("my_mocked_module.tests", "tests.py")
-        my_mocked_module_tests = importlib.util.module_from_spec(tests_spec)
-        sys.modules["my_mocked_module.tests"] = my_mocked_module_tests
-        tests_spec.loader.exec_module(my_mocked_module_tests)
-
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            assert my_mocked_module_tests.my_test().execute()
-
-            my_mocked_module_tests.my_test.save(tmpdirname)
-            meta = my_mocked_module_tests.my_test.meta
-
-            # Unimport my_mocked_module
-            del sys.modules["my_mocked_module.tests"]
-            del sys.modules["my_mocked_module.utils"]
-            del my_mocked_module_tests
-            del my_mocked_module_utils
-            os.remove("tests.py")
-            os.remove("utils.py")
-
-            my_test = giskard.GiskardTest.load(Path(tmpdirname), meta.uuid, meta)
-            assert my_test().execute()
-    finally:
-        if Path("utils.py").exists():
-            os.remove("utils.py")
-        if Path("tests.py").exists():
-            os.remove("tests.py")
