4767a123c89ab6bd776a1b78143613fc89813a78
mykytaalekseiev <alekseyevnf@gmail.com>
2024-01-08 12:49:29 +0100

    Implemented the final PredictUserInputTool.

--- giskard/llm/talk/config.py
+++ giskard/llm/talk/config.py
@@ -2,21 +2,25 @@ from enum import Enum
 
 LLM_MODEL = "gpt-4-1106-preview"
 
-MODEL_INSTRUCTION = """You are an agent designed to help a user obtain information about the model and/or it's predictions.
+MODEL_INSTRUCTION = """You are an agent designed to help a user obtain information about the model and/or it's 
+predictions.
 
-You interact with the model through different tools. Tools are functions, whose response is used to enrich you with the necessary context to answer a user's question. 
+You interact with the model through different tools. Tools are functions, whose response is used to enrich you with 
+the necessary context to answer a user's question.
 
 The descriptions of these tools is provided below:
 Tools descriptions: 
 {tools_descriptions}
 
-Your main goal is to choose and execute an appropriate tool, which help you to answer a user's question.
-For the chosen tool you need to create an input, regarding provided tool specification.
-If there is an error during the tool calling, you will get this information. You need to make a summary about this error and inform the user.
+Your main goal is to choose and execute an appropriate tool, which help you to answer a user's question. For the 
+chosen tool you need to create an input, regarding provided tool specification. If there is an error during the tool 
+calling, you will get this information. You need to make a summary about this error and inform the user.
 
-Please provide polite, and concise answers to the user and avoid explaining the result, until the user explicitly asks you to do it.
-Please, take into account, that user not necessarily have computer science background, thus your answers must be clear to people from different domains.
-Please note that you cannot share any confidential information with the user, and if the question is not related to the model, you must return an explanation why you cannot answer.
+Please provide polite, and concise answers to the user and avoid explaining the result, until the user explicitly 
+asks you to do it. Please, take into account, that user not necessarily have computer science background, 
+thus your answers must be clear to people from different domains. Please note that you cannot share any confidential 
+information with the user, and if the question is not related to the model, you must return an explanation why you 
+cannot answer.
 
 Your will interact with the following model:
 Model name: {model_name}
@@ -31,9 +35,16 @@ Tool arguments: "{tool_args}"\n
 """
 
 # Tools descriptions (instructions).
-_PREDICT_FROM_DATASET_TOOL_DESCRIPTION = ("You expect a dictionary with features and their values to filter rows from "
-                                          "the dataset, then you run the model prediction on that rows and finally "
-                                          "return the prediction result.")
+_PREDICT_DATASET_INPUT_TOOL_DESCRIPTION = ("Your task is to return prediction of a feature vector extracted from the "
+                                           "dataset. You expect a dictionary with features and their values to filter "
+                                           "rows from the dataset, then you run the model prediction on that rows and "
+                                           "finally return the prediction result.")
+
+_PREDICT_USER_INPUT_TOOL_DESCRIPTION = ("Your task is to return prediction of a feature vector (partially) built from "
+                                        "the user input. You expect a dictionary with features and their values, "
+                                        "extracted from the user input, then you fill the values of features not "
+                                        "mentioned in the user query and build input row. Then you run the model "
+                                        "prediction on that row and finally return the prediction result.")
 
 _SHAP_EXPLANATION_TOOL_DESCRIPTION = ("You expect a dictionary with feature names as keys and their values as dict "
                                       "values, which you use to filter rows in the dataset, then you run the SHAP "
@@ -48,11 +59,9 @@ _ISSUES_SCANNER_TOOL_DESCRIPTION = ("Your task is to give the user a summary of 
                                     "correlation, overconfidence, etc. You return a table, where the important "
                                     "columns are 'Description', 'Vulnerability', 'Level', 'Metric' and 'Deviation'")
 
-_PREDICT_USER_INPUT_TOOL_DESCRIPTION = ""
-
 
 class ToolDescription(str, Enum):
-    PREDICT_FROM_DATASET = _PREDICT_FROM_DATASET_TOOL_DESCRIPTION
+    PREDICT_DATASET_INPUT = _PREDICT_DATASET_INPUT_TOOL_DESCRIPTION
+    PREDICT_USER_INPUT = _PREDICT_USER_INPUT_TOOL_DESCRIPTION
     SHAP_EXPLANATION = _SHAP_EXPLANATION_TOOL_DESCRIPTION
     ISSUES_SCANNER = _ISSUES_SCANNER_TOOL_DESCRIPTION
-    PREDICT_USER_INPUT = _PREDICT_USER_INPUT_TOOL_DESCRIPTION
--- giskard/llm/talk/tools.py
+++ giskard/llm/talk/tools.py
@@ -1,9 +1,8 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
-from typing import TYPE_CHECKING
-
 from shap import Explanation
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from giskard.models.base import BaseModel
@@ -18,15 +17,14 @@ class BaseTool(ABC):
     default_description: str = ...
 
     def __init__(self,
-                 model: "BaseModel",
-                 dataset: Dataset,
-                 scan_result: "ScanReport",
+                 model: "BaseModel" = None,
+                 dataset: Dataset = None,
+                 scan_result: "ScanReport" = None,
                  name: str = None,
                  description: str = None):
         self._model = model
         self._dataset = dataset
         self._scan_result = scan_result
-
         self._name = name if name is not None else self.default_name
         self._description = description if description is not None else self.default_description
 
@@ -48,7 +46,7 @@ class BaseTool(ABC):
         ...
 
 
-class PredictBaseTool(BaseTool):
+class BasePredictTool(BaseTool, ABC):
     def _get_feature_json_type(self) -> dict[any, str]:
         number_columns = {column: "number" for column in self._dataset.df.select_dtypes(include=(int, float)).columns}
         string_columns = {column: "string" for column in self._dataset.df.select_dtypes(exclude=(int, float)).columns}
@@ -66,13 +64,13 @@ class PredictBaseTool(BaseTool):
                 "parameters": {
                     "type": "object",
                     "properties": {
-                        "row_filter": {
+                        "features_dict": {
                             "type": "object",
                             "properties": {feature: {"type": dtype} for feature, dtype in
                                            list(feature_json_type.items())}
                         }
                     },
-                    "required": ["row_filter"]
+                    "required": ["features_dict"]
                 }
             }
         }
@@ -87,39 +85,11 @@ class PredictBaseTool(BaseTool):
         return ", ".join(prediction)
 
 
-class PredictFromDatasetTool(BaseTool):
-    default_name: str = "predict_from_dataset"
-    default_description: str = ToolDescription.PREDICT_FROM_DATASET.value
+class PredictDatasetInputTool(BasePredictTool):
+    default_name: str = "predict_dataset_input"
+    default_description: str = ToolDescription.PREDICT_DATASET_INPUT.value
 
-    def _get_feature_json_type(self) -> dict[any, str]:
-        number_columns = {column: "number" for column in self._dataset.df.select_dtypes(include=(int, float)).columns}
-        string_columns = {column: "string" for column in self._dataset.df.select_dtypes(exclude=(int, float)).columns}
-        return number_columns | string_columns
-
-    @property
-    def specification(self) -> str:
-        feature_json_type = self._get_feature_json_type()
-
-        return {
-            "type": "function",
-            "function": {
-                "name": self.name,
-                "description": self.description,
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "row_filter": {
-                            "type": "object",
-                            "properties": {feature: {"type": dtype} for feature, dtype in
-                                           list(feature_json_type.items())}
-                        }
-                    },
-                    "required": ["row_filter"]
-                }
-            }
-        }
-
-    def _filter_dataset(self, row_filter: dict) -> Dataset:
+    def _prepare_input(self, row_filter: dict) -> Dataset:
         filtered_df = self._dataset.df.copy()
         for col_name, col_value in list(row_filter.items()):
             if filtered_df[col_name].dtype == "object":
@@ -129,15 +99,27 @@ class PredictFromDatasetTool(BaseTool):
 
         return Dataset(filtered_df)
 
-    def __call__(self, row_filter: dict) -> str:
-        filtered_dataset = self._filter_dataset(row_filter)
-        prediction = self._model.predict(filtered_dataset).prediction
 
-        # Finalise the result.
-        return ", ".join(prediction)
+class PredictUserInputTool(BasePredictTool):
+    default_name: str = "predict_user_input"
+    default_description: str = ToolDescription.PREDICT_USER_INPUT.value
+
+    def _prepare_input(self, feature_values: dict) -> Dataset:
+        from giskard.models.model_explanation import _get_background_example
+
+        # Prepare background sample.
+        background_df = self._model.prepare_dataframe(self._dataset.df, self._dataset.column_dtypes,
+                                                      self._dataset.target)
+        background_sample = _get_background_example(background_df, self._dataset.column_types)
+
+        # Fill background sample with known values.
+        for col_name, col_value in list(feature_values.items()):
+            background_sample.loc[0, col_name] = col_value
+
+        return Dataset(background_sample)
 
 
-class SHAPExplanationTool(PredictFromDatasetTool):
+class SHAPExplanationTool(PredictDatasetInputTool):
     default_name: str = "shap_prediction_explanation"
     default_description: str = ToolDescription.SHAP_EXPLANATION.value
     _result_template: str = "'{feature_name}' | {attributions_values}"
@@ -173,25 +155,32 @@ class SHAPExplanationTool(PredictFromDatasetTool):
                                         feature_names=feature_names)
         return shap_explanations
 
-    def __call__(self, row_filter: dict) -> str:
-        filtered_dataset = self._filter_dataset(row_filter)
-        explanations = self._get_shap_explanations(filtered_dataset)
-
-        # Finalise the result.
-        shap_result = [self._result_template.format(feature_name=f_name,
-                                                    attributions_values=list(zip(np.abs(explanations[:, f_name].values),
-                                                                                 explanations[:, f_name].data)))
-                       for f_name in explanations.feature_names]
+    @staticmethod
+    def _finalise_output(prediction_result: str, shap_result: np.ndarray) -> str:
         shap_result = "\n".join(shap_result)
-
-        # Get prediction result from the parent tool, to add more context to the result.
-        prediction_result = super().__call__(row_filter)
         return (f"Prediction result:\n"
                 f"{prediction_result}\n\n"
                 f"SHAP result: \n"
                 f"'Feature name' | [('SHAP value', 'Feature value'), ...]\n"
                 f"--------------------------------------------------------\n"
                 f"{shap_result}\n\n")
+
+    def __call__(self, features_dict: dict) -> str:
+        # Get prediction result from the parent tool, to add more context to the result.
+        prediction_result = super().__call__(features_dict)
+
+        model_input = self._prepare_input(features_dict)
+        explanations = self._get_shap_explanations(model_input)
+
+        # Finalise the result.
+        shap_result = [self._result_template.format(feature_name=f_name,
+                                                    attributions_values=list(
+                                                        zip(
+                                                            np.abs(explanations[:, f_name].values),
+                                                            explanations[:, f_name].data
+                                                        )))
+                       for f_name in explanations.feature_names]
+        return self._finalise_output(prediction_result, shap_result)
 
 
 class IssuesScannerTool(BaseTool):
@@ -208,48 +197,10 @@ class IssuesScannerTool(BaseTool):
             }
         }
 
+    @staticmethod
+    def _finalise_output(scan_result: str) -> str:
+        return f"ML model performance issues scanner result:\n {scan_result}"
+
     def __call__(self) -> str:
-        return f"ML model performance issues scanner result:\n {self._scan_result.to_markdown()}"
-
-
-class PredictUserInputTool(BaseTool):
-    default_name: str = "predict_user_input"
-    default_description: str = ToolDescription.PREDICT_USER_INPUT.value
-
-    def _get_feature_json_type(self) -> dict[any, str]:
-        number_columns = {column: "number" for column in self._dataset.df.select_dtypes(include=(int, float)).columns}
-        string_columns = {column: "string" for column in self._dataset.df.select_dtypes(exclude=(int, float)).columns}
-        return number_columns | string_columns
-
-    @property
-    def specification(self) -> str:
-        feature_json_type = self._get_feature_json_type()
-
-        return {
-            "type": "function",
-            "function": {
-                "name": self.name,
-                "description": self.description,
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "row_filter": {
-                            "type": "object",
-                            "properties": {feature: {"type": dtype} for feature, dtype in
-                                           list(feature_json_type.items())}
-                        }
-                    },
-                    "required": ["row_filter"]
-                }
-            }
-        }
-
-    def _form_model_input(self, feature_values: dict) -> Dataset:
-        ...
-
-    def __call__(self, feature_values: dict) -> str:
-        model_input = self._form_model_input(feature_values)
-        prediction = self._model.predict(model_input).prediction
-
-        # Finalise the result.
-        return ", ".join(prediction)
+        scan_result = self._scan_result.to_markdown()
+        return self._finalise_output(scan_result)
