caaef7248d81c07d99cce6f4132a249a6d6d54c7
Pierre Le Jeune <pierre@giskard.ai>
2024-01-17 10:24:16 +0100

    Add testset generator

---
+++ giskard/rag/knowledge_base_testset_generator.py
@@ -0,0 +1,139 @@
+import numpy as np
+import pandas as pd
+
+from ..datasets import Dataset
+from ..llm.errors import LLMGenerationError
+from ..llm.generators import BaseDataGenerator
+from .embeddings import EmbeddingsBase, OpenAIEmbeddings
+from .prompts import ANSWER_GENERATION_PROMPT, QUESTION_GENERATION_PROMPT
+from .vector_store import VectorStore
+
+
+class KnowledgeBaseTestsetGenerator(BaseDataGenerator):
+    _question_generation_prompt = QUESTION_GENERATION_PROMPT
+    _answer_generation_prompt = ANSWER_GENERATION_PROMPT
+    _difficulty_level = 1
+
+    def __init__(
+        self,
+        knowledge_df,
+        model_name: str,
+        model_description: str,
+        context_neighbors: int = 4,
+        context_similarity_threshold: float = 0.2,
+        context_window_length: int = 8192,
+        embedding_model: EmbeddingsBase = None,
+        language: str = "english",
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.model_name = model_name
+        self.model_description = model_description
+        self.context_neighbors = context_neighbors
+        self.context_similarity_threshold = context_similarity_threshold
+
+        # ideally should be moved into llm_client object but OpenAI has no API to retrieve
+        # model context length
+        self.context_window_length = context_window_length
+        self.embedding_model = embedding_model if embedding_model is not None else OpenAIEmbeddings()
+        self.language = language
+
+        self.knowledge_base = VectorStore.from_df(knowledge_df, self.embedding_model)
+
+    def _make_generate_input_functions(self, return_attribute_name):
+        return [
+            {
+                "name": "generate_inputs",
+                "description": "generates inputs for model audit",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "inputs": {
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "properties": {return_attribute_name: {"type": "string"}},
+                            },
+                        }
+                    },
+                    "required": ["inputs"],
+                },
+            }
+        ]
+
+    def _generate_question_from_context(self, context):
+        prompt = self._question_generation_prompt.format(
+            context=context,
+            model_name=self.model_name,
+            model_description=self.model_description,
+            language=self.language,
+        )
+
+        prompt = self._prevent_context_window_overflow(prompt)
+        return self._llm_complete(prompt, self._make_generate_input_functions("question"))
+
+    def _generate_answer_from_context(self, question, context):
+        prompt = self._answer_generation_prompt.format(question=question, context=context)
+        prompt = self._prevent_context_window_overflow(prompt)
+        return self._llm_complete(prompt, self._make_generate_input_functions("answer"))
+
+    def _extract_seed_context(self):
+        seed_context = np.random.choice(self.knowledge_base.documents)
+        relevant_contexts = [
+            context
+            for (context, score) in self.knowledge_base.similarity_search_with_score(
+                seed_context.page_content, k=self.context_neighbors
+            )
+            if score < self.context_similarity_threshold  # should we keep it or not ?
+        ]
+
+        print(f"Retrieved {len(relevant_contexts)} relevant contexts.")
+        return relevant_contexts
+
+    def _format_context(self, contexts):
+        context_string = "\n\n".join(
+            ["### Context {} ###\n{}\n######".format(idx + 1, c.page_content) for idx, c in enumerate(contexts)]
+        )
+        return context_string
+
+    def _prevent_context_window_overflow(self, prompt):
+        # Prevent context overflow
+        # general rule of thumbs to count tokens: 1 token ~ 4 characters
+        # https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
+        return prompt[: self.context_window_length // 4]
+
+    def _llm_complete(self, prompt, functions):
+        try:
+            out = self.llm_client.complete(
+                messages=[{"role": "system", "content": prompt}],
+                functions=functions,
+                function_call={"name": "generate_inputs"},
+                temperature=self.llm_temperature,
+                caller_id=self.__class__.__name__,
+            )
+            generated = out.function_call.args["inputs"]
+        except (AttributeError, KeyError) as err:
+            raise LLMGenerationError("Could not parse generated inputs") from err
+
+        return generated
+
+    def generate_testset(self, num_samples: int = 10) -> Dataset:
+        generated_questions = []
+        for idx in range(num_samples):
+            seed_contexts = self._extract_seed_context()
+            context = self._format_context(seed_contexts)
+
+            question = self._generate_question_from_context(context)[0]
+            answer = self._generate_answer_from_context(question["question"], context)[0]
+
+            generated_questions.append(
+                {
+                    "question": question["question"],
+                    "reference_answer": answer["answer"],
+                    "reference_context": context,
+                    "difficulty_level": self._difficulty_level,
+                }
+            )
+
+        return pd.DataFrame(generated_questions)
---
+++ giskard/rag/prompts.py
@@ -0,0 +1,33 @@
+QUESTION_GENERATION_PROMPT = """You are a client from an online shop called {model_name}. Shop description: {model_description}
+You are looking for information about specific products that are sold on by this shop and about the shop's activities.   
+
+Your task is to generate questions about the products, the ordering process and the shop's activities in general. Your question must be related to a provided context.  
+Please respect the following rules to generate the question:
+- The answer to the question should be found, at least partially, inside the provided context.  
+- The question must be self-contained and understandable by humans. 
+- The question must be in {language}.
+
+Here is the context:
+<context>
+{context}
+</context>
+
+Remember you are a client of {model_name}, you are looking for information to help you with your shopping.
+Please call the `generate_inputs` function with the generated inputs.
+"""
+
+ANSWER_GENERATION_PROMPT = """Your task is to answer a question based on a provided context.
+The answer should be clear and concise. Think step by step and answer the question thoroughly. 
+Your answer must only contain information provided by the context.
+
+Here is the context and the question:
+<context>
+{context}
+</context>
+
+<question>
+{question}
+</question>
+
+Please call the `generate_inputs` function with the generated inputs.
+"""
