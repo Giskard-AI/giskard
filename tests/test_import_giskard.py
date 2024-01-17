47e52aa468bac4d68079905202b3c4be35c92fb6
Andrey Avtomonov <andreybavt@gmail.com>
2024-01-17 19:55:19 +0100

    fix test

--- tests/test_import_giskard.py
+++ tests/test_import_giskard.py
@@ -1,8 +1,8 @@
 import subprocess
 from time import time
 
-# As of today. The import of giskard takes about 2.5-3 seconds
-IMPORT_TIME_THRESHOLD_SECOND = 4
+# As of 17.01.2024. The import of giskard takes about 2.5-3 seconds on a 2021 MacBook Pro.
+IMPORT_TIME_THRESHOLD_SECOND = 6
 
 
 def test_import_giskard():
