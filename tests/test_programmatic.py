eb70813fc53454b8157368a268084eb8db4e0d40
Kevin Messiaen <kevin.messiaen@icloud.com>
2024-01-19 13:47:26 +0700

    GSK-2617 Missing kwargs in a test make the whole suite fail (Hub)

--- tests/test_programmatic.py
+++ tests/test_programmatic.py
@@ -46,18 +46,35 @@ def test_a_greater_b_pass():
 
 
 def test_missing_arg():
-    with pytest.raises(Exception, match="Missing 1 required parameters: {'b': <class 'int'>}"):
-        Suite().add_test(_test_a_greater_b(a=2)).run()
+    result = Suite().add_test(_test_a_greater_b(a=2)).run()
+
+    assert not result.passed
+
+    assert len(result.results) == 1
+    _, test_result, _ = result.results[0]
+    assert "1 validation error for _test_a_greater_b" in test_result.messages[0].text
 
 
 def test_missing_args():
-    with pytest.raises(Exception, match="Missing 2 required parameters: {'a': <class 'int'>, 'b': <class 'int'>}"):
-        Suite().add_test(_test_a_greater_b()).run()
+    result = Suite().add_test(_test_a_greater_b()).run()
+
+    assert not result.passed
+
+    assert len(result.results) == 1
+    _, test_result, _ = result.results[0]
+    assert test_result.is_error
+    assert "2 validation errors for _test_a_greater_b" in test_result.messages[0].text
 
 
 def test_missing_arg_one_global():
-    with pytest.raises(Exception, match="Missing 1 required parameters: {'b': <class 'int'>}"):
-        Suite().add_test(_test_a_greater_b()).run(a=2)
+    result = Suite().add_test(_test_a_greater_b()).run(a=2)
+
+    assert not result.passed
+
+    assert len(result.results) == 1
+    _, test_result, _ = result.results[0]
+    assert test_result.is_error
+    assert "1 validation error for _test_a_greater_b" in test_result.messages[0].text
 
 
 def test_all_global():
