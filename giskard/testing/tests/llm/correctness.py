9f9d0aeb9cfed1df343bae2adb0eac88728ee109
Pierre Le Jeune <pierre@giskard.ai>
2024-01-19 18:26:05 +0100

    Add threshold and failure examples in correctness test output

--- giskard/testing/tests/llm/correctness.py
+++ giskard/testing/tests/llm/correctness.py
@@ -11,14 +11,20 @@ from .. import debug_description_prefix
     tags=["llm", "llm-as-a-judge"],
     debug_description=debug_description_prefix + "that are <b>failing the evaluation criteria</b>.",
 )
-def test_llm_correctness(model: BaseModel, dataset: Dataset):
+def test_llm_correctness(model: BaseModel, dataset: Dataset, threshold: float = 0.5):
     correctness_evaluator = CorrectnessEvaluator()
-    eval_result = correctness_evaluator.evaluate(model, dataset)
+    eval_result, failed_idx = correctness_evaluator.evaluate(model, dataset)
+    output_ds = list()
+    if not eval_result.passed:
+        output_ds.append(dataset.slice(lambda df: df.loc[failed_idx], row_level=False))
+
+    passed = bool(eval_result.passed_ratio > threshold)
 
     return TestResult(
-        passed=eval_result.passed,
-        metric=len(eval_result.failure_examples),
-        metric_name="Failing examples",
+        passed=passed,
+        metric=eval_result.passed_ratio,
+        metric_name="Failing examples ratio",
         is_error=eval_result.has_errors,
         details=eval_result.details,
+        output_ds=output_ds,
     )
