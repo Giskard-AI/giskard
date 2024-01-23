5b9cdfb604398328c0370e03e37b4e0d54aca800
Bazire <bazire@giskard.ai>
2024-01-23 14:54:24 +0100

    Change hex to lower in some tests for windows compatibility

--- tests/models/test_base_model.py
+++ tests/models/test_base_model.py
@@ -154,4 +154,5 @@ def test_unnamed_model_str():
 
 def test_repr_named_model():
     model = _CustomModel(model_type="regression")
-    assert repr(model) == f"<test_base_model._CustomModel object at {hex(id(model))}>"
+    assert hex(id(model)).lower()[2:] in repr(model).lower()
+    assert "<test_base_model._CustomModel object at" in repr(model)
