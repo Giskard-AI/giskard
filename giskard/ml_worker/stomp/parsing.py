ba3ac7e28f68eac71658864707e6342beb84ac5c
Kevin Messiaen <kevin.messiaen@icloud.com>
2024-02-06 14:07:12 +0700

    GSK-1993 Fixed issue

--- giskard/ml_worker/stomp/parsing.py
+++ giskard/ml_worker/stomp/parsing.py
@@ -171,7 +171,7 @@ class FrameParser:
         return Frame(body=self.body, command=self.command, headers=self.headers)
 
 
-class StompFrame(str, Enum):
+class StompFrame(Enum):
     def __init__(
         self, command: StompCommand, command_type: CommandType, mandatory_headers: Set[HeaderType], allow_body=False
     ):
