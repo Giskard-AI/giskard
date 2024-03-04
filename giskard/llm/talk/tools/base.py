ffe97889d0b5cffd5bb6a77b8e0dedc24ff02faf
mykytaalekseiev <alekseyevnf@gmail.com>
2024-03-04 14:42:27 +0100

    Added docstrings.

--- giskard/llm/talk/tools/base.py
+++ giskard/llm/talk/tools/base.py
@@ -12,6 +12,16 @@ from giskard.datasets.base import Dataset
 
 
 class BaseTool(ABC):
+    """Base class of the Tool object.
+
+    Attributes
+    ----------
+    default_name : str
+        The default name of the Tool. Can be re-defined with constructor.
+    default_description: str
+        The default description of the Tool's functioning. Can be re-defined with constructor.
+    """
+
     default_name: str = ...
     default_description: str = ...
 
@@ -23,6 +33,21 @@ class BaseTool(ABC):
         name: str = None,
         description: str = None,
     ):
+        """Constructor of the class.
+
+        Parameters
+        ----------
+        model : BaseModel
+            The Giskard Model.
+        dataset : Dataset
+            The Giskard Dataset.
+        scan_result : ScanReport
+            The Giskard ScanReport object.
+        name : str
+            The name of the Tool. If not set, the `default_name` is used.
+        description : str
+            The description of the Tool. If not set, the `default_description` is used.
+        """
         self._model = model
         self._dataset = dataset
         self._scan_result = scan_result
@@ -31,23 +56,65 @@ class BaseTool(ABC):
 
     @property
     def name(self) -> str:
+        """Return the name of the Tool.
+
+        Returns
+        -------
+        str
+            The name of the Tool.
+        """
         return self._name
 
     @property
     def description(self) -> str:
+        """Return the description of the Tool.
+
+        Returns
+        -------
+        str
+            The description of the Tool.
+        """
         return self._description
 
     @property
     @abstractmethod
     def specification(self) -> str:
+        """Return the Tool's specification in a JSON Schema format.
+
+        Returns
+        -------
+        str
+            The Tool's specification.
+        """
         ...
 
     @abstractmethod
     def __call__(self, *args, **kwargs) -> str:
+        """Execute the Tool's functionality.
+
+        Returns
+        -------
+        str
+            The result of the Tool's execution.
+        """
         ...
 
 
-def get_feature_json_type(dataset: Dataset) -> dict[any, str]:
+def get_feature_json_type(dataset: Dataset) -> dict[str, str]:
+    """Get features' JSON type.
+
+    Determine the JSON type of features from the given `dataset`.
+
+    Parameters
+    ----------
+    dataset : Dataset
+        The Giskard Dataset.
+
+    Returns
+    -------
+    dict[str, str]
+        The dictionary with columns and related JSON types.
+    """
     number_columns = {column: "number" for column in dataset.df.select_dtypes(include=(int, float)).columns}
     string_columns = {column: "string" for column in dataset.df.select_dtypes(exclude=(int, float)).columns}
     return number_columns | string_columns
