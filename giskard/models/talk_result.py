d186fe7f8af41e095585365f867e39e67302ca35
mykytaalekseiev <alekseyevnf@gmail.com>
2024-03-04 16:27:29 +0100

    Added docstrings.

--- giskard/models/talk_result.py
+++ giskard/models/talk_result.py
@@ -5,6 +5,16 @@ from giskard.llm.client import LLMMessage
 
 @dataclass
 class TalkResult:
+    """The dataclass containing the result of the 'talk' call.
+
+    Attributes
+    ----------
+    response : LLMMessage
+        The response to the user's query.
+    summary : LLMMessage
+        The summary of the conversation between the user and the LLM agent.
+    """
+
     response: LLMMessage
     summary: LLMMessage
 
