e2ee5ea7a4d0b9720def201def8cafe792a802e2
Pierre Le Jeune <pierre@giskard.ai>
2024-03-13 15:59:07 +0100

    Add example in correctness prompt to fix bad json formatting

--- giskard/rag/metrics/correctness.py
+++ giskard/rag/metrics/correctness.py
@@ -10,14 +10,20 @@ from ...llm.errors import LLMGenerationError
 from ..testset import QATestset
 from .base import Metric
 
-CORRECTNESS_EVALUATION_PROMPT = """Your role is to test AI assistants. Your task consists in assessing whether a assistant output correctly answers a question. 
+CORRECTNESS_EVALUATION_SYSTEM_PROMPT = """Your role is to test AI assistants. Your task consists in assessing whether a assistant output correctly answers a question. 
 You are provided with the ground truth answer to the question. Your task is then to evaluate if the assistant answer is close to the ground thruth answer. 
 
 You are auditing the following assistant:
 {assistant_description}
 
-Here is the question that was asked to the assistant, its output, and the expected ground truth answer delimited with XML tags:
-<question>
+Think step by step and consider the assistant output in its entirety. Remember: you need to have a strong and sound reason to support your evaluation.
+If the assistant answer is correct, return True. If the assistant answer is incorrect, return False along with the reason.
+You must output a single JSON object with keys 'correctness' and 'correctness_reason'. Make sure you return a valid JSON object.
+
+The question that was asked to the assistant, its output, and the expected ground truth answer will be delimited with XML tags.
+"""
+
+CORRECTNESS_INPUT_TEMPLATE = """<question>
 {question}
 </question>
 
@@ -28,10 +34,24 @@ Here is the question that was asked to the assistant, its output, and the expect
 <ground_truth>
 {ground_truth}
 </ground_truth>
+"""
 
-Think step by step and consider the assistant output in its entirety. Remember: you need to have a strong and sound reason to support your evaluation.
-If the assistant answer is correct, return True. If the assistant answer is incorrect, return False along with the reason.
-You must output a single JSON object with keys 'correctness' and 'correctness_reason'. Make sure you return a valid JSON object."""
+
+CORRECTNESS_TRUE_EXAMPLE_INPUT = CORRECTNESS_INPUT_TEMPLATE.format(
+    question="What is the capital of France?", assistant_answer="The capital of France is Paris.", ground_truth="Paris."
+)
+
+CORRECTNESS_TRUE_EXAMPLE_OUTPUT = """{"correctness": true, "correctness_reason": ""}"""
+
+CORRECTNESS_FALSE_EXAMPLE_INPUT = CORRECTNESS_INPUT_TEMPLATE.format(
+    question="What is the capital of Denmark?",
+    assistant_answer="The capital of Denmark is Paris.",
+    ground_truth="Copenhagen.",
+)
+
+CORRECTNESS_FALSE_EXAMPLE_OUTPUT = (
+    """{"correctness": false, "correctness_reason": "The capital of Denmark is Copenhagen, not Paris."}"""
+)
 
 
 class CorrectnessMetric(Metric):
@@ -52,12 +72,19 @@ class CorrectnessMetric(Metric):
                 out = llm_client.complete(
                     messages=[
                         LLMMessage(
+                            role="system",
+                            content=CORRECTNESS_EVALUATION_SYSTEM_PROMPT.format(
+                                assistant_description=self.assistant_description
+                            ),
+                        ),
+                        LLMMessage(role="user", content=CORRECTNESS_TRUE_EXAMPLE_INPUT),
+                        LLMMessage(role="assistant", content=CORRECTNESS_TRUE_EXAMPLE_OUTPUT),
+                        LLMMessage(role="user", content=CORRECTNESS_FALSE_EXAMPLE_INPUT),
+                        LLMMessage(role="assistant", content=CORRECTNESS_FALSE_EXAMPLE_OUTPUT),
+                        LLMMessage(
                             role="user",
-                            content=CORRECTNESS_EVALUATION_PROMPT.format(
-                                assistant_description=self.assistant_description,
-                                question=sample.question,
-                                assistant_answer=answer,
-                                ground_truth=sample.reference_answer,
+                            content=CORRECTNESS_INPUT_TEMPLATE.format(
+                                question=sample.question, assistant_answer=answer, ground_truth=sample.reference_answer
                             ),
                         ),
                     ],
