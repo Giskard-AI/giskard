aa25458c89afdec2e7ed273f30a68af584651729
Matteo Dora <matteo@giskard.ai>
2024-03-28 14:01:32 +0100

    Add test for Mistral

--- tests/llm/test_llm_client.py
+++ tests/llm/test_llm_client.py
@@ -1,10 +1,14 @@
 from unittest.mock import Mock
 
+from mistralai.models.chat_completion import ChatCompletionResponse, ChatCompletionResponseChoice
+from mistralai.models.chat_completion import ChatMessage as MistralChatMessage
+from mistralai.models.chat_completion import FinishReason, UsageInfo
 from openai.types import CompletionUsage
 from openai.types.chat import ChatCompletion, ChatCompletionMessage
 from openai.types.chat.chat_completion import Choice
 
 from giskard.llm.client import ChatMessage
+from giskard.llm.client.mistral import MistralClient
 from giskard.llm.client.openai import OpenAIClient
 
 DEMO_OPENAI_RESPONSE = ChatCompletion(
@@ -24,6 +28,22 @@ DEMO_OPENAI_RESPONSE = ChatCompletion(
 )
 
 
+DEMO_MISTRAL_RESPONSE = ChatCompletionResponse(
+    id="2d62260a7a354e02922a4f6ad36930d3",
+    object="chat.completion",
+    created=1630000000,
+    model="mistral-large",
+    choices=[
+        ChatCompletionResponseChoice(
+            index=0,
+            message=MistralChatMessage(role="assistant", content="This is a test!", name=None, tool_calls=None),
+            finish_reason=FinishReason.stop,
+        )
+    ],
+    usage=UsageInfo(prompt_tokens=9, total_tokens=89, completion_tokens=80),
+)
+
+
 def test_llm_complete_message():
     client = Mock()
     client.chat.completions.create.return_value = DEMO_OPENAI_RESPONSE
@@ -35,6 +55,23 @@ def test_llm_complete_message():
     assert client.chat.completions.create.call_args[1]["messages"] == [{"role": "system", "content": "Hello"}]
     assert client.chat.completions.create.call_args[1]["temperature"] == 0.11
     assert client.chat.completions.create.call_args[1]["max_tokens"] == 1
+
+    assert isinstance(res, ChatMessage)
+    assert res.content == "This is a test!"
+
+
+def test_mistral_client():
+    client = Mock()
+    client.chat.return_value = DEMO_MISTRAL_RESPONSE
+
+    res = MistralClient(model="mistral-large", client=client).complete(
+        [ChatMessage(role="user", content="Hello")], temperature=0.11, max_tokens=12
+    )
+
+    client.chat.assert_called_once()
+    assert client.chat.call_args[1]["messages"] == [MistralChatMessage(role="user", content="Hello")]
+    assert client.chat.call_args[1]["temperature"] == 0.11
+    assert client.chat.call_args[1]["max_tokens"] == 12
 
     assert isinstance(res, ChatMessage)
     assert res.content == "This is a test!"
