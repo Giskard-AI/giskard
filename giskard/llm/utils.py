a0f8832512053ff6ec69b82e450065befa1feb91
Matteo Dora <matteo@giskard.ai>
2024-03-28 15:26:29 +0100

    Fix simple test dataset generation

--- giskard/llm/utils.py
+++ giskard/llm/utils.py
@@ -1,13 +1,10 @@
-from typing import Optional
-
 from ..models.base.model import BaseModel
-from .generators.base import LLMBasedDataGenerator
+from .generators.simple import SimpleDataGenerator
 
 
 def generate_test_dataset(
     model: BaseModel,
     num_samples: int = 10,
-    prompt: Optional[str] = None,
     temperature=0.5,
     column_types=None,
     llm_seed: int = 1729,
@@ -20,8 +17,6 @@ def generate_test_dataset(
         The model to generate a test dataset for.
     num_samples : int
         The number of samples to generate, by default 10.
-    prompt : Optional[str]
-        The prompt to use for the generation, if not specified a default will be used.
     temperature : Optional[float]
         The temperature to use for the generation, by default 0.5.
     column_types :
@@ -43,5 +38,6 @@ def generate_test_dataset(
     --------
     :class:`giskard.llm.generators.BaseDataGenerator`
     """
-    generator = LLMBasedDataGenerator(prompt=prompt, llm_temperature=temperature, llm_seed=llm_seed)
+    generator = SimpleDataGenerator(llm_temperature=temperature, llm_seed=llm_seed)
+
     return generator.generate_dataset(model, num_samples, column_types)
