3b8f451ca097172d78befb97ec0128f9524646f8
Pierre Le Jeune <pierre@giskard.ai>
2024-04-15 11:58:34 +0200

    Fix typo

--- giskard/datasets/metadata/text_metadata_provider.py
+++ giskard/datasets/metadata/text_metadata_provider.py
@@ -20,7 +20,6 @@ class TextMetadataProvider(MetadataProvider):
                 "text_length": values.map(len),
                 "charset": pd.Categorical(values.map(_detect_charset)),
                 "language": values.map(detect_lang),
-                "language": values.map(detect_lang),
             },
             index=values.index,
         )
