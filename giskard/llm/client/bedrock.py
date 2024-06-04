c53d47552b3534542c147ebfa6008737df429582
Kevin Messiaen <kevin.messiaen@icloud.com>
2024-06-04 15:52:28 +0700

    Code reformatting

--- giskard/llm/client/bedrock.py
+++ giskard/llm/client/bedrock.py
@@ -1,6 +1,7 @@
-from typing import List, Optional, Sequence
+from typing import Dict, List, Optional, Sequence
 
 import json
+from abc import ABC, abstractmethod
 
 from ..config import LLMConfigurationError
 from ..errors import LLMImportError
@@ -15,41 +16,26 @@ except ImportError as err:
     ) from err
 
 
-def _format_messages_claude(messages: Sequence[ChatMessage]):
-    input_msg_prompt: List = []
-    system_prompts = []
-
-    for msg in messages:
-        # System prompt is a specific parameter in Claude
-        if msg.role.lower() == "system":
-            system_prompts.append(msg.content)
-            continue
-
-        # Only role user and assistant are allowed
-        role = msg.role.lower()
-        role = role if role in ["assistant", "user"] else "user"
-
-        # Consecutive messages need to be grouped
-        last_message = None if len(input_msg_prompt) == 0 else input_msg_prompt[-1]
-        if last_message is not None and last_message["role"] == role:
-            last_message["content"].append({"type": "text", "text": msg.content})
-            continue
-
-        input_msg_prompt.append({"role": role, "content": [{"type": "text", "text": msg.content}]})
-
-    return input_msg_prompt, "\n".join(system_prompts)
-
-
-class ClaudeBedrockClient(LLMClient):
-    def __init__(
-        self,
-        bedrock_runtime_client,
-        model: str = "anthropic.claude-3-sonnet-20240229-v1:0",
-        anthropic_version: str = "bedrock-2023-05-31",
-    ):
+class BaseBedrockClient(LLMClient, ABC):
+    def __init__(self, bedrock_runtime_client, model: str):
         self._client = bedrock_runtime_client
         self.model = model
-        self.anthropic_version = anthropic_version
+
+    @abstractmethod
+    def _format_body(
+        self,
+        messages: Sequence[ChatMessage],
+        temperature: float = 1,
+        max_tokens: Optional[int] = 1000,
+        caller_id: Optional[str] = None,
+        seed: Optional[int] = None,
+        format=None,
+    ) -> Dict:
+        ...
+
+    @abstractmethod
+    def _parse_completion(self, completion, caller_id: Optional[str] = None) -> ChatMessage:
+        ...
 
     def complete(
         self,
@@ -60,22 +46,8 @@ class ClaudeBedrockClient(LLMClient):
         seed: Optional[int] = None,
         format=None,
     ) -> ChatMessage:
-        # only supporting claude 3 to start
-        if "claude-3" not in self.model:
-            raise LLMConfigurationError(f"Only claude-3 models are supported as of now, got {self.model}")
-
-        messages, system = _format_messages_claude(messages)
-
         # create the json body to send to the API
-        body = json.dumps(
-            {
-                "anthropic_version": "bedrock-2023-05-31",
-                "max_tokens": max_tokens,
-                "temperature": temperature,
-                "system": system,
-                "messages": messages,
-            }
-        )
+        body = self._format_body(messages, temperature, max_tokens, caller_id, seed, format)
 
         # invoke the model and get the response
         try:
@@ -86,9 +58,67 @@ class ClaudeBedrockClient(LLMClient):
         except RuntimeError as err:
             raise LLMConfigurationError("Could not get response from Bedrock API") from err
 
+        return self._parse_completion(completion, caller_id)
+
+
+class ClaudeBedrockClient(BaseBedrockClient):
+    def __init__(
+        self,
+        bedrock_runtime_client,
+        model: str = "anthropic.claude-3-sonnet-20240229-v1:0",
+        anthropic_version: str = "bedrock-2023-05-31",
+    ):
+        # only supporting claude 3
+        if "claude-3" not in self.model:
+            raise LLMConfigurationError(f"Only claude-3 models are supported as of now, got {self.model}")
+
+        super().__init__(bedrock_runtime_client, model)
+        self.anthropic_version = anthropic_version
+
+    def _format_body(
+        self,
+        messages: Sequence[ChatMessage],
+        temperature: float = 1,
+        max_tokens: Optional[int] = 1000,
+        caller_id: Optional[str] = None,
+        seed: Optional[int] = None,
+        format=None,
+    ) -> Dict:
+        input_msg_prompt: List = []
+        system_prompts = []
+
+        for msg in messages:
+            # System prompt is a specific parameter in Claude
+            if msg.role.lower() == "system":
+                system_prompts.append(msg.content)
+                continue
+
+            # Only role user and assistant are allowed
+            role = msg.role.lower()
+            role = role if role in ["assistant", "user"] else "user"
+
+            # Consecutive messages need to be grouped
+            last_message = None if len(input_msg_prompt) == 0 else input_msg_prompt[-1]
+            if last_message is not None and last_message["role"] == role:
+                last_message["content"].append({"type": "text", "text": msg.content})
+                continue
+
+            input_msg_prompt.append({"role": role, "content": [{"type": "text", "text": msg.content}]})
+
+        return json.dumps(
+            {
+                "anthropic_version": "bedrock-2023-05-31",
+                "max_tokens": max_tokens,
+                "temperature": temperature,
+                "system": "\n".join(system_prompts),
+                "messages": messages,
+            }
+        )
+
+    def _parse_completion(self, completion, caller_id: Optional[str] = None) -> ChatMessage:
         self.logger.log_call(
             prompt_tokens=completion["usage"]["input_tokens"],
-            sampled_tokens=completion["usage"]["input_tokens"],
+            sampled_tokens=completion["usage"]["output_tokens"],
             model=self.model,
             client_class=self.__class__.__name__,
             caller_id=caller_id,
@@ -98,16 +128,15 @@ class ClaudeBedrockClient(LLMClient):
         return ChatMessage(role="assistant", content=msg)
 
 
-class LLamaBedrockClient(LLMClient):
-    def __init__(
-        self,
-        bedrock_runtime_client,
-        model: str = "meta.llama3-8b-instruct-v1:0",
-    ):
-        self._client = bedrock_runtime_client
-        self.model = model
+class LLamaBedrockClient(BaseBedrockClient):
+    def __init__(self, bedrock_runtime_client, model: str = "meta.llama3-8b-instruct-v1:0"):
+        # only supporting llama
+        if "llama" not in self.model:
+            raise LLMConfigurationError(f"Only Llama models are supported as of now, got {self.model}")
 
-    def complete(
+        super().__init__(bedrock_runtime_client, model)
+
+    def _format_body(
         self,
         messages: Sequence[ChatMessage],
         temperature: float = 1,
@@ -115,11 +144,7 @@ class LLamaBedrockClient(LLMClient):
         caller_id: Optional[str] = None,
         seed: Optional[int] = None,
         format=None,
-    ) -> ChatMessage:
-        # only supporting llama
-        if "llama" not in self.model:
-            raise LLMConfigurationError(f"Only Llama models are supported as of now, got {self.model}")
-
+    ) -> Dict:
         # Create the messages format needed for llama bedrock specifically
         prompts = []
         for msg in messages:
@@ -127,7 +152,7 @@ class LLamaBedrockClient(LLMClient):
 
         # create the json body to send to the API
         messages = "\n".join(prompts)
-        body = json.dumps(
+        return json.dumps(
             {
                 "max_gen_len": max_tokens,
                 "temperature": temperature,
@@ -135,15 +160,7 @@ class LLamaBedrockClient(LLMClient):
             }
         )
 
-        # invoke the model and get the response
-        try:
-            accept = "application/json"
-            contentType = "application/json"
-            response = self._client.invoke_model(body=body, modelId=self.model, accept=accept, contentType=contentType)
-            completion = json.loads(response.get("body").read())
-        except RuntimeError as err:
-            raise LLMConfigurationError("Could not get response from Bedrock API") from err
-
+    def _parse_completion(self, completion, caller_id: Optional[str] = None) -> ChatMessage:
         self.logger.log_call(
             prompt_tokens=completion["prompt_token_count"],
             sampled_tokens=completion["generation_token_count"],
