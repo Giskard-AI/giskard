7703d516dff6d2cc302737a05da38ec5d043ea81
Kevin Messiaen <kevin.messiaen@icloud.com>
2024-11-15 11:03:24 +0100

    Reintroduced old way to set LLM models

--- giskard/llm/__init__.py
+++ giskard/llm/__init__.py
@@ -1,4 +1,4 @@
-from .client import get_default_client, set_llm_model
+from .client import get_default_client, set_default_client, set_llm_api, set_llm_model
 from .embeddings import set_default_embedding, set_embedding_model
 from .errors import LLMImportError
 
@@ -6,6 +6,8 @@ __all__ = [
     "LLMImportError",
     "get_default_client",
     "set_llm_model",
+    "set_default_client",
+    "set_llm_api",
     "set_default_embedding",
     "set_embedding_model",
 ]
