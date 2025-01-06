52b1ae0ba47adb9743c181e917462aaeb6cce4c5
Henrique Chaves <henrique@giskard.ai>
2025-01-06 20:48:25 -0300

    Create a base PerturbationFunction class

--- giskard/scanner/robustness/numerical_transformations.py
+++ giskard/scanner/robustness/numerical_transformations.py
@@ -1,45 +1,28 @@
 import numpy as np
 import pandas as pd
 
-from ...core.core import DatasetProcessFunctionMeta
-from ...registry.registry import get_object_uuid
-from ...registry.transformation_function import TransformationFunction
+from .base_perturbation_function import PerturbationFunction
 
 
-class NumericalTransformation(TransformationFunction):
-    name: str
-
-    def __init__(self, column, needs_dataset=False):
-        super().__init__(None, row_level=False, cell_level=False, needs_dataset=needs_dataset)
-        self.column = column
-        self.meta = DatasetProcessFunctionMeta(type="TRANSFORMATION")
-        self.meta.uuid = get_object_uuid(self)
-        self.meta.code = self.name
-        self.meta.name = self.name
-        self.meta.display_name = self.name
-        self.meta.tags = ["pickle", "scan"]
-        self.meta.doc = self.meta.default_doc("Automatically generated transformation function")
-
-    def __str__(self):
-        return self.name
+class NumericalTransformation(PerturbationFunction):
+    def __init__(self, column: str, needs_dataset: bool = False) -> None:
+        super().__init__(column, needs_dataset=needs_dataset)
 
     def execute(self, data: pd.DataFrame) -> pd.DataFrame:
         feature_data = data[self.column].dropna()
         data.loc[feature_data.index, self.column] = self.make_perturbation(feature_data)
         return data
 
-    def make_perturbation(self, values: pd.Series) -> pd.Series:
-        raise NotImplementedError()
-
 
 class MultiplyByFactor(NumericalTransformation):
     name = "Multiply by factor"
 
-    def __init__(self, column: str, factor: float):
+    def __init__(self, column: str, factor: float) -> None:
         super().__init__(column)
         self.factor = factor
 
     def make_perturbation(self, values: pd.Series) -> pd.Series:
+        # Round if the column is an integer type
         if np.issubdtype(values.dtype, np.integer):
             return np.round(values * self.factor).astype(values.dtype)
         return values * self.factor
@@ -48,12 +31,13 @@ class MultiplyByFactor(NumericalTransformation):
 class AddGaussianNoise(NumericalTransformation):
     name = "Add Gaussian noise"
 
-    def __init__(self, column: str, mean: float = 0, std: float = 0.01):
+    def __init__(self, column: str, mean: float = 0, std: float = 0.01) -> None:
         super().__init__(column)
         self.mean = mean
         self.std = std
 
     def make_perturbation(self, values: pd.Series) -> pd.Series:
+        noise = np.random.normal(self.mean, self.std, values.shape)
         if np.issubdtype(values.dtype, np.integer):
-            return np.round(values + np.random.normal(self.mean, self.std, values.shape)).astype(values.dtype)
-        return values + np.random.normal(self.mean, self.std, values.shape)
+            return np.round(values + noise).astype(values.dtype)
+        return values + noise
--- giskard/scanner/robustness/text_transformations.py
+++ giskard/scanner/robustness/text_transformations.py
@@ -8,37 +8,19 @@ import numpy as np
 import pandas as pd
 from num2words import num2words
 
-from ...core.core import DatasetProcessFunctionMeta
 from ...datasets import Dataset
 from ...functions.transformation import gruber
-from ...registry.registry import get_object_uuid
-from ...registry.transformation_function import TransformationFunction
+from .base_perturbation_function import PerturbationFunction
 
 
-class TextTransformation(TransformationFunction):
-    name: str
-
-    def __init__(self, column, needs_dataset=False):
-        super().__init__(None, row_level=False, cell_level=False, needs_dataset=needs_dataset)
-        self.column = column
-        self.meta = DatasetProcessFunctionMeta(type="TRANSFORMATION")
-        self.meta.uuid = get_object_uuid(self)
-        self.meta.code = self.name
-        self.meta.name = self.name
-        self.meta.display_name = self.name
-        self.meta.tags = ["pickle", "scan"]
-        self.meta.doc = self.meta.default_doc("Automatically generated transformation function")
-
-    def __str__(self):
-        return self.name
+class TextTransformation(PerturbationFunction):
+    def __init__(self, column: str, needs_dataset: bool = False) -> None:
+        super().__init__(column, needs_dataset=needs_dataset)
 
     def execute(self, data: pd.DataFrame) -> pd.DataFrame:
         feature_data = data[self.column].dropna().astype(str)
         data.loc[feature_data.index, self.column] = feature_data.apply(self.make_perturbation)
         return data
-
-    def make_perturbation(self, text: str) -> str:
-        raise NotImplementedError()
 
 
 class TextUppercase(TextTransformation):
